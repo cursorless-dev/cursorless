652013b593c4bd146a926fb3ae6ae28854bf4043
Pokey Rule <pokey.rule@gmail.com>
2021-02-23 18:13:13 +0000

    Clean up color settings

--- src/constants.ts
+++ src/constants.ts
@@ -2,13 +2,4 @@ export const DEBOUNCE_DELAY = 50;
 
 export const TOKEN_MATCHER = /[a-zA-Z_][a-zA-Z_0-9]+|\-?((\d+(\.\d*)?)|(\.\d+))|[^\s\w]/g;
 
-export const COLORS = [
-  "default",
-  "blue",
-  "green",
-  "red",
-  "brown",
-  "mauve",
-  "gray",
-  "teal",
-];
+export const COLORS = ["default", "blue", "green", "red", "yellow", "mauve"];
