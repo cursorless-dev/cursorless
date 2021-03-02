021c17512a76921a48879946dcb90bd392df29cd
Pokey Rule <pokey.rule@gmail.com>
2021-03-02 17:32:19 +0000

    Random fixes

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import * as vscode from "vscode";
 import { addDecorationsToEditor } from "./addDecorationsToEditor";
-import { COLORS, DEBOUNCE_DELAY } from "./constants";
+import { COLORS, DEBOUNCE_DELAY, SymbolColor } from "./constants";
 import Decorations from "./Decorations";
 import NavigationMap from "./NavigationMap";
 
@@ -41,8 +41,18 @@ export function activate(context: vscode.ExtensionContext) {
 
   const selectTokenDisposable = vscode.commands.registerTextEditorCommand(
     "decorative-navigation.selectToken",
-    (editor: vscode.TextEditor) => {
+    (
+      editor: vscode.TextEditor,
+      edit: vscode.TextEditorEdit,
+      color?: SymbolColor,
+      character?: string
+    ) => {
       if (navigationMap === null) {
+        return;
+      }
+
+      if (color !== undefined && character !== undefined) {
+        selectToken(color, character, editor);
         return;
       }
 
@@ -50,21 +60,23 @@ export function activate(context: vscode.ExtensionContext) {
       inputBox.show();
 
       inputBox.onDidChangeValue((value) => {
+        if (color !== undefined && value.length === 1) {
+          inputBox.dispose();
+
+          character = value[0];
+          selectToken(color, character, editor);
+        }
         if (value.length === 2) {
           inputBox.dispose();
 
-          const color = COLORS.find((c) => c.startsWith(value[0]));
+          color = COLORS.find((c) => c.startsWith(value[0]));
 
           if (color === undefined) {
             return;
           }
 
-          const character = value[1];
-          const token = navigationMap!.getToken(color, character);
-          editor.selection = new vscode.Selection(
-            token.range.start,
-            token.range.end
-          );
+          character = value[1];
+          selectToken(color, character, editor);
         }
       });
     }
@@ -86,6 +98,15 @@ export function activate(context: vscode.ExtensionContext) {
       },
     }
   );
+
+  function selectToken(
+    color: SymbolColor,
+    character: string,
+    editor: vscode.TextEditor
+  ) {
+    const token = navigationMap!.getToken(color, character);
+    editor.selection = new vscode.Selection(token.range.start, token.range.end);
+  }
 }
 
 // this method is called when your extension is deactivated
