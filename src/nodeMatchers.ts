681435e1cf4c05b786f4a35effb479aa7e79042b
Pokey Rule <pokey.rule@gmail.com>
2021-04-27 17:48:43 +0100

    Separate extractors and matchers

--- src/nodeMatchers.ts
+++ src/nodeMatchers.ts
@@ -1,14 +1,46 @@
 import { SyntaxNode } from "tree-sitter";
+import { Position, Selection } from "vscode";
 
 type NodeMatcher = (node: SyntaxNode) => boolean;
 
-function makeSimpleTypeMatcher(...typeNames: string[]): NodeMatcher {
+type NodeSelectionExtractor = (
+  node: SyntaxNode,
+  isInside: boolean
+) => Selection | null;
+
+interface NodeDescriptor {
+  matcher: NodeMatcher;
+  extractor: NodeSelectionExtractor;
+}
+
+function hasType(...typeNames: string[]): NodeMatcher {
   return (node: SyntaxNode) => typeNames.includes(node.type);
 }
 
+const makeSimpleNodeDescriptor = (...typeNames: string[]) => ({
+  matcher: hasType("pair"),
+  extractor: simpleSelectionExtractor,
+});
+
+function simpleSelectionExtractor(node: SyntaxNode, isInside: boolean) {
+  return new Selection(
+    new Position(node.startPosition.row, node.startPosition.column),
+    new Position(node.endPosition.row, node.endPosition.column)
+  );
+}
+
 const nodeMatchers = {
-  class: makeSimpleTypeMatcher("class_declaration"),
-  arrowFunction: makeSimpleTypeMatcher("arrow_function"),
+  class: makeSimpleNodeDescriptor("class_declaration"),
+  arrowFunction: makeSimpleNodeDescriptor("arrow_function"),
+  pair: {
+    matcher: hasType("pair"),
+    extractor: (node: SyntaxNode, isInside: boolean) => {
+      return new Selection(
+        new Position(node.startPosition.row, node.startPosition.column),
+        new Position(node.endPosition.row, node.endPosition.column)
+      );
+    },
+  },
   namedFunction(node: SyntaxNode) {
     // Simple case, eg
     // function foo() {}
@@ -52,7 +84,7 @@ const nodeMatchers = {
 
     return false;
   },
-  ifStatement: makeSimpleTypeMatcher("if_statement"),
+  ifStatement: makeSimpleNodeDescriptor("if_statement"),
 };
 
 export default nodeMatchers;
