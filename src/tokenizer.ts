c75e2b1348e960c331625cd9095489ed411c80ad
Andreas Arvidsson <andreas.arvidsson@redpill-linpro.com>
2021-06-28 21:11:28 +0200

    Moved token matcher from constants to own tokenizer file

---
+++ src/tokenizer.ts
@@ -0,0 +1,43 @@
+const REPEATABLE_SYMBOLS = [
+  "+",
+  "-",
+  "*",
+  "/",
+  "=",
+  "<",
+  ">",
+  "_",
+  "#",
+  ".",
+  "|",
+  "&",
+];
+
+const FIXED_TOKENS = ["!==", "!=", "+=", "-=", "*=", "/=", "%=", "<=", ">="];
+const REPEATABLE_SYMBOLS_REGEX = REPEATABLE_SYMBOLS.map(escapeRegExp)
+  .map((s) => `${s}+`)
+  .join("|");
+const FIXED_TOKENS_REGEX = FIXED_TOKENS.map(escapeRegExp).join("|");
+const IDENTIFIERS_REGEX = "[a-zA-Z_0-9]+";
+const SINGLE_SYMBOLS_REGEX = "[^\\s\\w]";
+
+const REGEX = [
+  IDENTIFIERS_REGEX,
+  FIXED_TOKENS_REGEX,
+  REPEATABLE_SYMBOLS_REGEX,
+  SINGLE_SYMBOLS_REGEX,
+].join("|");
+
+const TOKEN_MATCHER = new RegExp(REGEX, "g");
+
+export function tokenize<T>(
+  text: string,
+  mapfn: (v: RegExpMatchArray, k: number) => T
+) {
+  return Array.from(text.matchAll(TOKEN_MATCHER), mapfn);
+}
+
+//https://stackoverflow.com/a/6969486
+function escapeRegExp(string: string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
+}
