804bd9bf1a6e6aaa401e19a58f3bfff4a0fdce59
Brock McElroy <28877984+brxck@users.noreply.github.com>
2021-06-30 14:23:19 -0700

    Add Python type transform support

--- src/treeSitterUtils.ts
+++ src/treeSitterUtils.ts
@@ -3,13 +3,6 @@ import { SyntaxNode } from "web-tree-sitter";
 export const getValueNode = (node: SyntaxNode) =>
   node.childForFieldName("value");
 
-export const getTypeNode = (node: SyntaxNode) => {
-  const typeAnnotationNode = node.children.find(
-    (child) => child.type === "type_annotation"
-  );
-  return typeAnnotationNode?.lastChild ?? null;
-};
-
 export const getKeyNode = (node: SyntaxNode) => node.childForFieldName("key");
 
 export const getDefinitionNode = (node: SyntaxNode) =>
