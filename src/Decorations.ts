4dc05f815846c843f3552ffb504a9718c40c723a
Pokey Rule <pokey.rule@gmail.com>
2021-07-02 16:49:22 +0100

    Working computations

--- src/Decorations.ts
+++ src/Decorations.ts
@@ -20,13 +20,14 @@ export default class Decorations {
   decorationMap: DecorationMap;
 
   constructor(fontSize: FontSize) {
-    const iconWidthPx = 4;
-    const iconPadding = 0;
-    const iconWidth = iconWidthPx + "px";
-    const iconHeight = `${fontSize.fontHeight + iconWidthPx + iconPadding}px`;
-    const spanWidthPx = iconWidthPx + (fontSize.fontWidth - iconWidthPx) / 2;
-    const iconPath = join(__dirname, "..", "images", "round-hat.svg");
-    const rawSvg = readFileSync(iconPath, "utf8");
+    const hatWidthToCharacterWidthRatio = 0.5;
+    const hatVerticalOffset = -3;
+    const { svg, svgWidthPx, svgHeightPx } = this.processSvg(
+      fontSize,
+      hatWidthToCharacterWidthRatio,
+      hatVerticalOffset
+    );
+    const spanWidthPx = svgWidthPx + (fontSize.fontWidth - svgWidthPx) / 2;
 
     this.decorations = COLORS.map((color) => {
       var colorSetting = vscode.workspace
@@ -40,26 +41,23 @@ export default class Decorations {
           light: {
             after: {
               contentIconPath: this.constructColoredSvgDataUri(
-                rawSvg,
-                colorSetting.light,
-                iconWidth,
-                iconHeight
+                svg,
+                colorSetting.light
               ),
             },
           },
           dark: {
             after: {
               contentIconPath: this.constructColoredSvgDataUri(
-                rawSvg,
-                colorSetting.dark,
-                iconWidth,
-                iconHeight
+                svg,
+                colorSetting.dark
               ),
             },
           },
           after: {
-            margin: `-${iconHeight} 0 0 -${spanWidthPx}px`,
+            margin: `-${svgHeightPx}px 0 0 -${spanWidthPx}px`,
             width: `${spanWidthPx}px`,
+            height: `${svgHeightPx}px`,
           },
         }),
       };
@@ -70,20 +68,66 @@ export default class Decorations {
     );
   }
 
-  private constructColoredSvgDataUri(
-    rawSvg: string,
-    color: string,
-    width: string,
-    height: string
-  ) {
-    const svg = rawSvg
+  private constructColoredSvgDataUri(originalSvg: string, color: string) {
+    const svg = originalSvg
       .replace(/fill="[^"]+"/, `fill="${color}"`)
-      .replace(/fill:[^;]+;/, `fill:${color};`)
-      .replace(/width="[^"]+"/, `width="${width}"`)
-      .replace(/height="[^"]+"/, `height="${height}"`);
+      .replace(/fill:[^;]+;/, `fill:${color};`);
 
     const encoded = Buffer.from(svg).toString("base64");
 
     return vscode.Uri.parse(`data:image/svg+xml;base64,${encoded}`);
+  }
+
+  private processSvg(
+    fontSize: FontSize,
+    hatWidthToCharacterWidthRatio: number,
+    hatVerticalOffset: number
+  ) {
+    const iconPath = join(__dirname, "..", "images", "round-hat.svg");
+    const rawSvg = readFileSync(iconPath, "utf8");
+
+    const { originalViewBoxHeight, originalViewBoxWidth } =
+      this.getViewBoxDimensions(rawSvg);
+
+    const hatWidthPx = hatWidthToCharacterWidthRatio * fontSize.fontWidth;
+
+    const hatHeightPx =
+      (originalViewBoxHeight / originalViewBoxWidth) * hatWidthPx;
+    const svgHeightPx = fontSize.fontHeight + hatHeightPx + hatVerticalOffset;
+    const svgWidthPx = Math.ceil(fontSize.fontWidth);
+
+    const newViewBoxWidth =
+      ((originalViewBoxWidth / hatWidthToCharacterWidthRatio) *
+        fontSize.fontWidth) /
+      svgWidthPx;
+    const newViewBoxHeight = (newViewBoxWidth * svgHeightPx) / svgWidthPx;
+    const newViewBoxX = -(newViewBoxWidth - originalViewBoxWidth) / 2;
+    const newViewBoxY = 0;
+
+    const newViewBoxString = `${newViewBoxX} ${newViewBoxY} ${newViewBoxWidth} ${newViewBoxHeight}`;
+
+    const svg = rawSvg
+      .replace(/width="[^"]+"/, `width="${svgWidthPx}px"`)
+      .replace(/height="[^"]+"/, `height="${svgHeightPx}px"`)
+      .replace(/viewBox="([^"]+)"/, `viewBox="${newViewBoxString}"`);
+
+    return {
+      svg,
+      svgHeightPx,
+      svgWidthPx,
+    };
+  }
+
+  private getViewBoxDimensions(rawSvg: string) {
+    const viewBoxMatch = rawSvg.match(/viewBox="([^"]+)"/);
+    if (viewBoxMatch == null) {
+      throw Error("View box not found in svg");
+    }
+    const originalViewBoxString = viewBoxMatch[1];
+    const [_0, _1, originalViewBoxWidthStr, originalViewBoxHeightStr] =
+      originalViewBoxString.split(" ");
+    const originalViewBoxWidth = Number(originalViewBoxWidthStr);
+    const originalViewBoxHeight = Number(originalViewBoxHeightStr);
+    return { originalViewBoxHeight, originalViewBoxWidth };
   }
 }
