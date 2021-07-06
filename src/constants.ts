b60697db23d00a5aa8445b40de5117134668b55a
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2021-07-06 15:08:56 +0200

    Updated regexp to support fixed tokens and sequential symbols (#33)

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,6 @@
-export const DEBOUNCE_DELAY = 175;
-
-export const TOKEN_MATCHER = /[a-zA-Z_0-9]+|[^\s\w]/g;
-
 export const SUBWORD_MATCHER = /[A-Z]?[a-z]+/g;
+
+export const DEBOUNCE_DELAY = 175;
 
 export const COLORS = [
   "default",
