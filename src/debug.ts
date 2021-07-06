c77d9f074b330d9c07bb258386062b025fbb1917
Brock McElroy <28877984+brxck@users.noreply.github.com>
2021-07-06 11:56:10 -0700

    Refactor branch debug logging

--- src/debug.ts
+++ src/debug.ts
@@ -3,21 +3,22 @@ import { SyntaxNode } from "web-tree-sitter";
 
 export function logBranchTypes(getNodeAtLocation: any) {
   return (event: vscode.TextEditorSelectionChangeEvent) => {
-    const getBranch = (branch: SyntaxNode[]): SyntaxNode[] => {
-      if (branch[0].parent) {
-        return getBranch([branch[0].parent, ...branch]);
-      }
-      return branch;
-    };
-
     const location = new vscode.Location(
       vscode.window.activeTextEditor!.document.uri,
       event.selections[0]
     );
-    const leaf: SyntaxNode = getNodeAtLocation(location);
-    const branch = getBranch([leaf]);
-    branch.forEach((node, i) => console.debug(">".repeat(i + 1), node.type));
-    const leafText = leaf.text.replace(/\s+/g, " ").substring(0, 100);
-    console.debug(">".repeat(branch.length), `"${leafText}"`);
+
+    const ancestors: SyntaxNode[] = [];
+    let node: SyntaxNode = getNodeAtLocation(location);
+    while (node.parent) {
+      ancestors.unshift(node.parent);
+      node = node.parent;
+    }
+
+    ancestors.forEach((node, i) => console.debug(">".repeat(i + 1), node.type));
+    const leafText = ancestors[ancestors.length - 1].text
+      .replace(/\s+/g, " ")
+      .substring(0, 100);
+    console.debug(">".repeat(ancestors.length), `"${leafText}"`);
   };
 }
