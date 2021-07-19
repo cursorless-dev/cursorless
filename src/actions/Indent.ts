cd843fdac4cb93d2f963e9f3469d09ceb41917a9
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2021-07-20 01:01:51 +0200

    Renames indent classes

--- src/actions/Indent.ts
+++ src/actions/Indent.ts
@@ -1,13 +1,13 @@
 import { Graph } from "../Types";
 import CommandAction from "../CommandAction";
 
-export class Indent extends CommandAction {
+export class IndentLines extends CommandAction {
   constructor(graph: Graph) {
     super(graph, "editor.action.indentLines");
   }
 }
 
-export class Outdent extends CommandAction {
+export class OutdentLines extends CommandAction {
   constructor(graph: Graph) {
     super(graph, "editor.action.outdentLines");
   }
