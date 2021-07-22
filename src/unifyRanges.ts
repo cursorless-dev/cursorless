ba6a61899ccb8d00f2689554c575ceb41bef75d5
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2021-07-23 00:07:11 +0200

    added comments

--- src/unifyRanges.ts
+++ src/unifyRanges.ts
@@ -2,27 +2,30 @@ import { Range } from "vscode";
 
 /** Unifies overlapping/intersecting ranges */
 export default function unifyRanges(ranges: Range[]): Range[] {
-  let haveChanges = true;
-  while (haveChanges) {
-    [ranges, haveChanges] = onePass(ranges);
+  let run = true;
+  while (run) {
+    [ranges, run] = onePass(ranges);
   }
   return ranges;
 }
 
 function onePass(ranges: Range[]): [Range[], boolean] {
   const wrappers: { range: Range }[] = [];
-  let haveChanges = false;
+  let madeChanges = false;
   ranges.forEach((range) => {
     const wrapper = wrappers.find(
       (wrapper) => wrapper.range.intersection(range) != null
     );
+    // Update existing intersecting range
     if (wrapper != null) {
       wrapper.range = wrapper.range.union(range);
-      haveChanges = true;
-    } else {
+      madeChanges = true;
+    }
+    // Add new range
+    else {
       wrappers.push({ range });
     }
   });
   const results = wrappers.map((wrapper) => wrapper.range);
-  return [results, haveChanges];
+  return [results, madeChanges];
 }
