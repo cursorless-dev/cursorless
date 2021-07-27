459c719865d8d150d28fbd45007038e24f6ada7e
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2021-07-27 17:56:53 +0200

    Compensate for paragraphs

--- src/actions/EditNewLine.ts
+++ src/actions/EditNewLine.ts
@@ -5,16 +5,37 @@ import {
   Graph,
   TypedSelection,
 } from "../Types";
-import { commands } from "vscode";
+import { commands, Selection } from "vscode";
 
 class EditNewLine implements Action {
-  targetPreferences: ActionPreferences[] = [{ insideOutsideType: "outside" }];
+  targetPreferences: ActionPreferences[] = [{ insideOutsideType: "inside" }];
 
   constructor(private graph: Graph, private isAbove: boolean) {
     this.run = this.run.bind(this);
   }
 
+  private correctForParagraph(targets: TypedSelection[]) {
+    targets.forEach((target) => {
+      let { start, end } = target.selection.selection;
+      if (target.selectionType === "paragraph") {
+        if (
+          this.isAbove &&
+          target.selectionContext.leadingDelimiterRange != null
+        ) {
+          start = start.translate({ lineDelta: -1 });
+        } else if (
+          !this.isAbove &&
+          target.selectionContext.trailingDelimiterRange != null
+        ) {
+          end = end.translate({ lineDelta: 1 });
+        }
+        target.selection.selection = new Selection(start, end);
+      }
+    });
+  }
+
   async run([targets]: [TypedSelection[]]): Promise<ActionReturnValue> {
+    this.correctForParagraph(targets);
     if (this.isAbove) {
       await this.graph.actions.setSelectionBefore.run([targets]);
       await commands.executeCommand("editor.action.insertLineBefore");
