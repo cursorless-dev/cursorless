b8eba9c3bcc295a9ffa116c42ce23543367c422a
Pokey Rule <pokey.rule@gmail.com>
2021-08-30 20:26:30 +0100

    Remove extraneous files

--- scripts/rename-action-in-tests.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Script to change action names in tests
-old_name="$1"
-new_name="$2"
-
-fd '.yml' . | xargs sed -E -i '' "s/actionName: ${old_name}$/actionName: ${new_name}/g"
