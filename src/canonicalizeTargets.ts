1da3f435dc987caec5d4936ce8cd4722573aaa9c
Pokey Rule <pokey.rule@gmail.com>
2021-09-06 13:48:27 +0100

    Rename purple to pink

--- src/canonicalizeTargets.ts
+++ src/canonicalizeTargets.ts
@@ -1,6 +1,12 @@
-import { PartialTarget, ScopeType } from "./typings/Types";
+import {
+  PartialPrimitiveTarget,
+  PartialTarget,
+  ScopeType,
+} from "./typings/Types";
 import update from "immutability-helper";
 import { transformPrimitiveTargets } from "./util/targetUtils";
+import { HatStyleName } from "./core/constants";
+import { flow } from "lodash";
 
 const scopeTypeAliasToCanonicalName: Record<string, ScopeType> = {
   arrowFunction: "anonymousFunction",
@@ -8,15 +14,37 @@ const scopeTypeAliasToCanonicalName: Record<string, ScopeType> = {
   regex: "regularExpression",
 };
 
+const colorAliasToCanonicalName: Record<string, HatStyleName> = {
+  purple: "pink",
+};
+
+const canonicalizeScopeTypes = (
+  target: PartialPrimitiveTarget
+): PartialPrimitiveTarget =>
+  target.modifier?.type === "containingScope"
+    ? update(target, {
+        modifier: {
+          scopeType: (scopeType: string) =>
+            scopeTypeAliasToCanonicalName[scopeType] ?? scopeType,
+        },
+      })
+    : target;
+
+const canonicalizeColors = (
+  target: PartialPrimitiveTarget
+): PartialPrimitiveTarget =>
+  target.mark?.type === "decoratedSymbol"
+    ? update(target, {
+        mark: {
+          symbolColor: (symbolColor: string) =>
+            colorAliasToCanonicalName[symbolColor] ?? symbolColor,
+        },
+      })
+    : target;
+
 export default function canonicalizeTargets(partialTargets: PartialTarget[]) {
-  return transformPrimitiveTargets(partialTargets, (target) =>
-    target.modifier?.type === "containingScope"
-      ? update(target, {
-          modifier: {
-            scopeType: (scopeType) =>
-              scopeTypeAliasToCanonicalName[scopeType] ?? scopeType,
-          },
-        })
-      : target
+  return transformPrimitiveTargets(
+    partialTargets,
+    flow(canonicalizeScopeTypes, canonicalizeColors)
   );
 }
