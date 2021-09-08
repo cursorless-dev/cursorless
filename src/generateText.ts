5ca0722652bcb5414274a44508837af3da1213b6
Pokey Rule <pokey.rule@gmail.com>
2021-09-08 14:15:09 +0100

    Cleanup generateTexts

--- src/generateText.ts
+++ src/generateText.ts
@@ -1,24 +1,21 @@
+import { range } from "lodash";
 import { pairDelimiterToText } from "./pairDelimiters";
 import { TextGenerator } from "./typings/Types";
 
 export default function (generator: TextGenerator, numTargets: number) {
-  const result = [];
-
   switch (generator.type) {
     case "range":
-      for (let i = 0; i < numTargets; ++i) {
-        result[i] = (generator.start + i).toString();
-      }
-      return result;
+      const { start } = generator;
+
+      return range(start, start + numTargets).map((num) => num.toString());
 
     case "pair":
       if (numTargets % 2 !== 0) {
         throw new Error("Number of targets is not divisible by two");
       }
+
       const pairTexts = pairDelimiterToText[generator.pair];
-      for (let i = 0; i < numTargets; ++i) {
-        result[i] = pairTexts[i % 2];
-      }
-      return result;
+
+      return range(numTargets / 2).flatMap(() => pairTexts);
   }
 }
