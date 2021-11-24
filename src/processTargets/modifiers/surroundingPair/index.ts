90bbc2738aa7e29e3d0a8fe1660e6ea2bf132d38
Pokey Rule <pokey.rule@gmail.com>
2021-11-24 15:19:33 +0000

    More dock strings

--- src/processTargets/modifiers/surroundingPair/index.ts
+++ src/processTargets/modifiers/surroundingPair/index.ts
@@ -17,18 +17,30 @@ import {
 import { SelectionWithEditorWithContext } from "../processModifier";
 import { complexDelimiterMap } from "./delimiterMaps";
 
+/**
+ * Applies the surrounding pair modifier to the given selection. First looks to
+ * see if the target is itself adjacent to or contained by a modifier token. If
+ * so it will expand the selection to the opposite delimiter token. Otherwise,
+ * or if the opposite token wasn't found, it will proceed by finding the
+ * smallest pair of delimiters which contains the selection.
+ *
+ * @param context Context to be leveraged by modifier
+ * @param selection The selection to process
+ * @param modifier The surrounding pair modifier information
+ * @returns The new selection expanded to the containing surrounding pair or
+ * `null` if none was found
+ */
 export function processSurroundingPair(
   context: ProcessedTargetsContext,
   selection: SelectionWithEditor,
   modifier: SurroundingPairModifier
 ): SelectionWithEditorWithContext[] | null {
-  let node: SyntaxNode | null;
-
   const document = selection.editor.document;
   const delimiters = complexDelimiterMap[
     modifier.delimiter as ComplexSurroundingPairName
   ] ?? [modifier.delimiter];
 
+  let node: SyntaxNode | null;
   let stringNodeMatcher: NodeMatcher;
   let commentNodeMatcher: NodeMatcher;
   try {
@@ -39,6 +51,8 @@ export function processSurroundingPair(
     commentNodeMatcher = getNodeMatcher(document.languageId, "comment", false);
   } catch (err) {
     if ((err as Error).name === "UnsupportedLanguageError") {
+      // If we're in a language where we don't have a parse tree we use the text
+      // based algorithm
       return findSurroundingPairTextBased(
         selection.editor,
         selection.selection,
@@ -51,6 +65,8 @@ export function processSurroundingPair(
     }
   }
 
+  // If we have a parse tree but we are in a string node or in a comment node,
+  // then we use the text-based algorithm
   const isStringNode = stringNodeMatcher(selection, node) != null;
   if (isStringNode || commentNodeMatcher(selection, node) != null) {
     let nodeRange: Range;
@@ -89,6 +105,9 @@ export function processSurroundingPair(
     }
   }
 
+  // If we have a parse tree and either we are not in a string or comment or we
+  // couldn't find a surrounding pair within a string or comment, we use the
+  // parse tree-based algorithm
   return findSurroundingPairParseTreeBased(
     selection.editor,
     selection.selection,
