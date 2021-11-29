6c85e3730c403a4cd736cdac1cecce920529a0ce
Pokey Rule <pokey.rule@gmail.com>
2021-11-29 15:09:05 +0000

    Some cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,13 @@
 import * as vscode from "vscode";
 import graphFactories from "./util/graphFactories";
-import inferFullTargets from "./core/inferFullTargets";
-import processTargets from "./processTargets";
-import { Graph, PartialTarget, ProcessedTargetsContext } from "./typings/Types";
+import { Graph } from "./typings/Types";
 import makeGraph, { FactoryMap } from "./util/makeGraph";
 import { logBranchTypes } from "./util/debug";
 import { ThatMark } from "./core/ThatMark";
 import { TestCaseRecorder } from "./testUtil/TestCaseRecorder";
 import { getCommandServerApi, getParseTreeApi } from "./util/getExtensionApi";
-import { canonicalizeAndValidateCommand } from "./util/canonicalizeAndValidateCommand";
-import { doTargetsUsePrePhraseSnapshot } from "./util/doTargetsUsePrePhraseSnapshot";
 import isTesting from "./testUtil/isTesting";
+import { registerCursorlessCommand } from "./registerCursorlessCommand";
 
 export async function activate(context: vscode.ExtensionContext) {
   const { getNodeAtLocation } = await getParseTreeApi();
@@ -45,119 +42,13 @@ export async function activate(context: vscode.ExtensionContext) {
     }
   );
 
-  const cursorlessCommandDisposable = vscode.commands.registerCommand(
-    "cursorless.command",
-    async (
-      spokenForm: string,
-      inputActionName: string,
-      inputPartialTargets: PartialTarget[],
-      ...inputExtraArgs: unknown[]
-    ) => {
-      try {
-        const { actionName, partialTargets, extraArgs } =
-          canonicalizeAndValidateCommand(
-            inputActionName,
-            inputPartialTargets,
-            inputExtraArgs
-          );
-
-        const usePrePhraseSnapshot =
-          doTargetsUsePrePhraseSnapshot(partialTargets);
-        const readableHatMap = await graph.hatTokenMap.getReadableMap(
-          usePrePhraseSnapshot
-        );
-
-        console.debug(`spokenForm: ${spokenForm}`);
-        console.debug(`action: ${actionName}`);
-        console.debug(`partialTargets:`);
-        console.debug(JSON.stringify(partialTargets, null, 3));
-        console.debug(`extraArgs:`);
-        console.debug(JSON.stringify(extraArgs, null, 3));
-
-        const action = graph.actions[actionName];
-
-        if (action == null) {
-          throw new Error(`Unknown action ${actionName}`);
-        }
-
-        const targets = inferFullTargets(
-          partialTargets,
-          action.getTargetPreferences(...extraArgs)
-        );
-
-        const processedTargetsContext: ProcessedTargetsContext = {
-          currentSelections:
-            vscode.window.activeTextEditor?.selections.map((selection) => ({
-              selection,
-              editor: vscode.window.activeTextEditor!,
-            })) ?? [],
-          currentEditor: vscode.window.activeTextEditor,
-          hatTokenMap: readableHatMap,
-          thatMark: thatMark.exists() ? thatMark.get() : [],
-          sourceMark: sourceMark.exists() ? sourceMark.get() : [],
-          getNodeAtLocation,
-        };
-
-        const selections = processTargets(processedTargetsContext, targets);
-
-        if (testCaseRecorder.active) {
-          const command = { actionName, partialTargets, extraArgs };
-          const context = {
-            targets,
-            thatMark,
-            sourceMark,
-            hatTokenMap: readableHatMap,
-            spokenForm,
-          };
-          await testCaseRecorder.preCommandHook(command, context);
-        }
-
-        const {
-          returnValue,
-          thatMark: newThatMark,
-          sourceMark: newSourceMark,
-        } = await action.run(selections, ...extraArgs);
-
-        thatMark.set(newThatMark);
-        sourceMark.set(newSourceMark);
-
-        if (testCaseRecorder.active) {
-          await testCaseRecorder.postCommandHook(returnValue);
-        }
-
-        return returnValue;
-
-        // writeFileSync(
-        //   "/Users/pokey/src/cursorless-vscode/inferFullTargetsTests.jsonl",
-        //   JSON.stringify({
-        //     input: { context, partialTargets, preferredPositions },
-        //     expectedOutput: targets,
-        //   }) + "\n",
-        //   { flag: "a" }
-        // );
-
-        // writeFileSync(
-        //   "/Users/pokey/src/cursorless-vscode/processTargetsTests.jsonl",
-        //   JSON.stringify({
-        //     input: {
-        //       context: processedTargetsContext,
-        //       targets,
-        //     },
-        //     expectedOutput: selections,
-        //   }) + "\n",
-        //   { flag: "a" }
-        // );
-
-        // const processedTargets = processTargets(hatTokenMap!, targets);
-      } catch (e) {
-        testCaseRecorder.commandErrorHook();
-        const err = e as Error;
-        vscode.window.showErrorMessage(err.message);
-        console.debug(err.message);
-        console.debug(err.stack);
-        throw err;
-      }
-    }
+  // TODO: Do this using the graph once we migrate its dependencies onto the graph
+  const cursorlessCommandDisposable = registerCursorlessCommand(
+    graph,
+    thatMark,
+    sourceMark,
+    getNodeAtLocation,
+    testCaseRecorder
   );
 
   // Disabled for now.
