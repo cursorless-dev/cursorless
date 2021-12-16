ce3ba65b2a1bfb729fd4b8516063b59206592392
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2021-12-16 22:04:08 +0100

    Enable to fold in a different editor/across split

--- src/actions/Fold.ts
+++ src/actions/Fold.ts
@@ -1,4 +1,4 @@
-import { commands } from "vscode";
+import { commands, window } from "vscode";
 import {
   Action,
   ActionPreferences,
@@ -6,6 +6,7 @@ import {
   Graph,
   TypedSelection,
 } from "../typings/Types";
+import { focusEditor } from "../util/setSelectionsAndFocusEditor";
 import { ensureSingleEditor } from "../util/targetUtils";
 
 class FoldAction implements Action {
@@ -21,7 +22,12 @@ class FoldAction implements Action {
     TypedSelection[],
     TypedSelection[]
   ]): Promise<ActionReturnValue> {
-    ensureSingleEditor(targets);
+    const originalEditor = window.activeTextEditor;
+    const editor = ensureSingleEditor(targets);
+
+    if (originalEditor !== editor) {
+      await focusEditor(editor);
+    }
 
     await commands.executeCommand(this.command, {
       levels: 1,
@@ -30,6 +36,11 @@ class FoldAction implements Action {
         (target) => target.selection.selection.start.line
       ),
     });
+
+    // If necessary focus back original editor
+    if (originalEditor != null && originalEditor !== window.activeTextEditor) {
+      await focusEditor(originalEditor);
+    }
 
     return {
       thatMark: targets.map((target) => target.selection),
