db2f0b625e7ddef5d10305d0683c1fe532a55d79
Stefan du Fresne <stefan@sdufresne.info>
2022-01-06 15:20:34 +0000

    paired delimiters

--- src/languages/getTextFragmentExtractor.ts
+++ src/languages/getTextFragmentExtractor.ts
@@ -149,7 +149,10 @@ const textFragmentExtractors: Record<
     jsonStringTextFragmentExtractor
   ),
   python: constructDefaultTextFragmentExtractor("python"),
-  scala: constructDefaultTextFragmentExtractor("scala"),
+  scala: constructDefaultTextFragmentExtractor(
+    "scala",
+    constructHackedStringTextFragmentExtractor("scala")
+  ),
   typescript: constructDefaultTextFragmentExtractor(
     "typescript",
     typescriptStringTextFragmentExtractor
