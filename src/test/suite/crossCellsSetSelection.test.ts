004ce1405162efda251826f6ae01c1fc1bf697db
Pokey Rule <pokey.rule@gmail.com>
2022-01-08 16:05:02 +0000

    Cross-cell set selection (#430)
    * Initial work
    
    * More stuff
    
    * Initial working version
    
    * Ad comment
    
    * Add test
    
    * Removed notebook
    
    * Add documentation
    
    * Fix typo
    
    * Up da documentation
    
    * Try to fix ci
    
    * Add comment
    
    * Another attempt at ci
    
    * ci
    
    Co-authored-by: Andreas Arvidsson <andreas.arvidsson87@gmail.com>

---
+++ src/test/suite/crossCellsSetSelection.test.ts
@@ -0,0 +1,54 @@
+import * as assert from "assert";
+import * as vscode from "vscode";
+import * as sinon from "sinon";
+import { getCursorlessApi } from "../../util/getExtensionApi";
+import { openNewEditor, openNewNotebookEditor } from "../openNewEditor";
+import sleep from "../../util/sleep";
+
+// Check that setSelection is able to focus the correct cell
+suite("Cross-cell set selection", async function () {
+  this.timeout("100s");
+  this.retries(3);
+
+  teardown(() => {
+    sinon.restore();
+  });
+
+  test("Cross-cell set selection", runTest);
+});
+
+async function runTest() {
+  const graph = (await getCursorlessApi()).graph!;
+
+  await openNewNotebookEditor(['"hello"', '"world"']);
+
+  // FIXME: There seems to be some timing issue when you create a notebook
+  // editor
+  await sleep(1000);
+
+  await graph.hatTokenMap.addDecorations();
+
+  await vscode.commands.executeCommand(
+    "cursorless.command",
+    "take whale",
+    "setSelection",
+    [
+      {
+        type: "primitive",
+        mark: {
+          type: "decoratedSymbol",
+          symbolColor: "default",
+          character: "w",
+        },
+      },
+    ]
+  );
+
+  const editor = vscode.window.activeTextEditor;
+
+  if (editor == null) {
+    assert(false, "No editor was focused");
+  }
+
+  assert.deepStrictEqual(editor.document.getText(editor.selection), "world");
+}
