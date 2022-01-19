3e29ea277e6311524e1c97353946e0bd54e3c507
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-01-19 07:07:28 +0100

    Added breakpoint removed tests

--- src/test/suite/breakpoints.test.ts
+++ src/test/suite/breakpoints.test.ts
@@ -8,6 +8,10 @@ suite("breakpoints", async function () {
   this.timeout("100s");
   this.retries(3);
 
+  setup(() => {
+    removeBreakpoints();
+  });
+
   teardown(() => {
     sinon.restore();
   });
@@ -16,18 +20,16 @@ suite("breakpoints", async function () {
     removeBreakpoints();
   });
 
-  test("breakpoint harp", breakpointHarp);
-  test("breakpoint token harp", breakpointTokenHarp);
+  test("breakpoint harp add", breakpointHarpAdd);
+  test("breakpoint token harp add", breakpointTokenHarpAdd);
+  test("breakpoint harp remove", breakpointHarpRemove);
+  test("breakpoint Token harp remove", breakpointTokenHarpRemove);
 });
 
-async function breakpointHarp() {
+async function breakpointHarpAdd() {
   const graph = (await getCursorlessApi()).graph!;
-
   await openNewEditor("  hello");
-
   await graph.hatTokenMap.addDecorations();
-
-  removeBreakpoints();
 
   await vscode.commands.executeCommand(
     "cursorless.command",
@@ -46,21 +48,16 @@ async function breakpointHarp() {
   );
 
   const breakpoints = vscode.debug.breakpoints;
-
   assert.deepStrictEqual(breakpoints.length, 1);
   assert.ok(breakpoints[0] instanceof vscode.SourceBreakpoint);
   const breakpoint = <vscode.SourceBreakpoint>breakpoints[0];
   assert.ok(breakpoint.location.range.isEqual(new vscode.Range(0, 0, 0, 0)));
 }
 
-async function breakpointTokenHarp() {
+async function breakpointTokenHarpAdd() {
   const graph = (await getCursorlessApi()).graph!;
-
   await openNewEditor("  hello");
-
   await graph.hatTokenMap.addDecorations();
-
-  removeBreakpoints();
 
   await vscode.commands.executeCommand(
     "cursorless.command",
@@ -80,11 +77,75 @@ async function breakpointTokenHarp() {
   );
 
   const breakpoints = vscode.debug.breakpoints;
-
   assert.deepStrictEqual(breakpoints.length, 1);
   assert.ok(breakpoints[0] instanceof vscode.SourceBreakpoint);
   const breakpoint = <vscode.SourceBreakpoint>breakpoints[0];
   assert.ok(breakpoint.location.range.isEqual(new vscode.Range(0, 2, 0, 7)));
+}
+
+async function breakpointHarpRemove() {
+  const graph = (await getCursorlessApi()).graph!;
+  const editor = await openNewEditor("  hello");
+  await graph.hatTokenMap.addDecorations();
+
+  vscode.debug.addBreakpoints([
+    new vscode.SourceBreakpoint(
+      new vscode.Location(editor.document.uri, new vscode.Range(0, 0, 0, 0))
+    ),
+  ]);
+
+  assert.deepStrictEqual(vscode.debug.breakpoints.length, 1);
+
+  await vscode.commands.executeCommand(
+    "cursorless.command",
+    "breakpoint harp",
+    "setBreakpoint",
+    [
+      {
+        type: "primitive",
+        mark: {
+          type: "decoratedSymbol",
+          symbolColor: "default",
+          character: "h",
+        },
+      },
+    ]
+  );
+
+  assert.deepStrictEqual(vscode.debug.breakpoints.length, 0);
+}
+
+async function breakpointTokenHarpRemove() {
+  const graph = (await getCursorlessApi()).graph!;
+  const editor = await openNewEditor("  hello");
+  await graph.hatTokenMap.addDecorations();
+
+  vscode.debug.addBreakpoints([
+    new vscode.SourceBreakpoint(
+      new vscode.Location(editor.document.uri, new vscode.Range(0, 2, 0, 7))
+    ),
+  ]);
+
+  assert.deepStrictEqual(vscode.debug.breakpoints.length, 1);
+
+  await vscode.commands.executeCommand(
+    "cursorless.command",
+    "breakpoint token harp",
+    "setBreakpoint",
+    [
+      {
+        type: "primitive",
+        selectionType: "token",
+        mark: {
+          type: "decoratedSymbol",
+          symbolColor: "default",
+          character: "h",
+        },
+      },
+    ]
+  );
+
+  assert.deepStrictEqual(vscode.debug.breakpoints.length, 0);
 }
 
 function removeBreakpoints() {
