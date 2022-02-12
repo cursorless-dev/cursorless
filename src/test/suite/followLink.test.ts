f5dde3e5bf6246ee586b41d7bde484458bf569c3
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-02-12 12:13:28 +0100

    Added test to assert that go to definition works

---
+++ src/test/suite/followLink.test.ts
@@ -0,0 +1,45 @@
+import * as assert from "assert";
+import * as vscode from "vscode";
+import * as sinon from "sinon";
+import { openNewEditor } from "../openNewEditor";
+
+suite("followLink", async function () {
+  this.timeout("100s");
+  this.retries(3);
+
+  teardown(() => {
+    sinon.restore();
+  });
+
+  test("follow fine", followFine);
+});
+
+async function followFine() {
+  const editor = await openNewEditor(
+    "const foo = 'hello';\nconst bar = foo;",
+    "typescript"
+  );
+  await vscode.commands.executeCommand("revealLine", {
+    lineNumber: 1,
+    at: "top",
+  });
+  editor.selections = [new vscode.Selection(1, 12, 1, 15)];
+
+  assert.equal(editor.visibleRanges[0].start.line, 1);
+
+  await vscode.commands.executeCommand(
+    "cursorless.command",
+    "follow fine",
+    "followLink",
+    [
+      {
+        type: "primitive",
+        mark: {
+          type: "cursor",
+        },
+      },
+    ]
+  );
+
+  assert.equal(editor.visibleRanges[0].start.line, 0);
+}
