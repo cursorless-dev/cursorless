6a171eb03027c6bb490b115840e30257c983ba94
Kirill Dubovitskiy <kirill2003de@gmail.com>
2022-02-21 23:10:03 -0800

    Fixed links to repository sub directories

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -5,12 +5,41 @@ const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const path = require('path');
 
+function remarkPluginFixLinks() {
+  /** @type {import('unified').Transformer} */
+  const transformer = async (ast, file) => {
+    // Package does not support require es modules
+    let { visit } = await import('unist-util-visit');
+    visit(ast, (node) => {
+      if (node.type !== 'link') {
+        return;
+      }
+      /** @type string */
+      let link = node.url;
+      if (link.indexOf('http') !== -1) {
+        return;
+      }
+      let srcDirPath = path.resolve(__dirname, '..');
+      let absolutePath = path.resolve(__dirname, '..', file.dirname, link);
+      let pathRelativeToSrc = path.relative(srcDirPath, absolutePath);
+      if (pathRelativeToSrc.startsWith('src')
+        || pathRelativeToSrc.startsWith('cursorless-snippets')
+        || pathRelativeToSrc.startsWith('.github')) {
+        const repositoryPath = 'https://github.com/cursorless-dev/cursorless-vscode/tree/main/';
+        const linkToRepositoryArtifact = repositoryPath.concat(pathRelativeToSrc);
+        node.url = linkToRepositoryArtifact;
+      }
+    });
+  };
+  return transformer;
+};
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Cursorless',
   tagline: 'Never use the cursor again',
-  url: 'https://github.com/pokey/cursorless-vscode',
-  baseUrl: '/',
+  url: 'https://github.com/',
+  baseUrl: '/cursorless-dev/cursorless-vscode/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
@@ -62,6 +91,7 @@ const config = {
           path: '../docs',
           routeBasePath: '/',
           sidebarPath: require.resolve('./contributorSidebar.js'),
+          beforeDefaultRemarkPlugins: [remarkPluginFixLinks],
         },
       })
     ],
