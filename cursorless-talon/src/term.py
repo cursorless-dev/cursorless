e6c4925f39767e1593b687b06a6fe06d93c2b6ef
Will Sommers <will.sommers@gmail.com>
2022-03-10 17:53:38 -0500

    Fix lint error in terms.py

--- cursorless-talon/src/term.py
+++ cursorless-talon/src/term.py
@@ -7,6 +7,7 @@ mod = Module()
 
 CURSORLESS_WINDOW_TRIGGER = "(cursorless | cursor list | cursor less)"
 
+
 @mod.capture(rule=CURSORLESS_WINDOW_TRIGGER)
 def cursorless_window_trigger(m) -> str:
     """Term to trigger cursorless help functions in talon"""
