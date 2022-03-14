b784e5229836148da793d93fb64a6a895dcb49db
Kirill Dubovitskiy <kirill2003de@gmail.com>
2022-03-14 01:38:19 -0700

    Update docs-build.sh to clean out old artifacts
    Co-authored-by: Pokey Rule <755842+pokey@users.noreply.github.com>

--- docs-build.sh
+++ docs-build.sh
@@ -12,5 +12,6 @@ cd ..
 # Since baseUrl in Docusaurus is /docs, for links within our website
 # to work correctly we need to serve /gh-pages-root with /docs subfolder
 # containting the build
+rm -rf gh-pages-root/docs
 mkdir -p gh-pages-root/docs
 cp -r website/build/* gh-pages-root/docs
