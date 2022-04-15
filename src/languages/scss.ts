3a87f6e8849faad68e89d076c1899cf2ba06886f
Pokey Rule <755842+pokey@users.noreply.github.com>
2022-04-15 07:21:49 -0700

    Add docstring

--- src/languages/scss.ts
+++ src/languages/scss.ts
@@ -1,3 +1,10 @@
+import { SyntaxNode } from "web-tree-sitter";
+import {
+  NodeMatcherAlternative,
+  ScopeType,
+  SelectionWithEditor,
+} from "../typings/Types";
+import { patternFinder } from "../util/nodeFinders";
 import {
   cascadingMatcher,
   conditionMatcher,
@@ -7,13 +14,10 @@ import {
   trailingMatcher,
 } from "../util/nodeMatchers";
 import {
-  NodeMatcherAlternative,
-  ScopeType,
-  SelectionWithEditor,
-} from "../typings/Types";
-import { patternFinder } from "../util/nodeFinders";
-import { getNodeRange, childRangeSelector, delimitedSelector } from "../util/nodeSelectors";
-import { SyntaxNode } from "web-tree-sitter";
+  childRangeSelector,
+  delimitedSelector,
+  getNodeRange,
+} from "../util/nodeSelectors";
 
 // curl https://raw.githubusercontent.com/serenadeai/tree-sitter-scss/c478c6868648eff49eb04a4df90d703dc45b312a/src/node-types.json \
 //  | jq '[.[] | select(.type =="stylesheet") | .children.types[] | select(.type !="declaration") | .type ]'
@@ -45,10 +49,18 @@ const STATEMENT_TYPES = [
 
 function isArgumentListDelimiter(node: SyntaxNode) {
   return [",", "(", ")"].includes(node.type) || isAtDelimiter(node);
-};
+}
 
+/**
+ * Determines whether the given `node` is an `at` delimiter node, used in a css
+ * / scss argument list.  For example, the `at` in the call
+ * `ellipse(115px 55px at 50% 40%)`
+ *
+ * @param node The node to check
+ * @returns `true` if the node is an `at` delimiter node
+ */
 function isAtDelimiter(node: SyntaxNode) {
-  return (node.type === "plain_value" && node.text === "at");
+  return node.type === "plain_value" && node.text === "at";
 }
 
 /**
@@ -58,9 +70,11 @@ function isAtDelimiter(node: SyntaxNode) {
  * @param siblingFunc returns the previous or next sibling of the current node if present.
  * @returns A non-delimiter node
  */
-function findAdjacentArgValues(siblingFunc: (node: SyntaxNode) => SyntaxNode | null) {
+function findAdjacentArgValues(
+  siblingFunc: (node: SyntaxNode) => SyntaxNode | null
+) {
   return (node: SyntaxNode) => {
-    // Handle the case where we are the cursor is placed before a delimiter, e.g. "|at" 
+    // Handle the case where we are the cursor is placed before a delimiter, e.g. "|at"
     // and we erroneously expand in both directions.
     if (isAtDelimiter(node) || node.type === ",") {
       node = node.previousSibling!;
@@ -83,7 +97,8 @@ const nodeMatchers: Partial<Record<ScopeType, NodeMatcherAlternative>> = {
     patternMatcher(...STATEMENT_TYPES),
     matcher(
       patternFinder("attribute_selector"),
-      childRangeSelector([], ["attribute_name", "string_value"]))
+      childRangeSelector([], ["attribute_name", "string_value"])
+    )
   ),
   string: "string_value",
   functionCall: "call_expression",
