bbe5eb00645fb07c8a27f593e99629dcdf17270b
James Stout <james.wolf.stout@gmail.com>
2022-05-03 12:12:37 -0700

    Support line numbers separated by list connectives.

--- cursorless-talon/src/marks/lines_number.py
+++ cursorless-talon/src/marks/lines_number.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Optional
 
 from talon import Context, Module
 
@@ -32,27 +33,44 @@ directions_map = {d.cursorlessIdentifier: d for d in directions}
 DEFAULT_DIRECTIONS = {d.defaultSpokenForm: d.cursorlessIdentifier for d in directions}
 
 
+@dataclass
+class LineNumberRange:
+    start_number: int
+    range_connective_with_type: Optional[dict] = None
+    end_number: Optional[int] = None
+
+
 @mod.capture(
     rule=(
-        "{user.cursorless_line_direction} <number_small> "
-        "[<user.cursorless_range_connective_with_type> <number_small>]"
+        "<number_small> [<user.cursorless_range_connective_with_type> <number_small>]"
     )
 )
-def cursorless_line_number(m) -> str:
-    direction = directions_map[m.cursorless_line_direction]
+def cursorless_line_number_range(m) -> LineNumberRange:
+    if len(m.number_small_list) == 1:
+        return LineNumberRange(start_number=m.number_small)
+    return LineNumberRange(
+        start_number=m.number_small_1,
+        range_connective_with_type=m.cursorless_range_connective_with_type,
+        end_number=m.number_small_2,
+    )
+
+
+def create_line_number_target(
+    direction: CustomizableTerm, range: LineNumberRange
+) -> dict:
     start_line = {
-        "lineNumber": direction.formatter(m.number_small_1),
+        "lineNumber": direction.formatter(range.start_number),
         "type": direction.type,
     }
-    if len(m.number_small_list) == 2:
-        range_connective_with_type = m.cursorless_range_connective_with_type
+    if range.range_connective_with_type:
+        range_connective_with_type = range.range_connective_with_type
         range_connective = range_connective_with_type["connective"]
         range_type = range_connective_with_type["type"]
         end_line = {
-            "lineNumber": direction.formatter(m.number_small_2),
+            "lineNumber": direction.formatter(range.end_number),
             "type": direction.type,
         }
-        range = {
+        target = {
             "type": "range",
             "start": {
                 "type": "primitive",
@@ -78,10 +96,11 @@ def cursorless_line_number(m) -> str:
             "excludeEnd": range_connective in ["rangeExclusive", "rangeExcludingEnd"],
         }
         if range_type:
-            range["rangeType"] = range_type
-        return range
+            target["rangeType"] = range_type
+        return target
     else:
         return {
+            "type": "primitive",
             "selectionType": "line",
             "mark": {
                 "type": "lineNumber",
@@ -89,3 +108,20 @@ def cursorless_line_number(m) -> str:
                 "active": start_line,
             },
         }
+
+
+@mod.capture(
+    rule=(
+        "{user.cursorless_line_direction} <user.cursorless_line_number_range> "
+        "({user.cursorless_list_connective} <user.cursorless_line_number_range>)*"
+    )
+)
+def cursorless_line_number(m) -> dict:
+    direction = directions_map[m.cursorless_line_direction]
+    if len(m.cursorless_line_number_range_list) == 1:
+        return create_line_number_target(direction, m.cursorless_line_number_range)
+    targets = [
+        create_line_number_target(direction, range)
+        for range in m.cursorless_line_number_range_list
+    ]
+    return {"type": "list", "elements": targets}
