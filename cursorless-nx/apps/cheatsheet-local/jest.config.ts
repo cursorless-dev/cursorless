e89cab6923ed6ab55eb8c15d5d415dde2647c9f6
Pokey Rule <755842+pokey@users.noreply.github.com>
2022-05-07 18:19:24 +0100

    `nx generate @nrwl/react:application cheatsheet-local`

---
+++ cursorless-nx/apps/cheatsheet-local/jest.config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'cheatsheet-local',
+  preset: '../../jest.preset.ts',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/cheatsheet-local',
+};
