eb982227d26e9d4167ff4defb4e6a54e46193226
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-05-16 14:35:44 +0200

    Restored old implementation of edit newline

--- src/actions/EditNewLine.ts
+++ src/actions/EditNewLine.ts
@@ -1,13 +1,26 @@
-import { commands, Position, Range, Selection, TextEditor } from "vscode";
+import { commands, Range, TextEditor } from "vscode";
 import { Target } from "../typings/target.types";
 import { Graph } from "../typings/Types";
 import { getNotebookFromCellDocument } from "../util/notebook";
-import { createThatMark, runOnTargetsForEachEditor } from "../util/targetUtils";
 import { Action, ActionReturnValue } from "./actions.types";
 
 class EditNewLine implements Action {
   constructor(private graph: Graph, private isAbove: boolean) {
     this.run = this.run.bind(this);
+  }
+
+  private correctForParagraph(targets: Target[]) {
+    targets.forEach((target) => {
+      let { start, end } = target.contentRange;
+      if (target.scopeType === "paragraph") {
+        if (this.isAbove && target.leadingDelimiterRange != null) {
+          start = start.translate({ lineDelta: -1 });
+        } else if (!this.isAbove && target.trailingDelimiterRange != null) {
+          end = end.translate({ lineDelta: 1 });
+        }
+        target.contentRange = new Range(start, end);
+      }
+    });
   }
 
   private isNotebookEditor(editor: TextEditor) {
@@ -25,77 +38,28 @@ class EditNewLine implements Action {
         ? "jupyter.insertCellAbove"
         : "jupyter.insertCellBelow";
     }
-    return null;
+    return this.isAbove
+      ? "editor.action.insertLineBefore"
+      : "editor.action.insertLineAfter";
   }
 
   async run([targets]: [Target[]]): Promise<ActionReturnValue> {
-    const command = this.getCommand(targets[0]);
-    if (command) {
-      if (this.isAbove) {
-        await this.graph.actions.setSelectionBefore.run([targets]);
-      } else {
-        await this.graph.actions.setSelectionAfter.run([targets]);
-      }
-      await commands.executeCommand(command);
-      return { thatMark: createThatMark(targets) };
+    this.correctForParagraph(targets);
+
+    if (this.isAbove) {
+      await this.graph.actions.setSelectionBefore.run([targets]);
+    } else {
+      await this.graph.actions.setSelectionAfter.run([targets]);
     }
 
-    const thatMark = await runOnTargetsForEachEditor(
-      targets,
-      async (editor, targets) => {
-        const edits = targets.map((target) => {
-          const delimiter = target.scopeType === "paragraph" ? "\n\n" : "\n";
-          const lineNumber = this.isAbove
-            ? target.contentRange.start.line
-            : target.contentRange.end.line;
-          const line = editor.document.lineAt(lineNumber);
-          const { firstNonWhitespaceCharacterIndex } = line;
-          const padding = line.text.slice(0, firstNonWhitespaceCharacterIndex);
-          const text = this.isAbove ? padding + delimiter : delimiter + padding;
-          const position = this.isAbove ? line.range.start : line.range.end;
-          const lineDelta = delimiter.length;
-          return {
-            contentRange: target.contentRange,
-            lineDelta,
-            firstNonWhitespaceCharacterIndex,
-            position,
-            text,
-          };
-        });
-
-        await editor.edit((editBuilder) => {
-          edits.forEach((edit) => {
-            editBuilder.insert(edit.position, edit.text);
-          });
-        });
-
-        editor.selections = edits.map((edit) => {
-          const selectionLineNum = this.isAbove
-            ? edit.position.line
-            : edit.position.line + edit.lineDelta;
-          const positionSelection = new Position(
-            selectionLineNum,
-            edit.firstNonWhitespaceCharacterIndex
-          );
-          return new Selection(positionSelection, positionSelection);
-        });
-
-        const thatMarkRanges = edits.map((edit) => {
-          const { contentRange, lineDelta } = edit;
-          return this.isAbove
-            ? new Range(
-                contentRange.start.translate({ lineDelta }),
-                contentRange.end.translate({ lineDelta })
-              )
-            : contentRange;
-        });
-
-        return createThatMark(targets, thatMarkRanges);
-      }
-    );
+    const command = this.getCommand(targets[0]);
+    await commands.executeCommand(command);
 
     return {
-      thatMark: thatMark.flat(),
+      thatMark: targets.map((target) => ({
+        selection: target.editor.selection,
+        editor: target.editor,
+      })),
     };
   }
 }
