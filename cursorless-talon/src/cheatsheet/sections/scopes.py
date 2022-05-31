338e6ab68d50025c5893f7164db01d8fb2d16270
Will Sommers <will.sommers@gmail.com>
2022-05-31 16:41:03 -0400

    Add head/tail to scopes list

--- cursorless-talon/src/cheatsheet/sections/scopes.py
+++ cursorless-talon/src/cheatsheet/sections/scopes.py
@@ -4,7 +4,7 @@ from ..get_list import get_lists
 def get_scopes():
     return {
         **get_lists(
-            ["scope_type", "selection_type", "subtoken_scope_type"],
+            ["scope_type", "selection_type", "subtoken_scope_type", "head_tail"],
             {"argumentOrParameter": "Argument"},
         ),
         "<P>": "Paired delimiter",
