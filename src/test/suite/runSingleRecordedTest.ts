51abd442842e230390f194b6c832c3d7fd1b8178
Pokey Rule <755842+pokey@users.noreply.github.com>
2022-06-02 00:10:06 +0100

    Add run single test launch config

--- src/test/suite/runSingleRecordedTest.ts
+++ src/test/suite/runSingleRecordedTest.ts
@@ -1,14 +1,12 @@
 /**
  * Add the file path ending of a recorded test and ONLY that one will run
+ * when using the "Run Single Extension Test" launch configuration.
  */
 
-// example:
-// const filenameEnd = "textual/takePairRound.yml";
-
-const filenameEnd = "";
+const filenameEnd = "actions/postVest.yml";
 
 export function runSingleTest() {
-  return !!filenameEnd;
+  return process.env.CURSORLESS_RUN_SINGLE_TEST === "true";
 }
 
 export function getSingleTestFilename(): string {
