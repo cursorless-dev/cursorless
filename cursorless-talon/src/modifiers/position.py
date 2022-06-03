1c3b39111eeef510c0ac19ba9163031bf3de0680
Pokey Rule <755842+pokey@users.noreply.github.com>
2022-06-03 11:51:54 +0100

    More positional tweaks on talon side

--- cursorless-talon/src/modifiers/position.py
+++ cursorless-talon/src/modifiers/position.py
@@ -1,14 +1,21 @@
 from typing import Any
 
-from talon import Context, Module
+from talon import Context, Module, app
+
+from ..csv_overrides import init_csv_and_watch_changes
 
 mod = Module()
 ctx = Context()
 
-mod.list("cursorless_position", desc="Types of positions")
-ctx.lists["self.cursorless_position"] = {
+mod.list("cursorless_position", desc='Positions such as "before", "after" etc')
+
+# NOTE: Please do not change these dicts.  Use the CSVs for customization.
+# See https://www.cursorless.org/docs/user/customization/
+positions = {
     "start of": "start",
     "end of": "end",
+    "before": "before",
+    "after": "after",
 }
 
 
@@ -18,13 +25,16 @@ def construct_positional_modifier(position: str) -> dict:
 
 # Note that we allow positional connectives such as "before" and "after" to appear
 # as modifiers. We may disallow this in the future.
-@mod.capture(
-    rule="{user.cursorless_position} | {user.cursorless_positional_connective}"
-)
+@mod.capture(rule="{user.cursorless_position}")
 def cursorless_position(m) -> dict[str, Any]:
-    try:
-        position = m.cursorless_position
-    except AttributeError:
-        position = m.cursorless_positional_connective
+    return construct_positional_modifier(m.cursorless_position)
 
-    return construct_positional_modifier(position)
+
+def on_ready():
+    init_csv_and_watch_changes(
+        "positions",
+        {"position": positions},
+    )
+
+
+app.register("ready", on_ready)
--- cursorless-talon/src/positional_target.py
+++ cursorless-talon/src/positional_target.py
@@ -1,31 +1,20 @@
-from talon import Context, Module
+from talon import Module
 
 from .modifiers.position import construct_positional_modifier
 
 mod = Module()
-ctx = Context()
-
-mod.list(
-    "cursorless_positional_connective",
-    desc='Connectives used to separate source and destination targets that indicate position, eg "before" or "after"',
-)
-
-ctx.lists["self.cursorless_positional_connective"] = {
-    "before": "before",
-    "after": "after",
-}
 
 
 @mod.capture(
     rule=(
-        "({user.cursorless_positional_connective} | {user.cursorless_source_destination_connective}) "
+        "({user.cursorless_position} | {user.cursorless_source_destination_connective}) "
         "<user.cursorless_target>"
     )
 )
 def cursorless_positional_target(m) -> list[dict]:
     target = m.cursorless_target
     try:
-        modifier = construct_positional_modifier(m.cursorless_positional_connective)
+        modifier = construct_positional_modifier(m.cursorless_position)
         return update_first_primitive_target(target, modifier)
     except AttributeError:
         return target
