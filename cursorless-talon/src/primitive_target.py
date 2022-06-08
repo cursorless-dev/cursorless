50ebcfa6b22090565f075927cd8773d717004322
Pokey Rule <755842+pokey@users.noreply.github.com>
2022-06-08 15:48:43 +0100

    Fix `"bring line before bat"`

--- cursorless-talon/src/primitive_target.py
+++ cursorless-talon/src/primitive_target.py
@@ -1,3 +1,4 @@
+from contextlib import suppress
 from typing import Any
 
 from talon import Module
@@ -8,8 +9,10 @@ BASE_TARGET: dict[str, Any] = {"type": "primitive"}
 IMPLICIT_TARGET = {"type": "primitive", "isImplicit": True}
 
 
+# NB: We don't include positions here because we require them to be first so
+# that we don't end up with ambiguity.
+# See https://github.com/cursorless-dev/cursorless/issues/747
 modifiers = [
-    "<user.cursorless_position>",  # before, end of
     "<user.cursorless_simple_modifier>",  # eg inside, bounds, just, head, tail, leading, trailing
     "<user.cursorless_containing_scope>",  # funk, state, class
     "<user.cursorless_subtoken_scope>",  # first past second word
@@ -23,21 +26,27 @@ def cursorless_modifier(m) -> str:
     return m[0]
 
 
+# NB: We require positions to be first so that we don't end up with ambiguity
+# See https://github.com/cursorless-dev/cursorless/issues/747
 @mod.capture(
-    rule="<user.cursorless_modifier>+ [<user.cursorless_mark>] | <user.cursorless_mark>"
+    rule=(
+        "[<user.cursorless_position>] "
+        "(<user.cursorless_modifier>+ [<user.cursorless_mark>] | <user.cursorless_mark>)"
+    )
 )
 def cursorless_primitive_target(m) -> dict[str, Any]:
     """Supported extents for cursorless navigation"""
     result = BASE_TARGET.copy()
 
-    try:
-        result["mark"] = m.cursorless_mark
-    except AttributeError:
-        pass
+    modifiers = [
+        *getattr(m, "cursorless_position_list", []),
+        *getattr(m, "cursorless_modifier_list", []),
+    ]
 
-    try:
-        result["modifiers"] = m.cursorless_modifier_list
-    except AttributeError:
-        pass
+    if modifiers:
+        result["modifiers"] = modifiers
+
+    with suppress(AttributeError):
+        result["mark"] = m.cursorless_mark
 
     return result
