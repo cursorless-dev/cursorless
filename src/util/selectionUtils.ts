0336b56c9a2a43dbb8ebc6a0ca2912dfea4577a0
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-06-13 11:22:14 +0200

    Updated shrink range to fit content

--- src/util/selectionUtils.ts
+++ src/util/selectionUtils.ts
@@ -44,56 +44,13 @@ export function selectionFromRange(isReversed: boolean, range: Range) {
 }
 
 export function shrinkRangeToFitContent(editor: TextEditor, range: Range) {
-  const { lineAt } = editor.document;
-
-  const start = (() => {
-    const firstLine = lineAt(range.start);
-    const text = firstLine.text.substring(range.start.character);
-    if (text.trim().length === 0) {
-      for (let i = firstLine.lineNumber + 1; i <= range.end.line; ++i) {
-        const line = lineAt(i);
-        if (!line.isEmptyOrWhitespace) {
-          return new Position(i, line.firstNonWhitespaceCharacterIndex);
-        }
-      }
-      return range.start;
-    }
-    return new Position(
-      firstLine.lineNumber,
-      firstNonWhitespaceCharacterIndex(range, text)
-    );
-  })();
-
-  const end = (() => {
-    const endLine = lineAt(range.end);
-    const text = endLine.text.substring(0, range.end.character);
-    if (text.trim().length === 0) {
-      for (let i = endLine.lineNumber - 1; i >= range.start.line; --i) {
-        const line = lineAt(i);
-        if (!line.isEmptyOrWhitespace) {
-          return new Position(
-            i,
-            lastNonWhitespaceCharacterIndex(line.range, line.text)
-          );
-        }
-      }
-      return range.end;
-    }
-    return new Position(
-      endLine.lineNumber,
-      lastNonWhitespaceCharacterIndex(range, text)
-    );
-  })();
-
+  const { document } = editor;
+  const text = document.getText(range);
+  const startDelta = text.length - text.trimStart().length;
+  const endDelta = text.length - text.trimEnd().length;
+  const startOffset = document.offsetAt(range.start) + startDelta;
+  const endOffset = document.offsetAt(range.end) - endDelta;
+  const start = document.positionAt(startOffset);
+  const end = document.positionAt(endOffset);
   return new Range(start, end);
-}
-
-function firstNonWhitespaceCharacterIndex(range: Range, text: string) {
-  const characterDelta = text.length - text.trimStart().length;
-  return range.start.character + characterDelta;
-}
-
-function lastNonWhitespaceCharacterIndex(range: Range, text: string) {
-  const characterDelta = text.length - text.trimEnd().length;
-  return range.end.character - characterDelta;
 }
