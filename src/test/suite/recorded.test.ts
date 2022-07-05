b43367ce92e987c591fe1b8158af3c49372c551a
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-07-05 17:04:21 +0200

    Fall back to creating a new editor if it can't be reused (#832)
    * Create new editor on error
    
    * Open new editor for every test
    
    * Correctly handle eol
    
    * empty
    
    Co-authored-by: Andreas Arvidsson <andreas.arvidsson@redpill-linpro.com>

--- src/test/suite/recorded.test.ts
+++ src/test/suite/recorded.test.ts
@@ -23,7 +23,7 @@ import {
 import { Clipboard } from "../../util/Clipboard";
 import { getCursorlessApi } from "../../util/getExtensionApi";
 import sleep from "../../util/sleep";
-import { openNewEditor, reuseEditor } from "../openNewEditor";
+import { openNewEditor } from "../openNewEditor";
 import asyncSafety from "../util/asyncSafety";
 import { getRecordedTestPaths } from "../util/getFixturePaths";
 
@@ -41,8 +41,6 @@ suite("recorded test cases", async function () {
   this.timeout("100s");
   this.retries(5);
 
-  let editor: vscode.TextEditor;
-
   teardown(() => {
     sinon.restore();
   });
@@ -50,20 +48,17 @@ suite("recorded test cases", async function () {
   suiteSetup(async () => {
     // Necessary because opening a notebook opens the panel for some reason
     await vscode.commands.executeCommand("workbench.action.closePanel");
-
-    // NB: For some unfathomable reason the content string can't be empty or the first test will fail.
-    editor = await openNewEditor(" ");
   });
 
   getRecordedTestPaths().forEach((path) =>
     test(
       path.split(".")[0],
-      asyncSafety(() => runTest(editor, path))
+      asyncSafety(() => runTest(path))
     )
   );
 });
 
-async function runTest(editor: vscode.TextEditor, file: string) {
+async function runTest(file: string) {
   const buffer = await fsp.readFile(file);
   const fixture = yaml.load(buffer.toString()) as TestCaseFixture;
   const excludeFields: ExcludableSnapshotField[] = [];
@@ -76,8 +71,7 @@ async function runTest(editor: vscode.TextEditor, file: string) {
   const graph = cursorlessApi.graph!;
   graph.editStyles.testDecorations = [];
 
-  await reuseEditor(
-    editor,
+  const editor = await openNewEditor(
     fixture.initialState.documentContents,
     fixture.languageId
   );
