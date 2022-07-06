9fc6923171df759be608634414bf087417d9a52a
Andreas Arvidsson <andreas.arvidsson@redpill-linpro.com>
2022-07-06 17:37:41 +0200

    Treat leading and trailing modifier on empty content range as a token

--- src/processTargets/modifiers/LeadingTrailingStages.ts
+++ src/processTargets/modifiers/LeadingTrailingStages.ts
@@ -4,15 +4,16 @@ import {
   TrailingModifier,
 } from "../../typings/targetDescriptor.types";
 import { ProcessedTargetsContext } from "../../typings/Types";
+import getModifierStage from "../getModifierStage";
 import { ModifierStage } from "../PipelineStages.types";
 
 export class LeadingStage implements ModifierStage {
   constructor(private modifier: LeadingModifier) {}
 
   run(context: ProcessedTargetsContext, target: Target): Target[] {
-    const leading = target.getLeadingDelimiterTarget();
+    const leading = getTargetToUse(context, target).getLeadingDelimiterTarget();
     if (leading == null) {
-      throw Error("No available leading range");
+      throw Error("No available leading delimiter range");
     }
     return [leading];
   }
@@ -22,10 +23,25 @@ export class TrailingStage implements ModifierStage {
   constructor(private modifier: TrailingModifier) {}
 
   run(context: ProcessedTargetsContext, target: Target): Target[] {
-    const trailing = target.getTrailingDelimiterTarget();
+    const trailing = getTargetToUse(
+      context,
+      target
+    ).getTrailingDelimiterTarget();
     if (trailing == null) {
-      throw Error("No available trailing range");
+      throw Error("No available trailing delimiter range");
     }
     return [trailing];
   }
+}
+
+/**If the content range of the given target is empty convert it to a token target. If not just return it unmodified. */
+function getTargetToUse(context: ProcessedTargetsContext, target: Target) {
+  if (target.contentRange.isEmpty) {
+    const tokenStage = getModifierStage({
+      type: "containingScope",
+      scopeType: { type: "token" },
+    });
+    return tokenStage.run(context, target)[0];
+  }
+  return target;
 }
