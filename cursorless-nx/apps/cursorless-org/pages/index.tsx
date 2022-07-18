ba5424e846001c5e08ab99a22cddadc7dbfa82d0
Pokey Rule <755842+pokey@users.noreply.github.com>
2022-07-18 12:19:19 +0100

    Some style tweaks

--- cursorless-nx/apps/cursorless-org/pages/index.tsx
+++ cursorless-nx/apps/cursorless-org/pages/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function LandingPage() {
   return (
-    <main className="p-8 text-[#EEDFDF]">
-      <h1 className="text-2xl uppercase mb-8 place-content-center">
+    <main className="p-9 text-[#410808] font-mono">
+      <h1 className="text-2xl uppercase mb-8 place-content-center tracking-[0.6em]">
         Cursorless
       </h1>
-      <div className="text-2xl bg-black h-[720px] grid place-content-center">
+      <div className="text-2xl h-[720px] grid place-content-center tracking-wide">
         Structural voice coding at the speed of thought.
       </div>
     </main>
