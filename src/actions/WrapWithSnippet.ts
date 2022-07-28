5694551d3d441adef646ed202239e68a9e95cf2a
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-07-28 10:21:24 +0200

    Reworked weak target (#888)
    * Renamed weak target, Renamed is weak, Added explicit range attribute
    
    * Don't remove leading or trailing delimited ranges for empty selection
    
    * Added clone parameters
    
    * Updated comment
    
    * Always create continuous range
    
    * Renamed UnTypedRangeTarget to UnTypedTarget
    
    * Process targets before test recorded command hook
    
    * Added tests
    
    * Renamed to ifUntyped
    
    * Added item tests
    
    * Cleanup
    
    * Update src/typings/targetDescriptor.types.ts
    
    Co-authored-by: Pokey Rule <755842+pokey@users.noreply.github.com>
    
    * Update src/typings/targetDescriptor.types.ts
    
    Co-authored-by: Pokey Rule <755842+pokey@users.noreply.github.com>
    
    * Cleanup
    
    * Cleanup
    
    * Added tests
    
    * Cleanup
    
    * Added tests
    
    * added tests
    
    * Use undefined return value in error test cases
    
    * Docstring
    
    * Better error type for no inside / bounds
    
    * Remove code sharing from ThatStage and CursorStage
    
    * Doc strings
    
    * Switch test subset
    
    * Fix docstring link
    
    * Record alternate "leading funk" test
    
    * Record some more tests
    
    * Add a bunch of tests
    
    * Cleaned up from review
    
    * Docstring
    
    * More tests
    
    * One more test
    
    * Another test
    
    * Rollback imports to avoid circular import
    
    * Improve test
    
    * Revert "Rollback imports to avoid circular import"
    
    This reverts commit 9ad7e9fdf95a226d9235918f6a4d8c90af1b23fa.
    
    * Fix imports
    
    * Add tests; fix bug with `"every block"`
    
    * Minor tweak
    
    * Fix doc
    
    * Docstrings
    
    Co-authored-by: Pokey Rule <755842+pokey@users.noreply.github.com>

--- src/actions/WrapWithSnippet.ts
+++ src/actions/WrapWithSnippet.ts
@@ -1,6 +1,6 @@
 import { commands } from "vscode";
 import { callFunctionAndUpdateSelections } from "../core/updateSelections/updateSelections";
-import ModifyIfWeakStage from "../processTargets/modifiers/ModifyIfWeakStage";
+import ModifyIfUntypedStage from "../processTargets/modifiers/ModifyIfUntypedStage";
 import { Target } from "../typings/target.types";
 import { Graph } from "../typings/Types";
 import {
@@ -28,8 +28,8 @@ export default class WrapWithSnippet implements Action {
     }
 
     return [
-      new ModifyIfWeakStage({
-        type: "modifyIfWeak",
+      new ModifyIfUntypedStage({
+        type: "modifyIfUntyped",
         modifier: {
           type: "containingScope",
           scopeType: {
--- src/processTargets/marks/CursorStage.ts
+++ src/processTargets/marks/CursorStage.ts
@@ -3,24 +3,20 @@ import type { CursorMark } from "../../typings/targetDescriptor.types";
 import { ProcessedTargetsContext } from "../../typings/Types";
 import { isReversed } from "../../util/selectionUtils";
 import { MarkStage } from "../PipelineStages.types";
-import WeakTarget from "../targets/WeakTarget";
-import TokenTarget from "../targets/TokenTarget";
+import UntypedTarget from "../targets/UntypedTarget";
 
 export default class CursorStage implements MarkStage {
-  constructor(_modifier: CursorMark) {
-    // takes mark for consistency and does nothing
-  }
+  constructor(private modifier: CursorMark) {}
 
   run(context: ProcessedTargetsContext): Target[] {
-    return context.currentSelections.map((selection) => {
-      const parameters = {
-        editor: selection.editor,
-        isReversed: isReversed(selection.selection),
-        contentRange: selection.selection,
-      };
-      return selection.selection.isEmpty
-        ? new WeakTarget(parameters)
-        : new TokenTarget(parameters);
-    });
+    return context.currentSelections.map(
+      (selection) =>
+        new UntypedTarget({
+          editor: selection.editor,
+          isReversed: isReversed(selection.selection),
+          contentRange: selection.selection,
+          hasExplicitRange: !selection.selection.isEmpty,
+        })
+    );
   }
 }
