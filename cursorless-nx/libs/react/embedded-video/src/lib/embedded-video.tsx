b8277e417a2b7cee1a94d8b2dbc5ffb587a676d3
Pokey Rule <755842+pokey@users.noreply.github.com>
2022-08-10 16:28:03 +0100

    Switch to react-player; better error handling

--- cursorless-nx/libs/react/embedded-video/src/lib/embedded-video.tsx
+++ cursorless-nx/libs/react/embedded-video/src/lib/embedded-video.tsx
@@ -1,91 +1,35 @@
-import { useCallback, useState } from 'react';
-import { useKey } from 'react-use';
-import YouTube, { YouTubeEvent } from 'react-youtube';
-import PlayerStates from 'youtube-player/dist/constants/PlayerStates';
-import { YouTubePlayer } from 'youtube-player/dist/types';
 import styles from './embedded-video.module.css';
-import useInterval from './hooks/useInterval';
+
+import { useState } from 'react';
+import ReactPlayer from 'react-player/youtube';
 
 interface Props {
   youtubeSlug: string;
-  controller?: Controller;
 }
 
-interface Controller {
-  onReady(event: YouTubeEvent): void;
-}
+export function EmbeddedVideo({ youtubeSlug }: Props) {
+  const [isError, setIsError] = useState(false);
 
-export function EmbeddedVideo({ youtubeSlug, controller }: Props) {
   return (
-    <YouTube
-      className={styles['embedContainer']}
-      videoId={youtubeSlug}
-      onReady={controller?.onReady}
-      opts={{ playerVars: { rel: 0 } }}
-    />
+    <div className={styles['playerWrapper']}>
+      {isError ? (
+        <div
+          className={`${styles['reactPlayer']} text-red-600 text-center w-full h-full flex border border-black`}
+        >
+          <div className="m-auto">Error loading YouTube video</div>
+        </div>
+      ) : (
+        <ReactPlayer
+          className={styles['reactPlayer']}
+          url={`https://www.youtube-nocookie.com/watch?v=${youtubeSlug}`}
+          width="100%"
+          height="100%"
+          onError={(e) => {
+            console.log(`Error loading YouTube video: ${e}`);
+            setIsError(true);
+          }}
+        />
+      )}
+    </div>
   );
-}
-
-/**
- * Keys that can be used to toggle playback
- */
-const TOGGLE_KEYS = [' ', 'k'];
-
-export function useEmbeddedVideoController(playbackUpdateIntervalMs = 500) {
-  const [player, setPlayer] = useState<YouTubePlayer | null>(null);
-  const [playbackTime, setPlaybackTime] = useState<number | null>(null);
-
-  const onReady = useCallback((event: YouTubeEvent) => {
-    setPlayer(event.target);
-  }, []);
-
-  const updatePlaybackTime = useCallback(async () => {
-    if (player != null) {
-      setPlaybackTime(await player.getCurrentTime());
-    }
-  }, [player]);
-
-  const togglePlayback = async () => {
-    if (player == null) {
-      return;
-    }
-
-    if ((await player.getPlayerState()) === PlayerStates.PLAYING) {
-      player.pauseVideo();
-    } else {
-      player.playVideo();
-    }
-  };
-
-  useKey(
-    (event) => TOGGLE_KEYS.includes(event.key),
-    async (event) => {
-      await togglePlayback();
-      event.preventDefault();
-    },
-    {},
-    [player]
-  );
-
-  const seekTo = useCallback(
-    (seconds: number, allowSeekAhead: boolean) => {
-      if (player != null) {
-        player.seekTo(seconds, allowSeekAhead);
-      }
-    },
-    [player]
-  );
-
-  useInterval(
-    updatePlaybackTime,
-    player == null ? null : playbackUpdateIntervalMs
-  );
-
-  return {
-    playbackTime,
-    setPlaybackTime: seekTo,
-    controller: {
-      onReady,
-    } as Controller,
-  };
 }
