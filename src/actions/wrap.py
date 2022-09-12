f5c529bc1cec8b892fc26508685d18579d806cfc
David Vo <auscompgeek@users.noreply.github.com>
2022-09-13 02:28:05 +1000

    Use dataclass in wrap action (#923)

--- src/actions/wrap.py
+++ src/actions/wrap.py
@@ -1,8 +1,16 @@
-from typing import Any
+from dataclasses import dataclass
+from typing import Literal
 
 from talon import Module, actions
 
 from ..paired_delimiter import paired_delimiters_map
+
+
+@dataclass
+class Wrapper:
+    type: Literal["pairedDelimiter", "snippet"]
+    extra_args: list[str]
+
 
 mod = Module()
 
@@ -12,24 +20,21 @@ mod.list("cursorless_wrap_action", desc="Cursorless wrap action")
 @mod.capture(
     rule="<user.cursorless_wrapper_paired_delimiter> | {user.cursorless_wrapper_snippet}"
 )
-def cursorless_wrapper(m) -> dict[str, Any]:
+def cursorless_wrapper(m) -> Wrapper:
     try:
         paired_delimiter_info = paired_delimiters_map[
             m.cursorless_wrapper_paired_delimiter
         ]
-        return {
-            "type": "pairedDelimiter",
-            "extra_args": [paired_delimiter_info.left, paired_delimiter_info.right],
-        }
+        return Wrapper(
+            type="pairedDelimiter",
+            extra_args=[paired_delimiter_info.left, paired_delimiter_info.right],
+        )
     except AttributeError:
-        return {
-            "type": "snippet",
-            "extra_args": [m.cursorless_wrapper_snippet],
-        }
+        return Wrapper(type="snippet", extra_args=[m.cursorless_wrapper_snippet])
 
 
 # Maps from (action_type, wrapper_type) to action name
-action_map: dict[tuple[str, str], str] = {
+action_map: dict[tuple[str, Literal["pairedDelimiter", "snippet"]], str] = {
     ("wrapWithPairedDelimiter", "pairedDelimiter"): "wrapWithPairedDelimiter",
     # This is awkward because we used an action name which was to verbose previously
     ("wrapWithPairedDelimiter", "snippet"): "wrapWithSnippet",
@@ -41,13 +46,11 @@ action_map: dict[tuple[str, str], str] = {
 
 @mod.action_class
 class Actions:
-    def cursorless_wrap(
-        action_type: str, targets: dict, cursorless_wrapper: dict[str, Any]
-    ):
+    def cursorless_wrap(action_type: str, targets: dict, cursorless_wrapper: Wrapper):
         """Perform cursorless wrap action"""
-        wrapper_type = cursorless_wrapper["type"]
+        wrapper_type = cursorless_wrapper.type
         action = action_map[(action_type, wrapper_type)]
 
         actions.user.cursorless_single_target_command_with_arg_list(
-            action, targets, cursorless_wrapper["extra_args"]
+            action, targets, cursorless_wrapper.extra_args
         )
