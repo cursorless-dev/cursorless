65047ff9a1b598e205604fb640c0c9bd4c40d5bf
Pokey Rule <755842+pokey@users.noreply.github.com>
2022-09-14 15:30:18 +0100

    Revert "Hack url"
    This reverts commit 77c109d5211918a8ca9f8851232dcce9bb528d6f.

--- cursorless-nx/apps/cursorless-org/components/constants.ts
+++ cursorless-nx/apps/cursorless-org/components/constants.ts
@@ -1,6 +1,6 @@
 export const DESCRIPTION = 'Voice coding at the speed of thought';
 export const TITLE = `Cursorless: ${DESCRIPTION}`;
-export const BASE_URL = 'https://deploy-preview-857--cursorless.netlify.app/';
+export const BASE_URL = 'https://cursorless.org/';
 export const VIDEO_SHARE_THUMBNAIL_URL = `${BASE_URL}video-share-thumbnail.jpg`;
 export const VIDEO_SHARE_THUMBNAIL_WIDTH = '1280';
 export const VIDEO_SHARE_THUMBNAIL_HEIGHT = '720';
