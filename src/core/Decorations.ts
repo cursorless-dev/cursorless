88b6fff792285c6fa12aea30ae34583d492f817e
Pokey Rule <755842+pokey@users.noreply.github.com>
2022-09-27 14:42:04 +0100

    Refactor to remove `constants.ts`

--- src/core/Decorations.ts
+++ src/core/Decorations.ts
@@ -8,7 +8,7 @@ import {
   HAT_NON_DEFAULT_SHAPES,
   HatStyle,
   HatColor,
-} from "./constants";
+} from "./hatStyles";
 import { readFileSync } from "fs";
 import FontMeasurements from "./FontMeasurements";
 import { pull, sortBy, filter } from "lodash";
