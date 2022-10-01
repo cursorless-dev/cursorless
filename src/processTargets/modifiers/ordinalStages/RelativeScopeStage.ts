8c413dfd1ff92160cdd56e5360efb6794a82f896
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-10-01 10:21:25 +0200

    Added support for non containing relative scope

--- src/processTargets/modifiers/ordinalStages/RelativeScopeStage.ts
+++ src/processTargets/modifiers/ordinalStages/RelativeScopeStage.ts
@@ -1,4 +1,6 @@
+import { findLastIndex } from "lodash";
 import { Range } from "vscode";
+import { NoContainingScopeError } from "../../../errors";
 import { Target } from "../../../typings/target.types";
 import { RelativeScopeModifier } from "../../../typings/targetDescriptor.types";
 import { ProcessedTargetsContext } from "../../../typings/Types";
@@ -13,24 +15,41 @@ export class RelativeScopeStage implements ModifierStage {
   constructor(private modifier: RelativeScopeModifier) {}
 
   run(context: ProcessedTargetsContext, target: Target): Target[] {
+    const includeContaining = this.modifier.offset === 0;
+    const isForward = this.modifier.direction === "forward";
     const targets = getEveryScopeTargets(
       context,
       createTargetWithoutExplicitRange(target),
       this.modifier.scopeType
     );
-    const containingIndices = getContainingIndices(
-      target.contentRange,
-      targets
-    );
+
+    const containingIndices = (() => {
+      const containingIndices = getContainingIndices(
+        target.contentRange,
+        targets
+      );
+
+      // The content range isnt containing any scopes, but they should not be
+      // included anyhow. Find a non containing relative index.
+      if (containingIndices.length === 0 && !includeContaining) {
+        return getNonContainingIndices(target.contentRange, targets, isForward);
+      }
+
+      return containingIndices;
+    })();
+
+    if (containingIndices.length === 0) {
+      throw new NoContainingScopeError(this.modifier.scopeType.type);
+    }
+
     const containingStartIndex = containingIndices[0];
     const containingEndIndex = containingIndices.at(-1)!;
-    const isForward = this.modifier.direction === "forward";
 
     /** Reference index. This is the index closest to the target content range. */
     let refIndex: number;
 
     // Include containing scopes
-    if (this.modifier.offset === 0) {
+    if (includeContaining) {
       // Number of current containing scopes is already greater than desired length.
       if (containingIndices.length > this.modifier.length) {
         throw new Error(
@@ -79,7 +98,6 @@ function getContainingIndices(
 
   // Content range is empty. Use rightmost target and accept weak containment.
   if (inputTargetRange.isEmpty) {
-    // FIXME: Handle case where no targets intersect with the input range
     return targetsWithIntersection.slice(-1).map((t) => t.index);
   }
 
@@ -87,6 +105,27 @@ function getContainingIndices(
   return targetsWithIntersection
     .filter((t) => !t.intersection!.isEmpty)
     .map((t) => t.index);
+}
+
+/** Get index of closest target to content range in given direction */
+function getNonContainingIndices(
+  inputTargetRange: Range,
+  targets: Target[],
+  isForward: boolean
+): number[] {
+  const index = isForward
+    ? targets.findIndex((t) =>
+        t.contentRange.start.isAfter(inputTargetRange.start)
+      )
+    : findLastIndex(targets, (t) =>
+        t.contentRange.start.isBefore(inputTargetRange.start)
+      );
+
+  if (index > -1) {
+    return [isForward ? index - 1 : index + 1];
+  }
+
+  return [];
 }
 
 function createTargetWithoutExplicitRange(target: Target) {
