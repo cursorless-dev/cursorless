49697c6f9b6739b804dd6f533808a5f384b7a59d
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-10-03 12:31:41 +0200

    Updated modifier grammar

--- cursorless-talon/src/cheatsheet_html/sections/modifiers.py
+++ cursorless-talon/src/cheatsheet_html/sections/modifiers.py
@@ -4,6 +4,11 @@ MODIFIER_LIST_NAMES = [
     "simple_modifier",
     "interior_modifier",
     "head_tail_modifier",
+    "every_modifier",
+    "first_modifier",
+    "last_modifier",
+    "previous_next_modifier",
+    "backward_modifier",
 ]
 
 
@@ -15,6 +20,12 @@ def get_modifiers():
     complex_modifier_ids = [
         "extendThroughStartOf",
         "extendThroughEndOf",
+        "every",
+        "first",
+        "last",
+        "previous",
+        "next",
+        "backward",
     ]
     simple_modifiers = {
         key: value
@@ -63,6 +74,92 @@ def get_modifiers():
                 {
                     "spokenForm": f"{complex_modifiers['extendThroughEndOf']} <M>",
                     "description": "Extend through end of <M>",
+                },
+            ],
+        },
+        {
+            "id": "every",
+            "type": "modifier",
+            "variations": [
+                {
+                    "spokenForm": f"{complex_modifiers['every']} <scope>",
+                    "description": "Every instance of <scope>",
+                },
+            ],
+        },
+        {
+            "id": "previous",
+            "type": "modifier",
+            "variations": [
+                {
+                    "spokenForm": f"{complex_modifiers['previous']} <scope>",
+                    "description": "Previous instance of <scope>",
+                },
+                {
+                    "spokenForm": f"<ordinal> {complex_modifiers['previous']} <scope>",
+                    "description": "<ordinal> previous instance of <scope>",
+                },
+            ],
+        },
+        {
+            "id": "next",
+            "type": "modifier",
+            "variations": [
+                {
+                    "spokenForm": f"{complex_modifiers['next']} <scope>",
+                    "description": "Next instance of <scope>",
+                },
+                {
+                    "spokenForm": f"<ordinal> {complex_modifiers['next']} <scope>",
+                    "description": "<ordinal> next instance of <scope>",
+                },
+            ],
+        },
+        {
+            "id": "first",
+            "type": "modifier",
+            "variations": [
+                {
+                    "spokenForm": f"{complex_modifiers['first']} <number> <scope>",
+                    "description": "First <number> instances of <scope>",
+                },
+            ],
+        },
+        {
+            "id": "last",
+            "type": "modifier",
+            "variations": [
+                {
+                    "spokenForm": f"{complex_modifiers['last']} <number> <scope>",
+                    "description": "Last <number> instances of <scope>",
+                },
+            ],
+        },
+        {
+            "id": "relative",
+            "type": "modifier",
+            "variations": [
+                {
+                    "spokenForm": f"<number> <scope>",
+                    "description": "<number> instances of <scope>",
+                },
+                {
+                    "spokenForm": f"<number> <scope> {complex_modifiers['backward']}",
+                    "description": "<number> instances of <scope> backwards",
+                },
+            ],
+        },
+        {
+            "id": "ordinal",
+            "type": "modifier",
+            "variations": [
+                {
+                    "spokenForm": f"<ordinal> <scope>",
+                    "description": "<ordinal> instance of <scope> counting from front",
+                },
+                {
+                    "spokenForm": f"<ordinal> {complex_modifiers['last']} <scope>",
+                    "description": "<ordinal> instance of <scope> counting from back",
                 },
             ],
         },
