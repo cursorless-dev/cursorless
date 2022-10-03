a80f066d554af915c6f2b1d06e1289780ba9080a
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-10-03 14:09:56 +0200

    Added support for line number ranges (#998)
    * Added support for line number ranges
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Added tests
    
    * Clean up
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/marks/lines_number.py
+++ src/marks/lines_number.py
@@ -4,6 +4,8 @@ from typing import Any
 
 from talon import Context, Module
 
+from ..compound_targets import is_active_included, is_anchor_included
+
 mod = Module()
 ctx = Context()
 
@@ -34,11 +36,33 @@ directions_map = {d.cursorlessIdentifier: d for d in directions}
 DEFAULT_DIRECTIONS = {d.defaultSpokenForm: d.cursorlessIdentifier for d in directions}
 
 
-@mod.capture(rule="{user.cursorless_line_direction} <number_small>")
+@mod.capture(
+    rule="{user.cursorless_line_direction} <number_small> [{user.cursorless_range_connective} <number_small>]"
+)
 def cursorless_line_number(m) -> dict[str, Any]:
     direction = directions_map[m.cursorless_line_direction]
+    anchor = create_line_number_mark(
+        direction.type, direction.formatter(m.number_small_list[0])
+    )
+    if len(m.number_small_list) > 1:
+        active = create_line_number_mark(
+            direction.type, direction.formatter(m.number_small_list[1])
+        )
+        include_anchor = is_anchor_included(m.cursorless_range_connective)
+        include_active = is_active_included(m.cursorless_range_connective)
+        return {
+            "type": "range",
+            "anchor": anchor,
+            "active": active,
+            "excludeAnchor": not include_anchor,
+            "excludeActive": not include_active,
+        }
+    return anchor
+
+
+def create_line_number_mark(line_number_type: str, line_number: int) -> dict[str, Any]:
     return {
         "type": "lineNumber",
-        "lineNumberType": direction.type,
-        "lineNumber": direction.formatter(m.number_small),
+        "lineNumberType": line_number_type,
+        "lineNumber": line_number,
     }
