115682beaa9cdffdffb8eb5616a0f6c1db322f0d
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-10-07 20:58:00 +0200

    Support complex relative grammar (#987)
    * Added support for plural lists
    
    * Updated relative scope modifier grammar
    
    * Added ordinals to relative modifier
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Added plural list for regex scopes
    
    * Update cursorless-talon/src/modifiers/relative_scope.py
    
    Co-authored-by: Pokey Rule <755842+pokey@users.noreply.github.com>
    
    * Update cursorless-talon/src/modifiers/relative_scope.py
    
    Co-authored-by: Pokey Rule <755842+pokey@users.noreply.github.com>
    
    * Added link to inflection license
    
    * Use plural scope type in ordinal scope
    
    * Added support for <ordinal> last scope grammar
    
    * Clean up
    
    * Added words to csv
    
    * Updated modifier grammar
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Updated legend
    
    * Added docs
    
    * Update defaults.json
    
    * Start tweaking modifier cheatsheet
    
    * More cheatsheet updates
    
    * More tweaks
    
    * Cleanup cheatsheet representation
    
    * Some talon cleanup
    
    * Change formatting for cheatsheet captures
    
    * Fix captures in cheatsheet
    
    * More capture cleanup
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Revert ts config
    
    * Revert old cheatsheet changes
    
    * Fixes
    
    * Tweak docs
    
    * More doc tweaks
    
    * Add diagram
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Pokey Rule <755842+pokey@users.noreply.github.com>

--- src/modifiers/relative_scope.py
+++ src/modifiers/relative_scope.py
@@ -2,16 +2,79 @@ from typing import Any
 
 from talon import Module
 
+previous_next_modifiers = {"previous": "previous", "next": "next"}
+backward_modifiers = {"backward": "backward"}
+
 mod = Module()
 
+mod.list("cursorless_previous_next_modifier", desc="Cursorless previous/next modifiers")
+mod.list("cursorless_backward_modifier", desc="Cursorless backward modifiers")
 
-@mod.capture(rule="(previous | next) <user.cursorless_scope_type>")
+
+@mod.capture(rule="{user.cursorless_previous_next_modifier}")
+def cursorless_relative_direction(m) -> str:
+    """Previous/next"""
+    return "backward" if m[0] == "previous" else "forward"
+
+
+@mod.capture(
+    rule="[<user.ordinals_small>] <user.cursorless_relative_direction> <user.cursorless_scope_type>"
+)
+def cursorless_relative_scope_singular(m) -> dict[str, Any]:
+    """Relative previous/next singular scope, eg `"next funk"` or `"third next funk"`."""
+    return create_relative_scope_modifier(
+        m.cursorless_scope_type,
+        getattr(m, "ordinals_small", 1),
+        1,
+        m.cursorless_relative_direction,
+    )
+
+
+@mod.capture(
+    rule="<user.cursorless_relative_direction> <number_small> <user.cursorless_scope_type_plural>"
+)
+def cursorless_relative_scope_plural(m) -> dict[str, Any]:
+    """Relative previous/next plural scope. `next three funks`"""
+    return create_relative_scope_modifier(
+        m.cursorless_scope_type_plural,
+        1,
+        m.number_small,
+        m.cursorless_relative_direction,
+    )
+
+
+@mod.capture(
+    rule="<number_small> <user.cursorless_scope_type_plural> [{user.cursorless_backward_modifier}]"
+)
+def cursorless_relative_scope_count(m) -> dict[str, Any]:
+    """Relative count scope. `three funks`"""
+    return create_relative_scope_modifier(
+        m.cursorless_scope_type_plural,
+        0,
+        m.number_small,
+        getattr(m, "cursorless_backward_modifier", "forward"),
+    )
+
+
+@mod.capture(
+    rule=(
+        "<user.cursorless_relative_scope_singular> | "
+        "<user.cursorless_relative_scope_plural> | "
+        "<user.cursorless_relative_scope_count>"
+    )
+)
 def cursorless_relative_scope(m) -> dict[str, Any]:
     """Previous/next scope"""
+    return m[0]
+
+
+def create_relative_scope_modifier(
+    scope_type: dict, offset: int, length: int, direction: str
+) -> dict[str, Any]:
     return {
         "type": "relativeScope",
-        "scopeType": m.cursorless_scope_type,
-        "offset": 1,
-        "length": 1,
-        "direction": "backward" if m[0] == "previous" else "forward",
+        "scopeType": scope_type,
+        "offset": offset,
+        "length": length,
+        "direction": direction,
     }
