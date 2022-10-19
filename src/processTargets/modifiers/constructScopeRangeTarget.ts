616e8282412d7bc6b10815e79a455f7b2008dfb4
Pokey Rule <755842+pokey@users.noreply.github.com>
2022-10-19 19:24:53 +0100

    docstrings

--- src/processTargets/modifiers/constructScopeRangeTarget.ts
+++ src/processTargets/modifiers/constructScopeRangeTarget.ts
@@ -1,6 +1,18 @@
 import { Target } from "../../typings/target.types";
 import { TargetScope } from "./scopeHandlers/scope.types";
 
+/**
+ * Constructs a target consisting of a range between {@link scope1} and
+ * {@link scope2}. The order of {@link scope1} and {@link scope2} doesn't matter;
+ * this function will automatically figure out which one should be the start of
+ * the range, and which should be the end.
+ * @param isReversed Whether the returned target should have active before
+ * anchor
+ * @param scope1 The scope forming one end of the range
+ * @param scope2 A scope forming another end of the range
+ * @returns A target consisting of a range between {@link scope1} and
+ * {@link scope2}
+ */
 export function constructScopeRangeTarget(
   isReversed: boolean,
   scope1: TargetScope,
--- src/processTargets/modifiers/getLegacyScopeStage.ts
+++ src/processTargets/modifiers/getLegacyScopeStage.ts
@@ -22,6 +22,19 @@ import {
 import { CharacterStage, WordStage } from "./scopeTypeStages/SubTokenStages";
 import SurroundingPairStage from "./SurroundingPairStage";
 
+/**
+ * Any scope type that has not been fully migrated to the new
+ * {@link ScopeHandler} setup should have a branch in this `switch` statement.
+ * Once the scope type is fully migrated, remove the branch and the legacy
+ * modifier stage.
+ *
+ * Note that it is possible for a scope type to be partially migrated.  For
+ * example, we could support modern scope handlers for a certain scope type in
+ * Ruby, but not yet in Python.
+ *
+ * @param modifier The modifier for which to get the modifier stage
+ * @returns A scope stage implementing the modifier for the given scope type
+ */
 export default function getLegacyScopeStage(
   modifier: ContainingScopeModifier | EveryScopeModifier
 ): ModifierStage {
