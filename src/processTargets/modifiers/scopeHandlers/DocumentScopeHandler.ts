9aa68a02b5a5100f69d341702814d8089261407f
Pokey Rule <755842+pokey@users.noreply.github.com>
2022-10-23 14:54:14 +0100

    Tweaks

--- src/processTargets/modifiers/scopeHandlers/DocumentScopeHandler.ts
+++ src/processTargets/modifiers/scopeHandlers/DocumentScopeHandler.ts
@@ -4,68 +4,55 @@ import { getDocumentRange } from "../../../util/rangeUtils";
 import { DocumentTarget } from "../../targets";
 import { OutOfRangeError } from "../targetSequenceUtils";
 import NotHierarchicalScopeError from "./NotHierarchicalScopeError";
-import { IterationScope, TargetScope } from "./scope.types";
+import { TargetScope } from "./scope.types";
 import { ScopeHandler } from "./scopeHandler.types";
 
 export default class DocumentScopeHandler implements ScopeHandler {
   iterationScopeType = undefined;
 
-  constructor(public scopeType: ScopeType, private languageId: string) {}
+  constructor(public scopeType: ScopeType, private _languageId: string) {}
 
   getScopesTouchingPosition(
     editor: TextEditor,
-    position: Position,
+    _position: Position,
     ancestorIndex: number = 0
   ): TargetScope[] {
     if (ancestorIndex !== 0) {
       throw new NotHierarchicalScopeError(this.scopeType);
     }
 
-    return this.getIterationScope(editor).getScopes();
+    return [getDocumentScope(editor)];
   }
 
   getScopesOverlappingRange(editor: TextEditor, _range: Range): TargetScope[] {
-    return this.getIterationScope(editor).getScopes();
-  }
-
-  getIterationScopesTouchingPosition(
-    editor: TextEditor,
-    _position: Position
-  ): IterationScope[] {
-    return [this.getIterationScope(editor)];
+    return [getDocumentScope(editor)];
   }
 
   getScopeRelativeToPosition(
     editor: TextEditor,
-    position: Position,
+    _position: Position,
     offset: number,
     _direction: Direction
   ): TargetScope {
     if (offset === 0) {
-      return this.getIterationScope(editor).getScopes()[0];
+      return getDocumentScope(editor);
     }
 
     throw new OutOfRangeError();
   }
+}
 
-  private getIterationScope(editor: TextEditor): IterationScope {
-    const contentRange = getDocumentRange(editor.document);
+function getDocumentScope(editor: TextEditor): TargetScope {
+  const contentRange = getDocumentRange(editor.document);
 
-    return {
-      editor,
-      domain: contentRange,
-      getScopes: () => [
-        {
-          editor,
-          domain: contentRange,
-          getTarget: (isReversed) =>
-            new DocumentTarget({
-              editor,
-              isReversed,
-              contentRange,
-            }),
-        },
-      ],
-    };
-  }
+  return {
+    editor,
+    domain: contentRange,
+    getTarget: (isReversed) =>
+      new DocumentTarget({
+        editor,
+        isReversed,
+        contentRange,
+      }),
+  };
 }
