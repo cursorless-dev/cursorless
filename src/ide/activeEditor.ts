c86f93441056df914510c6723610214dbef62c53
Phil Cohen <phillip@phillip.io>
2022-10-31 15:14:31 -0700

    activeEditor: improve comment

--- src/ide/activeEditor.ts
+++ src/ide/activeEditor.ts
@@ -1,9 +1,12 @@
 import { TextEditor, window } from "vscode";
 
 /**
+ * Returns the active text editor; should be used rather than reading
+ * `vscode.window.activeTextEditor` directly.
+ *
  * This exists for ease of overriding in Cursorless Everywhere.
  *
- * It will eventually be replaced by the `ide` abstraction.
+ * It will eventually be replaced by `ide`'s `Editor` abstraction.
  */
 export function getActiveEditor(): TextEditor | undefined {
   return window.activeTextEditor;
