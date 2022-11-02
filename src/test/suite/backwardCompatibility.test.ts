a352e2e3ed47c6ef3d09ab879fe5e9abb8c9764f
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-11-02 14:18:21 +0100

    Restore backwards compatibility tests

--- src/test/suite/backwardCompatibility.test.ts
+++ src/test/suite/backwardCompatibility.test.ts
@@ -1,6 +1,6 @@
 import * as assert from "assert";
 import * as vscode from "vscode";
-import { runCursorlessCommand } from "../../client-e2e-test/runCommand";
+import { cursorlessCommandId } from "../../common/commandIds";
 import { getCursorlessApi } from "../../util/getExtensionApi";
 import { openNewEditor } from "../openNewEditor";
 import { standardSuiteSetup } from "./standardSuiteSetup";
@@ -20,18 +20,14 @@ async function runTest() {
 
   await graph.hatTokenMap.addDecorations();
 
-  await runCursorlessCommand({
-    version: 1,
-    action: "wrapWithPairedDelimiter",
-    extraArgs: ["(", ")"],
-    targets: [
-      {
-        type: "primitive",
-        selectionType: "line",
-        mark: { type: "cursor" },
-      },
-    ],
-  });
+  await vscode.commands.executeCommand(
+    cursorlessCommandId,
+    "whatever",
+    "wrapWithPairedDelimiter",
+    [{ type: "primitive", selectionType: "line", mark: { type: "cursor" } }],
+    "(",
+    ")",
+  );
 
   assert.deepStrictEqual(editor.document.getText(), "()");
 }
