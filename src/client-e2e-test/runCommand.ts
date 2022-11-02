0ff0ab697b68234eaeb0a85748805604f61c41e6
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-11-02 14:16:41 +0100

    Use import type

--- src/client-e2e-test/runCommand.ts
+++ src/client-e2e-test/runCommand.ts
@@ -1,6 +1,6 @@
 import * as vscode from "vscode";
 import { cursorlessCommandId } from "../common/commandIds";
-import { Command } from "../core/commandRunner/command.types";
+import type { Command } from "../core/commandRunner/command.types";
 
 export function runCursorlessCommand(command: Command) {
   return vscode.commands.executeCommand(cursorlessCommandId, command);
