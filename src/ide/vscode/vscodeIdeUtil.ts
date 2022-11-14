f9f5540051ae016b1db89f331c42c3020dab64f8
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-11-14 15:33:35 +0100

    update

--- src/ide/vscode/vscodeIdeUtil.ts
+++ src/ide/vscode/vscodeIdeUtil.ts
@@ -1,7 +1,17 @@
-import { TextDocument, TextLine } from "@cursorless/common";
+import { TextDocument, TextEditor, TextLine } from "@cursorless/common";
 import * as vscode from "vscode";
 import { VscodeTextDocumentImpl } from "./VscodeTextDocumentImpl";
+import { VscodeTextEditorImpl } from "./VscodeTextEditorImpl";
 import VscodeTextLineImpl from "./VscodeTextLineImpl";
+
+const editorMap = new WeakMap<vscode.TextEditor, TextEditor>();
+
+export function fromVscodeEditor(editor: vscode.TextEditor): TextEditor {
+  if (!editorMap.has(editor)) {
+    editorMap.set(editor, new VscodeTextEditorImpl(editor));
+  }
+  return editorMap.get(editor)!;
+}
 
 export function fromVscodeDocument(
   document: vscode.TextDocument,
