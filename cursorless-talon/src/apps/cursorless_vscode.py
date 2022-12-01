17cb13a6c243d0ab6fa2ae0f39b227e958adb1f4
Pokey Rule <755842+pokey@users.noreply.github.com>
2022-12-01 11:45:47 -0800

    Limit the amount of text used in `scout` and `scout all`

--- cursorless-talon/src/apps/cursorless_vscode.py
+++ cursorless-talon/src/apps/cursorless_vscode.py
@@ -1,4 +1,4 @@
-from talon import Context, actions
+from talon import Context, actions, app
 
 from ..actions.get_text import get_text
 from ..cursorless_command_server import run_rpc_command_no_wait
@@ -17,9 +17,13 @@ class Actions:
     def cursorless_private_run_find_action(targets: dict):
         """Find text of targets in editor"""
         texts = get_text(targets, ensure_single_target=True)
+        search_text = texts[0]
+        if len(search_text) > 200:
+            search_text = search_text[:200]
+            app.notify("Search text is longer than 200 characters; truncating")
         run_rpc_command_no_wait("actions.find")
         actions.sleep("50ms")
-        actions.insert(texts[0])
+        actions.insert(search_text)
 
     def cursorless_show_settings_in_ide():
         """Show Cursorless-specific settings in ide"""
