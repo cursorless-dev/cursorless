1cddc548027ec26b6b43c0194515799d00e78ba4
Pokey Rule <755842+pokey@users.noreply.github.com>
2022-12-14 17:14:09 +0000

    Fix types

--- src/core/commandRunner/typings/command.types.ts
+++ src/core/commandRunner/typings/command.types.ts
@@ -1,7 +1,7 @@
 import type { ActionCommand } from "./ActionCommand";
 import type { CommandV0, CommandV1 } from "./legacy/CommandV0V1.types";
 import type { CommandV2 } from "./legacy/CommandV2.types";
-import type { CommandV3 } from "./legacy/CommandV3.types";
+import type { CommandV3 } from "./CommandV3.types";
 
 export type CommandComplete = Required<Omit<CommandLatest, "spokenForm">> &
   Pick<CommandLatest, "spokenForm"> & { action: Required<ActionCommand> };
