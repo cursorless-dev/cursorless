8e585694a31e57116e6b98d63cc434a236deff6b
Pokey Rule <755842+pokey@users.noreply.github.com>
2022-12-21 18:52:30 +0000

    Improve condition support

--- src/languages/rust.ts
+++ src/languages/rust.ts
@@ -146,7 +146,10 @@ const nodeMatchers: Partial<
   ),
   string: ["raw_string_literal", "string_literal"],
   ifStatement: ["if_expression", "if_let_expression"],
-  condition: leadingMatcher(["*.match_pattern![condition]"], ["if"]),
+  condition: cascadingMatcher(
+    patternMatcher("while_expression[condition]", "if_expression[condition]"),
+    leadingMatcher(["*.match_pattern![condition]"], ["if"]),
+  ),
   functionCall: ["call_expression", "macro_invocation", "struct_expression"],
   functionCallee: "call_expression[function]",
   comment: ["line_comment", "block_comment"],
