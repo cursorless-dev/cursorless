cursorless-dev/cursorless

commit 1a0616521bbc6d264f22fdad41e9a7773f9b0bee

Allow using "this" to refer to current token with empty selection (#1094)
- Fixes #1043
- Fixes #1141 
- Depends on https://github.com/cursorless-dev/cursorless/pull/1187

## Checklist

- [x] I have added
[tests](https://www.cursorless.org/docs/contributing/test-case-recorder/)
- [x] I have updated the
[docs](https://github.com/cursorless-dev/cursorless/tree/main/docs) and
[cheatsheet](https://github.com/cursorless-dev/cursorless/tree/main/cursorless-talon/src/cheatsheet)

Co-authored-by: Pokey Rule <755842+pokey@users.noreply.github.com>
Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

src/keyboard/KeyboardCommandsTargeted.ts
@@ -12,6 +12,7 @@ import {
 import { getStyleName } from "../core/getStyleName";
 import ide from "../libs/cursorless-engine/singletons/ide.singleton";
 import {
+  ImplicitTargetDescriptor,
   PartialPrimitiveTargetDescriptor,
   PartialTargetDescriptor,
   SimpleScopeTypeType,
@@ -202,7 +203,10 @@ export default class KeyboardCommandsTargeted {
    * @returns A promise that resolves to the result of the cursorless command
    */
   performActionOnTarget = async (action: ActionType) => {
-    const targets: PartialPrimitiveTargetDescriptor[] = [
+    const targets: (
+      | PartialPrimitiveTargetDescriptor
+      | ImplicitTargetDescriptor
+    )[] = [
       {
         type: "primitive",
         mark: {
@@ -214,8 +218,7 @@ export default class KeyboardCommandsTargeted {
     if (MULTIPLE_TARGET_ACTIONS.includes(action)) {
       // For multi-target actiosn (eg "bring"), we just use implicit destination
       targets.push({
-        type: "primitive",
-        isImplicit: true,
+        type: "implicit",
       });
     }
 

cursorless-talon/src/command.py
@@ -12,6 +12,7 @@ from .primitive_target import IMPLICIT_TARGET
 mod = Module()
 
 CURSORLESS_COMMAND_ID = "cursorless.command"
+CURSORLESS_COMMAND_VERSION = 4
 last_phrase = None
 
 
@@ -136,7 +137,7 @@ def construct_cursorless_command_argument(
         use_pre_phrase_snapshot = False
 
     return {
-        "version": 3,
+        "version": CURSORLESS_COMMAND_VERSION,
         "spokenForm": get_spoken_form(),
         "action": {
             "name": action,

cursorless-talon/src/compound_targets.py
@@ -3,7 +3,7 @@ from typing import Any
 from talon import Module
 
 from .connective import default_range_connective
-from .primitive_target import BASE_TARGET
+from .primitive_target import IMPLICIT_TARGET
 
 mod = Module()
 
@@ -46,7 +46,7 @@ def cursorless_range(m) -> dict[str, Any]:
         return primitive_targets[0]
 
     if len(primitive_targets) == 1:
-        anchor = BASE_TARGET.copy()
+        anchor = IMPLICIT_TARGET.copy()
     else:
         anchor = primitive_targets[0]
 

cursorless-talon/src/primitive_target.py
@@ -6,7 +6,7 @@ from talon import Module
 mod = Module()
 
 BASE_TARGET: dict[str, Any] = {"type": "primitive"}
-IMPLICIT_TARGET = {"type": "primitive", "isImplicit": True}
+IMPLICIT_TARGET = {"type": "implicit"}
 
 
 @mod.capture(

src/actions/InsertSnippet.ts
@@ -5,7 +5,7 @@ import {
   getSelectionInfo,
 } from "../core/updateSelections/updateSelections";
 import ide from "../libs/cursorless-engine/singletons/ide.singleton";
-import ModifyIfUntypedStage from "../processTargets/modifiers/ModifyIfUntypedStage";
+import { ModifyIfUntypedExplicitStage } from "../processTargets/modifiers/ConditionalModifierStages";
 import { Snippet, SnippetDefinition } from "../typings/snippet";
 import { Target } from "../typings/target.types";
 import { Graph } from "../typings/Types";
@@ -30,17 +30,14 @@ export default class InsertSnippet implements Action {
     }
 
     return [
-      new ModifyIfUntypedStage({
-        type: "modifyIfUntyped",
-        modifier: {
-          type: "cascading",
-          modifiers: defaultScopeTypes.map((scopeType) => ({
-            type: "containingScope",
-            scopeType: {
-              type: scopeType,
-            },
-          })),
-        },
+      new ModifyIfUntypedExplicitStage({
+        type: "cascading",
+        modifiers: defaultScopeTypes.map((scopeType) => ({
+          type: "containingScope",
+          scopeType: {
+            type: scopeType,
+          },
+        })),
       }),
     ];
   }

src/actions/WrapWithSnippet.ts
@@ -1,6 +1,6 @@
 import { callFunctionAndUpdateSelections } from "../core/updateSelections/updateSelections";
 import ide from "../libs/cursorless-engine/singletons/ide.singleton";
-import ModifyIfUntypedStage from "../processTargets/modifiers/ModifyIfUntypedStage";
+import { ModifyIfUntypedStage } from "../processTargets/modifiers/ConditionalModifierStages";
 import { Target } from "../typings/target.types";
 import { Graph } from "../typings/Types";
 import {

src/core/commandRunner/typings/CommandV4.types.ts
@@ -1,11 +1,11 @@
 import type { PartialTargetDescriptor } from "./PartialTargetDescriptor.types";
 import type { ActionCommand } from "./ActionCommand";
 
-export interface CommandV3 {
+export interface CommandV4 {
   /**
    * The version number of the command API
    */
-  version: 3;
+  version: 4;
 
   /**
    * The spoken form of the command if issued from a voice command system

src/core/commandRunner/typings/command.types.ts
@@ -1,15 +1,16 @@
 import type { ActionCommand } from "./ActionCommand";
 import type { CommandV0, CommandV1 } from "./legacy/CommandV0V1.types";
 import type { CommandV2 } from "./legacy/CommandV2.types";
-import type { CommandV3 } from "./CommandV3.types";
+import type { CommandV3 } from "./legacy/CommandV3.types";
+import type { CommandV4 } from "./CommandV4.types";
 
 export type CommandComplete = Required<Omit<CommandLatest, "spokenForm">> &
   Pick<CommandLatest, "spokenForm"> & { action: Required<ActionCommand> };
 
-export const LATEST_VERSION = 3 as const;
+export const LATEST_VERSION = 4 as const;
 
 export type CommandLatest = Command & {
   version: typeof LATEST_VERSION;
 };
 
-export type Command = CommandV0 | CommandV1 | CommandV2 | CommandV3;
+export type Command = CommandV0 | CommandV1 | CommandV2 | CommandV3 | CommandV4;

commit e984d36626a2950cd34f0f64f88e3da900c2770c

Fix modal keyboard interface broken by #1094

src/keyboard/KeyboardCommandsTargeted.ts
@@ -247,7 +247,7 @@ export default class KeyboardCommandsTargeted {
   targetSelection = () =>
     executeCursorlessCommand({
       action: {
-        name: "setSelection",
+        name: "highlight",
       },
       targets: [
         {
@@ -255,6 +255,7 @@ export default class KeyboardCommandsTargeted {
           mark: {
             type: "cursor",
           },
+          modifiers: [{ type: "toRawSelection" }],
         },
       ],
     });

src/apps/cursorless-vscode-e2e/suite/keyboard/basic.test.ts
@@ -6,8 +6,14 @@ import { endToEndTestSetup, sleepWithBackoff } from "../../endToEndTestSetup";
 suite("Basic keyboard test", async function () {
   endToEndTestSetup(this);
 
+  this.afterEach(async () => {
+    await vscode.commands.executeCommand("cursorless.keyboard.modal.modeOff");
+  });
+
   test("Don't take keyboard control on startup", () => checkKeyboardStartup());
   test("Basic keyboard test", () => basic());
+  test("Check that entering and leaving mode is no-op", () =>
+    enterAndLeaveIsNoOp());
 });
 
 async function checkKeyboardStartup() {
@@ -46,6 +52,21 @@ async function basic() {
   await typeText("a");
 
   assert.equal(editor.document.getText().trim(), "a");
+}
+
+async function enterAndLeaveIsNoOp() {
+  const editor = await openNewEditor("hello");
+
+  const originalSelection = new vscode.Selection(0, 0, 0, 0);
+  editor.selection = originalSelection;
+
+  await vscode.commands.executeCommand("cursorless.keyboard.modal.modeOn");
+
+  assert.isTrue(editor.selection.isEqual(originalSelection));
+
+  await vscode.commands.executeCommand("cursorless.keyboard.modal.modeOff");
+
+  assert.isTrue(editor.selection.isEqual(originalSelection));
 }
 
 async function typeText(text: string) {