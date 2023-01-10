cursorless-dev/cursorless

commit f041217f11d690455a282f9fec850b1ae1cba937

Free actions from vscode dependencies (#1143)
## Checklist

- [ ] I have added
[tests](https://www.cursorless.org/docs/contributing/test-case-recorder/)
- [ ] I have updated the
[docs](https://github.com/cursorless-dev/cursorless/tree/main/docs) and
[cheatsheet](https://github.com/cursorless-dev/cursorless/tree/main/cursorless-talon/src/cheatsheet)
- [x] I have not broken the cheatsheet

Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
Co-authored-by: Pokey Rule <755842+pokey@users.noreply.github.com>

src/ide/vscode/VscodeTextEditorImpl.ts
@@ -1,7 +1,11 @@
-import type {
+import {
+  BreakpointDescriptor,
+  EditableTextEditor,
   Position,
   Range,
+  RevealLineAt,
   Selection,
+  sleep,
   TextDocument,
   TextEditor,
   TextEditorDecorationType,
@@ -18,11 +22,19 @@ import {
 import * as vscode from "vscode";
 import vscodeEdit from "./VscodeEdit";
 import vscodeFocusEditor from "./VscodeFocusEditor";
+import { vscodeFold, vscodeUnfold } from "./VscodeFold";
 import VscodeIDE from "./VscodeIDE";
+import { vscodeInsertSnippet } from "./VscodeInsertSnippets";
+import {
+  vscodeEditNewNotebookCellAbove,
+  vscodeEditNewNotebookCellBelow,
+} from "./VscodeNotebooks";
 import vscodeOpenLink from "./VscodeOpenLink";
+import { vscodeRevealLine } from "./VscodeRevealLine";
 import { VscodeTextDocumentImpl } from "./VscodeTextDocumentImpl";
+import { vscodeToggleBreakpoint } from "./VscodeToggleBreakpoint";
 
-export class VscodeTextEditorImpl implements TextEditor {
+export class VscodeTextEditorImpl implements EditableTextEditor {
   readonly document: TextDocument;
 
   constructor(
@@ -65,21 +77,25 @@ export class VscodeTextEditorImpl implements TextEditor {
     return this.id === other.id;
   }
 
-  public revealRange(range: Range): void {
+  public async revealRange(range: Range): Promise<void> {
     this.editor.revealRange(toVscodeRange(range));
   }
 
-  public setDecorations(
+  public revealLine(lineNumber: number, at: RevealLineAt): Promise<void> {
+    return vscodeRevealLine(this, lineNumber, at);
+  }
+
+  public async setDecorations(
     decorationType: TextEditorDecorationType,
     ranges: readonly Range[],
-  ): void {
+  ): Promise<void> {
     this.editor.setDecorations(decorationType, ranges.map(toVscodeRange));
   }
 
   public edit(
     callback: (editBuilder: TextEditorEdit) => void,
     options?: { undoStopBefore: boolean; undoStopAfter: boolean },
-  ): Thenable<boolean> {
+  ): Promise<boolean> {
     return vscodeEdit(this.editor, callback, options);
   }
 
@@ -87,7 +103,99 @@ export class VscodeTextEditorImpl implements TextEditor {
     return vscodeFocusEditor(this.ide, this);
   }
 
-  public openLink(location: Position | Range): Promise<boolean> {
-    return vscodeOpenLink(this.editor, toVscodePositionOrRange(location));
+  public editNewNotebookCellAbove(): Promise<
+    (selection: Selection) => Selection
+  > {
+    return vscodeEditNewNotebookCellAbove(this);
+  }
+
+  public editNewNotebookCellBelow(): Promise<void> {
+    return vscodeEditNewNotebookCellBelow(this);
+  }
+
+  public openLink(location?: Position | Range): Promise<boolean> {
+    return vscodeOpenLink(
+      this.editor,
+      location != null ? toVscodePositionOrRange(location) : undefined,
+    );
+  }
+
+  public fold(ranges?: Range[]): Promise<void> {
+    return vscodeFold(this.ide, this, ranges);
+  }
+
+  public unfold(ranges?: Range[]): Promise<void> {
+    return vscodeUnfold(this.ide, this, ranges);
+  }
+
+  public toggleBreakpoint(descriptors?: BreakpointDescriptor[]): Promise<void> {
+    return vscodeToggleBreakpoint(this, descriptors);
+  }
+
+  public async toggleLineComment(_ranges?: Range[]): Promise<void> {
+    await vscode.commands.executeCommand("editor.action.commentLine");
+  }
+
+  public async clipboardCopy(_ranges?: Range[]): Promise<void> {
+    await vscode.commands.executeCommand("editor.action.clipboardCopyAction");
+  }
+
+  public async clipboardPaste(_ranges?: Range[]): Promise<void> {
+    await vscode.commands.executeCommand("editor.action.clipboardPasteAction");
+  }
+
+  public async indentLine(_ranges?: Range[]): Promise<void> {
+    await vscode.commands.executeCommand("editor.action.indentLines");
+  }
+
+  public async outdentLine(_ranges?: Range[]): Promise<void> {
+    await vscode.commands.executeCommand("editor.action.outdentLines");
+  }
+
+  public async insertLineAfter(ranges?: Range[]): Promise<void> {
+    if (ranges != null) {
+      this.selections = ranges.map((range) => range.toSelection(false));
+    }
+    await vscode.commands.executeCommand("editor.action.insertLineAfter");
+  }
+
+  public insertSnippet(snippet: string, ranges?: Range[]): Promise<void> {
+    return vscodeInsertSnippet(this, snippet, ranges);
+  }
+
+  public async rename(_range?: Range): Promise<void> {
+    await vscode.commands.executeCommand("editor.action.rename");
+  }
+
+  public async showReferences(_range?: Range): Promise<void> {
+    await vscode.commands.executeCommand("references-view.find");
+  }
+
+  public async quickFix(_range?: Range): Promise<void> {
+    await vscode.commands.executeCommand("editor.action.quickFix");
+    await sleep(100);
+  }
+
+  public async revealDefinition(_range?: Range): Promise<void> {
+    await vscode.commands.executeCommand("editor.action.revealDefinition");
+  }
+
+  public async revealTypeDefinition(_range?: Range): Promise<void> {
+    await vscode.commands.executeCommand("editor.action.goToTypeDefinition");
+  }
+
+  public async showHover(_range?: Range): Promise<void> {
+    await vscode.commands.executeCommand("editor.action.showHover");
+  }
+
+  public async showDebugHover(_range?: Range): Promise<void> {
+    await vscode.commands.executeCommand("editor.debug.action.showDebugHover");
+  }
+
+  public async extractVariable(_range?: Range): Promise<void> {
+    await vscode.commands.executeCommand("editor.action.codeAction", {
+      kind: "refactor.extract.constant",
+      preferred: true,
+    });
   }
 }

src/actions/Actions.ts
@@ -1,10 +1,10 @@
 import { Graph } from "../typings/Types";
 import { ActionRecord } from "./actions.types";
 import {
-  CommentLines,
+  ToggleLineComment,
   ExtractVariable,
-  IndentLines,
-  OutdentLines,
+  IndentLine,
+  OutdentLine,
   Rename,
   RevealDefinition,
   RevealTypeDefinition,
@@ -12,16 +12,18 @@ import {
   ShowHover,
   ShowQuickFix,
   ShowReferences,
-} from "./MakeshiftActions";
+  CopyToClipboard,
+  Fold,
+  Unfold,
+} from "./SimpleIdeCommandActions";
 import { Bring, Move, Swap } from "./BringMoveSwap";
 import Call from "./Call";
 import Clear from "./Clear";
-import { Copy, Cut } from "./CutCopy";
+import { CutToClipboard } from "./CutToClipboard";
 import Deselect from "./Deselect";
 import { EditNew, EditNewAfter, EditNewBefore } from "./EditNew";
 import ExecuteCommand from "./ExecuteCommand";
-import { FindInFiles } from "./Find";
-import { Fold, Unfold } from "./Fold";
+import { FindInWorkspace } from "./Find";
 import FollowLink from "./FollowLink";
 import GenerateSnippet from "./GenerateSnippet";
 import GetText from "./GetText";
@@ -36,7 +38,7 @@ import {
   InsertEmptyLinesAround,
 } from "./InsertEmptyLines";
 import InsertSnippet from "./InsertSnippet";
-import { Paste } from "./Paste";
+import { PasteFromClipboard } from "./PasteFromClipboard";
 import Remove from "./Remove";
 import Replace from "./Replace";
 import Rewrap from "./Rewrap";
@@ -56,21 +58,21 @@ class Actions implements ActionRecord {
 
   callAsFunction = new Call(this.graph);
   clearAndSetSelection = new Clear(this.graph);
-  copyToClipboard = new Copy(this.graph);
-  cutToClipboard = new Cut(this.graph);
+  copyToClipboard = new CopyToClipboard(this.graph);
+  cutToClipboard = new CutToClipboard(this.graph);
   deselect = new Deselect(this.graph);
   editNew = new EditNew(this.graph);
   editNewLineAfter = new EditNewAfter(this.graph);
   editNewLineBefore = new EditNewBefore(this.graph);
   executeCommand = new ExecuteCommand(this.graph);
   extractVariable = new ExtractVariable(this.graph);
-  findInWorkspace = new FindInFiles(this.graph);
+  findInWorkspace = new FindInWorkspace(this.graph);
   foldRegion = new Fold(this.graph);
   followLink = new FollowLink(this.graph);
   generateSnippet = new GenerateSnippet(this.graph);
   getText = new GetText(this.graph);
   highlight = new Highlight(this.graph);
-  indentLine = new IndentLines(this.graph);
+  indentLine = new IndentLine(this.graph);
   insertCopyAfter = new InsertCopyAfter(this.graph);
   insertCopyBefore = new InsertCopyBefore(this.graph);
   insertEmptyLineAfter = new InsertEmptyLineAfter(this.graph);
@@ -78,8 +80,8 @@ class Actions implements ActionRecord {
   insertEmptyLinesAround = new InsertEmptyLinesAround(this.graph);
   insertSnippet = new InsertSnippet(this.graph);
   moveToTarget = new Move(this.graph);
-  outdentLine = new OutdentLines(this.graph);
-  pasteFromClipboard = new Paste(this.graph);
+  outdentLine = new OutdentLine(this.graph);
+  pasteFromClipboard = new PasteFromClipboard(this.graph);
   randomizeTargets = new Random(this.graph);
   remove = new Remove(this.graph);
   rename = new Rename(this.graph);
@@ -102,7 +104,7 @@ class Actions implements ActionRecord {
   sortTargets = new Sort(this.graph);
   swapTargets = new Swap(this.graph);
   toggleLineBreakpoint = new ToggleBreakpoint(this.graph);
-  toggleLineComment = new CommentLines(this.graph);
+  toggleLineComment = new ToggleLineComment(this.graph);
   unfoldRegion = new Unfold(this.graph);
   wrapWithPairedDelimiter = new Wrap(this.graph);
   wrapWithSnippet = new WrapWithSnippet(this.graph);

src/actions/BringMoveSwap.ts
@@ -1,6 +1,9 @@
-import { Selection, TextEditor } from "@cursorless/common";
+import {
+  RangeExpansionBehavior,
+  Selection,
+  TextEditor,
+} from "@cursorless/common";
 import { flatten } from "lodash";
-import { DecorationRangeBehavior } from "vscode";
 import {
   getSelectionInfo,
   performEditsAndUpdateFullSelectionInfos,
@@ -169,7 +172,7 @@ class BringMoveSwap implements Action {
               getSelectionInfo(
                 editor.document,
                 range.toSelection(originalTarget.isReversed),
-                DecorationRangeBehavior.OpenOpen,
+                RangeExpansionBehavior.openOpen,
               ),
           );
 
@@ -177,7 +180,7 @@ class BringMoveSwap implements Action {
             getSelectionInfo(
               editor.document,
               selection,
-              DecorationRangeBehavior.ClosedClosed,
+              RangeExpansionBehavior.closedClosed,
             ),
           );
 

src/actions/CallbackAction.ts
@@ -1,0 +1,139 @@
+import { EditableTextEditor, TextEditor } from "@cursorless/common";
+import { flatten } from "lodash";
+import { selectionToThatTarget } from "../core/commandRunner/selectionToThatTarget";
+import { callFunctionAndUpdateSelections } from "../core/updateSelections/updateSelections";
+import ide from "../libs/cursorless-engine/singletons/ide.singleton";
+import { Target } from "../typings/target.types";
+import { Graph } from "../typings/Types";
+import {
+  setSelectionsAndFocusEditor,
+  setSelectionsWithoutFocusingEditor,
+} from "../util/setSelectionsAndFocusEditor";
+import {
+  ensureSingleEditor,
+  ensureSingleTarget,
+  runOnTargetsForEachEditor,
+  runOnTargetsForEachEditorSequentially,
+} from "../util/targetUtils";
+import { Action, ActionReturnValue } from "./actions.types";
+
+interface CallbackOptions {
+  callback: (editor: EditableTextEditor, targets: Target[]) => Promise<void>;
+  ensureSingleEditor: boolean;
+  ensureSingleTarget: boolean;
+  setSelection: boolean;
+  restoreSelection: boolean;
+  showDecorations: boolean;
+}
+
+/**
+ * This is a helper action that is used internally to implement various actions.
+ * It takes a {@link CallbackOptions.callback callback} that is called once for
+ * each editor, receiving all the targets that are in the given editor.
+ */
+export class CallbackAction implements Action {
+  constructor(private graph: Graph) {
+    this.run = this.run.bind(this);
+  }
+
+  async run(
+    [targets]: [Target[]],
+    options: CallbackOptions,
+  ): Promise<ActionReturnValue> {
+    if (options.showDecorations) {
+      await this.graph.editStyles.displayPendingEditDecorations(
+        targets,
+        this.graph.editStyles.referenced,
+      );
+    }
+
+    if (options.ensureSingleEditor) {
+      ensureSingleEditor(targets);
+    }
+
+    if (options.ensureSingleTarget) {
+      ensureSingleTarget(targets);
+    }
+
+    const originalEditor = ide().activeEditableTextEditor;
+
+    // If we are relying on selections we have to wait for one editor to finish
+    // before moving the selection to the next
+    const runOnTargets = options.setSelection
+      ? runOnTargetsForEachEditorSequentially
+      : runOnTargetsForEachEditor;
+
+    const thatTargets = flatten(
+      await runOnTargets(targets, (editor, targets) =>
+        this.runForEditor(options, editor, targets),
+      ),
+    );
+
+    // If necessary focus back original editor
+    if (
+      options.setSelection &&
+      options.restoreSelection &&
+      originalEditor != null &&
+      !originalEditor.isActive
+    ) {
+      // NB: We just do one editor focus at the end, instead of using
+      // setSelectionsAndFocusEditor because the command might operate on
+      // multiple editors, so we just do one focus at the end.
+      await originalEditor.focus();
+    }
+
+    return { thatTargets };
+  }
+
+  private async runForEditor(
+    options: CallbackOptions,
+    editor: TextEditor,
+    targets: Target[],
+  ): Promise<Target[]> {
+    const editableEditor = ide().getEditableTextEditor(editor);
+    const originalSelections = editor.selections;
+    const originalEditorVersion = editor.document.version;
+    const targetSelections = targets.map((target) => target.contentSelection);
+
+    // For this callback/command to the work we have to have the correct editor focused
+    if (options.setSelection) {
+      await setSelectionsAndFocusEditor(
+        editableEditor,
+        targetSelections,
+        false,
+      );
+    }
+
+    const [updatedOriginalSelections, updatedTargetSelections] =
+      await callFunctionAndUpdateSelections(
+        this.graph.rangeUpdater,
+        () => options.callback(editableEditor, targets),
+        editor.document,
+        [originalSelections, targetSelections],
+      );
+
+    // Reset original selections
+    if (options.setSelection && options.restoreSelection) {
+      // NB: We don't focus the editor here because we'll do that at the
+      // very end. This code can run on multiple editors in the course of
+      // one command, so we want to avoid focusing the editor multiple
+      // times.
+      setSelectionsWithoutFocusingEditor(
+        editableEditor,
+        updatedOriginalSelections,
+      );
+    }
+
+    // If the document hasn't changed then we just return the original targets
+    // so that we preserve their rich types, but if it has changed then we
+    // just downgrade them to untyped targets
+    return editor.document.version === originalEditorVersion
+      ? targets
+      : updatedTargetSelections.map((selection) =>
+          selectionToThatTarget({
+            editor,
+            selection,
+          }),
+        );
+  }
+}

src/actions/CommandAction.ts
@@ -1,150 +1,0 @@
-import { flatten } from "lodash";
-import { commands } from "vscode";
-import { selectionToThatTarget } from "../core/commandRunner/selectionToThatTarget";
-import { callFunctionAndUpdateSelections } from "../core/updateSelections/updateSelections";
-import ide from "../libs/cursorless-engine/singletons/ide.singleton";
-import { Target } from "../typings/target.types";
-import { Graph } from "../typings/Types";
-import {
-  setSelectionsAndFocusEditor,
-  setSelectionsWithoutFocusingEditor,
-} from "../util/setSelectionsAndFocusEditor";
-import {
-  ensureSingleEditor,
-  ensureSingleTarget,
-  runOnTargetsForEachEditor,
-} from "../util/targetUtils";
-import { Action, ActionReturnValue } from "./actions.types";
-
-export interface CommandOptions {
-  command?: string;
-  commandArgs?: any[];
-  restoreSelection?: boolean;
-  ensureSingleEditor?: boolean;
-  ensureSingleTarget?: boolean;
-  showDecorations?: boolean;
-}
-
-const defaultOptions: CommandOptions = {
-  commandArgs: [],
-  restoreSelection: true,
-  ensureSingleEditor: false,
-  ensureSingleTarget: false,
-  showDecorations: false,
-};
-
-export default class CommandAction implements Action {
-  constructor(private graph: Graph, private options: CommandOptions = {}) {
-    this.run = this.run.bind(this);
-  }
-
-  private async runCommandAndUpdateSelections(
-    targets: Target[],
-    options: Required<CommandOptions>,
-  ): Promise<Target[]> {
-    return flatten(
-      await runOnTargetsForEachEditor(targets, async (editor, targets) => {
-        const originalSelections = editor.selections;
-        const originalEditorVersion = editor.document.version;
-
-        const targetSelections = targets.map(
-          (target) => target.contentSelection,
-        );
-
-        // For command to the work we have to have the correct editor focused
-        await setSelectionsAndFocusEditor(
-          ide().getEditableTextEditor(editor),
-          targetSelections,
-          false,
-        );
-
-        const [updatedOriginalSelections, updatedTargetSelections] =
-          await callFunctionAndUpdateSelections(
-            this.graph.rangeUpdater,
-            () =>
-              commands.executeCommand(options.command, ...options.commandArgs),
-            editor.document,
-            [originalSelections, targetSelections],
-          );
-
-        // Reset original selections
-        if (options.restoreSelection) {
-          // NB: We don't focus the editor here because we'll do that at the
-          // very end.  This code can run on multiple editors in the course of
-          // one command, so we want to avoid focusing the editor multiple
-          // times.
-          setSelectionsWithoutFocusingEditor(
-            ide().getEditableTextEditor(editor),
-            updatedOriginalSelections,
-          );
-        }
-
-        // If the document hasn't changed then we just return the original targets
-        // so that we preserve their rich types, but if it has changed then we
-        // just downgrade them to untyped targets
-        return editor.document.version === originalEditorVersion
-          ? targets
-          : updatedTargetSelections.map((selection) =>
-              selectionToThatTarget({
-                editor,
-                selection,
-              }),
-            );
-      }),
-    );
-  }
-
-  async run(
-    [targets]: [Target[]],
-    options: CommandOptions = {},
-  ): Promise<ActionReturnValue> {
-    const partialOptions = Object.assign(
-      {},
-      defaultOptions,
-      this.options,
-      options,
-    );
-
-    if (partialOptions.command == null) {
-      throw Error("Command id must be specified");
-    }
-
-    const actualOptions = partialOptions as Required<CommandOptions>;
-
-    if (actualOptions.showDecorations) {
-      await this.graph.editStyles.displayPendingEditDecorations(
-        targets,
-        this.graph.editStyles.referenced,
-      );
-    }
-
-    if (actualOptions.ensureSingleEditor) {
-      ensureSingleEditor(targets);
-    }
-
-    if (actualOptions.ensureSingleTarget) {
-      ensureSingleTarget(targets);
-    }
-
-    const originalEditor = ide().activeEditableTextEditor;
-
-    const thatTargets = await this.runCommandAndUpdateSelections(
-      targets,
-      actualOptions,
-    );
-
-    // If necessary focus back original editor
-    if (
-      actualOptions.restoreSelection &&
-      originalEditor != null &&
-      !originalEditor.isActive
-    ) {
-      // NB: We just do one editor focus at the end, instead of using
-      // setSelectionsAndFocusEditor because the command might operate on
-      // multiple editors, so we just do one focus at the end.
-      await originalEditor.focus();
-    }
-
-    return { thatTargets };
-  }
-}

src/actions/CutToClipboard.ts
@@ -3,9 +3,8 @@ import { Target } from "../typings/target.types";
 import { Graph } from "../typings/Types";
 import { getOutsideOverflow } from "../util/targetUtils";
 import { Action, ActionReturnValue } from "./actions.types";
-import CommandAction from "./CommandAction";
 
-export class Cut implements Action {
+export class CutToClipboard implements Action {
   constructor(private graph: Graph) {
     this.run = this.run.bind(this);
   }
@@ -48,15 +47,5 @@ export class Cut implements Action {
     );
 
     return { thatSelections: thatMark };
-  }
-}
-
-export class Copy extends CommandAction {
-  constructor(graph: Graph) {
-    super(graph, {
-      command: "editor.action.clipboardCopyAction",
-      ensureSingleEditor: true,
-      showDecorations: true,
-    });
   }
 }

src/actions/EditNew/EditNew.ts
@@ -8,9 +8,9 @@ import { setSelectionsAndFocusEditor } from "../../util/setSelectionsAndFocusEdi
 import { createThatMark, ensureSingleEditor } from "../../util/targetUtils";
 import { Action, ActionReturnValue } from "../actions.types";
 import { State } from "./EditNew.types";
-import { runCommandTargets } from "./runCommandTargets";
+import { runInsertLineAfterTargets } from "./runInsertLineAfterTargets";
 import { runEditTargets } from "./runEditTargets";
-import { runNotebookCellTargets } from "./runNotebookCellTargets";
+import { runEditNewNotebookCellTargets } from "./runNotebookCellTargets";
 
 export class EditNew implements Action {
   getFinalStages(): ModifierStage[] {
@@ -26,7 +26,7 @@ export class EditNew implements Action {
       // It is not possible to "pour" a notebook cell and something else,
       // because each notebook cell is its own editor, and you can't have
       // cursors in multiple editors.
-      return runNotebookCellTargets(this.graph, targets);
+      return runEditNewNotebookCellTargets(this.graph, targets);
     }
 
     const editableEditor = ide().getEditableTextEditor(
@@ -43,7 +43,7 @@ export class EditNew implements Action {
       cursorRanges: new Array(targets.length).fill(undefined) as undefined[],
     };
 
-    state = await runCommandTargets(this.graph, editableEditor, state);
+    state = await runInsertLineAfterTargets(this.graph, editableEditor, state);
     state = await runEditTargets(this.graph, editableEditor, state);
 
     const newSelections = state.targets.map((target, index) =>

src/actions/EditNew/EditNew.types.ts
@@ -2,25 +2,6 @@ import type { Range } from "@cursorless/common";
 import type { Target } from "../../typings/target.types";
 
 /**
- * Internal type to be used for storing a reference to a target that will use a
- * VSCode command to insert a new target, eg `editor.action.insertLineAfter`.
- */
-export interface CommandTarget {
-  target: Target;
-
-  /**
-   * The original index of this target in the original list of targets passed
-   * to the action
-   */
-  index: number;
-
-  /**
-   * The VSCode command to run, eg `editor.action.insertLineAfter`
-   */
-  command: string;
-}
-
-/**
  * Internal type to be used for storing a reference to a target that will use an
  * edit action to insert a new target
  */

commit 479f80b89ea0661470a0a1a7b23098ffb311cb6c

Fix `"pour"` across split (#1219)
- This one most likely broke in #1143, but I'm not certain

## Checklist

- [x] I have added
[tests](https://www.cursorless.org/docs/contributing/test-case-recorder/)
- [ ] I have updated the
[docs](https://github.com/cursorless-dev/cursorless/tree/main/docs) and
[cheatsheet](https://github.com/cursorless-dev/cursorless/tree/main/cursorless-talon/src/cheatsheet)
- [ ] I have not broken the cheatsheet

Co-authored-by: Andreas Arvidsson <andreas.arvidsson87@gmail.com>

src/ide/vscode/VscodeTextEditorImpl.ts
@@ -156,6 +156,7 @@ export class VscodeTextEditorImpl implements EditableTextEditor {
     if (ranges != null) {
       this.selections = ranges.map((range) => range.toSelection(false));
     }
+    await this.focus();
     await vscode.commands.executeCommand("editor.action.insertLineAfter");
   }
 

src/libs/vscode-common/testUtil/openNewEditor.ts
@@ -1,20 +1,30 @@
 import { getParseTreeApi } from "@cursorless/vscode-common";
 import * as vscode from "vscode";
 
+interface NewEditorOptions {
+  languageId?: string;
+  openBeside?: boolean;
+}
+
 export async function openNewEditor(
   content: string,
-  language: string = "plaintext",
+  { languageId = "plaintext", openBeside = false }: NewEditorOptions = {},
 ) {
-  await vscode.commands.executeCommand("workbench.action.closeAllEditors");
+  if (!openBeside) {
+    await vscode.commands.executeCommand("workbench.action.closeAllEditors");
+  }
 
   const document = await vscode.workspace.openTextDocument({
-    language,
+    language: languageId,
     content,
   });
 
-  await (await getParseTreeApi()).loadLanguage(language);
+  await (await getParseTreeApi()).loadLanguage(languageId);
 
-  const editor = await vscode.window.showTextDocument(document);
+  const editor = await vscode.window.showTextDocument(
+    document,
+    openBeside ? vscode.ViewColumn.Beside : undefined,
+  );
 
   const eol = content.includes("\r\n")
     ? vscode.EndOfLine.CRLF

src/apps/cursorless-vscode-e2e/suite/fold.test.ts
@@ -12,7 +12,9 @@ suite("fold", async function () {
 });
 
 async function foldMade() {
-  const editor = await openNewEditor("function myFunk() {\n\n}", "typescript");
+  const editor = await openNewEditor("function myFunk() {\n\n}", {
+    languageId: "typescript",
+  });
 
   await runCursorlessCommand({
     version: 1,
@@ -35,7 +37,9 @@ async function foldMade() {
 }
 
 async function unfoldMade() {
-  const editor = await openNewEditor("function myFunk() {\n\n}", "typescript");
+  const editor = await openNewEditor("function myFunk() {\n\n}", {
+    languageId: "typescript",
+  });
   await vscode.commands.executeCommand("editor.fold", {
     selectionLines: [0],
   });

src/apps/cursorless-vscode-e2e/suite/followLink.test.ts
@@ -14,10 +14,9 @@ suite("followLink", async function () {
 });
 
 async function followDefinition() {
-  const editor = await openNewEditor(
-    "const foo = 'hello';\nconst bar = foo;",
-    "typescript",
-  );
+  const editor = await openNewEditor("const foo = 'hello';\nconst bar = foo;", {
+    languageId: "typescript",
+  });
   await vscode.commands.executeCommand("revealLine", {
     lineNumber: 1,
     at: "top",

src/apps/cursorless-vscode-e2e/suite/keyboard/basic.test.ts
@@ -29,7 +29,9 @@ async function checkKeyboardStartup() {
 async function basic() {
   const { graph } = (await getCursorlessApi()).testHelpers!;
 
-  const editor = await openNewEditor("function foo() {}\n", "typescript");
+  const editor = await openNewEditor("function foo() {}\n", {
+    languageId: "typescript",
+  });
   await graph.hatTokenMap.addDecorations();
 
   editor.selection = new vscode.Selection(1, 0, 1, 0);

src/apps/cursorless-vscode-e2e/suite/pourAcrossSplit.test.ts
@@ -1,0 +1,38 @@
+import { getCursorlessApi, openNewEditor } from "@cursorless/vscode-common";
+import * as assert from "assert";
+import { endToEndTestSetup } from "../endToEndTestSetup";
+import { runCursorlessCommand } from "../runCommand";
+
+suite("Pour across split", async function () {
+  endToEndTestSetup(this);
+
+  test("Pour across split", runTest);
+});
+
+async function runTest() {
+  const { graph } = (await getCursorlessApi()).testHelpers!;
+
+  const { document: document1 } = await openNewEditor("hello world");
+  const { document: document2 } = await openNewEditor("", { openBeside: true });
+
+  await graph.hatTokenMap.addDecorations();
+
+  await runCursorlessCommand({
+    version: 4,
+    action: { name: "editNewLineAfter" },
+    targets: [
+      {
+        type: "primitive",
+        mark: {
+          type: "decoratedSymbol",
+          symbolColor: "default",
+          character: "h",
+        },
+      },
+    ],
+    usePrePhraseSnapshot: false,
+  });
+
+  assert.deepStrictEqual(document1.getText(), "hello world\n");
+  assert.deepStrictEqual(document2.getText(), "");
+}

src/apps/cursorless-vscode-e2e/suite/recorded.test.ts
@@ -68,10 +68,9 @@ async function runTest(file: string, spyIde: SpyIDE) {
 
   graph.editStyles.testDecorations = [];
 
-  const editor = await openNewEditor(
-    fixture.initialState.documentContents,
-    fixture.languageId,
-  );
+  const editor = await openNewEditor(fixture.initialState.documentContents, {
+    languageId: fixture.languageId,
+  });
 
   // Override any user settings and make sure tests run with default tabs.
   editor.options = DEFAULT_TEXT_EDITOR_OPTIONS_FOR_TEST;