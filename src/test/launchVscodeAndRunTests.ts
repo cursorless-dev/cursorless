4192e6929c4f52d171fc23e5f3d252f187052daf
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-02-06 18:16:54 +0000

    more wteaks

--- src/test/launchVscodeAndRunTests.ts
+++ src/test/launchVscodeAndRunTests.ts
@@ -53,7 +53,6 @@ export async function launchVscodeAndRunTests(extensionTestsPath: string) {
       extensionDevelopmentPath,
       extensionTestsPath,
       launchArgs: [
-        "--verbose",
         `--crash-reporter-directory=${crashDir}`,
         `--logsPath=${logsDir}`,
       ],
