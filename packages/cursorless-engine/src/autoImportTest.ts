0319549155632da9a952325f06dbf466803d6600
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-03-06 16:58:46 +0000

    [DX experiment] Import / auto-import tests

---
+++ packages/cursorless-engine/src/autoImportTest.ts
@@ -0,0 +1,17 @@
+// D7.1: Should not get vscode
+// window
+
+// D7.2: Should not get vscode-uri
+// URI
+
+// D8.2: Should get lodash
+// sortBy
+
+// D9: Should get @cursorless/common
+// textFormatters
+
+// D10: Should not get @cursorless/vscode-common
+// openNewEditor
+
+// D11.2: Should not get @cursorless/cursorless-vscode-e2e
+// skipIfWindowsCi
---
+++ packages/cursorless-vscode/src/autoImportTest.ts
@@ -0,0 +1,8 @@
+// D7.2: Should not get lodash
+// sortBy
+
+// D8.1: Should get vscode
+// window
+
+// D11.1: Should not get @cursorless/cursorless-vscode-core/ide/vscode/VscodeMessages
+// VscodeMessages
