b50fe80a1e04116a3db3631a82eb38a249b8485e
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-03-10 18:16:03 +0000

    get typedoc monorepo working

--- packages/cursorless-org-docs/typedoc.js
+++ packages/cursorless-org-docs/typedoc.js
@@ -1,11 +1,22 @@
+const { readFileSync } = require("fs");
+const path = require("path");
+
+const packages = JSON.parse(
+  readFileSync(path.join("..", "..", "tsconfig.json")),
+).references.map(({ path }) => `../../${path}`);
+
+/** @type {import('typedoc').TypeDocOptions} */
 module.exports = {
-  tsconfig: "../cursorless-vscode/tsconfig.json",
-  entryPoints: ["../cursorless-vscode"],
+  tsconfig: "../../tsconfig.json",
+  entryPoints: packages,
   entryPointStrategy: "expand",
 
   readme: "../../docs/contributing/_api-index.md",
 
   exclude: ["**/node_modules/**", "**/out/**"],
+
+  // typedoc-plugin-missing-exports option
+  internalNamespace: "internal",
 
   plugin: [
     "typedoc-plugin-rename-defaults",
