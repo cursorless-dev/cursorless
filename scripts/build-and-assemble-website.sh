51d6a4295e9a6db86580d586f8fe036f1f6ae0c9
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-03-12 15:13:01 +0000

    docs build cleanup

--- scripts/build-and-assemble-website.sh
+++ scripts/build-and-assemble-website.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
+
+pnpm install
+pnpm compile
+pnpm --filter 'cursorless-org' --filter 'cursorless-org-*' build
 
 # Merge the root site and the documentation site, placing the documentation site
 # under docs/
-root_dir=dist/cursorless-org
-mkdir -p "$root_dir"
-cp -r packages/cursorless-org/out/* "$root_dir"
 
+root_dir=dist/cursorless-org
 docs_dir="$root_dir/docs"
+
+mkdir -p "$root_dir"
 mkdir -p "$docs_dir"
+
+cp -r packages/cursorless-org/out/* "$root_dir"
 cp -r packages/cursorless-org-docs/build/* "$docs_dir"
--- scripts/netlify.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-pnpm install
-pnpm compile
-pnpm build
-bash -x scripts/assemble-website.sh
