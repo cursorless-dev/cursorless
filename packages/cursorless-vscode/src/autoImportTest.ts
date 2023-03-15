83b1d21f0206dd2b11a8748a1eb4fc1c8f8b2b2c
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-03-15 15:48:43 +0000

    Remove import tests

--- packages/cursorless-vscode/src/autoImportTest.ts
+++
@@ -1,8 +0,0 @@
-// D7.2: Should not get lodash
-// sortBy
-
-// D8.1: Should get vscode
-// window
-
-// D11.1: Should not get @cursorless/cursorless-vscode-core/ide/vscode/VscodeMessages
-// VscodeMessages
