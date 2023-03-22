8187c55e99b33143b8eb7dba9f83dfbf050df7a6
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-03-22 15:57:05 +0000

    Make rule more conservative for unknown files

--- scripts/check-gitattributes.sh
+++ scripts/check-gitattributes.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
-# Checks that files passed in as arguments are explicitly listed in .gitattributes.
-# Based on https://github.com/alexkaratarakis/gitattributes#ci-step
+# Checks that files passed in as arguments are explicitly listed in
+# .gitattributes. We use a dummy catch-all rule `* cursorless.missing` in
+# `.gitattributes` that will catch any file that isn't explicitly listed.
 
-missing_attributes=$(git check-attr --all $* | grep "text: auto" || true)
+check_attr_output=$(git check-attr cursorless.missing $*)
+missing_attributes=$(echo "$check_attr_output" | grep "cursorless.missing: set" || true)
 
 if [ -n "$missing_attributes" ]; then
   echo "$missing_attributes" | cut -d: -f1
