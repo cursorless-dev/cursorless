cursorless-dev/cursorless

commit f90f49677d25265aba12e53dde0577688929b33b

Migrate to a pnpm monorepo (#1281)
- closes https://github.com/cursorless-dev/cursorless/issues/945
- closes https://github.com/cursorless-dev/cursorless/issues/1044
- depends on https://github.com/cursorless-dev/cursorless/pull/1322
- depends on https://github.com/cursorless-dev/cursorless/pull/1327

## Checklist

- [x] Split into smaller PRs for ease of review
- [x] Try DX tests again
https://github.com/cursorless-dev/cursorless/discussions/1289
- [x] I have updated the
[docs](https://github.com/cursorless-dev/cursorless/tree/main/docs) and
[cheatsheet](https://github.com/cursorless-dev/cursorless/tree/main/cursorless-talon/src/cheatsheet)
- [x] File issue for speeding up cheatsheet / making it not optional
locally https://github.com/cursorless-dev/cursorless/issues/1307
- [x] Set package manager in package.json
https://nodejs.org/api/packages.html#packagemanager
- [x] Re-add lint rule to prevent api types from importing anything
- [x] Try local install
  - [x] Make sure cheatsheet works
- [x] Make sure local extension build depends on building cheatsheet, or
allows no cheatsheet
- [x] Figure out why we're getting so many extra `internal` modules
- [x] Update https://github.com/cursorless-dev/cursorless/issues/931 to
indicate we're now patching instead of swizzling
- [x] Make sure lint rules are actually running in CI
- [x] Check that we've addressed everything in
https://github.com/cursorless-dev/cursorless/issues/945
- [x] Incorporate changes from
https://github.com/cursorless-dev/cursorless/pull/1166
- [x] ~~Make sure this PR doesn't break doc links; see
https://github.com/cursorless-dev/cursorless/pull/942#discussion_r968556276~~
There's no way to avoid these links getting broken if we upgrade
typedoc; filed https://github.com/cursorless-dev/cursorless/issues/1304
to track
- [x] Revert https://github.com/cursorless-dev/cursorless/pull/1284
- [x] I have added

packages/common/src/testUtil/getFixturePaths.ts
@@ -1,0 +1,26 @@
+import * as path from "path";
+import { walkFilesSync } from "../util/walkSync";
+import { getCursorlessRepoRoot } from "./getCursorlessRepoRoot";
+
+export function getFixturesPath() {
+  return path.join(
+    getCursorlessRepoRoot(),
+    "packages",
+    "cursorless-vscode-e2e",
+    "src",
+    "suite",
+    "fixtures",
+  );
+}
+
+export function getFixturePath(fixturePath: string) {
+  return path.join(getFixturesPath(), fixturePath);
+}
+
+export function getRecordedTestPaths() {
+  const directory = path.join(getFixturesPath(), "recorded");
+
+  return walkFilesSync(directory).filter(
+    (path) => path.endsWith(".yml") || path.endsWith(".yaml"),
+  );
+}

packages/cursorless-engine/src/testCaseRecorder/TestCaseRecorder.ts
@@ -1,0 +1,475 @@
+import {
+  DecoratedSymbolMark,
+  DEFAULT_TEXT_EDITOR_OPTIONS_FOR_TEST,
+  extractTargetedMarks,
+  ExtraSnapshotField,
+  getKey,
+  IDE,
+  marksToPlainObject,
+  serialize,
+  SerializedMarks,
+  showInfo,
+  sleep,
+  SpyIDE,
+  TextEditorOptions,
+  toLineRange,
+  walkDirsSync,
+  TestCaseCommand,
+} from "@cursorless/common";
+import * as fs from "fs";
+import { readFile } from "fs/promises";
+import { invariant } from "immutability-helper";
+import { merge } from "lodash";
+import * as path from "path";
+import { ide, injectIde } from "../singletons/ide.singleton";
+import { takeSnapshot } from "../testUtil/takeSnapshot";
+import { Graph } from "../typings/Graph";
+import { TestCase, TestCaseContext } from "./TestCase";
+
+const CALIBRATION_DISPLAY_DURATION_MS = 50;
+
+interface RecordTestCaseCommandArg {
+  /**
+   * If this is set to `true`, then for each test case that we record, we expect
+   * that the user will issue a second command in each phrase, which refers to a
+   * decorated mark whose range we'd like to check that it got updated properly
+   * during the previous command. We use this functionality in order to check
+   * that the token range update works properly. For example, you might say
+   * `"chuck second car ox air take air"` to check that removing a character
+   * from a token properly updates the token.
+   */
+  isHatTokenMapTest?: boolean;
+
+  /** If true decorations will be added to the test fixture */
+  isDecorationsTest?: boolean;
+
+  /**
+   * The directory in which to store the test cases that we record. If left out
+   * the user will be prompted to select a directory within the default recorded
+   * test case directory.
+   */
+  directory?: string;
+
+  /**
+   * If `true`, don't show a little pop up each time to indicate we've recorded a
+   * test case
+   */
+  isSilent?: boolean;
+
+  extraSnapshotFields?: ExtraSnapshotField[];
+
+  /**
+   * Whether to flash a background for calibrating a video recording
+   */
+  showCalibrationDisplay?: boolean;
+
+  /**
+   * Whether we should record a tests which yield errors in addition to tests
+   * which do not error.
+   */
+  recordErrors?: boolean;
+
+  /**
+   * Whether to capture the `that` mark returned by the action.
+   */
+  captureFinalThatMark?: boolean;
+}
+
+const TIMING_CALIBRATION_HIGHLIGHT_ID = "timingCalibration";
+
+export class TestCaseRecorder {
+  private active: boolean = false;
+  private workspacePath: string | null;
+  private workspaceName: string | null;
+  private fixtureRoot: string | null;
+  private targetDirectory: string | null = null;
+  private testCase: TestCase | null = null;
+  private isHatTokenMapTest: boolean = false;
+  private isDecorationsTest: boolean = false;
+  private isSilent?: boolean;
+  private startTimestamp?: bigint;
+  private extraSnapshotFields?: ExtraSnapshotField[];
+  private paused: boolean = false;
+  private isErrorTest: boolean = false;
+  /** We use this variable to capture editor settings and then restore them */
+  private originalTextEditorOptions: TextEditorOptions = {};
+  private captureFinalThatMark: boolean = false;
+  private spyIde: SpyIDE | undefined;
+  private originalIde: IDE | undefined;
+
+  constructor(private graph: Graph) {
+    const { runMode, assetsRoot, workspaceFolders } = ide();
+
+    this.workspacePath =
+      runMode === "development"
+        ? assetsRoot
+        : workspaceFolders?.[0].uri.path ?? null;
+
+    this.workspaceName = this.workspacePath
+      ? path.basename(this.workspacePath)
+      : null;
+
+    this.fixtureRoot = this.workspacePath
+      ? path.join(
+          this.workspacePath,
+          "packages/cursorless-vscode-e2e/src/suite/fixtures/recorded",
+        )
+      : null;
+
+    this.toggle = this.toggle.bind(this);
+    this.pause = this.pause.bind(this);
+    this.resume = this.resume.bind(this);
+    this.takeSnapshot = this.takeSnapshot.bind(this);
+  }
+
+  async toggle(arg?: RecordTestCaseCommandArg) {
+    if (this.active) {
+      showInfo(ide().messages, "recordStop", "Stopped recording test cases");
+      this.stop();
+    } else {
+      return await this.start(arg);
+    }
+  }
+  async pause() {
+    if (!this.active) {
+      throw Error("Asked to pause recording, but no recording active");
+    }
+
+    this.paused = true;
+  }
+
+  async resume() {
+    if (!this.active) {
+      throw Error("Asked to resume recording, but no recording active");
+    }
+
+    this.paused = false;
+  }
+
+  async takeSnapshot(
+    outPath: string,
+    metadata: unknown,
+    targetedMarks: DecoratedSymbolMark[],
+    usePrePhraseSnapshot: boolean,
+  ) {
+    let marks: SerializedMarks | undefined;
+    if (targetedMarks.length !== 0) {
+      const keys = targetedMarks.map(({ character, symbolColor }) =>
+        getKey(symbolColor, character),
+      );
+      const readableHatMap = await this.graph.hatTokenMap.getReadableMap(
+        usePrePhraseSnapshot,
+      );
+      marks = marksToPlainObject(extractTargetedMarks(keys, readableHatMap));
+    } else {
+      marks = undefined;
+    }
+
+    const snapshot = await takeSnapshot(
+      undefined,
+      undefined,
+      ["clipboard"],
+      this.active ? this.extraSnapshotFields : undefined,
+      ide().activeTextEditor!,
+      ide(),
+      marks,
+      this.active ? { startTimestamp: this.startTimestamp } : undefined,
+      metadata,
+    );
+
+    await this.writeToFile(outPath, serialize(snapshot));
+  }
+
+  isActive() {
+    return this.active && !this.paused;
+  }
+
+  async start(arg?: RecordTestCaseCommandArg) {
+    const { directory, ...explicitConfig } = arg ?? {};
+
+    /**
+     * A list of paths of every parent directory between the root fixture
+     * directory and the user's chosen recording directory
+     */
+    let parentDirectories: string[];
+
+    if (directory != null) {
+      this.targetDirectory = directory;
+      parentDirectories = [directory];
+    } else {
+      this.targetDirectory = (await this.promptSubdirectory()) ?? null;
+
+      if (this.targetDirectory == null) {
+        return null;
+      }
+
+      const parentNames = path
+        .relative(this.fixtureRoot!, this.targetDirectory)
+        .split(path.sep);
+
+      parentDirectories = [this.fixtureRoot!];
+      let currentDirectory = this.fixtureRoot!;
+      for (const name of parentNames) {
+        currentDirectory = path.join(currentDirectory, name);
+        parentDirectories.push(currentDirectory);
+      }
+    }
+
+    // Look for a `config.json` file in ancestors of the recording directory,
+    // and merge it with the config provided when calling the command.
+    const config: RecordTestCaseCommandArg = merge(
+      {},
+      ...(await Promise.all(
+        parentDirectories.map((parent) =>
+          readJsonIfExists(path.join(parent, "config.json")),
+        ),
+      )),
+      explicitConfig,
+    );
+
+    const {
+      isHatTokenMapTest = false,
+      isDecorationsTest = false,
+      isSilent = false,
+      extraSnapshotFields = [],
+      showCalibrationDisplay = false,
+      recordErrors: isErrorTest = false,
+      captureFinalThatMark = false,
+    } = config;
+
+    this.active = true;
+
+    const startTimestampISO = await this.recordStartTime(
+      showCalibrationDisplay,
+    );
+    this.isHatTokenMapTest = isHatTokenMapTest;
+    this.captureFinalThatMark = captureFinalThatMark;
+    this.isDecorationsTest = isDecorationsTest;
+    this.isSilent = isSilent;
+    this.extraSnapshotFields = extraSnapshotFields;
+    this.isErrorTest = isErrorTest;
+    this.paused = false;
+
+    showInfo(
+      ide().messages,
+      "recordStart",
+      `Recording test cases for following commands in:\n${this.targetDirectory}`,
+    );
+
+    return { startTimestampISO };
+  }
+
+  private async recordStartTime(showCalibrationDisplay: boolean) {
+    if (showCalibrationDisplay) {
+      await Promise.all(
+        ide().visibleTextEditors.map((editor) =>
+          ide().setHighlightRanges(TIMING_CALIBRATION_HIGHLIGHT_ID, editor, [
+            toLineRange(editor.document.range),
+          ]),
+        ),
+      );
+
+      await sleep(CALIBRATION_DISPLAY_DURATION_MS);
+    }
+
+    // NB: Record timestamp here so that timestamp is last frame of calibration
+    // display
+    this.startTimestamp = process.hrtime.bigint();
+    const timestampISO = new Date().toISOString();
+
+    if (showCalibrationDisplay) {
+      await Promise.all(
+        ide().visibleTextEditors.map((editor) =>
+          ide().setHighlightRanges(TIMING_CALIBRATION_HIGHLIGHT_ID, editor, []),
+        ),
+      );
+    }
+
+    return timestampISO;
+  }
+
+  stop() {
+    this.active = false;
+    this.paused = false;
+  }
+
+  async preCommandHook(command: TestCaseCommand, context: TestCaseContext) {
+    if (this.testCase != null) {
+      // If testCase is not null and we are just before a command, this means
+      // that this command is the follow up command indicating which marks we
+      // cared about from the last command
+      invariant(
+        this.testCase.awaitingFinalMarkInfo,
+        () => "expected to be awaiting final mark info",
+      );
+      this.testCase.filterMarks(command, context);
+      await this.finishTestCase();
+    } else {
+      // Otherwise, we are starting a new test case
+      this.originalIde = ide();
+      this.spyIde = new SpyIDE(this.originalIde);
+      injectIde(this.spyIde!);
+
+      this.testCase = new TestCase(
+        command,
+        context,
+        this.spyIde,
+        this.isHatTokenMapTest,
+        this.isDecorationsTest,
+        this.startTimestamp!,
+        this.captureFinalThatMark,
+        this.extraSnapshotFields,
+      );
+
+      await this.testCase.recordInitialState();
+
+      const editor = ide().activeTextEditor!;
+      // NB: We need to copy the editor options rather than storing a reference
+      // because its properties are lazy
+      this.originalTextEditorOptions = { ...editor.options };
+      ide().getEditableTextEditor(editor).options =
+        DEFAULT_TEXT_EDITOR_OPTIONS_FOR_TEST;
+    }
+  }
+
+  async postCommandHook(returnValue: any) {
+    if (this.testCase == null) {
+      // If test case is null then this means that this was just a follow up
+      // command for a navigation map test
+      return;
+    }
+
+    await this.testCase.recordFinalState(returnValue);
+
+    if (this.testCase.awaitingFinalMarkInfo) {
+      // We don't finish the test case here in the case of a navigation map
+      // test because we'll do it after we get the follow up command indicating
+      // which marks we wanted to track
+      return;
+    }
+
+    await this.finishTestCase();
+  }
+
+  async finishTestCase(): Promise<void> {
+    const outPath = this.calculateFilePath(this.testCase!);
+    const fixture = this.testCase!.toYaml();
+    await this.writeToFile(outPath, fixture);
+
+    if (!this.isSilent) {
+      showInfo(
+        ide().messages,
+        "testCaseSaved",
+        "Cursorless test case saved.",
+        "View",
+      ).then(async (action) => {
+        if (action === "View") {
+          await ide().openTextDocument(outPath);
+        }
+      });
+    }
+
+    this.testCase = null;
+  }
+
+  private async writeToFile(outPath: string, fixture: string) {
+    fs.writeFileSync(outPath, fixture);
+  }
+
+  private async promptSubdirectory(): Promise<string | undefined> {
+    if (
+      this.workspaceName == null ||
+      this.fixtureRoot == null ||
+      !["cursorless-vscode", "cursorless"].includes(this.workspaceName)
+    ) {
+      throw new Error(
+        '"Cursorless record" must be run from within cursorless directory',
+      );
+    }
+
+    const subdirectorySelection = await ide().showQuickPick(
+      walkDirsSync(this.fixtureRoot).concat("/"),
+      {
+        title: "Select directory for new test cases",
+        unknownValues: {
+          allowed: true,
+          newValueTemplate: "Create new directory '{}' →",
+        },
+      },
+    );
+
+    if (subdirectorySelection == null) {
+      return undefined;
+    }
+
+    return path.join(this.fixtureRoot, subdirectorySelection);
+  }
+
+  private calculateFilePath(testCase: TestCase): string {
+    if (this.targetDirectory == null) {
+      throw new Error("Target directory isn't defined");
+    }
+
+    if (!fs.existsSync(this.targetDirectory)) {
+      fs.mkdirSync(this.targetDirectory, { recursive: true });
+    }
+
+    const filename = camelize(testCase.command.spokenForm ?? "command");
+    let filePath = path.join(this.targetDirectory, `${filename}.yml`);
+
+    let i = 2;
+    while (fs.existsSync(filePath)) {
+      filePath = path.join(this.targetDirectory, `${filename}${i++}.yml`);
+    }
+
+    return filePath;
+  }
+
+  async commandErrorHook(error: Error) {
+    if (this.isErrorTest && this.testCase) {
+      this.testCase.thrownError = { name: error.name };
+      await this.finishTestCase();
+    } else {
+      this.testCase = null;
+    }
+  }
+
+  finallyHook() {
+    injectIde(this.originalIde!);
+    this.spyIde = undefined;
+    this.originalIde = undefined;
+
+    const editor = ide().activeTextEditor!;
+    ide().getEditableTextEditor(editor).options =
+      this.originalTextEditorOptions;
+  }
+}
+
+function camelize(str: string) {
+  return str
+    .split(" ")
+    .map((str, index) => (index === 0 ? str : capitalize(str)))
+    .join("");
+}
+
+function capitalize(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+async function readJsonIfExists(
+  path: string,
+): Promise<RecordTestCaseCommandArg> {
+  let rawText: string;
+
+  try {
+    rawText = await readFile(path, { encoding: "utf-8" });
+  } catch (err) {
+    if ((err as NodeJS.ErrnoException).code === "ENOENT") {
+      return {};
+    }
+
+    throw err;
+  }
+
+  return JSON.parse(rawText);
+}

packages/common/testUtil/getFixturePaths.ts
@@ -1,21 +1,0 @@
-import * as path from "path";
-import { walkFilesSync } from "../util/walkSync";
-
-export function getFixturesPath() {
-  return path.join(
-    __dirname,
-    "../../../packages/cursorless-vscode-e2e/suite/fixtures",
-  );
-}
-
-export function getFixturePath(fixturePath: string) {
-  return path.join(getFixturesPath(), fixturePath);
-}
-
-export function getRecordedTestPaths() {
-  const directory = path.join(getFixturesPath(), "recorded");
-
-  return walkFilesSync(directory).filter(
-    (path) => path.endsWith(".yml") || path.endsWith(".yaml"),
-  );
-}

commit 235b1b9a6525617f1d45bb34e69d4a45786ac1c9

Fix test case recorder (#1341)
The test case recorder broke in #1281 due to the fact we're now
launching extension in subdirectory called `dist` rather than top-level
repo. This PR fixes it

## Checklist

- [x] I have added
[tests](https://www.cursorless.org/docs/contributing/test-case-recorder/)
- [ ] I have updated the
[docs](https://github.com/cursorless-dev/cursorless/tree/main/docs) and
[cheatsheet](https://github.com/cursorless-dev/cursorless/tree/main/cursorless-talon/src/cheatsheet)
- [ ] I have not broken the cheatsheet

packages/common/src/ide/fake/FakeIDE.ts
@@ -35,6 +35,7 @@ export default class FakeIDE implements IDE {
   workspaceFolders: readonly WorkspaceFolder[] | undefined = undefined;
   private disposables: Disposable[] = [];
   private assetsRoot_: string | undefined;
+  private quickPickReturnValue: string | undefined = undefined;
 
   async flashRanges(_flashDescriptors: FlashDescriptor[]): Promise<void> {
     // empty
@@ -93,11 +94,15 @@ export default class FakeIDE implements IDE {
     throw Error("Not implemented");
   }
 
-  public showQuickPick(
+  public setQuickPickReturnValue(value: string | undefined) {
+    this.quickPickReturnValue = value;
+  }
+
+  public async showQuickPick(
     _items: readonly string[],
     _options?: QuickPickOptions,
   ): Promise<string | undefined> {
-    throw new Error("Method not implemented.");
+    return this.quickPickReturnValue;
   }
 
   public showInputBox(_options?: any): Promise<string | undefined> {

packages/common/src/ide/normalized/NormalizedIDE.ts
@@ -7,6 +7,7 @@ import FakeIDE from "../fake/FakeIDE";
 import PassthroughIDEBase from "../PassthroughIDEBase";
 import { FlashDescriptor } from "../types/FlashDescriptor";
 import type { IDE } from "../types/ide.types";
+import { QuickPickOptions } from "../types/QuickPickOptions";
 
 export class NormalizedIDE extends PassthroughIDEBase {
   configuration: FakeConfiguration;
@@ -15,7 +16,7 @@ export class NormalizedIDE extends PassthroughIDEBase {
 
   constructor(
     original: IDE,
-    private fakeIde: FakeIDE,
+    public fakeIde: FakeIDE,
     private isSilent: boolean,
   ) {
     super(original);
@@ -60,5 +61,14 @@ export class NormalizedIDE extends PassthroughIDEBase {
     return this.isSilent
       ? this.fakeIde.setHighlightRanges(highlightId, editor, ranges)
       : super.setHighlightRanges(highlightId, editor, ranges);
+  }
+
+  public async showQuickPick(
+    _items: readonly string[],
+    _options?: QuickPickOptions,
+  ): Promise<string | undefined> {
+    return this.isSilent
+      ? this.fakeIde.showQuickPick(_items, _options)
+      : super.showQuickPick(_items, _options);
   }
 }

packages/common/src/testUtil/getFixturePaths.ts
@@ -17,8 +17,12 @@ export function getFixturePath(fixturePath: string) {
   return path.join(getFixturesPath(), fixturePath);
 }
 
+export function getRecordedTestsDirPath() {
+  return path.join(getFixturesPath(), "recorded");
+}
+
 export function getRecordedTestPaths() {
-  const directory = path.join(getFixturesPath(), "recorded");
+  const directory = getRecordedTestsDirPath();
 
   return walkFilesSync(directory).filter(
     (path) => path.endsWith(".yml") || path.endsWith(".yaml"),

packages/cursorless-engine/src/testCaseRecorder/TestCaseRecorder.ts
@@ -15,9 +15,10 @@ import {
   toLineRange,
   walkDirsSync,
   TestCaseCommand,
+  showError,
 } from "@cursorless/common";
 import * as fs from "fs";
-import { readFile } from "fs/promises";
+import { access, readFile } from "fs/promises";
 import { invariant } from "immutability-helper";
 import { merge } from "lodash";
 import * as path from "path";
@@ -79,8 +80,6 @@ const TIMING_CALIBRATION_HIGHLIGHT_ID = "timingCalibration";
 
 export class TestCaseRecorder {
   private active: boolean = false;
-  private workspacePath: string | null;
-  private workspaceName: string | null;
   private fixtureRoot: string | null;
   private targetDirectory: string | null = null;
   private testCase: TestCase | null = null;
@@ -100,18 +99,14 @@ export class TestCaseRecorder {
   constructor(private graph: Graph) {
     const { runMode, assetsRoot, workspaceFolders } = ide();
 
-    this.workspacePath =
-      runMode === "development"
-        ? assetsRoot
+    const workspacePath =
+      runMode === "development" || runMode === "test"
+        ? path.join(assetsRoot, "../../..")
         : workspaceFolders?.[0].uri.path ?? null;
 
-    this.workspaceName = this.workspacePath
-      ? path.basename(this.workspacePath)
-      : null;
-
-    this.fixtureRoot = this.workspacePath
+    this.fixtureRoot = workspacePath
       ? path.join(
-          this.workspacePath,
+          workspacePath,
           "packages/cursorless-vscode-e2e/src/suite/fixtures/recorded",
         )
       : null;
@@ -377,14 +372,16 @@ export class TestCaseRecorder {
   }
 
   private async promptSubdirectory(): Promise<string | undefined> {
-    if (
-      this.workspaceName == null ||
-      this.fixtureRoot == null ||
-      !["cursorless-vscode", "cursorless"].includes(this.workspaceName)
-    ) {
-      throw new Error(
-        '"Cursorless record" must be run from within cursorless directory',
-      );
+    try {
+      if (this.fixtureRoot == null) {
+        throw Error();
+      }
+      await access(this.fixtureRoot);
+    } catch (err) {
+      const errorMessage =
+        '"Cursorless record" must be run from within cursorless directory';
+      showError(ide().messages, "promptSubdirectoryError", errorMessage);
+      throw new Error(errorMessage);
     }
 
     const subdirectorySelection = await ide().showQuickPick(

packages/cursorless-vscode-e2e/src/suite/fixtures/recorded/testCaseRecorder/takeHarp.yml
@@ -1,0 +1,23 @@
+languageId: plaintext
+command:
+  version: 4
+  spokenForm: take harp
+  action: {name: setSelection}
+  targets:
+    - type: primitive
+      mark: {type: decoratedSymbol, symbolColor: default, character: h}
+  usePrePhraseSnapshot: false
+initialState:
+  documentContents: hello world
+  selections:
+    - anchor: {line: 0, character: 11}
+      active: {line: 0, character: 11}
+  marks:
+    default.h:
+      start: {line: 0, character: 0}
+      end: {line: 0, character: 5}
+finalState:
+  documentContents: hello world
+  selections:
+    - anchor: {line: 0, character: 0}
+      active: {line: 0, character: 5}

packages/cursorless-vscode-e2e/src/suite/testCaseRecorder.test.ts
@@ -1,0 +1,115 @@
+import {
+  getFixturePath,
+  getRecordedTestsDirPath,
+  HatTokenMap,
+} from "@cursorless/common";
+import {
+  getCursorlessApi,
+  openNewEditor,
+  runCursorlessCommand,
+} from "@cursorless/vscode-common";
+import { assert } from "chai";
+import * as crypto from "crypto";
+import { mkdir, readdir, readFile, rm } from "fs/promises";
+import * as path from "path";
+import * as os from "os";
+import { basename } from "path";
+import * as vscode from "vscode";
+import { endToEndTestSetup } from "../endToEndTestSetup";
+
+// Ensure that the test case recorder works
+suite("testCaseRecorder", async function () {
+  endToEndTestSetup(this);
+
+  test("no args", testCaseRecorderNoArgs);
+  test("path arg", testCaseRecorderPathArg);
+});
+
+async function testCaseRecorderNoArgs() {
+  const {
+    hatTokenMap,
+    ide: { fakeIde },
+  } = (await getCursorlessApi()).testHelpers!;
+  const dirName = crypto.randomBytes(16).toString("hex");
+  fakeIde.setQuickPickReturnValue(dirName);
+  const tmpdir = path.join(getRecordedTestsDirPath(), dirName);
+
+  try {
+    await runAndCheckTestCaseRecorder(hatTokenMap, tmpdir);
+  } finally {
+    fakeIde.setQuickPickReturnValue(undefined);
+    await rm(tmpdir, { recursive: true, force: true });
+  }
+}
+
+async function testCaseRecorderPathArg() {
+  const { hatTokenMap } = (await getCursorlessApi()).testHelpers!;
+  const tmpdir = path.join(os.tmpdir(), crypto.randomBytes(16).toString("hex"));
+  await mkdir(tmpdir, { recursive: true });
+
+  try {
+    await runAndCheckTestCaseRecorder(hatTokenMap, tmpdir, {
+      directory: tmpdir,
+    });
+  } finally {
+    await rm(tmpdir, { recursive: true, force: true });
+  }
+}
+
+async function runAndCheckTestCaseRecorder(
+  hatTokenMap: HatTokenMap,
+  tmpdir: string,
+  ...extraArgs: unknown[]
+) {
+  const editor = await openNewEditor("hello world");
+
+  editor.selections = [new vscode.Selection(0, 11, 0, 11)];
+
+  await hatTokenMap.allocateHats();
+
+  await vscode.commands.executeCommand(
+    "cursorless.recordTestCase",
+    ...extraArgs,
+  );
+
+  await runCursorlessCommand({
+    version: 4,
+    action: { name: "setSelection" },
+    targets: [
+      {
+        type: "primitive",
+        mark: {
+          type: "decoratedSymbol",
+          symbolColor: "default",
+          character: "h",
+        },
+      },
+    ],
+    usePrePhraseSnapshot: false,
+    spokenForm: "take harp",
+  });
+
+  await vscode.commands.executeCommand("cursorless.recordTestCase");
+
+  const paths = await readdir(tmpdir);
+  assert.lengthOf(paths, 1);
+
+  const actualRecordedTestPath = paths[0];
+  assert.equal(basename(actualRecordedTestPath), "takeHarp.yml");
+
+  const expected = (
+    await readFile(
+      getFixturePath("recorded/testCaseRecorder/takeHarp.yml"),
+      "utf8",
+    )
+  )
+    // We use this to ensure that the test works on Windows. Depending on user
+    // / CI git config, the file might be checked out with CRLF line endings
+    .replaceAll("\r\n", "\n");
+  const actualRecordedTest = await readFile(
+    path.join(tmpdir, actualRecordedTestPath),
+    "utf8",
+  );
+
+  assert.equal(actualRecordedTest, expected);
+}