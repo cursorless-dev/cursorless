18d04c787998b517e349e558d58cb22d724eec7b
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-04-04 11:39:38 +0100

    Add all commands to `contributes`; even hidden

--- packages/common/src/cursorlessCommandIds.ts
+++ packages/common/src/cursorlessCommandIds.ts
@@ -1,35 +1,25 @@
 export const CURSORLESS_COMMAND_ID =
   "cursorless.command" satisfies CursorlessCommandId;
 
-export interface VisibleCommandDescription {
-  readonly isVisible: true;
+export interface CommandDescription {
+  readonly isVisible: boolean;
   readonly title: string;
 }
 
-interface HiddenCommandDescription {
-  readonly isVisible: false;
-}
-
-export type CommandDescription =
-  | VisibleCommandDescription
-  | HiddenCommandDescription;
-
-abstract class Command {}
-
-class HiddenCommand extends Command implements HiddenCommandDescription {
-  readonly isVisible = false as const;
-}
-
-class VisibleCommand extends Command implements VisibleCommandDescription {
-  readonly isVisible = true as const;
-
-  constructor(private baseTitle: string) {
-    super();
-  }
+abstract class Command {
+  constructor(private baseTitle: string) {}
 
   get title(): string {
     return `Cursorless: ${this.baseTitle}`;
   }
+}
+
+class HiddenCommand extends Command implements CommandDescription {
+  readonly isVisible = false as const;
+}
+
+class VisibleCommand extends Command implements CommandDescription {
+  readonly isVisible = true as const;
 }
 
 export const cursorlessCommandIds = [
@@ -74,18 +64,44 @@ export const cursorlessCommandDescriptions: Record<
   ),
   ["cursorless.showDocumentation"]: new VisibleCommand("Show documentation"),
 
-  ["cursorless.command"]: new HiddenCommand(),
-  ["cursorless.showQuickPick"]: new HiddenCommand(),
-  ["cursorless.showCheatsheet"]: new HiddenCommand(),
-  ["cursorless.internal.updateCheatsheetDefaults"]: new HiddenCommand(),
-  ["cursorless.takeSnapshot"]: new HiddenCommand(),
-  ["cursorless.keyboard.escape"]: new HiddenCommand(),
-  ["cursorless.keyboard.targeted.targetHat"]: new HiddenCommand(),
-  ["cursorless.keyboard.targeted.targetScope"]: new HiddenCommand(),
-  ["cursorless.keyboard.targeted.targetSelection"]: new HiddenCommand(),
-  ["cursorless.keyboard.targeted.clearTarget"]: new HiddenCommand(),
-  ["cursorless.keyboard.targeted.runActionOnTarget"]: new HiddenCommand(),
-  ["cursorless.keyboard.modal.modeOn"]: new HiddenCommand(),
-  ["cursorless.keyboard.modal.modeOff"]: new HiddenCommand(),
-  ["cursorless.keyboard.modal.modeToggle"]: new HiddenCommand(),
+  ["cursorless.command"]: new HiddenCommand("The core cursorless command"),
+  ["cursorless.showQuickPick"]: new HiddenCommand(
+    "Pop up a quick pick of all cursorless commands",
+  ),
+  ["cursorless.showCheatsheet"]: new HiddenCommand(
+    "Display the cursorless cheatsheet",
+  ),
+  ["cursorless.internal.updateCheatsheetDefaults"]: new HiddenCommand(
+    "Update the default values of the cheatsheet payload used on the website and for local development. Be sure to run this on stock knausj and cursorless.",
+  ),
+  ["cursorless.takeSnapshot"]: new HiddenCommand(
+    "Take a snapshot of the current editor state",
+  ),
+  ["cursorless.keyboard.escape"]: new HiddenCommand(
+    "Should be mapped to the escape key when using cursorless keyboard. By default, exits modal keyboard mode, but changes behaviour when Cursorless is expecting a continuation keystroke.  For example, when you type a color and Cursorless is waiting for a character, it cancels the color and switches back to modal mode.",
+  ),
+  ["cursorless.keyboard.targeted.targetHat"]: new HiddenCommand(
+    "Sets the keyboard target to the given hat",
+  ),
+  ["cursorless.keyboard.targeted.targetScope"]: new HiddenCommand(
+    "Sets the keyboard target to the scope containing the current target",
+  ),
+  ["cursorless.keyboard.targeted.targetSelection"]: new HiddenCommand(
+    "Sets the keyboard target to the current selection",
+  ),
+  ["cursorless.keyboard.targeted.clearTarget"]: new HiddenCommand(
+    "Clears the current keyboard target",
+  ),
+  ["cursorless.keyboard.targeted.runActionOnTarget"]: new HiddenCommand(
+    "Run the given action on the current keyboard target",
+  ),
+  ["cursorless.keyboard.modal.modeOn"]: new HiddenCommand(
+    "Turn on the cursorless modal mode",
+  ),
+  ["cursorless.keyboard.modal.modeOff"]: new HiddenCommand(
+    "Turn off the cursorless modal mode",
+  ),
+  ["cursorless.keyboard.modal.modeToggle"]: new HiddenCommand(
+    "Toggle the cursorless modal mode",
+  ),
 };
