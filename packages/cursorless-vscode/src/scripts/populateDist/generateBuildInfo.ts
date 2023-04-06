3916ad58dbcddb958b916d91580982acfc9246d3
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-04-06 08:25:08 -0700

    Refactor `populateDist` (#1380)
    Behaviour should be identical; just splits `populateDist` into multiple
    files
    
    ## Checklist
    
    - [ ] I have added
    [tests](https://www.cursorless.org/docs/contributing/test-case-recorder/)
    - [ ] I have updated the
    [docs](https://github.com/cursorless-dev/cursorless/tree/main/docs) and
    [cheatsheet](https://github.com/cursorless-dev/cursorless/tree/main/cursorless-talon/src/cheatsheet)
    - [ ] I have not broken the cheatsheet

---
+++ packages/cursorless-vscode/src/scripts/populateDist/generateBuildInfo.ts
@@ -0,0 +1,23 @@
+import { getEnvironmentVariableStrict } from "@cursorless/common";
+import { runCommand } from "./runCommand";
+import { Context } from "./context";
+
+export async function generateBuildInfo({ isCi }: Context) {
+  // In CI, we generate a file called `build-info.json` that contains
+  // information about the build for provenance. We include the git sha of
+  // the current branch as well as the build URL.
+  if (!isCi) {
+    return undefined;
+  }
+
+  // These are automatically set for Github actions
+  // See https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
+  const repository = getEnvironmentVariableStrict("GITHUB_REPOSITORY");
+  const runId = getEnvironmentVariableStrict("GITHUB_RUN_ID");
+  const githubBaseUrl = getEnvironmentVariableStrict("GITHUB_SERVER_URL");
+
+  return JSON.stringify({
+    gitSha: (await runCommand("git rev-parse HEAD")).trim(),
+    buildUrl: `${githubBaseUrl}/${repository}/actions/runs/${runId}`,
+  });
+}
