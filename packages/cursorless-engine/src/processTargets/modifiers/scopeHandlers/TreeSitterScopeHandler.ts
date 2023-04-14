c697971155049ffe8519c99da58090ce8f773a85
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-04-14 16:37:51 +0100

    docs

--- packages/cursorless-engine/src/processTargets/modifiers/scopeHandlers/TreeSitterScopeHandler.ts
+++ packages/cursorless-engine/src/processTargets/modifiers/scopeHandlers/TreeSitterScopeHandler.ts
@@ -62,6 +62,15 @@ export class TreeSitterScopeHandler extends BaseScopeHandler {
       .sort((a, b) => compareTargetScopes(direction, position, a, b));
   }
 
+  /**
+   * Constructs a range to pass to {@link Query.matches} to find scopes. Note
+   * that {@link Query.matches} will only return scopes that have non-empty
+   * intersection with this range.  Also note that the base
+   * {@link BaseScopeHandler.generateScopes} will filter out any extra scopes
+   * that we yield, so we don't need to be totally precise.
+   *
+   * @returns Range to pass to {@link Query.matches}
+   */
   private getQueryRange(
     document: TextDocument,
     position: Position,
@@ -73,12 +82,18 @@ export class TreeSitterScopeHandler extends BaseScopeHandler {
       distalPosition == null ? null : document.offsetAt(distalPosition);
 
     if (containment === "required") {
+      // If containment is required, we smear the position left and right by one
+      // character so that we have a non-empty intersection with any scope that
+      // touches position
       return {
         start: document.positionAt(offset - 1),
         end: document.positionAt(offset + 1),
       };
     }
 
+    // If containment is disallowed, we can shift the position forward by a character to avoid
+    // matching scopes that touch position.  Otherwise, we shift the position backward by a
+    // character to ensure we get scopes that touch position.
     const proximalShift = containment === "disallowed" ? 1 : -1;
 
     // FIXME: Don't go all the way to end of document when there is no distalPosition?
