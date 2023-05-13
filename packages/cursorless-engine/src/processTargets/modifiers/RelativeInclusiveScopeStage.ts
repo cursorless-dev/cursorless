15ac3ebcee123330e678a71f52ad210f1328c13b
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-05-13 12:48:11 +0100

    Consistent relative inclusive between empty / nonempty

--- packages/cursorless-engine/src/processTargets/modifiers/RelativeInclusiveScopeStage.ts
+++ packages/cursorless-engine/src/processTargets/modifiers/RelativeInclusiveScopeStage.ts
@@ -17,11 +17,11 @@ import { OutOfRangeError } from "./targetSequenceUtils";
  * eg `"two funks"`, `"token backward"`, etc.  Proceeds as follows:
  *
  * 1. Constructs the initial range to use as the starting point for the relative
- *    scope search.  If the input range is empty, then we use the preferred
- *    scope touching the input position, breaking ties in direction
- *    {@link direction} rather than the tie-breaking heuristics we use for
- *    containing scope, to make this modifier easier to reason about when
- *    between scopes.  Otherwise, we use the input range itself.
+ *    scope search.  Expands from the proximal end of the input target (start
+ *    for "forward", end for "backward") to the smallest containing scope,
+ *    breaking ties in direction {@link direction} rather than the tie-breaking
+ *    heuristics we use for containing scope, to make this modifier easier to
+ *    reason about when between scopes.
  * 2. Calls {@link ScopeHandler.generateScopes} to get as many scopes as
  *    desired, starting from the proximal end of the initial range (ie the start
  *    if direction is "forward", the end if direction is "backward").
@@ -51,14 +51,12 @@ export class RelativeInclusiveScopeStage implements ModifierStage {
     const { isReversed, editor, contentRange } = target;
     const { length: desiredScopeCount, direction } = this.modifier;
 
-    const initialRange = contentRange.isEmpty
-      ? getPreferredScopeTouchingPosition(
-          scopeHandler,
-          editor,
-          contentRange.start,
-          direction,
-        )?.domain
-      : contentRange;
+    const initialRange = getPreferredScopeTouchingPosition(
+      scopeHandler,
+      editor,
+      direction === "forward" ? contentRange.start : contentRange.end,
+      direction,
+    )?.domain;
 
     if (initialRange == null) {
       throw new NoContainingScopeError(this.modifier.scopeType.type);
