3b5ea3516d271151fe45c4d5139fee96e3ef7b6f
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-05-16 19:57:29 +0100

    Ensure that all marks are captured in recorded tests (#1467)
    - depends on #1468 
    
    ## Checklist
    
    - [ ] I have added
    [tests](https://www.cursorless.org/docs/contributing/test-case-recorder/)
    - [ ] I have updated the
    [docs](https://github.com/cursorless-dev/cursorless/tree/main/docs) and
    [cheatsheet](https://github.com/cursorless-dev/cursorless/tree/main/cursorless-talon/src/cheatsheet)
    - [ ] I have not broken the cheatsheet

--- packages/cursorless-engine/src/testUtil/extractTargetKeys.ts
+++ packages/cursorless-engine/src/testUtil/extractTargetKeys.ts
@@ -1,10 +1,16 @@
-import { getKey } from "@cursorless/common";
+import {
+  PartialPrimitiveTargetDescriptor,
+  PartialTargetDescriptor,
+  getKey,
+} from "@cursorless/common";
 import {
   PrimitiveTargetDescriptor,
   TargetDescriptor,
 } from "../typings/TargetDescriptor";
 
-export function extractTargetKeys(target: TargetDescriptor): string[] {
+export function extractTargetKeys(
+  target: TargetDescriptor | PartialTargetDescriptor,
+): string[] {
   switch (target.type) {
     case "primitive":
       return extractPrimitiveTargetKeys(target);
@@ -18,14 +24,17 @@ export function extractTargetKeys(target: TargetDescriptor): string[] {
         ...extractPrimitiveTargetKeys(target.active),
       ];
 
-    default:
+    case "implicit":
       return [];
   }
 }
-function extractPrimitiveTargetKeys(...targets: PrimitiveTargetDescriptor[]) {
+
+function extractPrimitiveTargetKeys(
+  ...targets: (PrimitiveTargetDescriptor | PartialPrimitiveTargetDescriptor)[]
+) {
   const keys: string[] = [];
   targets.forEach((target) => {
-    if (target.mark.type === "decoratedSymbol") {
+    if (target.mark?.type === "decoratedSymbol") {
       const { character, symbolColor } = target.mark;
       keys.push(getKey(symbolColor, character));
     }
