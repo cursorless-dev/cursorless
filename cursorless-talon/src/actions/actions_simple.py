f67147ddb6fee73c2c8e8e825d5d5ae65249a6b6
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-05-20 19:35:11 +0100

    Remove spoken form by default

--- cursorless-talon/src/actions/actions_simple.py
+++ cursorless-talon/src/actions/actions_simple.py
@@ -22,7 +22,6 @@ simple_action_defaults = {
     "float": "insertEmptyLineAfter",
     "fold": "foldRegion",
     "follow": "followLink",
-    "from": "setImplicit",
     "give": "deselect",
     "highlight": "highlight",
     "hover": "showHover",
