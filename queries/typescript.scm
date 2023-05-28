db58ecb0c501e487147c32781a6ede09e7a493a5
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-05-28 19:27:03 +0100

    tweak

--- queries/typescript.scm
+++ queries/typescript.scm
@@ -1,3 +1,7 @@
+;; Note that we don't just import `javascript.scm` because that includes
+;; `javascript.jsx.scm`, and tree-sitter would complain because those node
+;; types are not defined in the typescript grammar.
+
 ;; import javascript.core.scm
 
 (optional_parameter
