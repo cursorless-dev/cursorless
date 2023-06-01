73d416486766487a448fcefc815b7155999bda81
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-06-01 16:42:26 +0200

    Rename incorrect usage of `targets` (#1513)
    ## Checklist
    
    - [ ] I have added
    [tests](https://www.cursorless.org/docs/contributing/test-case-recorder/)
    - [ ] I have updated the
    [docs](https://github.com/cursorless-dev/cursorless/tree/main/docs) and
    [cheatsheet](https://github.com/cursorless-dev/cursorless/tree/main/cursorless-talon/src/cheatsheet)
    - [ ] I have not broken the cheatsheet

--- src/actions/find.py
+++ src/actions/find.py
@@ -5,5 +5,5 @@ mod = Module()
 
 @mod.action_class
 class Actions:
-    def cursorless_private_run_find_action(targets: dict):
-        """Find text of targets in editor"""
+    def cursorless_private_run_find_action(target: dict):
+        """Find text of target in editor"""
