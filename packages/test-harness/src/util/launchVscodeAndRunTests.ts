26367aa42ad6bf76e4c881533cc647d933bb944f
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-06-15 14:25:50 +0100

    remove launchArgs

--- packages/test-harness/src/util/launchVscodeAndRunTests.ts
+++ packages/test-harness/src/util/launchVscodeAndRunTests.ts
@@ -62,15 +62,6 @@ export async function launchVscodeAndRunTests(extensionTestsPath: string) {
       vscodeExecutablePath,
       extensionDevelopmentPath,
       extensionTestsPath,
-      // Note: Crash dump causes legacy VSCode to hang, so we just don't bother
-      launchArgs: useLegacyVscode
-        ? undefined
-        : [
-            `--crash-reporter-directory=${crashDir}`,
-            `--logsPath=${logsDir}`,
-            "--disable-gpu",
-            "--disable-software-rasterizer",
-          ],
     });
 
     console.log(`Returned from "runTests" with value: ${code}`);
