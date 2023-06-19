40876ecf1271ea883cdaade1e26610dcde3eecc4
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-06-19 15:41:15 +0100

    Make it easier to have unit tests in VSCode packages

--- packages/test-harness/src/scripts/runTestsCI.ts
+++ packages/test-harness/src/scripts/runTestsCI.ts
@@ -8,10 +8,7 @@ import { launchVscodeAndRunTests } from "../util/launchVscodeAndRunTests";
 
 (async function main() {
   // Note that we run all tests, including unit tests, in VSCode, even though
-  // unit tests could be run separately.  If we wanted to run unit tests
-  // separately, we could instead use `../runners/endToEndOnly` instead of
-  // `../runners/all` and then just call `await runUnitTests()` beforehand to
-  // run the unit tests directly, instead of as part of VSCode runner.
+  // unit tests could be run separately.
   const extensionTestsPath = path.resolve(
     getCursorlessRepoRoot(),
     "packages/test-harness/out/runners/all",
