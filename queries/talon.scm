8b5210583b94b2727bc79a9391abf1757476a107
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-06-29 12:06:09 +0200

    Added name capture

--- queries/talon.scm
+++ queries/talon.scm
@@ -1,6 +1,30 @@
+;;!! not mode: command
+;;!  ----^^^^---------
+;;!! slap: key("end enter")
+;;!  ^^^^------------------
 (_
   (_
-    left: (_) @collectionKey
+    left: (_) @name.end
+  ) @_.domain
+) @_.iteration
+
+;;!! not mode: command
+;;!  ^^^^^^^^---------
+;;!! slap: key("end enter")
+;;!  ^^^^------------------
+(_
+  (_
+    modifiers: (_)? @collectionKey.start
+    left: (_) @collectionKey.end
+  ) @_.domain
+) @_.iteration
+
+;;!! not mode: command
+;;!  ---------x^^^^^^^
+;;!! slap: key("end enter")
+;;!  -----x^^^^^^^^^^^^^^^^
+(_
+  (_
     right: (_) @value
   ) @_.domain
 ) @_.iteration
