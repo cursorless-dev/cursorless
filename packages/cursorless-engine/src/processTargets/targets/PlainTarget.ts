8265c3e0312180b9b0146eaf870efc9b3826b8a8
Josh Bleecher Snyder <josharian@gmail.com>
2023-07-12 17:12:16 -0700

    fix typo in comment

--- packages/cursorless-engine/src/processTargets/targets/PlainTarget.ts
+++ packages/cursorless-engine/src/processTargets/targets/PlainTarget.ts
@@ -5,7 +5,7 @@ interface PlainTargetParameters extends CommonTargetParameters {
 }
 
 /**
- * A target that has no leading or trailing delimiters so it's removal range
+ * A target that has no leading or trailing delimiters so its removal range
  * just consists of the content itself. Its insertion delimiter is empty string.
  */
 export default class PlainTarget extends BaseTarget<PlainTargetParameters> {
