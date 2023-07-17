b3d9018d97f976a48df6ccbe7b50c8855479d680
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-07-17 17:36:00 +0100

    Add the scope visualizer

---
+++ cursorless-talon/src/scope_visualizer.py
@@ -0,0 +1,48 @@
+from talon import Module, app
+
+from .csv_overrides import init_csv_and_watch_changes
+from .cursorless_command_server import run_rpc_command_no_wait
+
+mod = Module()
+mod.list("cursorless_show_scope_visualizer", desc="Show scope visualizer")
+mod.list("cursorless_hide_scope_visualizer", desc="Hide scope visualizer")
+mod.list(
+    "cursorless_visualization_type",
+    desc='Cursorless visualization type, e.g. "removal" or "iteration"',
+)
+
+# NOTE: Please do not change these dicts.  Use the CSVs for customization.
+# See https://www.cursorless.org/docs/user/customization/
+visualization_types = {
+    "removal": "removal",
+    "iteration": "iteration",
+}
+
+
+@mod.action_class
+class Actions:
+    def private_cursorless_show_scope_visualizer(
+        scope_type: dict, visualization_type: str
+    ):
+        """Shows scope visualizer"""
+        run_rpc_command_no_wait(
+            "cursorless.showScopeVisualizer", scope_type, visualization_type
+        )
+
+    def private_cursorless_hide_scope_visualizer():
+        """Hides scope visualizer"""
+        run_rpc_command_no_wait("cursorless.hideScopeVisualizer")
+
+
+def on_ready():
+    init_csv_and_watch_changes(
+        "scope_visualizer",
+        {
+            "show_scope_visualizer": {"visualize": "showScopeVisualizer"},
+            "hide_scope_visualizer": {"visualize nothing": "hideScopeVisualizer"},
+            "visualization_type": visualization_types,
+        },
+    )
+
+
+app.register("ready", on_ready)
---
+++ packages/cursorless-vscode/src/ide/vscode/VSCodeScopeVisualizer/VscodeScopeRenderer.ts
@@ -0,0 +1,75 @@
+import {
+  Disposable,
+  GeneralizedRange,
+  isGeneralizedRangeEqual,
+} from "@cursorless/common";
+import { VscodeTextEditorImpl } from "../VscodeTextEditorImpl";
+import { RangeTypeColors } from "./RangeTypeColors";
+import { VscodeFancyRangeHighlighter } from "./VscodeFancyRangeHighlighter";
+import { blendRangeTypeColors } from "./blendRangeTypeColors";
+
+export interface RendererScope {
+  domain: GeneralizedRange;
+  nestedRanges: GeneralizedRange[];
+}
+
+/**
+ * Responsible for rendering scopes, as used by {@link VscodeScopeVisualizer}.
+ * Includes a hack where we color blend domain and nested ranges that are
+ * identical, to reduce load on VSCode renderer and to work around some
+ * glitchiness.
+ */
+export class VscodeScopeRenderer implements Disposable {
+  private domainHighlighter: VscodeFancyRangeHighlighter;
+  private nestedRangeHighlighter: VscodeFancyRangeHighlighter;
+  /**
+   * A highlighter that blends domain and nested range colors when they have
+   * identical ranges
+   */
+  private domainEqualsNestedHighlighter: VscodeFancyRangeHighlighter;
+
+  constructor(
+    domainColors: RangeTypeColors,
+    nestedRangeColors: RangeTypeColors,
+  ) {
+    this.domainHighlighter = new VscodeFancyRangeHighlighter(domainColors);
+    this.nestedRangeHighlighter = new VscodeFancyRangeHighlighter(
+      nestedRangeColors,
+    );
+    this.domainEqualsNestedHighlighter = new VscodeFancyRangeHighlighter(
+      blendRangeTypeColors(domainColors, nestedRangeColors),
+    );
+  }
+
+  setScopes(editor: VscodeTextEditorImpl, scopes: RendererScope[]) {
+    const domainRanges: GeneralizedRange[] = [];
+    const allNestedRanges: GeneralizedRange[] = [];
+    const domainEqualsNestedRanges: GeneralizedRange[] = [];
+
+    for (const { domain, nestedRanges } of scopes) {
+      if (
+        nestedRanges.length === 1 &&
+        isGeneralizedRangeEqual(nestedRanges[0], domain)
+      ) {
+        domainEqualsNestedRanges.push(domain);
+        continue;
+      }
+
+      domainRanges.push(domain);
+      allNestedRanges.push(...nestedRanges);
+    }
+
+    this.domainHighlighter.setRanges(editor, domainRanges);
+    this.nestedRangeHighlighter.setRanges(editor, allNestedRanges);
+    this.domainEqualsNestedHighlighter.setRanges(
+      editor,
+      domainEqualsNestedRanges,
+    );
+  }
+
+  dispose(): void {
+    this.domainHighlighter.dispose();
+    this.nestedRangeHighlighter.dispose();
+    this.domainEqualsNestedHighlighter.dispose();
+  }
+}
