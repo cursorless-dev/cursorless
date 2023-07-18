d967c541f259137f6fd3074f821dbeded3a6d678
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-07-18 12:25:39 +0100

    Add `ScopeRangeProvider` (#1650)
    - Required by #1653 
    - Depends on #1647 
    
    ## Checklist
    
    - [ ] I have added
    [tests](https://www.cursorless.org/docs/contributing/test-case-recorder/)
    - [ ] I have updated the
    [docs](https://github.com/cursorless-dev/cursorless/tree/main/docs) and
    [cheatsheet](https://github.com/cursorless-dev/cursorless/tree/main/cursorless-talon/src/cheatsheet)
    - [ ] I have not broken the cheatsheet

---
+++ packages/cursorless-engine/src/api/CursorlessEngineApi.ts
@@ -0,0 +1,30 @@
+import { Command, HatTokenMap, IDE } from "@cursorless/common";
+import { Snippets } from "../core/Snippets";
+import { StoredTargetMap } from "../core/StoredTargets";
+import { TestCaseRecorder } from "../testCaseRecorder/TestCaseRecorder";
+import { ScopeProvider } from "./ScopeProvider";
+
+export interface CursorlessEngine {
+  commandApi: CommandApi;
+  scopeProvider: ScopeProvider;
+  testCaseRecorder: TestCaseRecorder;
+  storedTargets: StoredTargetMap;
+  hatTokenMap: HatTokenMap;
+  snippets: Snippets;
+  injectIde: (ide: IDE | undefined) => void;
+  runIntegrationTests: () => Promise<void>;
+}
+
+export interface CommandApi {
+  /**
+   * Runs a command.  This is the core of the Cursorless engine.
+   * @param command The command to run
+   */
+  runCommand(command: Command): Promise<unknown>;
+
+  /**
+   * Designed to run commands that come directly from the user.  Ensures that
+   * the command args are of the correct shape.
+   */
+  runCommandSafe(...args: unknown[]): Promise<unknown>;
+}
