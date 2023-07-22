69af417196e1d45702f712a326d5a0386004c257
Josh Bleecher Snyder <josharian@gmail.com>
2023-07-22 07:18:19 -0700

    snap again

--- queries/go.scm
+++ queries/go.scm
@@ -117,21 +117,33 @@
 (
   (composite_literal
     body:
-      (literal_value
-        ["," "{"] @collectionKey.leading.start.after
-        .
-        (keyed_element
-          (_) @collectionKey @value.leading.start.after @collectionKey.leading.end.before @collectionKey.trailing.start.after
-          ":" 
-          (_) @value @value.leading.end.before @value.trailing.start.after @collectionKey.trailing.end.before
-        ) @value.domain @collectionKey.domain
-        .
-        ["," "}"] @value.trailing.end.before
-      ) @collectionKey.iteration @value.iteration
+;; import go_key_value.scm
   ) @_comp_lit
   (#not-parent-type? @_comp_lit unary_expression)
 )
 @map
+
+;; T{a: 1}
+;;(
+;;  (composite_literal
+;;    body:
+;;      (literal_value
+;;        (keyed_element
+;;          ( 
+;;            (_) @collectionKey @value.leading.start 
+;;            (#end-position! @value.leading.start)
+;;          )
+;;          ":"
+;;          (
+;;            (_) @value @value.leading.end
+;;            (#start-position! @value.leading.end)
+;;          )
+;;        ) @value.domain @collectionKey.domain
+;;      ) @collectionKey.iteration @value.iteration
+;;  ) @_comp_lit
+;;  (#not-parent-type? @_comp_lit unary_expression)
+;;)
+;;@map
 
 ;; {a: 1}
 (
