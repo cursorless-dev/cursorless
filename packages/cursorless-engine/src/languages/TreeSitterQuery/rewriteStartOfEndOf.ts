d04cbbb91c3f6a2a4c08b834749955b9782279b5
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-08-02 11:11:45 +0100

    Update packages/cursorless-engine/src/languages/TreeSitterQuery/rewriteStartOfEndOf.ts

--- packages/cursorless-engine/src/languages/TreeSitterQuery/rewriteStartOfEndOf.ts
+++ packages/cursorless-engine/src/languages/TreeSitterQuery/rewriteStartOfEndOf.ts
@@ -1,7 +1,9 @@
 import { MutableQueryCapture } from "./QueryCapture";
 
 /**
- * Rewrite captures, absorbing .startOf and .endOf into ranges.
+ * Modifies captures by applying any `.startOf` or `.endOf` suffixes. For
+ * example, if we have a capture `@value.startOf`, we would rename it to
+ * `@value` and adjust the range to be the start of the original range.
  *
  * @param captures A list of captures
  * @returns rewritten captures, with .startOf and .endOf removed
