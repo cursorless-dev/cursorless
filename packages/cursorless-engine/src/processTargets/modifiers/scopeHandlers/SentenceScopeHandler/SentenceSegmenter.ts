9e44a170f2d35a1cacfe5cd7c649da0752b36b27
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-08-09 14:48:05 +0100

    Update comment

--- packages/cursorless-engine/src/processTargets/modifiers/scopeHandlers/SentenceScopeHandler/SentenceSegmenter.ts
+++ packages/cursorless-engine/src/processTargets/modifiers/scopeHandlers/SentenceScopeHandler/SentenceSegmenter.ts
@@ -6,11 +6,12 @@ const leadingOffsetRegex = /\S*\p{L}/u;
 /**
  * This regex is used to split the text that comes back from sbd. Anything
  * matching this regex will be discarded from the returned sentence, and split
- * the sentence in two if it occurs in the middle of a sentence.
- * 1. Lines with no letters.
- * 2. Lines ending with [.!?].
- * FIX ME: Remove second term once bug is fixed.
- * https://github.com/cursorless-dev/cursorless/issues/1753
+ * the sentence in two if it occurs in the middle of a sentence. The regex
+ * matches either of the following:
+ * 1. An entire line containing no letters.
+ * 2. If a line ends in `[.!?]`, possibly followed by whitespace, we split on
+ *    that. This is a workaround for #1753. FIXME: Remove this second term once
+ *    #1753 is fixed.
  */
 const skipPartRegex = /(\r?\n[^\p{L}]*\r?\n)|(?<=[.!?])(\s*\r?\n)/gu;
 
