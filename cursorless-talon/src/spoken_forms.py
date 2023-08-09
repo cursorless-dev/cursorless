063ca35db83a9434a26e7c03ae208993282940c8
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-08-09 12:14:30 +0100

    docstring

--- cursorless-talon/src/spoken_forms.py
+++ cursorless-talon/src/spoken_forms.py
@@ -22,15 +22,30 @@ P = ParamSpec("P")
 R = TypeVar("R")
 
 
-def get_defaults_from_file(
+def auto_construct_defaults(
     spoken_forms: dict[str, dict[str, dict[str, str]]],
     f: Callable[Concatenate[str, dict[str, dict[str, str]], P], R],
 ):
-    def wrapper(filename: str, *args: P.args, **kwargs: P.kwargs) -> R:
+    """
+    Decorator that automatically constructs the default values for the
+    `default_values` parameter of `f` based on the spoken forms in
+    `spoken_forms`, by extracting the value at the key given by the csv
+    filename.
+
+    Note that we only ever pass `init_csv_and_watch_changes` as `f`. The
+    reason we have this decorator is so that we can destructure the kwargs
+    of `init_csv_and_watch_changes` to remove the `default_values` parameter.
+
+    Args:
+        spoken_forms (dict[str, dict[str, dict[str, str]]]): The spoken forms
+        f (Callable[Concatenate[str, dict[str, dict[str, str]], P], R]): Will always be `init_csv_and_watch_changes`
+    """
+
+    def ret(filename: str, *args: P.args, **kwargs: P.kwargs) -> R:
         default_values = spoken_forms[filename]
         return f(filename, default_values, *args, **kwargs)
 
-    return wrapper
+    return ret
 
 
 def update():
@@ -42,44 +57,44 @@ def update():
     with open(JSON_FILE) as file:
         spoken_forms = json.load(file)
 
-    watch_csv = get_defaults_from_file(spoken_forms, init_csv_and_watch_changes)
+    handle_csv = auto_construct_defaults(spoken_forms, init_csv_and_watch_changes)
 
     disposables = [
-        watch_csv("actions.csv"),
-        watch_csv("target_connectives.csv"),
-        watch_csv("modifiers.csv"),
-        watch_csv("positions.csv"),
-        watch_csv("paired_delimiters.csv"),
-        watch_csv("special_marks.csv"),
-        watch_csv("scope_visualizer.csv"),
-        watch_csv("experimental/experimental_actions.csv"),
-        watch_csv("experimental/miscellaneous.csv"),
-        watch_csv(
+        handle_csv("actions.csv"),
+        handle_csv("target_connectives.csv"),
+        handle_csv("modifiers.csv"),
+        handle_csv("positions.csv"),
+        handle_csv("paired_delimiters.csv"),
+        handle_csv("special_marks.csv"),
+        handle_csv("scope_visualizer.csv"),
+        handle_csv("experimental/experimental_actions.csv"),
+        handle_csv("experimental/miscellaneous.csv"),
+        handle_csv(
             "modifier_scope_types.csv",
             pluralize_lists=["scope_type"],
         ),
-        watch_csv(
+        handle_csv(
             "experimental/wrapper_snippets.csv",
             allow_unknown_values=True,
             default_list_name="wrapper_snippet",
         ),
-        watch_csv(
+        handle_csv(
             "experimental/insertion_snippets.csv",
             allow_unknown_values=True,
             default_list_name="insertion_snippet_no_phrase",
         ),
-        watch_csv(
+        handle_csv(
             "experimental/insertion_snippets_single_phrase.csv",
             allow_unknown_values=True,
             default_list_name="insertion_snippet_single_phrase",
         ),
-        watch_csv(
+        handle_csv(
             "experimental/actions_custom.csv",
             headers=[SPOKEN_FORM_HEADER, "VSCode command"],
             allow_unknown_values=True,
             default_list_name="custom_action",
         ),
-        watch_csv(
+        handle_csv(
             "experimental/regex_scope_types.csv",
             headers=[SPOKEN_FORM_HEADER, "Regex"],
             allow_unknown_values=True,
