d7f10e75e459f2e5b0ff3d96b8d008dd80a593a1
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-08-14 23:05:21 +0100

    cleanup

--- cursorless-talon/src/public_api/extract_decorated_marks.py
+++ cursorless-talon/src/public_api/extract_decorated_marks.py
@@ -14,52 +14,34 @@ from ..targets.target_types import (
 
 
 def extract_decorated_marks(capture: Any) -> list[dict]:
-    if isinstance(capture, ListTarget):
-        return [
-            mark
-            for target in capture.elements
-            for mark in extract_decorated_marks(target)
-        ]
-
-    if isinstance(capture, RangeTarget):
-        return extract_decorated_marks(capture.anchor) + extract_decorated_marks(
-            capture.active
-        )
-
-    if isinstance(capture, PrimitiveTarget):
-        return extract_decorated_marks_from_primitive_target(capture)
-
-    if isinstance(capture, ImplicitTarget):
-        return []
-
-    if isinstance(capture, ListDestination):
-        return [
-            mark
-            for destination in capture.destinations
-            for mark in extract_decorated_marks(destination)
-        ]
-
-    if isinstance(capture, PrimitiveDestination):
-        return extract_decorated_marks(capture.target)
-
-    if isinstance(capture, ImplicitDestination):
-        return []
-
-    if isinstance(capture, BringMoveTargets):
-        return extract_decorated_marks(capture.source) + extract_decorated_marks(
-            capture.destination
-        )
-
-    if isinstance(capture, SwapTargets):
-        return extract_decorated_marks(capture.target1) + extract_decorated_marks(
-            capture.target2
-        )
-
-    raise TypeError(f"Unknown capture type: {type(capture)}")
-
-
-def extract_decorated_marks_from_primitive_target(target: PrimitiveTarget):
-    if target.mark is None or target.mark["type"] != "decoratedSymbol":
-        return []
-
-    return [target.mark]
+    match capture:
+        case PrimitiveTarget(mark=mark):
+            if mark is None or mark["type"] != "decoratedSymbol":
+                return []
+            return [mark]
+        case ImplicitTarget():
+            return []
+        case RangeTarget(anchor=anchor, active=active):
+            return extract_decorated_marks(anchor) + extract_decorated_marks(active)
+        case ListTarget(elements=elements):
+            return [
+                mark
+                for target in elements
+                for mark in extract_decorated_marks(target)
+            ]
+        case PrimitiveDestination(target=target):
+            return extract_decorated_marks(target)
+        case ImplicitDestination():
+            return []
+        case ListDestination(destinations=destinations):
+            return [
+                mark
+                for destination in destinations
+                for mark in extract_decorated_marks(destination)
+            ]
+        case BringMoveTargets(source=source, destination=destination):
+            return extract_decorated_marks(source) + extract_decorated_marks(destination)
+        case SwapTargets(target1=target1, target2=target2):
+            return extract_decorated_marks(target1) + extract_decorated_marks(target2)
+        case _:
+            raise TypeError(f"Unknown capture type: {type(capture)}")
