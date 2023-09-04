74ac7ee2208895e6ab4e81876665d62d861d12b2
River Tae Smith <22485304+r-tae@users.noreply.github.com>
2023-09-04 22:00:27 +1000

    Address comments and format elixir.scm

--- queries/elixir.scm
+++ queries/elixir.scm
@@ -1,41 +1,60 @@
 (comment) @comment
 (anonymous_function) @anonymousFunction
-(arguments) @arguments_list
+
 (call
-  target: (_) @functionCallee) @functionCall @functionCallee.domain
+  target: (_) @functionCallee
+) @functionCall @functionCallee.domain
 
-;;(keywords
-;;  (pair
-;;    key: (_) @collectionKey @collectionItem.trailing.end.startOf
-;;    value: (_) @value) @collectionItem @collectionItem.trailing.start.endOf
-;;    (",")? @collectionItem.trailing.start.startOf
-;;    (#allow-multiple! @collectionItem))
+(keywords
+  (pair
+    key: (_) @collectionKey @collectionItem.trailing.end.startOf
+    value: (_) @value
+  ) @collectionItem @collectionItem.trailing.start.endOf
+  (
+    ","
+  )
+?
+ @collectionItem.trailing.start.startOf
+  (#allow-multiple! @collectionItem)
+)
 
-;; How does surrounding whitespace work for this
+;; %{:a => "lorem", "b" => "ipsum", 3 => "dolor"},
 (map_content
+  (_)? @_.leading.start.endOf
+  .
   (binary_operator
-      left: (_) @collectionKey @collectionKey.trailing.start.endOf
-      right: (_) @value @collectionKey.trailing.end.startOf) @collectionItem
-      ","? @collectionItem.trailing
-      (#shrink-to-match! @collectionItem "([^,]+)"))
+    left: (_) @collectionKey
+    right: (_) @value
+  ) @collectionItem @collectionKey.domain @value.domain @_.leading.end.startOf @_.trailing.start.endOf
+  .
+  (_)? @_.trailing.end.startOf
+  (#insertion-delimiter! @collectionItem ", ")
+)
 
 (call
-  target: (identifier) @ignore
+  target: (identifier) @_target
   (arguments
     [
       ; zero-arity functions with no parentheses
       (identifier) @functionName
       ; regular function clause
-      (call target: (identifier) @functionName)
+      (call
+        target: (identifier) @functionName
+      )
       ; function clause with a guard clause
       (binary_operator
-        left: (call target: (identifier) @functionName)
-        operator: "when")
-    ])
-    (do_block
-      .
-      "do" @namedFunction.interior.start.endOf
-      .
-      "end" @namedFunction.interior.end.startOf
-      .)?
-  (#match? @ignore "^(def|defp|defdelegate|defguard|defguardp|defmacro|defmacrop|defn|defnp)$"))  @namedFunction @functionName.domain
+        left: (call
+          target: (identifier) @functionName
+        )
+        operator: "when"
+      )
+    ]
+  )
+  (do_block
+    .
+    "do" @namedFunction.interior.start.endOf
+    "end" @namedFunction.interior.end.startOf
+    .
+  )?
+  (#match? @_target "^(def|defp|defdelegate|defguard|defguardp|defmacro|defmacrop|defn|defnp)$")
+) @namedFunction @functionName.domain
