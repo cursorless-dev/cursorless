2df85de19b1f9166bf511782f7511e4574a165ff
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-09-14 11:50:01 +0200

    Revert

--- packages/common/src/extensionDependencies.ts
+++ packages/common/src/extensionDependencies.ts
@@ -1,6 +1,11 @@
 export const extensionDependencies = [
   "pokey.parse-tree",
 
+  // Register necessary file extensions for tests
+  "scalameta.metals",
+  "mrob95.vscode-talonscript",
+  "jrieken.vscode-tree-sitter-query",
+
   // Necessary for the `drink cell` and `pour cell` tests
   "ms-toolsai.jupyter",
 ];
