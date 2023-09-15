52fc129ffd8d753b5733080c9242f297b8f7f10e
River Tae Smith <22485304+r-tae@users.noreply.github.com>
2023-09-16 00:32:46 +1000

    Add comment explaining elixir-ls dependency

--- packages/common/src/extensionDependencies.ts
+++ packages/common/src/extensionDependencies.ts
@@ -1,11 +1,11 @@
 export const extensionDependencies = [
   "pokey.parse-tree",
-
   // Register necessary file extensions for tests
   "scalameta.metals",
   "mrob95.vscode-talonscript",
   "jrieken.vscode-tree-sitter-query",
+  // necessary for Elixir tests
+  "jakebecker.elixir-ls",
   // Necessary for the `drink cell` and `pour cell` tests
   "ms-toolsai.jupyter",
-  "jakebecker.elixir-ls",
 ];
