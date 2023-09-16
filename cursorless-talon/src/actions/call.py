f942ff023173f7eaf13573c8026346c520fba1b5
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-09-16 15:35:31 +0200

    Update cursorless-talon/src/actions/call.py
    Co-authored-by: David Vo <auscompgeek@users.noreply.github.com>

--- cursorless-talon/src/actions/call.py
+++ cursorless-talon/src/actions/call.py
@@ -11,7 +11,7 @@ mod.list("cursorless_call_action", desc="Cursorless call action")
 class Actions:
     def private_cursorless_call(
         callee: CursorlessTarget,
-        argument: Optional[CursorlessTarget] = ImplicitTarget(),
+        argument: CursorlessTarget = ImplicitTarget(),
     ):
         """Execute Cursorless call action"""
         actions.user.private_cursorless_command_and_wait(
