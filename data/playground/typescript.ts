762366e3ea06a914c494c41d43441b71bad5abd7
Josh Bleecher Snyder <josharian@gmail.com>
2023-09-17 11:04:18 -0700

    give up on the typescript playground stuff

--- data/playground/typescript.ts
+++ data/playground/typescript.ts
@@ -1,14 +1,3 @@
 function helloWorld() {
   const value = "value";
 }
-
-function ifElseChain() {
-  const x = true;
-  if (x) {
-    return;
-  } else if (x) {
-    return;
-  } else {
-    return;
-  }
-}
