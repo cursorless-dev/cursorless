fcca9aedafc276d79593c911cca34a873d1b80c1
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-09-28 16:36:41 +0200

    Added argument to call action (#1900)
    `call air on bat`
    
    ## Checklist
    
    - [x] I have added
    [tests](https://www.cursorless.org/docs/contributing/test-case-recorder/)
    - [x] I have updated the
    [docs](https://github.com/cursorless-dev/cursorless/tree/main/docs) and
    [cheatsheet](https://github.com/cursorless-dev/cursorless/tree/main/cursorless-talon/src/cheatsheet)
    - [x] I have not broken the cheatsheet
    
    ---------
    
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>
    Co-authored-by: David Vo <auscompgeek@users.noreply.github.com>
    Co-authored-by: Pokey Rule <755842+pokey@users.noreply.github.com>

--- src/actions/call.py
+++ src/actions/call.py
@@ -1,13 +1,22 @@
-from talon import actions
+from talon import Module, actions
 
 from ..targets.target_types import CursorlessTarget, ImplicitTarget
 
+mod = Module()
+mod.list("cursorless_call_action", desc="Cursorless call action")
 
-def cursorless_call_action(target: CursorlessTarget):
-    actions.user.private_cursorless_command_and_wait(
-        {
-            "name": "callAsFunction",
-            "callee": target,
-            "argument": ImplicitTarget(),
-        }
-    )
+
+@mod.action_class
+class Actions:
+    def private_cursorless_call(
+        callee: CursorlessTarget,
+        argument: CursorlessTarget = ImplicitTarget(),
+    ):
+        """Execute Cursorless call action"""
+        actions.user.private_cursorless_command_and_wait(
+            {
+                "name": "callAsFunction",
+                "callee": callee,
+                "argument": argument,
+            }
+        )
