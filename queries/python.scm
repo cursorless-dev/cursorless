e9d20558e3a056c1b567d228bcdfa8c340b00171
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-10-02 14:47:21 +0100

    Migrate Python name and value scopes

--- queries/python.scm
+++ queries/python.scm
@@ -38,14 +38,74 @@
   right: (_) @value @_.leading.end.startOf
 ) @_.domain
 
+;; value:
 ;;!! a /= 25
 ;;!       ^^
 ;;!   xxxxxx
 ;;!  -------
+;; name:
+;;!! a /= 25
+;;!  ^
+;;!  xxxxx
+;;!  -------
 (augmented_assignment
+  left: (_) @name @name.trailing.start.endOf @value.leading.start.endOf
+  right: (_) @value @value.leading.end.startOf @name.trailing.end.startOf
+) @_.domain
+
+;;!! a = 25
+;;!  ^
+;;!  xxxx
+;;!  ------
+;;!! a: int = 25
+;;!  ^
+;;!  xxxxxxxxx
+;;!  -----------
+(assignment
+  left: (_) @name @name.trailing.start.endOf
+  right: (_)? @name.trailing.end.startOf
+) @_.domain
+
+(_
+  name: (_) @name
+) @_.domain
+
+;;!! def aaa(bbb):
+;;!          ^^^
+(parameters
+  (identifier) @name
+)
+
+;;!! def aaa(bbb: str):
+;;!          ^^^
+;;!          --------
+(typed_parameter
+  .
+  (_) @name
+) @_.domain
+
+;; Matches any node at field `type` of its parent, with leading delimiter until
+;; previous named node. For example:
+;;!! aaa: str = "bbb";
+;;!       ^^^
+;;!  -----------------
+;;!     xxxxx
+(_
   (_) @_.leading.start.endOf
   .
-  right: (_) @value @_.leading.end.startOf
+  type: (_) @type @_.leading.end.startOf
+) @_.domain
+
+;;!!  def aaa() -> str:
+;;!                ^^^
+;;!            xxxxxxx
+;;!  [-----------------
+;;!!      pass
+;;!   --------]
+(function_definition
+  (_) @_.leading.start.endOf
+  .
+  return_type: (_) @type @_.leading.end.startOf
 ) @_.domain
 
 ;;!! d = {"a": 1234}
@@ -127,7 +187,7 @@
 ) @class @className.domain
 
 (module) @className.iteration @class.iteration
-(module) @statement.iteration
+(module) @statement.iteration @value.iteration @name.iteration
 (module) @namedFunction.iteration @functionName.iteration
 (class_definition) @namedFunction.iteration @functionName.iteration
 
@@ -138,7 +198,8 @@
 ;;!      *****
 ;;!!     c = 2
 ;;!      *****>
-(block) @statement.iteration @value.iteration
+(block) @statement.iteration @value.iteration @name.iteration
+(block) @type.iteration
 
 ;;!! {"a": 1, "b": 2, "c": 3}
 ;;!   **********************
@@ -150,6 +211,6 @@
 ;;!! def func(a=0, b=1):
 ;;!           ********
 (parameters
-  "(" @value.iteration.start.endOf
-  ")" @value.iteration.end.startOf
+  "(" @value.iteration.start.endOf @name.iteration.start.endOf @type.iteration.start.endOf
+  ")" @value.iteration.end.startOf @name.iteration.end.startOf @type.iteration.end.startOf
 )
