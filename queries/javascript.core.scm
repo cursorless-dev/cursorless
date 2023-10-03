1164e2f3775e0f0026a60877ca6b676507d45c0d
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-10-03 11:00:09 +0100

    Migrate Typescript "value" to next gen; improve "name" (#1924)
    ## Checklist
    
    - [ ] I have added
    [tests](https://www.cursorless.org/docs/contributing/test-case-recorder/)
    - [ ] I have updated the
    [docs](https://github.com/cursorless-dev/cursorless/tree/main/docs) and
    [cheatsheet](https://github.com/cursorless-dev/cursorless/tree/main/cursorless-talon/src/cheatsheet)
    - [ ] I have not broken the cheatsheet

--- queries/javascript.core.scm
+++ queries/javascript.core.scm
@@ -34,21 +34,94 @@
 ;; this full grandparent statement.
 (
   [
+    ;; name:
     ;;!! (const | let) foo = ...;
-    ;;!  --------------^^^-------
+    ;;!                ^^^
+    ;;!  xxxxxxxxxxxxxxxxxxxx
+    ;;!  ------------------------
+    ;; value:
+    ;;!! (const | let) foo = ...;
+    ;;!                      ^^^
+    ;;!                   xxxxxx
+    ;;!  ------------------------
     (lexical_declaration
+      .
       (variable_declarator
-        name: (_) @name
+        name: (_) @name @name.removal.end.endOf @value.leading.start.endOf
+        value: (_)? @value @name.removal.end.startOf @value.leading.end.startOf
       )
     )
 
+    ;; name:
     ;;!! var foo = ...;
-    ;;!  ----^^^-------
+    ;;!      ^^^
+    ;;!  xxxxxxxxxx
+    ;;!  --------------
+    ;; value:
+    ;;!! var foo = ...;
+    ;;!            ^^^
+    ;;!         xxxxxx
+    ;;!  --------------
     ;; Note that we can't merge this with the variable declaration above because
     ;; of https://github.com/tree-sitter/tree-sitter/issues/1442#issuecomment-1584628651
     (variable_declaration
+      .
       (variable_declarator
-        name: (_) @name
+        name: (_) @name @name.removal.end.endOf @value.leading.start.endOf
+        value: (_)? @value @name.removal.end.startOf @value.leading.end.startOf
+      )
+    )
+  ] @_.domain @name.removal.start.startOf
+  (#not-parent-type? @_.domain export_statement)
+
+  ;; Handle multiple variable declarators in one statement, eg
+  ;;!! (let | const | var) aaa = ..., ccc = ...;
+  ;;!  --------------------^^^--------^^^-------
+  (#allow-multiple! @name)
+  (#allow-multiple! @value)
+)
+
+;; Note that the same domains below will also have the first variable
+;; as a target, but that is matched above
+(
+  [
+    ;; name:
+    ;;!! (const | let) aaa = 0, bbb = 0;
+    ;;!                         ^^^
+    ;;!                         xxxxxx
+    ;;!  -------------------------------
+    ;; value:
+    ;;!! (const | let) aaa = 0, bbb = 0;
+    ;;!                               ^
+    ;;!                            xxxx
+    ;;!  -------------------------------
+    (lexical_declaration
+      (variable_declarator)
+      .
+      (variable_declarator
+        name: (_) @name @name.trailing.start.endOf @value.leading.start.endOf
+        value: (_)? @value @name.trailing.end.startOf @value.leading.end.startOf
+      )
+    )
+
+    ;; name:
+    ;;!! var aaa = 0, bbb = 0;
+    ;;!               ^^^
+    ;;!               xxxxxx
+    ;;!  ---------------------
+    ;; value:
+    ;;!! var aaa = 0, bbb = 0;
+    ;;!                     ^
+    ;;!                  xxxx
+    ;;!  ---------------------
+    ;; Note that we can't merge this with the variable declaration above because
+    ;; of https://github.com/tree-sitter/tree-sitter/issues/1442#issuecomment-1584628651
+    (variable_declaration
+      (variable_declarator)
+      .
+      (variable_declarator
+        name: (_) @name @name.trailing.start.endOf @value.leading.start.endOf
+        value: (_)? @value @name.trailing.end.startOf @value.leading.end.startOf
       )
     )
   ] @_.domain
@@ -58,15 +131,25 @@
   ;;!! (let | const | var) aaa = ..., ccc = ...;
   ;;!  --------------------^^^--------^^^-------
   (#allow-multiple! @name)
+  (#allow-multiple! @value)
 )
 
 (
   (export_statement
     (_
-      ;;!! export [default] (let | const | var) foo = ...;
-      ;;!  -------------------------------------^^^-------
+      ;; name:
+      ;;!! export (const | let | var) foo = ...;
+      ;;!                             ^^^
+      ;;!                             xxxxxx
+      ;;!  -------------------------------------
+      ;; value:
+      ;;!! export (const | let | var) foo = 0;
+      ;;!                                   ^
+      ;;!                                xxxx
+      ;;!  -----------------------------------
       (variable_declarator
-        name: (_) @name
+        name: (_) @name @name.trailing.start.endOf @value.leading.start.endOf
+        value: (_)? @value @name.trailing.end.startOf @value.leading.end.startOf
       )
     )
   ) @_.domain
@@ -75,24 +158,162 @@
   ;;!! var foo = ..., bar = ...;
   ;;!  ----^^^--------^^^-------
   (#allow-multiple! @name)
+  (#allow-multiple! @value)
 )
 
-;;!! foo += ...;
-;;!  ^^^--------
-(augmented_assignment_expression
-  left: (_) @name
+;; name:
+;;!! (const | let | var) aaa = 0, bbb = 0;
+;;!                      ^^^
+;;!  xxxxxxxxxxxxxxxxxxxxxxxxxx
+;;!                      -------
+;; value:
+;;!! (const | let | var) aaa = 0, bbb = 0;
+;;!                            ^
+;;!                         xxxx
+;;!                      -------
+(
+  (_
+    .
+    (variable_declarator
+      name: (_) @name @name.removal.end.endOf @value.leading.start.endOf
+      value: (_)? @value @name.removal.end.startOf @value.leading.end.startOf
+    ) @_.domain
+    .
+    (variable_declarator)
+  ) @dummy @name.removal.start.startOf
+)
+
+;; name:
+;;!! (const | let | var) aaa = 0, bbb = 0;
+;;!1                              ^^^
+;;!1                              xxxxxx
+;;!1                              -------
+;; value:
+;;!! (const | let | var) aaa = 0, bbb = 0;
+;;!1                                    ^
+;;!1                                 xxxx
+;;!1                              -------
+(_
+  (variable_declarator)
+  .
+  (variable_declarator
+    name: (_) @name @name.trailing.start.endOf @value.leading.start.endOf
+    value: (_)? @value @name.trailing.end.startOf @value.leading.end.startOf
+  ) @_.domain
+) @dummy
+
+(expression_statement
+  [
+    ;; name:
+    ;;!! foo = 0;
+    ;;!  ^^^
+    ;;!  xxxxxx
+    ;;!  --------
+    ;; value:
+    ;;!! foo = 0;
+    ;;!        ^
+    ;;!     xxxx
+    ;;!  --------
+    (assignment_expression
+      left: (_) @name @value.leading.start.endOf @name.trailing.start.endOf
+      right: (_) @value @name.trailing.end.startOf @value.leading.end.startOf
+    )
+
+    ;; name:
+    ;;!! foo += 1;
+    ;;!  ^^^
+    ;;!  xxxxxxx
+    ;;!  ---------
+    ;; value:
+    ;;!! foo += 1;
+    ;;!         ^
+    ;;!     xxxxx
+    ;;!  ---------
+    (augmented_assignment_expression
+      left: (_) @name @value.leading.start.endOf @name.trailing.start.endOf
+      right: (_) @value @name.trailing.end.startOf @value.leading.end.startOf
+    )
+  ]
 ) @_.domain
 
-;;!! foo = ...;
-;;!  ^^^-------
-(assignment_expression
-  left: (_) @name
+(
+  [
+    ;; name:
+    ;;!! aaa = 0, bbb = 0;
+    ;;!1 ^^^
+    ;;!1 xxxxxx
+    ;;!1 -------
+    ;;!2          ^^^
+    ;;!2          xxxxxx
+    ;;!2          -------
+    ;; value:
+    ;;!! aaa = 0, bbb = 0;
+    ;;!1       ^
+    ;;!1    xxxx
+    ;;!1 -------
+    ;;!2                ^
+    ;;!2             xxxx
+    ;;!2          -------
+    (assignment_expression
+      left: (_) @name @value.leading.start.endOf @name.trailing.start.endOf
+      right: (_) @value @name.trailing.end.startOf @value.leading.end.startOf
+    )
+
+    ;; name:
+    ;;!! aaa += 0, bbb += 0;
+    ;;!1 ^^^
+    ;;!1 xxxxxxx
+    ;;!1 --------
+    ;;!2           ^^^
+    ;;!2           xxxxxxx
+    ;;!2           --------
+    ;; value:
+    ;;!! aaa += 0, bbb += 0;
+    ;;!1        ^
+    ;;!1    xxxxx
+    ;;!1 --------
+    ;;!2                  ^
+    ;;!2              xxxxx
+    ;;!2           --------
+    (augmented_assignment_expression
+      left: (_) @name @value.leading.start.endOf @name.trailing.start.endOf
+      right: (_) @value @name.trailing.end.startOf @value.leading.end.startOf
+    )
+  ] @_.domain
+
+  (#not-parent-type? @_.domain expression_statement)
+)
+
+;; Match nodes at field `value` of their parent node, setting leading delimiter
+;; to be the range until the previous named node
+(_
+  (_)? @value.leading.start.endOf
+  .
+  value: (_) @value @value.leading.end.startOf
+) @_.domain
+
+;;!! const aaa = {bbb};
+;;!               ^^^
+(shorthand_property_identifier) @value
+
+;;!! return 0;
+;;!         ^
+;;!  ---------
+(return_statement
+  (_) @value
+) @_.domain
+
+;;!! yield 0;
+;;!        ^
+;;!  --------
+(yield_expression
+  (_) @value
 ) @_.domain
 
 [
   (program)
   (formal_parameters)
-] @name.iteration
+] @name.iteration @value.iteration
 
 ;; Treat interior of all bodies as iteration scopes for `name`, eg
 ;;!! function foo() {   }
@@ -100,8 +321,15 @@
 (_
   body: (_
     .
-    "{" @name.iteration.start.endOf
-    "}" @name.iteration.end.startOf
+    "{" @name.iteration.start.endOf @value.iteration.start.endOf
+    "}" @name.iteration.end.startOf @value.iteration.end.startOf
     .
   )
+)
+
+;;!! const aaa = {bbb: 0, ccc: 0};
+;;!               **************
+(object
+  "{" @value.iteration.start.endOf
+  "}" @value.iteration.end.startOf
 )
