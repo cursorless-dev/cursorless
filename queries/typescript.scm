34c0813416bde3d5a8cba990f4d06fc47c35f832
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-10-03 13:14:25 +0100

    support type assertions

--- queries/typescript.scm
+++ queries/typescript.scm
@@ -1,1 +1,14 @@
 ;; import typescript.core.scm
+
+;; Define this here because type assertions don't exist in TSX because
+;; they are not valid JSX.
+;;!! <Aaa>bbb;
+;;!  ^^^^^
+(type_assertion
+  (type_arguments
+    [
+      (generic_type)
+      (predefined_type)
+    ] @type
+  ) @_.removal
+) @_.domain
