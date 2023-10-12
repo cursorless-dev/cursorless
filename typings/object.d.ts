644f49dd5a7732ae544b0b93804d87e6c7eb3176
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-10-12 16:21:46 +0100

    Add stronger typing for `Object.fromEntries`

--- typings/object.d.ts
+++ typings/object.d.ts
@@ -9,4 +9,24 @@ type ObjectKeys<T> = T extends object
 
 interface ObjectConstructor {
   keys<T>(o: T): ObjectKeys<T>;
+
+  fromEntries<
+    V extends PropertyKey,
+    T extends [readonly [V, any]] | Array<readonly [V, any]>,
+  >(
+    entries: T,
+  ): Flatten<UnionToIntersection<FromEntries<T[number]>>>;
 }
+
+// From https://github.com/microsoft/TypeScript/issues/35745#issuecomment-566932289
+type UnionToIntersection<T> = (T extends T ? (p: T) => void : never) extends (
+  p: infer U,
+) => void
+  ? U
+  : never;
+type FromEntries<T extends readonly [PropertyKey, any]> = T extends T
+  ? Record<T[0], T[1]>
+  : never;
+type Flatten<T> = object & {
+  [P in keyof T]: T[P];
+};
