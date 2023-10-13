24225520828e0647512725de5c5dc1fe8b183d93
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-10-13 15:38:43 +0100

    Add spam-proof email links

---
+++ packages/cursorless-org/src/components/SpamProofEmailLink.tsx
@@ -0,0 +1,47 @@
+import { EmailAddress } from "../pages/parseEmailAddress";
+
+interface Props {
+  address: EmailAddress;
+}
+
+/**
+ * Encodes a string for use in a URL, but unlike encodeURIComponent, it encodes
+ * every character, including regular ASCII characters [a-zA-Z] etc. For example:
+ *
+ *     encodeURIComponent("user@example.com") === "%75%73%65%72%40%65%78%61%6D%70%6C%65%2E%63%6F%6D"
+ *
+ * @param str The string to encode
+ * @returns A URL-encoded version of the string, where every character is encoded
+ */
+function strictEncodeURIComponent(str: string) {
+  const components: string[] = [];
+  for (let i = 0; i < str.length; i++) {
+    components.push("%" + str.charCodeAt(i).toString(16).toUpperCase());
+  }
+  return components.join("");
+}
+
+/**
+ * A link to an email address, attempting to prevent spam bots from finding it.
+ * Encodes the URI for the href using very aggressive uri encoding, and for the
+ * displayed email text, uses a hidden span to prevent bots from finding it.
+ *
+ * Tricks taken from https://spencermortensen.com/articles/email-obfuscation/
+ *
+ * @param param0 The email address to use
+ * @returns A link to the email address, attempting to prevent spam bots from
+ *         finding it
+ */
+export function SpamProofEmailLink({ address: { username, domain } }: Props) {
+  // URL encode every character of the email address, including the mailto: prefix
+  const rawEmailHref = `${username}@${domain}`;
+  const href = `mailto:${strictEncodeURIComponent(rawEmailHref)}`;
+
+  return (
+    <a href={href}>
+      {`${username}@`}
+      <span className="hidden">Die spam!</span>
+      {domain}
+    </a>
+  );
+}
--- packages/cursorless-org/src/pages/enablement-group.tsx
+++ packages/cursorless-org/src/pages/enablement-group.tsx
@@ -3,22 +3,38 @@ import {
   meta,
 } from "../content/enablement-group.mdx";
 import { Layout, bodyClasses } from "../components/Layout";
+import { env } from "process";
+import { parseEmailAddress, EmailAddress } from "./parseEmailAddress";
 
 const RELATIVE_URL = "cursorless-enablement";
 
 // See https://github.com/vercel/next.js/discussions/12325#discussioncomment-1116108
 export async function getStaticProps() {
-  return { props: { bodyClasses } };
+  return {
+    props: {
+      bodyClasses,
+      //! IMPORTANT: Don't return the email address unparsed, because it will
+      //! be serialized as JSON and exposed to the client, so spam bots might
+      //! find it. Instead, parse it and return the parsed object.
+      enablementGroupEmail: parseEmailAddress(
+        env["ENABLEMENT_GROUP_EMAIL"] ?? "user@example.com",
+      ),
+    },
+  };
 }
 
-export default function Page() {
+interface Props extends React.PropsWithChildren {
+  enablementGroupEmail: EmailAddress;
+}
+
+export default function Page({ enablementGroupEmail }: Props) {
   return (
     <Layout
       title={meta.title}
       description={meta.description}
       relativeUrl={RELATIVE_URL}
     >
-      <EnablementGroup />
+      <EnablementGroup enablementGroupEmail={enablementGroupEmail} />
     </Layout>
   );
 }
