78beb77728260383cb3403e60fbdc912d0e7f078
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-10-17 15:13:20 +0100

    Doc string

--- packages/common/src/util/camelCaseToAllDown.ts
+++ packages/common/src/util/camelCaseToAllDown.ts
@@ -1,3 +1,13 @@
+/**
+ * Converts a camelCase string to a string with spaces between each word, and
+ * all words in lowercase.
+ *
+ * Example: `camelCaseToAllDown("fooBarBaz")` returns `"foo bar baz"`.
+ *
+ * @param input A camelCase string
+ * @returns The same string, but with spaces between each word, and all words
+ * in lowercase
+ */
 export function camelCaseToAllDown(input: string): string {
   return input
     .replace(/([A-Z])/g, " $1")
