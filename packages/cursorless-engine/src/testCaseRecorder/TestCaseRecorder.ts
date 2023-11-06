39fa0cb9fc45689676873c53195e5d0c1922f467
Barry Jaspan <bjaspan@google.com>
2023-11-06 09:31:51 -0500

    Add "new" TestCaseRecorder.ts file (old one was renamed).

---
+++ packages/cursorless-engine/src/testCaseRecorder/TestCaseRecorder.ts
@@ -0,0 +1,24 @@
+import { DecoratedSymbolMark, ReadOnlyHatMap } from "@cursorless/common";
+import { CommandRunner } from "../CommandRunner";
+import { RecordTestCaseCommandOptions } from "./RecordTestCaseCommandOptions";
+
+/**
+ * Used for recording test cases
+ */
+export interface TestCaseRecorder {
+  toggle: (options?: RecordTestCaseCommandOptions) => Promise<{ startTimestampISO: string; } | null | undefined>;
+  recordOneThenPause: (options?: RecordTestCaseCommandOptions) => Promise<{ startTimestampISO: string; } | null | undefined>;
+  pause: () => Promise<void>;
+  resume: () => Promise<void>;
+  takeSnapshot: (
+    outPath: string,
+    metadata: unknown,
+    targetedMarks: DecoratedSymbolMark[],
+    usePrePhraseSnapshot: boolean,
+  ) => Promise<void>;
+  isActive: () => boolean;
+  wrapCommandRunner(
+    readableHatMap: ReadOnlyHatMap,
+    runner: CommandRunner,
+  ): CommandRunner;
+}
