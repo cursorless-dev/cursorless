638dca411b84ddc7899371190cf38890d4b960fe
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-11-07 13:11:42 +0000

    generate tmp file instead of using process substitution

--- packages/common/scripts/my-ts-node.sh
+++ packages/common/scripts/my-ts-node.sh
@@ -16,8 +16,21 @@ fi
 # Check if DEBUG is set and add sourcemaps if it is
 SOURCEMAP_FLAG=""
 if [ -n "${DEBUG:-}" ]; then
-  SOURCEMAP_FLAG="--sourcemap=inline"
+  SOURCEMAP_FLAG="--sourcemap"
 fi
 
+# Generate tmp dir for output and source map
+tmpdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'my-ts-node')
+function finish {
+  rm -rf "$tmpdir"
+}
+trap finish EXIT
+
+outfile="$tmpdir/out.js"
+
+echo "Bundling $FILE_TO_RUN to $outfile"
+
+esbuild $SOURCEMAP_FLAG --conditions=cursorless:bundler --bundle --platform=node "${FILE_TO_RUN}" --outfile="${outfile}"
+
 # Bundle the TypeScript file using esbuild and execute it with node, passing any additional arguments to node
-node <(esbuild $SOURCEMAP_FLAG --conditions=cursorless:bundler --bundle --platform=node "${FILE_TO_RUN}") "$@"
+node "$outfile" "$@"
