37c64c8e1c1796e1c66c547a95bfd5c0bfd6ee26
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-11-09 19:02:05 +0000

    More tweaks

--- packages/cheatsheet-local/tailwind.config.js
+++ packages/cheatsheet-local/tailwind.config.js
@@ -1,12 +1,7 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-/*eslint-env node*/
-
-import { join } from "path";
 import { readFileSync } from "fs";
 
 const references = JSON.parse(
-  readFileSync(join(__dirname, "tsconfig.json"), "utf-8"),
+  readFileSync("tsconfig.json", "utf-8"),
 ).references.map((ref) => ref.path);
 
 export const content = [".", ...references].map(
--- packages/cursorless-org-docs/babel.config.mjs
+++ packages/cursorless-org-docs/babel.config.mjs
@@ -1,3 +1,3 @@
-module.exports = {
+export default {
   presets: ["@docusaurus/core/lib/babel/preset"],
 };
