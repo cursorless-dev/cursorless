e8592823d22f6eda9459c059f0c7b301e48f004c
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-11-09 18:57:08 +0000

    more esm

--- packages/cheatsheet-local/tailwind.config.js
+++ packages/cheatsheet-local/tailwind.config.js
@@ -2,19 +2,17 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 /*eslint-env node*/
 
-const { join } = require("path");
-const { readFileSync } = require("fs");
+import { join } from "path";
+import { readFileSync } from "fs";
 
 const references = JSON.parse(
   readFileSync(join(__dirname, "tsconfig.json"), "utf-8"),
 ).references.map((ref) => ref.path);
 
-module.exports = {
-  content: [".", ...references].map(
-    (dir) => `${dir}/src/**/*!(*.stories|*.spec).{ts,tsx,html}`,
-  ),
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+export const content = [".", ...references].map(
+  (dir) => `${dir}/src/**/*!(*.stories|*.spec).{ts,tsx,html}`,
+);
+export const theme = {
+  extend: {},
 };
+export const plugins = [];
