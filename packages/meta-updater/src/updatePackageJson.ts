d614db2ae5733c2c315ca3ff3c0a0e1495efc1a5
Pokey Rule <755842+pokey@users.noreply.github.com>
2023-11-09 19:55:00 +0000

    tweak

--- packages/meta-updater/src/updatePackageJson.ts
+++ packages/meta-updater/src/updatePackageJson.ts
@@ -182,7 +182,7 @@ async function getTransformRecordedTestsScript(packageDir: string) {
   }
 
   const transformRecordedTestsScript =
-    "CURSORLESS_REPO_ROOT=. " +
+    "cross-env CURSORLESS_REPO_ROOT=. " +
     formatRecordedTestsEntry.slice("pnpm exec ".length);
 
   return transformRecordedTestsScript;
