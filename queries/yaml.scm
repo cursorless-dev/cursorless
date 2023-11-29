3f0fcd4deeef9cb9a475925b9ae213d3c26bd370
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-11-29 15:42:10 +0100

    Improve item

--- queries/yaml.scm
+++ queries/yaml.scm
@@ -1,13 +1,56 @@
-;;!! foo: bar
-;;!  ^^^  ^^^
+;; ;;!! foo: bar
+;; ;;!  ^^^  ^^^
 (_
     key: (_) @collectionKey @collectionKey.trailing.start.endOf @value.leading.start.endOf
     value: (_) @value @collectionKey.trailing.start.startOf @value.leading.start.startOf
-) @collectionItem @_.domain
+) @_.domain
 
-(block_mapping) @collectionItem.iteration @collectionKey.iteration @value.iteration
+;;!! foo: bar
+;;!  ^^^^^^^^
+(block_mapping
+    (block_mapping_pair)? @collectionItem.leading.start.endOf
+    .
+    (block_mapping_pair) @collectionItem @collectionItem.leading.end.startOf @collectionItem.trailing.end.endOf
+    .
+    (block_mapping_pair)? @collectionItem.trailing.end.startOf
+) @map
 
-(flow_pair) @collectionItem
+;;!! - 0
+;;!  ^^^
+(block_sequence
+    (block_sequence_item)? @collectionItem.leading.start.endOf
+    .
+    (block_sequence_item) @collectionItem @collectionItem.leading.end.startOf @collectionItem.trailing.end.endOf
+    .
+    (block_sequence_item)? @collectionItem.trailing.end.startOf
+) @list
+
+;;!! [0]
+;;!   ^
+(flow_sequence
+    (flow_node)? @collectionItem.leading.start.endOf
+    .
+    (flow_node) @collectionItem @collectionItem.leading.end.startOf @collectionItem.trailing.end.endOf
+    .
+    (flow_node)? @collectionItem.trailing.end.startOf
+) @list
+
+;;!! { foo: bar }
+;;!    ^^^^^^^^
+(flow_mapping
+    (flow_pair)? @collectionItem.leading.start.endOf
+    .
+    (flow_pair) @collectionItem @collectionItem.leading.end.startOf @collectionItem.trailing.end.endOf
+    .
+    (flow_pair)? @collectionItem.trailing.end.startOf
+) @map
+
+[
+    (block_mapping)
+    (block_sequence)
+    (flow_sequence)
+    (flow_mapping)
+] @collectionItem.iteration @collectionKey.iteration @value.iteration
 
 ;;!! languageId: plauintext
 ;;!              ^^^^^^^^^^
@@ -19,27 +62,3 @@
 
 ;;!! | block scalar
 (block_scalar) @textFragment
-
-;;!! { foo: bar }
-;;!  ^^^^^^^^^^^^
-(flow_mapping) @map
-(flow_mapping) @collectionItem.iteration @collectionKey.iteration @value.iteration
-
-[
-    ;;!! - 0\n- 1
-    ;;!  ^^^^^^^^
-    (block_sequence)
-    ;;!! [0, 1]
-    ;;!  ^^^^^^
-    (flow_sequence)
-] @list @collectionItem.iteration
-
-;;!! - 0
-;;!  ^^^
-(block_sequence_item) @collectionItem
-
-;;!! [0]
-;;!   ^
-(flow_sequence
-    (flow_node) @collectionItem
-)
