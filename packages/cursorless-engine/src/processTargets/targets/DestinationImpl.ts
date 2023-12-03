4dbd9f5474c864d8f27e15e7dfde50d2f9e0bba1
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-12-03 06:33:36 +0100

    cleanup

--- packages/cursorless-engine/src/processTargets/targets/DestinationImpl.ts
+++ packages/cursorless-engine/src/processTargets/targets/DestinationImpl.ts
@@ -194,10 +194,8 @@ export class DestinationImpl implements Destination {
     // Went inserting a new line with eol `CRLF` each `\n` will be converted to
     // `\r\n` and therefore the length is doubled.
     if (this.editor.document.eol === "CRLF") {
-      // This function is only called when inserting after a range. Therefore we
-      // only care about leading new lines in the insertion delimiter.
-      const match = this.insertionDelimiter.match(/\n+/);
-      const nlCount = match?.[0].length ?? 0;
+      const matches = this.insertionDelimiter.match(/\n/g);
+      const nlCount = matches?.length ?? 0;
       return this.insertionDelimiter.length + nlCount;
     }
     return this.insertionDelimiter.length;
