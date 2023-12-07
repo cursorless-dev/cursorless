1956c86d12e222112f8f4aa3f88b033307c28a7e
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-12-07 14:28:11 +0100

    remove unused typescript file

--- packages/cursorless-engine/src/languages/typescript.ts
+++
@@ -1,9 +0,0 @@
-import { SimpleScopeTypeType } from "@cursorless/common";
-import { NodeMatcherAlternative } from "../typings/Types";
-import { createPatternMatchers } from "../util/nodeMatchers";
-
-const nodeMatchers: Partial<
-  Record<SimpleScopeTypeType, NodeMatcherAlternative>
-> = {};
-
-export const patternMatchers = createPatternMatchers(nodeMatchers);
