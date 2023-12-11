3b850b027f98a30154955bb915d648d1992b724c
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-12-11 20:04:36 +0100

    Added glyph scope (#2050)
    `chuck glyph dollar red air`
    
    Fixes #54
    
    ## Checklist
    
    - [x] I have added
    [tests](https://www.cursorless.org/docs/contributing/test-case-recorder/)
    - [x] I have updated the
    [docs](https://github.com/cursorless-dev/cursorless/tree/main/docs) and
    [cheatsheet](https://github.com/cursorless-dev/cursorless/tree/main/cursorless-talon/src/cheatsheet)
    - [x] I have not broken the cheatsheet
    
    ---------
    
    Co-authored-by: Pokey Rule <755842+pokey@users.noreply.github.com>

--- src/cheatsheet/sections/scopes.py
+++ src/cheatsheet/sections/scopes.py
@@ -1,12 +1,25 @@
-from ..get_list import get_lists
+from ..get_list import get_lists, get_raw_list
 
 
 def get_scopes():
-    return get_lists(
-        ["scope_type"],
-        "scopeType",
+    complex_scopes = get_raw_list("glyph_scope_type")
+    return [
+        *get_lists(
+            ["scope_type"],
+            "scopeType",
+            {
+                "argumentOrParameter": "Argument",
+                "boundedNonWhitespaceSequence": "Non whitespace sequence stopped by surrounding pair delimeters",
+            },
+        ),
         {
-            "argumentOrParameter": "Argument",
-            "boundedNonWhitespaceSequence": "Non whitespace sequence stopped by surrounding pair delimeters",
+            "id": "glyph",
+            "type": "scopeType",
+            "variations": [
+                {
+                    "spokenForm": f"{complex_scopes['glyph']} <character>",
+                    "description": "Instance of single character <character>",
+                },
+            ],
         },
-    )
+    ]
