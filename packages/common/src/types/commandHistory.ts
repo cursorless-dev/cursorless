3d716bf8c81f2aa7ac23ae32e3366412eeb4f1fc
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-12-11 08:58:55 +0100

    Added docstring

--- packages/common/src/types/commandHistory.ts
+++ packages/common/src/types/commandHistory.ts
@@ -1,10 +1,13 @@
 import type { CommandComplete } from "./command/command.types";
 
+/**
+ * Represents a single line in a command history jsonl file.
+ */
 export interface CommandHistoryItem {
-  // eg 2023-09-05
+  // eg: "2023-09-05"
   date: string;
 
-  // eg 0.28.0-c7bcf64d
+  // eg: "0.28.0-c7bcf64d"
   cursorlessVersion: string;
 
   command: CommandComplete;
