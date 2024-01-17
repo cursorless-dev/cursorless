b6613a64dc8d3ebae8e1b08e5e8b9e8f9364a3e9
Wen Kokke <wenkokke@users.noreply.github.com>
2024-01-17 22:41:34 +0000

    take inside funk

--- queries/haskell.hs
+++ queries/haskell.hs
@@ -1,12 +1,30 @@
+{-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Redundant lambda" #-}
 module Main where
 
 -- This is a comment.
 main :: IO ()
 main = putStrLn "Hello, World!"
 
--- RFC: What should `argumentOrParameter` match?
+-- RFC: What should "arg" match?
 fst :: (a, b) -> a
 fst tup@(x, y) = x
 --  ^^^^^^^^^^     <- 1️⃣ the whole pattern
 --  ^^^            <- 2️⃣ only the name of the whole argument, if given
 --  ^^^  ^  ^      <- 3️⃣ all names in the pattern
+
+uncurry :: (a -> b -> c) -> (a, b) -> c
+uncurry f = \(x, y) -> f x y
+--          ^^^^^^^^^^^^^^^^ <- "lambda"
+--                     ^^^^^ <- "inside lambda"
+--           ^^^^^^          <- "pattern lambda"
+
+fromEither :: (a -> c) -> (b -> c) -> Either a b -> c
+fromEither f g = \case
+  Left x -> f x
+  Right y -> g y
+
+foo = bar
+  where
+    bar = 1
