30bbad8969ecf8be445b91063416e0b3364f9b00
Wen Kokke <wenkokke@users.noreply.github.com>
2024-01-17 13:45:56 +0000

    Changes

--- queries/haskell.hs
+++ queries/haskell.hs
@@ -4,3 +4,9 @@ module Main where
 main :: IO ()
 main = putStrLn "Hello, World!"
 
+-- RFC: What should `argumentOrParameter` match?
+fst :: (a, b) -> a
+fst tup@(x, y) = x
+--  ^^^^^^^^^^     <- 1️⃣ the whole pattern
+--  ^^^            <- 2️⃣ only the name of the whole argument, if given
+--  ^^^  ^  ^      <- 3️⃣ all names in the pattern
