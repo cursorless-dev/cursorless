366f02391309a708dd96213a8cc9ae2222aa8660
Jacob Egner <jmegner@gmail.com>
2024-02-25 10:47:37 -0600

    add `grand` to cheatsheet

--- cursorless-talon/src/cheatsheet/sections/modifiers.py
+++ cursorless-talon/src/cheatsheet/sections/modifiers.py
@@ -22,6 +22,7 @@ def get_modifiers():
         "extendThroughStartOf",
         "extendThroughEndOf",
         "every",
+        "grand",
         "ancestor",
         "first",
         "last",
@@ -99,6 +100,16 @@ def get_modifiers():
                 {
                     "spokenForm": f"{complex_modifiers['every']} <scope>",
                     "description": "Every instance of <scope>",
+                },
+            ],
+        },
+        {
+            "id": "grand",
+            "type": "modifier",
+            "variations": [
+                {
+                    "spokenForm": f"{complex_modifiers['grand']} <scope>",
+                    "description": "parent scope of the containing <scope>",
                 },
             ],
         },
