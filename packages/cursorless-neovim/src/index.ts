9ab745ab86913efcba8f070249be7289f4dc4336
Cedric Halbronn <cedric.halbronn@nccgroup.com>
2024-02-27 10:23:48 +0000

    demoes successful import of cursorless engine

--- packages/cursorless-neovim/src/index.ts
+++ packages/cursorless-neovim/src/index.ts
@@ -4,6 +4,7 @@
 // import { temp } from "./lib/temp";
 
 import { cursorlessCommandIds } from "@cursorless/common";
+import { createCursorlessEngine } from "@cursorless/cursorless-engine";
 
 export default function entry(plugin: any) {
   // Set your plugin to dev mode, which will cause the module to be reloaded on each invocation
@@ -15,7 +16,8 @@ export default function entry(plugin: any) {
     () => {
       console.warn("TALON");
       console.warn(cursorlessCommandIds[1]);
-      plugin.nvim.setLine(cursorlessCommandIds[1]);
+      // plugin.nvim.setLine(cursorlessCommandIds[2]);
+      plugin.nvim.setLine(createCursorlessEngine.toString().split("\n")[0]);
       // return plugin.nvim.setLine("TALON");
       // return plugin.nvim.setLine(temp[2]);
     },
