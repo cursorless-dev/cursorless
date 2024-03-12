06aae3bc5f02a79e135a924befa7628625bfea1b
FidgetingBits <fidgetingbits@users.noreply.github.com>
2024-03-12 16:02:36 +0800

    add lua dependency to see if it fixes tests

--- packages/common/src/extensionDependencies.ts
+++ packages/common/src/extensionDependencies.ts
@@ -6,6 +6,7 @@ export const extensionDependencies = [
   "scala-lang.scala", // scala
   "mrob95.vscode-talonscript", // talon
   "jrieken.vscode-tree-sitter-query", // scm
+  "sumneko.lua", // lua
 
   // Necessary for the `drink cell` and `pour cell` tests
   "ms-toolsai.jupyter",
