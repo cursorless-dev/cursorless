2bda8070f19c209b08b0c09b4b916c5fb4a30d09
Pokey Rule <755842+pokey@users.noreply.github.com>
2024-04-05 17:20:21 +0100

    Fix hot reload for dependencies of cursorless-org

--- packages/cursorless-org/next.config.js
+++ packages/cursorless-org/next.config.js
@@ -1,8 +1,27 @@
 import mdx from "@next/mdx";
+import { readFileSync } from "fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
 const withMDX = mdx({
   options: {
     providerImportSource: "@mdx-js/react",
   },
+});
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+/**
+ * The names of the packages that come from the same monorepo as this package.
+ * We want these to be transpiled by Next.js because we are directly importing
+ * the source typescript files from these packages.
+ */
+const references = JSON.parse(
+  readFileSync(join(__dirname, "tsconfig.json"), "utf-8"),
+).references.map(({ path }) => {
+  return JSON.parse(
+    readFileSync(join(__dirname, path, "package.json"), "utf-8"),
+  ).name;
 });
 
 /** @type {import('next').NextConfig} */
@@ -14,11 +33,20 @@ const nextConfig = {
       use: ["@svgr/webpack"],
     });
 
+    config.plugins.push({
+      apply(compiler) {
+        compiler.hooks.afterEnvironment.tap("NextEntryPlugin", () => {
+          compiler.options.resolve.conditionNames.push("cursorless:bundler");
+        });
+      },
+    });
+
     return config;
   },
   experimental: {
     mdxRs: true,
   },
+  transpilePackages: references,
   reactStrictMode: true,
   output: "export",
 };
