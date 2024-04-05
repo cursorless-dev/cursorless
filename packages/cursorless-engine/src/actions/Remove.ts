1ccb908c67d99a40255caea6d302721411f56ed0
Cedric Halbronn <cedric.halbronn@nccgroup.com>
2024-04-05 15:56:51 +0100

    refactor: delete old comments

--- packages/cursorless-engine/src/actions/Remove.ts
+++ packages/cursorless-engine/src/actions/Remove.ts
@@ -6,18 +6,11 @@ import {
 } from "@cursorless/common";
 import { flatten, zip } from "lodash";
 import { RangeUpdater } from "../core/updateSelections/RangeUpdater";
-import {
-  performEditsAndUpdateRanges,
-  performEditsAndUpdateSelectionsWithBehavior,
-} from "../core/updateSelections/updateSelections";
+import { performEditsAndUpdateSelectionsWithBehavior } from "../core/updateSelections/updateSelections";
 import { RawSelectionTarget } from "../processTargets/targets";
 import { ide } from "../singletons/ide.singleton";
 import { Target } from "../typings/target.types";
-import {
-  createThatMark,
-  flashTargets,
-  runOnTargetsForEachEditor,
-} from "../util/targetUtils";
+import { flashTargets, runOnTargetsForEachEditor } from "../util/targetUtils";
 import { unifyRemovalTargets } from "../util/unifyRanges";
 import { SimpleAction, ActionReturnValue } from "./actions.types";
 import { setSelectionsWithoutFocusingEditor } from "../util/setSelectionsAndFocusEditor";
@@ -49,14 +42,9 @@ export default class Delete implements SimpleAction {
   }
 
   private async runForEditor(editor: TextEditor, targets: Target[]) {
-    // range: 0:5-0:7, text: "", updateRange: updateRange
-    // [what range to remove]
     const edits = targets.map((target) => target.constructRemovalEdit());
 
-    // anchor: 0:4 => active: 0:9
-    // [original cursor selection was "world" token]
     const cursorSelections = { selections: editor.selections };
-    // anchor: 0:5 => active: 0:7 [what range to remove]
     const editSelections = {
       selections: edits.map(
         ({ range }) => new Selection(range.start, range.end),
@@ -66,32 +54,19 @@ export default class Delete implements SimpleAction {
 
     const editableEditor = ide().getEditableTextEditor(editor);
 
-    const [
-      // anchor: 0:4 => active: 0:7
-      // [new cursor selection is "wld"]
-      updatedEditorSelections,
-      // anchor: 0:5 => active: 0:5 [to be used for the "source" mark for the next command]
-      updatedEditSelections,
-    ]: Selection[][] = await performEditsAndUpdateSelectionsWithBehavior(
-      this.rangeUpdater,
-      editableEditor,
-      edits,
-      [cursorSelections, editSelections],
-    );
+    const [updatedEditorSelections, updatedEditSelections]: Selection[][] =
+      await performEditsAndUpdateSelectionsWithBehavior(
+        this.rangeUpdater,
+        editableEditor,
+        edits,
+        [cursorSelections, editSelections],
+      );
 
-    // range: 0:5-0:5?
-    // [the "that" range empty position where text was removed]
-    // const deletionRanges = zip(edits, updatedEditSelections).map(
-    //   ([edit, selection]) => edit!.updateRange(selection!),
-    // );
-
-    // update the selections in the editor
     await setSelectionsWithoutFocusingEditor(
       editableEditor,
       updatedEditorSelections,
     );
 
-    // return createThatMark(targets, updatedEditSelections);
     return zip(targets, updatedEditSelections).map(
       ([target, range]) =>
         new RawSelectionTarget({
