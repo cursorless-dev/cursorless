b9b2f5454118aa176d4299022cd16824325d55a9
Cedric Halbronn <cedric.halbronn@nccgroup.com>
2024-04-05 11:23:52 +0100

    refactor: how we run the "remove" command in order to prepare for better support for nvim.  Passed tests: 342 / 450 (failed: 108)

--- packages/cursorless-engine/src/actions/Remove.ts
+++ packages/cursorless-engine/src/actions/Remove.ts
@@ -1,4 +1,4 @@
-import { FlashStyle } from "@cursorless/common";
+import { FlashStyle, TextEditor } from "@cursorless/common";
 import { flatten, zip } from "lodash";
 import { RangeUpdater } from "../core/updateSelections/RangeUpdater";
 import { performEditsAndUpdateRanges } from "../core/updateSelections/updateSelections";
@@ -12,6 +12,7 @@ import { SimpleAction, ActionReturnValue } from "./actions.types";
 export default class Delete implements SimpleAction {
   constructor(private rangeUpdater: RangeUpdater) {
     this.run = this.run.bind(this);
+    this.runForEditor = this.runForEditor.bind(this);
   }
 
   async run(
@@ -28,28 +29,30 @@ export default class Delete implements SimpleAction {
     }
 
     const thatTargets = flatten(
-      await runOnTargetsForEachEditor(targets, async (editor, targets) => {
-        const edits = targets.map((target) => target.constructRemovalEdit());
-        const ranges = edits.map((edit) => edit.range);
-
-        const [updatedRanges] = await performEditsAndUpdateRanges(
-          this.rangeUpdater,
-          ide().getEditableTextEditor(editor),
-          edits,
-          [ranges],
-        );
-
-        return zip(targets, updatedRanges).map(
-          ([target, range]) =>
-            new RawSelectionTarget({
-              editor: target!.editor,
-              isReversed: target!.isReversed,
-              contentRange: range!,
-            }),
-        );
-      }),
+      await runOnTargetsForEachEditor(targets, this.runForEditor),
     );
 
     return { thatTargets };
+  }
+
+  private async runForEditor(editor: TextEditor, targets: Target[]) {
+    const edits = targets.map((target) => target.constructRemovalEdit());
+    const ranges = edits.map((edit) => edit.range);
+
+    const [updatedRanges] = await performEditsAndUpdateRanges(
+      this.rangeUpdater,
+      ide().getEditableTextEditor(editor),
+      edits,
+      [ranges],
+    );
+
+    return zip(targets, updatedRanges).map(
+      ([target, range]) =>
+        new RawSelectionTarget({
+          editor: target!.editor,
+          isReversed: target!.isReversed,
+          contentRange: range!,
+        }),
+    );
   }
 }
