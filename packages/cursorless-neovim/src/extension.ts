7215390957ff13becabb02660dc59d37f7eb4d72
Cedric Halbronn <cedric.halbronn@nccgroup.com>
2024-04-08 17:30:02 +0100

    refactor: disable tests missed in previous commit

--- packages/cursorless-neovim/src/extension.ts
+++ packages/cursorless-neovim/src/extension.ts
@@ -108,8 +108,8 @@ export async function activate(plugin: NvimPlugin) {
 
   // COMMENT ME IF YOU DON'T WANT TO RUN TESTS
   // WHEN USING TESTS, CHANGE neovimIDE.runMode = "test" in NeovimIDE.ts
-  console.warn("activate(): running the recorded test cases...");
-  await runRecordedTestCases();
+  // console.warn("activate(): running the recorded test cases...");
+  // await runRecordedTestCases();
   // console.warn("activate(): recorded test cases done");
   // COMMENT ME END
 
