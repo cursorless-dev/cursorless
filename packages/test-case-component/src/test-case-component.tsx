f2a6d6341e082ba9965d323dce4a637473cf05c4
Trillium Smith <spiteless@gmail.com>
2024-04-09 11:48:09 -0700

    wip: Update test-case-componet to import shiki - uses type: any for now

--- packages/test-case-component/src/test-case-component.tsx
+++ packages/test-case-component/src/test-case-component.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
+import { ShikiComponent } from './components/component-shiki'
 
-export const TestCaseComponentPage: React.FC = () => {
+export const TestCaseComponentPage: React.FC<{ data: any }> = ({ data }) => {
   return (
     <main className="dark:text-stone-100">
       <h1 className="mb-1 mt-2 text-center text-2xl md:text-3xl xl:mt-4">
@@ -13,6 +14,7 @@ export const TestCaseComponentPage: React.FC = () => {
           to learn more.
         </small>
       </h1>
+      { data.map( (item:any) => <ShikiComponent data={item} /> ) }
       <p className="mb-1 mt-2 text-center text-base xl:mt-4">This is just to get the page working with an initial render before adding any more functionality</p>
     </main>
   );
