c40fa1361a03b9ee8052caa4ed350e5a2347b1db
Cedric Halbronn <cedric.halbronn@nccgroup.com>
2024-04-18 11:38:15 +0100

    feat: simplified the neovim registry

--- packages/neovim-registry/src/index.ts
+++ packages/neovim-registry/src/index.ts
@@ -1,23 +1,5 @@
-import { NvimPlugin } from "neovim";
 import { NeovimRegistry } from "./NeovimRegistry";
-import { injectRegistry, registry } from "./singletons/registry.singleton";
-
-export default function entry(plugin: NvimPlugin) {
-  plugin.setOptions({ dev: false });
-
-  plugin.registerFunction(
-    "RegistryLoadExtension",
-    async () => await loadExtension(plugin),
-    { sync: false },
-  );
-}
-
-async function loadExtension(plugin: NvimPlugin) {
-  console.warn("loadExtension(neovim-registry): start");
-  const registry = new NeovimRegistry();
-  injectRegistry(registry);
-  console.warn("loadExtension(neovim-registry): done");
-}
+import { registry } from "./singletons/registry.singleton";
 
 export function getNeovimRegistry(): NeovimRegistry {
   return registry();
