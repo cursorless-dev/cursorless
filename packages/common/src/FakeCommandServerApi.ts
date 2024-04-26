cursorless-dev/cursorless

commit 1c18476c85c4e7e2b75da90780f515f48de79e53

Fallback to Talon actions when focus is not on the text editor (#2235)
Edit operations supported by community will now work in vscode outside
of the text editor. eg the search widget
`take line`
`chuck token`

Everything appears to be working when I have tested it. With that said I
have not tested on community and we should probably have a discussion
about some of the finer details of this.


## Checklist

- [x] I have added
[tests](https://www.cursorless.org/docs/contributing/test-case-recorder/)
- [ ] I have updated the
[docs](https://github.com/cursorless-dev/cursorless/tree/main/docs) and
[cheatsheet](https://github.com/cursorless-dev/cursorless/tree/main/cursorless-talon/src/cheatsheet)
- [x] I have not broken the cheatsheet

---------

Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>
Co-authored-by: Pokey Rule <755842+pokey@users.noreply.github.com>

packages/common/src/FakeCommandServerApi.ts
@@ -1,0 +1,25 @@
+import {
+  CommandServerApi,
+  FocusedElementType,
+  InboundSignal,
+} from "./types/CommandServerApi";
+
+export class FakeCommandServerApi implements CommandServerApi {
+  private focusedElementType: FocusedElementType | undefined;
+  signals: { prePhrase: InboundSignal };
+
+  constructor() {
+    this.signals = { prePhrase: { getVersion: async () => null } };
+    this.focusedElementType = "textEditor";
+  }
+
+  getFocusedElementType(): FocusedElementType | undefined {
+    return this.focusedElementType;
+  }
+
+  setFocusedElementType(
+    focusedElementType: FocusedElementType | undefined,
+  ): void {
+    this.focusedElementType = focusedElementType;
+  }
+}

packages/common/src/types/CommandServerApi.ts
@@ -2,10 +2,14 @@
  * API object for interacting with the command server
  */
 export interface CommandServerApi {
+  getFocusedElementType: () => FocusedElementType | undefined;
+
   signals: {
     prePhrase: InboundSignal;
   };
 }
+
+export type FocusedElementType = "textEditor" | "terminal";
 
 export interface InboundSignal {
   getVersion(): Promise<string | null>;

packages/common/src/types/TestCaseFixture.ts
@@ -1,6 +1,6 @@
-import { Command, CommandLatest } from "..";
-import { TestCaseSnapshot } from "../testUtil/TestCaseSnapshot";
-import { PlainSpyIDERecordedValues } from "../testUtil/spyToPlainObject";
+import type { Command, CommandLatest, Fallback, FocusedElementType } from "..";
+import type { TestCaseSnapshot } from "../testUtil/TestCaseSnapshot";
+import type { PlainSpyIDERecordedValues } from "../testUtil/spyToPlainObject";
 
 export type ThrownError = {
   name: string;
@@ -11,6 +11,11 @@ interface TestCaseFixtureBase {
   postEditorOpenSleepTimeMs?: number;
   postCommandSleepTimeMs?: number;
   spokenFormError?: string;
+
+  /**
+   * The type of element that is focused before the command is executed. If undefined default to text editor.
+   */
+  focusedElementType?: FocusedElementType | "other";
 
   /**
    * A list of marks to check in the case of navigation map test otherwise undefined
@@ -30,6 +35,11 @@ interface TestCaseFixtureBase {
    * error test case.
    */
   returnValue?: unknown;
+
+  /**
+   * The fallback of the command. Will be undefined if the command was executed by the extension.
+   */
+  fallback?: Fallback;
 }
 
 export interface TestCaseFixture extends TestCaseFixtureBase {

packages/cursorless-engine/src/core/getCommandFallback.ts
@@ -1,0 +1,166 @@
+import {
+  ActionDescriptor,
+  CommandComplete,
+  CommandServerApi,
+  DestinationDescriptor,
+  Fallback,
+  FallbackModifier,
+  PartialTargetDescriptor,
+} from "@cursorless/common";
+import type { ActionReturnValue } from "../actions/actions.types";
+
+export async function getCommandFallback(
+  commandServerApi: CommandServerApi | null,
+  runAction: (actionDescriptor: ActionDescriptor) => Promise<ActionReturnValue>,
+  command: CommandComplete,
+): Promise<Fallback | null> {
+  if (
+    commandServerApi == null ||
+    commandServerApi.getFocusedElementType() === "textEditor"
+  ) {
+    return null;
+  }
+
+  const action = command.action;
+
+  switch (action.name) {
+    case "replace":
+      return destinationIsSelection(action.destination) &&
+        Array.isArray(action.replaceWith)
+        ? {
+            action: "insert",
+            modifiers: getModifiersFromDestination(action.destination),
+            text: action.replaceWith.join("\n"),
+          }
+        : null;
+
+    case "replaceWithTarget":
+      if (destinationIsSelection(action.destination)) {
+        return {
+          action: "insert",
+          modifiers: getModifiersFromDestination(action.destination),
+          text: await getText(runAction, action.source),
+        };
+      }
+      return null;
+
+    case "moveToTarget":
+      if (destinationIsSelection(action.destination)) {
+        const text = await getText(runAction, action.source);
+        await remove(runAction, action.source);
+        return {
+          action: "insert",
+          modifiers: getModifiersFromDestination(action.destination),
+          text,
+        };
+      }
+      return null;
+
+    case "callAsFunction":
+      if (targetIsSelection(action.argument)) {
+        return {
+          action: action.name,
+          modifiers: getModifiersFromTarget(action.argument),
+          callee: await getText(runAction, action.callee),
+        };
+      }
+      return null;
+
+    case "wrapWithPairedDelimiter":
+    case "rewrapWithPairedDelimiter":
+      return targetIsSelection(action.target)
+        ? {
+            action: action.name,
+            modifiers: getModifiersFromTarget(action.target),
+            left: action.left,
+            right: action.right,
+          }
+        : null;
+
+    case "pasteFromClipboard":
+      return destinationIsSelection(action.destination)
+        ? {
+            action: action.name,
+            modifiers: getModifiersFromDestination(action.destination),
+          }
+        : null;
+
+    case "swapTargets":
+    case "editNew":
+    case "insertSnippet":
+    case "generateSnippet":
+    case "wrapWithSnippet":
+      return null;
+
+    default:
+      return targetIsSelection(action.target)
+        ? {
+            action: action.name,
+            modifiers: getModifiersFromTarget(action.target),
+          }
+        : null;
+  }
+}
+
+function destinationIsSelection(destination: DestinationDescriptor): boolean {
+  if (destination.type === "implicit") {
+    return true;
+  }
+  if (destination.type === "primitive") {
+    return (
+      destination.insertionMode === "to" &&
+      targetIsSelection(destination.target)
+    );
+  }
+  return false;
+}
+
+function targetIsSelection(target: PartialTargetDescriptor): boolean {
+  if (target.type === "implicit") {
+    return true;
+  }
+  if (target.type === "primitive") {
+    return target.mark == null || target.mark.type === "cursor";
+  }
+  return false;
+}
+
+function getModifiersFromDestination(
+  destination: DestinationDescriptor,
+): FallbackModifier[] {
+  if (destination.type === "primitive") {
+    return getModifiersFromTarget(destination.target);
+  }
+  return [];
+}
+
+function getModifiersFromTarget(
+  target: PartialTargetDescriptor,
+): FallbackModifier[] {
+  if (target.type === "primitive") {
+    if (target.modifiers != null && target.modifiers.length > 0) {
+      return target.modifiers;
+    }
+
+    if (target.mark?.type === "cursor") {
+      return [{ type: "containingTokenIfEmpty" }];
+    }
+  }
+  return [];
+}
+
+async function getText(
+  runAction: (actionDescriptor: ActionDescriptor) => Promise<ActionReturnValue>,
+  target: PartialTargetDescriptor,
+): Promise<string> {
+  const response = await runAction({ name: "getText", target });
+  const texts = response.returnValue as string[];
+  return texts.join("\n");
+}
+
+async function remove(
+  runAction: (actionDescriptor: ActionDescriptor) => Promise<ActionReturnValue>,
+  target: PartialTargetDescriptor,
+): Promise<void> {
+  await runAction({ name: "remove", target });
+}

packages/cursorless-engine/src/testCaseRecorder/TestCase.ts
@@ -1,9 +1,12 @@
 import {
   ActionType,
   CommandLatest,
+  CommandResponse,
   EnforceUndefined,
   extractTargetedMarks,
   ExtraSnapshotField,
+  Fallback,
+  FocusedElementType,
   marksToPlainObject,
   PartialTargetDescriptor,
   PlainSpyIDERecordedValues,
@@ -32,6 +35,7 @@ export class TestCase {
   private finalState?: TestCaseSnapshot;
   thrownError?: ThrownError;
   private returnValue?: unknown;
+  private fallback?: Fallback;
   private targetKeys: string[];
   private _awaitingFinalMarkInfo: boolean;
   private marksToCheck?: string[];
@@ -40,6 +44,7 @@ export class TestCase {
 
   constructor(
     command: CommandLatest,
+    private focusedElementType: FocusedElementType | undefined,
     private hatTokenMap: ReadOnlyHatMap,
     private storedTargets: StoredTargetMap,
     private spyIde: SpyIDE,
@@ -135,6 +140,10 @@ export class TestCase {
     }
     const fixture: EnforceUndefined<TestCaseFixture> = {
       languageId: this.languageId,
+      focusedElementType:
+        this.focusedElementType !== "textEditor"
+          ? this.focusedElementType ?? "other"
+          : undefined,
       postEditorOpenSleepTimeMs: undefined,
       postCommandSleepTimeMs: undefined,
       command: this.command,
@@ -143,6 +152,7 @@ export class TestCase {
       initialState: this.initialState,
       finalState: this.finalState,
       returnValue: this.returnValue,
+      fallback: this.fallback,
       thrownError: this.thrownError,
       ide: this.spyIdeValues,
     };
@@ -162,9 +172,16 @@ export class TestCase {
     );
   }
 
-  async recordFinalState(returnValue: unknown) {
+  async recordFinalState(returnValue: CommandResponse) {
     const excludeFields = this.getExcludedFields(false);
-    this.returnValue = returnValue;
+
+    if ("returnValue" in returnValue) {
+      this.returnValue = returnValue.returnValue;
+    }
+    if ("fallback" in returnValue) {
+      this.fallback = returnValue.fallback;
+    }
+
     this.finalState = await takeSnapshot(
       this.storedTargets,
       excludeFields,

packages/cursorless-vscode-e2e/src/suite/recorded.vscode.test.ts
@@ -1,8 +1,10 @@
 import {
   asyncSafety,
+  CommandResponse,
   DEFAULT_TEXT_EDITOR_OPTIONS_FOR_TEST,
   ExcludableSnapshotField,
   extractTargetedMarks,
+  Fallback,
   getRecordedTestPaths,
   HatStability,
   marksToPlainObject,
@@ -22,6 +24,7 @@ import {
   TestCaseFixtureLegacy,
   TextEditor,
   TokenHat,
+  clientSupportsFallback,
 } from "@cursorless/common";
 import {
   getCursorlessApi,
@@ -30,11 +33,11 @@ import {
 } from "@cursorless/vscode-common";
 import { assert } from "chai";
 import * as yaml from "js-yaml";
+import { isUndefined } from "lodash";
 import { promises as fsp } from "node:fs";
 import * as vscode from "vscode";
 import { endToEndTestSetup, sleepWithBackoff } from "../endToEndTestSetup";
 import { setupFake } from "./setupFake";
-import { isUndefined } from "lodash";
 
 function createPosition(position: PositionPlainObject) {
   return new vscode.Position(position.line, position.character);
@@ -74,7 +77,7 @@ async function runTest(file: string, spyIde: SpyIDE) {
   const usePrePhraseSnapshot = false;
 
   const cursorlessApi = await getCursorlessApi();
-  const { hatTokenMap, takeSnapshot, setStoredTarget } =
+  const { hatTokenMap, takeSnapshot, setStoredTarget, commandServerApi } =
     cursorlessApi.testHelpers!;
 
   const editor = await openNewEditor(fixture.initialState.documentContents, {
@@ -101,6 +104,12 @@ async function runTest(file: string, spyIde: SpyIDE) {
     // spyIde.clipboard.writeText(fixture.initialState.clipboard);
   }
 
+  commandServerApi.setFocusedElementType(
+    fixture.focusedElementType === "other"
+      ? undefined
+      : fixture.focusedElementType ?? "textEditor",
+  );
+
   // Ensure that the expected hats are present
   await hatTokenMap.allocateHats(
     getTokenHats(fixture.initialState.marks, spyIde.activeTextEditor!),
@@ -112,12 +121,22 @@ async function runTest(file: string, spyIde: SpyIDE) {
   checkMarks(fixture.initialState.marks, readableHatMap);
 
   let returnValue: unknown;
+  let fallback: Fallback | undefined;
 
   try {
     returnValue = await runCursorlessCommand({
       ...fixture.command,
       usePrePhraseSnapshot,
     });
+    if (clientSupportsFallback(fixture.command)) {
+      const commandResponse = returnValue as CommandResponse;
+      returnValue =
+        "returnValue" in commandResponse
+          ? commandResponse.returnValue
+          : undefined;
+      fallback =
+        "fallback" in commandResponse ? commandResponse.fallback : undefined;
+    }
   } catch (err) {
     const error = err as Error;
 
@@ -188,6 +207,7 @@ async function runTest(file: string, spyIde: SpyIDE) {
       ...fixture,
       finalState: resultState,
       returnValue,
+      fallback,
       ide: actualSpyIdeValues,
       thrownError: undefined,
     };
@@ -210,6 +230,12 @@ async function runTest(file: string, spyIde: SpyIDE) {
       returnValue,
       fixture.returnValue,
       "Unexpected return value",
+    );
+
+    assert.deepStrictEqual(
+      fallback,
+      fixture.fallback,
+      "Unexpected fallback value",
     );
 
     assert.deepStrictEqual(

cursorless-talon/src/command.py
@@ -3,10 +3,13 @@ from typing import Any
 
 from talon import Module, actions, speech_system
 
+from .fallback import perform_fallback
+from .versions import COMMAND_VERSION
+
 
 @dataclasses.dataclass
 class CursorlessCommand:
-    version = 6
+    version = COMMAND_VERSION
     spokenForm: str
     usePrePhraseSnapshot: bool
     action: dict
@@ -30,10 +33,12 @@ speech_system.register("pre:phrase", on_phrase)
 class Actions:
     def private_cursorless_command_and_wait(action: dict):
         """Execute cursorless command and wait for it to finish"""
-        actions.user.private_cursorless_run_rpc_command_and_wait(
+        response = actions.user.private_cursorless_run_rpc_command_get(
             CURSORLESS_COMMAND_ID,
             construct_cursorless_command(action),
         )
+        if "fallback" in response:
+            perform_fallback(response["fallback"])
 
     def private_cursorless_command_no_wait(action: dict):
         """Execute cursorless command without waiting"""
@@ -44,10 +49,15 @@ class Actions:
 
     def private_cursorless_command_get(action: dict):
         """Execute cursorless command and return result"""
-        return actions.user.private_cursorless_run_rpc_command_get(
+        response = actions.user.private_cursorless_run_rpc_command_get(
             CURSORLESS_COMMAND_ID,
             construct_cursorless_command(action),
         )
+        if "fallback" in response:
+            return perform_fallback(response["fallback"])
+        if "returnValue" in response:
+            return response["returnValue"]
+        return None
 
 
 def construct_cursorless_command(action: dict) -> dict:

cursorless-talon/src/fallback.py
@@ -1,0 +1,107 @@
+from typing import Callable
+
+from talon import actions
+
+from .versions import COMMAND_VERSION
+
+# This ensures that we remember to update fallback if the response payload changes
+assert COMMAND_VERSION == 7
+
+action_callbacks = {
+    "getText": lambda: [actions.edit.selected_text()],
+    "setSelection": actions.skip,
+    "setSelectionBefore": actions.edit.left,
+    "setSelectionAfter": actions.edit.right,
+    "copyToClipboard": actions.edit.copy,
+    "cutToClipboard": actions.edit.cut,
+    "pasteFromClipboard": actions.edit.paste,
+    "clearAndSetSelection": actions.edit.delete,
+    "remove": actions.edit.delete,
+    "editNewLineBefore": actions.edit.line_insert_up,
+    "editNewLineAfter": actions.edit.line_insert_down,
+}
+
+modifier_callbacks = {
+    "extendThroughStartOf.line": actions.user.select_line_start,
+    "extendThroughEndOf.line": actions.user.select_line_end,
+    "containingScope.document": actions.edit.select_all,
+    "containingScope.paragraph": actions.edit.select_paragraph,
+    "containingScope.line": actions.edit.select_line,
+    "containingScope.token": actions.edit.select_word,
+}
+
+
+def call_as_function(callee: str):
+    wrap_with_paired_delimiter(f"{callee}(", ")")
+
+
+def wrap_with_paired_delimiter(left: str, right: str):
+    selected = actions.edit.selected_text()
+    actions.insert(f"{left}{selected}{right}")
+    for _ in right:
+        actions.edit.left()
+
+
+def containing_token_if_empty():
+    if actions.edit.selected_text() == "":
+        actions.edit.select_word()
+
+
+def perform_fallback(fallback: dict):
+    try:
+        modifier_callbacks = get_modifier_callbacks(fallback)
+        action_callback = get_action_callback(fallback)
+        for callback in reversed(modifier_callbacks):
+            callback()
+        return action_callback()
+    except ValueError as ex:
+        actions.app.notify(str(ex))
+
+
+def get_action_callback(fallback: dict) -> Callable:
+    action = fallback["action"]
+
+    if action in action_callbacks:
+        return action_callbacks[action]
+
+    match action:
+        case "insert":
+            return lambda: actions.insert(fallback["text"])
+        case "callAsFunction":
+            return lambda: call_as_function(fallback["callee"])
+        case "wrapWithPairedDelimiter":
+            return lambda: wrap_with_paired_delimiter(
+                fallback["left"], fallback["right"]
+            )
+
+    raise ValueError(f"Unknown Cursorless fallback action: {action}")
+
+
+def get_modifier_callbacks(fallback: dict) -> list[Callable]:
+    return [get_modifier_callback(modifier) for modifier in fallback["modifiers"]]
+
+
+def get_modifier_callback(modifier: dict) -> Callable:
+    modifier_type = modifier["type"]
+
+    match modifier_type:
+        case "containingTokenIfEmpty":
+            return containing_token_if_empty
+        case "containingScope":
+            scope_type_type = modifier["scopeType"]["type"]
+            return get_simple_modifier_callback(f"{modifier_type}.{scope_type_type}")
+        case "extendThroughStartOf":
+            if "modifiers" not in modifier:
+                return get_simple_modifier_callback(f"{modifier_type}.line")
+        case "extendThroughEndOf":
+            if "modifiers" not in modifier:
+                return get_simple_modifier_callback(f"{modifier_type}.line")
+
+    raise ValueError(f"Unknown Cursorless fallback modifier: {modifier_type}")
+
+
+def get_simple_modifier_callback(key: str) -> Callable:
+    try:
+        return modifier_callbacks[key]
+    except KeyError:
+        raise ValueError(f"Unknown Cursorless fallback modifier: {key}")

commit 46c84a1ef0f8c9d09f3ec6f7bebba496f96ec9c1

Fix keyboard broken by #2235
This was not caught by our tests because we mock away command server. Not sure how to prevent this sort of thing in the future

data/fixtures/recorded/fallback/takeToken2.yml
@@ -1,0 +1,24 @@
+languageId: plaintext
+focusedElementType: textEditor
+command:
+  version: 7
+  spokenForm: take token
+  action:
+    name: setSelection
+    target:
+      type: primitive
+      modifiers:
+        - type: containingScope
+          scopeType: {type: token}
+  usePrePhraseSnapshot: true
+initialState:
+  documentContents: foo
+  selections:
+    - anchor: {line: 0, character: 0}
+      active: {line: 0, character: 0}
+  marks: {}
+finalState:
+  documentContents: foo
+  selections:
+    - anchor: {line: 0, character: 0}
+      active: {line: 0, character: 3}

packages/common/src/FakeCommandServerApi.ts
@@ -10,7 +10,6 @@ export class FakeCommandServerApi implements CommandServerApi {
 
   constructor() {
     this.signals = { prePhrase: { getVersion: async () => null } };
-    this.focusedElementType = "textEditor";
   }
 
   async getFocusedElementType(): Promise<FocusedElementType | undefined> {

packages/common/src/types/CommandServerApi.ts
@@ -9,7 +9,7 @@ export interface CommandServerApi {
   };
 }
 
-export type FocusedElementType = "textEditor" | "terminal";
+export type FocusedElementType = "textEditor" | "terminal" | "other";
 
 export interface InboundSignal {
   getVersion(): Promise<string | null>;

packages/common/src/types/TestCaseFixture.ts
@@ -15,7 +15,7 @@ interface TestCaseFixtureBase {
   /**
    * The type of element that is focused before the command is executed. If undefined default to text editor.
    */
-  focusedElementType?: FocusedElementType | "other";
+  focusedElementType?: FocusedElementType;
 
   /**
    * A list of marks to check in the case of navigation map test otherwise undefined

packages/cursorless-engine/src/core/getCommandFallback.ts
@@ -14,10 +14,9 @@ export async function getCommandFallback(
   runAction: (actionDescriptor: ActionDescriptor) => Promise<ActionReturnValue>,
   command: CommandComplete,
 ): Promise<Fallback | null> {
-  if (
-    commandServerApi == null ||
-    (await commandServerApi.getFocusedElementType()) === "textEditor"
-  ) {
+  const focusedElementType = await commandServerApi?.getFocusedElementType();
+
+  if (focusedElementType == null || focusedElementType === "textEditor") {
     return null;
   }
 

packages/cursorless-engine/src/testCaseRecorder/TestCase.ts
@@ -141,9 +141,9 @@ export class TestCase {
     const fixture: EnforceUndefined<TestCaseFixture> = {
       languageId: this.languageId,
       focusedElementType:
-        this.focusedElementType !== "textEditor"
-          ? this.focusedElementType ?? "other"
-          : undefined,
+        this.focusedElementType === "textEditor"
+          ? undefined
+          : this.focusedElementType,
       postEditorOpenSleepTimeMs: undefined,
       postCommandSleepTimeMs: undefined,
       command: this.command,

packages/cursorless-vscode-e2e/src/endToEndTestSetup.ts
@@ -28,7 +28,9 @@ export function endToEndTestSetup(suite: Mocha.Suite) {
     const title = this.test!.fullTitle();
     retryCount = title === previousTestTitle ? retryCount + 1 : 0;
     previousTestTitle = title;
-    ({ ide, injectIde } = (await getCursorlessApi()).testHelpers!);
+    const testHelpers = (await getCursorlessApi()).testHelpers!;
+    ({ ide, injectIde } = testHelpers);
+    testHelpers.commandServerApi.setFocusedElementType(undefined);
     spy = new SpyIDE(ide);
     injectIde(spy);
   });

packages/cursorless-vscode-e2e/src/suite/recorded.vscode.test.ts
@@ -104,11 +104,7 @@ async function runTest(file: string, spyIde: SpyIDE) {
     // spyIde.clipboard.writeText(fixture.initialState.clipboard);
   }
 
-  commandServerApi.setFocusedElementType(
-    fixture.focusedElementType === "other"
-      ? undefined
-      : fixture.focusedElementType ?? "textEditor",
-  );
+  commandServerApi.setFocusedElementType(fixture.focusedElementType);
 
   // Ensure that the expected hats are present
   await hatTokenMap.allocateHats(