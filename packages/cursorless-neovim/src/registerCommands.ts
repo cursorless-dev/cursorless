0a86aa87443c983995ef28ec0efb50338de4797f
Cedric Halbronn <cedric.halbronn@nccgroup.com>
2024-05-03 11:17:48 +0100

    refactor: switch mode based on fallback in command response + cleanup

--- packages/cursorless-neovim/src/registerCommands.ts
+++ packages/cursorless-neovim/src/registerCommands.ts
@@ -1,8 +1,10 @@
 import {
   CURSORLESS_COMMAND_ID,
   CommandLatest,
+  CommandResponse,
   CommandServerApi,
   CursorlessCommandId,
+  clientSupportsFallback,
 } from "@cursorless/common";
 
 import { CommandApi } from "@cursorless/cursorless-engine";
@@ -16,6 +18,7 @@ import { getNeovimRegistry } from "@cursorless/neovim-registry";
 import type { NeovimClient } from "neovim";
 // TODO - we need to fix that import as we should not be allowed to import it afaict?
 //import { ensureCommandShape } from "../../cursorless-engine/src/core/commandVersionUpgrades/ensureCommandShape";
+// TODO: move this to common
 import { ensureCommandShape } from "../../cursorless-engine/src/core/commandVersionUpgrades/ensureCommandShape";
 
 export async function registerCommands(
@@ -37,16 +40,22 @@ export async function registerCommands(
         await updateTextEditor(client, neovimIDE);
         const result = await commandApi.runCommandSafe(...args);
 
-        const command = ensureCommandShape(args) as CommandLatest;
+        const command = ensureCommandShape(args);
         const focusedElementType =
           await commandServerApi.getFocusedElementType();
         if (
           focusedElementType === "terminal" &&
-          command.action.name === "replaceWithTarget"
+          clientSupportsFallback(command)
         ) {
-          // if user runs a terminal, and a "bring" command was requested, switch back to "t" mode
-          // so the fallback can do its magic
-          modeSwitchTerminal(client);
+          const commandResponse = result as CommandResponse;
+          if (
+            "fallback" in commandResponse &&
+            commandResponse.fallback.action === "insert"
+          ) {
+            // if user runs a terminal, and a "bring" command was requested, switch back to "t" mode
+            // so the fallback can do its magic
+            modeSwitchTerminal(client);
+          }
         }
 
         return result;
@@ -59,62 +68,50 @@ export async function registerCommands(
         throw e;
       }
     },
+
     // Cheatsheet commands
     ["cursorless.showCheatsheet"]: dummyCommandHandler,
     ["cursorless.internal.updateCheatsheetDefaults"]: dummyCommandHandler,
+
     // Testcase recorder commands
     ["cursorless.recordTestCase"]: dummyCommandHandler,
     ["cursorless.recordOneTestCaseThenPause"]: dummyCommandHandler,
     ["cursorless.pauseRecording"]: dummyCommandHandler,
     ["cursorless.resumeRecording"]: dummyCommandHandler,
     ["cursorless.takeSnapshot"]: dummyCommandHandler,
+
     // Other commands
     ["cursorless.showQuickPick"]: dummyCommandHandler,
     ["cursorless.showDocumentation"]: dummyCommandHandler,
     ["cursorless.private.logQuickActions"]: dummyCommandHandler,
+
     // Hats
     ["cursorless.toggleDecorations"]: dummyCommandHandler,
     ["cursorless.recomputeDecorationStyles"]: dummyCommandHandler,
+
     // Scope visualizer
     ["cursorless.showScopeVisualizer"]: dummyCommandHandler,
     ["cursorless.hideScopeVisualizer"]: dummyCommandHandler,
+
     // Command history
     ["cursorless.analyzeCommandHistory"]: dummyCommandHandler,
+
     // General keyboard commands
     ["cursorless.keyboard.escape"]: dummyCommandHandler,
+
     // Targeted keyboard commands
     ["cursorless.keyboard.targeted.targetHat"]: dummyCommandHandler,
     ["cursorless.keyboard.targeted.targetScope"]: dummyCommandHandler,
     ["cursorless.keyboard.targeted.targetSelection"]: dummyCommandHandler,
     ["cursorless.keyboard.targeted.clearTarget"]: dummyCommandHandler,
     ["cursorless.keyboard.targeted.runActionOnTarget"]: dummyCommandHandler,
+
     // Modal keyboard commands
     ["cursorless.keyboard.modal.modeOn"]: dummyCommandHandler,
     ["cursorless.keyboard.modal.modeOff"]: dummyCommandHandler,
     ["cursorless.keyboard.modal.modeToggle"]: dummyCommandHandler,
   };
 
-  // if (command !== "cursorless.command") {
-  //   console.log(
-  //     `handleCommandInternal(): command=${command} is not supported`,
-  //   );
-  //   return new Promise((resolve) => []);
-  // }
-
-  // return commands["cursorless.command"](...rest);
-  // NOTE: making the below notation work is not needed anymore as we will use pure dependency injection
-  // const HandlerFunction = (command: string) => {
-  //   commands[command](...rest);
-  // };
-
-  // HandlerFunction(command);
-  // extensionContext.subscriptions.push(
-  //   ...Object.entries(commands).map(([commandId, callback]) =>
-  //     vscode.commands.registerCommand(commandId, callback),
-  //   ),
-  // );
-  // TODO: import from the top of the file and do not need a require since it will be external
-  // undo it everywhere else and also in the commandr server
   Object.entries(commands).map(([commandId, callback]) =>
     getNeovimRegistry().registerCommand(commandId, callback),
   );
