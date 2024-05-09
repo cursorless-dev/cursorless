3b11db85887fb5be97be34a14802193f26fb9ed8
Trillium Smith <spiteless@gmail.com>
2024-05-08 18:37:29 -0700

    feat: Create ShikiComponent -Displays before/after states -wip: During state needs work

--- packages/test-case-component/src/components/component-shiki.tsx
+++ packages/test-case-component/src/components/component-shiki.tsx
@@ -1,12 +1,33 @@
 import * as React from "react";
 
-
-
 export const ShikiComponent: React.FC<{ data: any }> = ({ data }) => {
 
   return (
-    <pre>
-        {JSON.stringify(data, null, 2)}
-    </pre>
+    <div className="px-4">
+      <div className="p-8">
+        <h2 className="dark:text-stone-100">{data.command}</h2>
+        <div key={data.before}>
+          {data.before && (
+            <div
+              className="p-4"
+              dangerouslySetInnerHTML={{ __html: data.before }}
+            />
+          )}
+          {data.during && (
+            <div
+              className="p-4"
+              dangerouslySetInnerHTML={{ __html: data.during }}
+            />
+          )}
+          {data.after && (
+            <div
+              className="p-4"
+              dangerouslySetInnerHTML={{ __html: data.after }}
+            />
+          )}
+        </div>
+      </div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
   );
 };
