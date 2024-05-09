63fb887f300ed70dcab80553fc1f834378241de3
Trillium Smith <spiteless@gmail.com>
2024-05-08 18:29:02 -0700

    wip: Remove filepath from error state in loadFixtures

--- packages/test-case-component/src/loadFixture.ts
+++ packages/test-case-component/src/loadFixture.ts
@@ -63,7 +63,7 @@ export async function loadFixture(
       after,
     };
   } catch (e) {
-    console.log("error", filepath, e);
+    console.log("error", e);
     console.log(JSON.stringify(data, null, 2));
     throw e;
   }
