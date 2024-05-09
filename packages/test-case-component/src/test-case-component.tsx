add08aedba052d27a4633f5db225345b6d0c2721
Trillium Smith <spiteless@gmail.com>
2024-05-08 18:39:18 -0700

    feat: Create test-case-componet page scaffolding

--- packages/test-case-component/src/test-case-component.tsx
+++ packages/test-case-component/src/test-case-component.tsx
@@ -1,21 +1,27 @@
 import * as React from "react";
-import { ShikiComponent } from './components/component-shiki'
+import { ShikiComponent } from "./components/component-shiki";
+import "./shiki.css";
+import "./styles.css";
 
-export const TestCaseComponentPage: React.FC<{ data: any }> = ({ data }) => {
+export const TestCaseComponentPage: React.FC<{ data: any; loaded: any }> = ({
+  data,
+  loaded,
+}) => {
   return (
     <main className="dark:text-stone-100">
       <h1 className="mb-1 mt-2 text-center text-2xl md:text-3xl xl:mt-4">
         Test Component Sheet{" "}
         <small className="block text-sm">
-          See the{" "}
-          {/* <SmartLink to={"https://www.cursorless.org/docs/"}> */}
-            full documentation
+          See the {/* <SmartLink to={"https://www.cursorless.org/docs/"}> */}
+          full documentation
           {/* </SmartLink>{" "} */}
           to learn more.
         </small>
       </h1>
-      { data.map( (item:any) => <ShikiComponent data={item} key={item.command.spokenForm} /> ) }
-      <p className="mb-1 mt-2 text-center text-base xl:mt-4">This is just to get the page working with an initial render before adding any more functionality</p>
+
+      {loaded.map((item: any) => (
+        <ShikiComponent data={item} key={item.command} />
+      ))}
     </main>
   );
 };
