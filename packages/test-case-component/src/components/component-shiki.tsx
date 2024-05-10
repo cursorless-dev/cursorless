16fdb7283432c43178a83342cd86f05d4c29f4f8
Trillium Smith <spiteless@gmail.com>
2024-05-09 22:24:59 -0700

    feat: Add ternary logic for data.decorations

--- packages/test-case-component/src/components/component-shiki.tsx
+++ packages/test-case-component/src/components/component-shiki.tsx
@@ -1,23 +1,25 @@
 import * as React from "react";
 
 export const ShikiComponent: React.FC<{ data: any }> = ({ data }) => {
-
   return (
-    <div className="px-4">
+    <div className="p-4 overflow-auto max-w-xl">
       <div className="p-8">
         <h2 className="dark:text-stone-100">{data.command}</h2>
-        <div key={data.before}>
+        <div className="border m-2">
           {data.before && (
             <div
-              className="p-4"
+            className="p-4"
               dangerouslySetInnerHTML={{ __html: data.before }}
             />
           )}
-          {data.during && (
-            <div
-              className="p-4"
-              dangerouslySetInnerHTML={{ __html: data.during }}
-            />
+          {(data.during || data.before) && (
+            <>
+              <div
+                className="p-4"
+                dangerouslySetInnerHTML={{ __html: data.during || data.before}}
+              />
+              <div className="command">{data.command}</div>
+            </>
           )}
           {data.after && (
             <div
