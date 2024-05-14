615c850408215cb18dedb689860b95112afe03aa
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-05-14 19:07:52 +0200

    Added rust

--- packages/common/src/scopeSupportFacets/getLanguageScopeSupport.ts
+++ packages/common/src/scopeSupportFacets/getLanguageScopeSupport.ts
@@ -14,6 +14,7 @@ import { luaScopeSupport } from "./lua";
 import { markdownScopeSupport } from "./markdown";
 import { pythonScopeSupport } from "./python";
 import { rubyScopeSupport } from "./ruby";
+import { rustScopeSupport } from "./rust";
 import { LanguageScopeSupportFacetMap } from "./scopeSupportFacets.types";
 import { scssScopeSupport } from "./scss";
 import { talonScopeSupport } from "./talon";
@@ -58,6 +59,8 @@ export function getLanguageScopeSupport(
       return pythonScopeSupport;
     case "ruby":
       return rubyScopeSupport;
+    case "rust":
+      return rustScopeSupport;
     case "talon":
       return talonScopeSupport;
     case "scss":
