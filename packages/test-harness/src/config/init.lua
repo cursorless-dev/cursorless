9762dd8de554e8a2beb9f088333fd09f40243540
Cedric Halbronn <cedric.halbronn@nccgroup.com>
2024-05-16 13:34:54 +0100

    more ci stuff

--- packages/test-harness/src/config/init.lua
+++ packages/test-harness/src/config/init.lua
@@ -1,27 +1,9 @@
--- [[ Install `lazy.nvim` plugin manager ]]
---    https://github.com/folke/lazy.nvim
---    `:help lazy.nvim.txt` for more info
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
+vim.cmd('source C:\\Users\\runneradmin\\BufOnly.vim\\BufOnly.vim')
 
-require('lazy').setup({
-  -- production cursorless.nvim
-  'hands-free-vim/cursorless.nvim',
-
-  'vim-scripts/BufOnly.vim',
-})
-
--- vim.o.runtimepath = vim.o.runtimepath .. "," .. "C:\\path\\cursorless\\dist\\cursorless.nvim"
+vim.o.runtimepath = vim.o.runtimepath .. ',' .. 'C:\\Users\\runneradmin\\talon.nvim'
+vim.o.runtimepath = vim.o.runtimepath
+  .. ','
+  .. 'C:\\a\\cursorless\\cursorless\\dist\\cursorless.nvim'
 
 require('talon').setup()
 require('cursorless').setup()
