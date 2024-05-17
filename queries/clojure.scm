f1af209747e15eb0db57929400926d980611a539
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-05-17 15:32:06 +0200

    Migrate clojure scopes

--- queries/clojure.scm
+++ queries/clojure.scm
@@ -1,3 +1,11 @@
 (comment) @comment @textFragment
 
 (str_lit) @string @textFragment
+
+(map_lit) @map
+
+;; A list is either a vector literal or a quoted list literal
+[
+  (vec_lit)
+  (quoting_lit)
+] @list
