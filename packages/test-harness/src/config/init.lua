a4d4ae113f70051abe28b1712573e8ddfce7a0c2
Cedric Halbronn <387346+saidelike@users.noreply.github.com>
2024-05-18 00:29:18 +0100

    Update init.lua

--- packages/test-harness/src/config/init.lua
+++ packages/test-harness/src/config/init.lua
@@ -1,9 +1,16 @@
-vim.cmd('source C:\\Users\\runneradmin\\BufOnly.vim\\plugin\\BufOnly.vim')
+-- vim.cmd('source C:\\Users\\runneradmin\\BufOnly.vim\\plugin\\BufOnly.vim')
 
-vim.o.runtimepath = vim.o.runtimepath .. ',' .. 'C:\\Users\\runneradmin\\talon.nvim'
+-- vim.o.runtimepath = vim.o.runtimepath .. ',' .. 'C:\\Users\\runneradmin\\talon.nvim'
+-- vim.o.runtimepath = vim.o.runtimepath
+--   .. ','
+--   .. 'C:\\a\\cursorless\\cursorless\\dist\\cursorless.nvim'
+
+vim.cmd('source /home/runner/BufOnly.vim/plugin/BufOnly.vim')
+
+vim.o.runtimepath = vim.o.runtimepath .. ',' .. '/home/runner/talon.nvim'
 vim.o.runtimepath = vim.o.runtimepath
   .. ','
-  .. 'C:\\a\\cursorless\\cursorless\\dist\\cursorless.nvim'
+  .. '/home/runner/cursorless/cursorless/dist/cursorless.nvim'
 
 require('talon').setup()
 require('cursorless').setup()
