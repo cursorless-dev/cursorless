87cd6907265e5f6817f6e9c69321290d51a1bf14
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-05-17 15:34:32 +0200

    Migrate cpp scopes

--- queries/cpp.scm
+++ queries/cpp.scm
@@ -1,1 +1,24 @@
 ;; import c.scm
+
+[
+  (class_specifier)
+  (struct_specifier)
+  (enum_specifier)
+  (union_specifier)
+] @class
+
+(_
+  (class_specifier
+    name: (_) @className
+  ) @_.domain.start
+  ";"? @_.domain.end
+)
+
+;;  void ClassName::method() {}
+(function_definition
+  declarator: (_
+    declarator: (_
+      namespace: (_) @className
+    )
+  )
+) @_.domain
