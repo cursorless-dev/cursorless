8c05e0d2adae9a4c94b4bde519cbf2ca2aca271d
Cedric Halbronn <387346+saidelike@users.noreply.github.com>
2024-05-18 01:33:13 +0100

    Update populate-cursorless-nvim.sh

--- packages/test-harness/scripts/populate-cursorless-nvim.sh
+++ packages/test-harness/scripts/populate-cursorless-nvim.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 out_dir=../../dist/cursorless.nvim
-#mkdir -p "$out_dir/node/test-harness/out"
+mkdir -p "$out_dir/node/test-harness/out"
 
 # copy the built .js file
-#cp package.json "$out_dir/node/test-harness/"
-#cp out/index.cjs "$out_dir/node/test-harness/out/"
+cp package.json "$out_dir/node/test-harness/"
+cp out/index.cjs "$out_dir/node/test-harness/out/"
 
-ln -s /home/runner/work/cursorless/cursorless/packages/test-harness "$out_dir/node/test-harness"
+#ln -s /home/runner/work/cursorless/cursorless/packages/test-harness "$out_dir/node/test-harness"
