fbeb382b56f6b8254f6c29dae8a52c7f63afa12f
Cedric Halbronn <cedric.halbronn@nccgroup.com>
2024-05-21 10:24:43 +0100

    t

--- packages/test-harness/src/config/init.vim
+++ packages/test-harness/src/config/init.vim
@@ -1,4 +1,8 @@
 " https://vi.stackexchange.com/questions/42366/if-i-set-cmdheight-0-i-get-the-press-enter-or-type-command-to-continue-message
-" set shortmess=csCFSW
+set shortmess=csCFSW
+" still hangs
+" set cmdheight=
+" still hangs and cmdheight is still 0???
+" set cmdheight=1
 " echo "CED: loading init.lua"
 lua require("init")
--- packages/test-harness/src/config/init_ced.lua
+++ packages/test-harness/src/config/init_ced.lua
@@ -1,3 +1,5 @@
+-- vim.o.cmdheight = 1000
+
 -- print('CED: loading BufOnly.vim')
 vim.cmd('source C:\\Users\\Cedric\\Desktop\\test\\BufOnly.vim\\plugin\\BufOnly.vim')
 
@@ -7,8 +9,33 @@ vim.o.runtimepath = vim.o.runtimepath
   .. ','
   .. 'C:\\work\\tools\\voicecoding\\cursorless_fork\\dist\\cursorless.nvim'
 
+vim.o.runtimepath = vim.o.runtimepath .. ',' .. 'C:\\Users\\Cedric\\Desktop\\test\\nui.nvim'
+vim.o.runtimepath = vim.o.runtimepath .. ',' .. 'C:\\Users\\Cedric\\Desktop\\test\\noice.nvim'
+
 -- print('CED: loading talon.vim')
 require('talon').setup()
 
--- print('CED: loading cursorless.vim')
+print('CED: loading cursorless.vim')
 require('cursorless').setup()
+
+require('noice').setup({
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+      ['vim.lsp.util.stylize_markdown'] = true,
+      ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+    },
+  },
+  -- you can enable a preset for easier configuration
+  presets = {
+    bottom_search = true, -- use a classic bottom cmdline for search
+    command_palette = true, -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false, -- add a border to hover docs and signature help
+  },
+})
+
+-- this crashes nvim, when noice is loaded
+-- print('CED: test print after loading noice')
