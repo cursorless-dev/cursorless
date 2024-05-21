6e2054375ebc41f95cfec3d19b5a29c317dfb07c
Cedric Halbronn <cedric.halbronn@nccgroup.com>
2024-05-21 21:13:38 +0100

    test

--- scripts/install-neovim-dependencies.sh
+++ scripts/install-neovim-dependencies.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo CURSORLESS_REPO_ROOT=${CURSORLESS_REPO_ROOT}
+echo HOME_ROOT=${HOME_ROOT}
 
-git clone https://github.com/vim-scripts/BufOnly.vim /home/runner/BufOnly.vim
-git clone https://github.com/hands-free-vim/talon.nvim /home/runner/talon.nvim
+# git clone https://github.com/vim-scripts/BufOnly.vim /home/runner/BufOnly.vim
+# git clone https://github.com/hands-free-vim/talon.nvim /home/runner/talon.nvim
+git clone https://github.com/vim-scripts/BufOnly.vim ${HOME_ROOT}/BufOnly.vim
+git clone https://github.com/hands-free-vim/talon.nvim ${HOME_ROOT}/talon.nvim
