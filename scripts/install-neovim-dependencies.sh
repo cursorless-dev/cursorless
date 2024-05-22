2c1c8881a07ce5dad923bbfcd0e67adb8e0d49c4
Cedric Halbronn <cedric.halbronn@nccgroup.com>
2024-05-22 06:09:45 +0100

    t

--- scripts/install-neovim-dependencies.sh
+++ scripts/install-neovim-dependencies.sh
@@ -7,5 +7,12 @@ echo HOME_ROOT=${HOME_ROOT}
 
 # git clone https://github.com/vim-scripts/BufOnly.vim /home/runner/BufOnly.vim
 # git clone https://github.com/hands-free-vim/talon.nvim /home/runner/talon.nvim
+
+# git clone https://github.com/vim-scripts/BufOnly.vim C:\Users\runneradmin\BufOnly.vim
+# git clone https://github.com/hands-free-vim/talon.nvim C:\Users\runneradmin\talon.nvim
+
+# git clone https://github.com/vim-scripts/BufOnly.vim /Users/runner/BufOnly.vim
+# git clone https://github.com/hands-free-vim/talon.nvim /Users/runner/talon.nvim
+
 git clone https://github.com/vim-scripts/BufOnly.vim ${HOME_ROOT}/BufOnly.vim
 git clone https://github.com/hands-free-vim/talon.nvim ${HOME_ROOT}/talon.nvim
