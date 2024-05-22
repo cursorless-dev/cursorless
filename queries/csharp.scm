84c4b1c8197f9755565067fd2079432c50ec99c2
AndrewDant <andrewdant17@gmail.com>
2024-05-21 23:02:01 -0400

    add domain and iteration range for c sharp class and className

--- queries/csharp.scm
+++ queries/csharp.scm
@@ -1,6 +1,21 @@
 (if_statement) @ifStatement
+
 (class_declaration
-name: (identifier) @className) @class
+  name: (identifier) @className
+) @class @_.domain
+(compilation_unit) @class.iteration @className.iteration
+;; Treat interior of all bodies as iteration scopes for class and classname, eg
+;;!! private static void foo() {   }
+;;!                             ***
+(_
+  body: (_
+    .
+    "{" @class.iteration.start.endOf @className.iteration.start.endOf
+    "}" @class.iteration.end.startOf @className.iteration.end.startOf
+    .
+  )
+)
+
 (
   (string_literal) @string @textFragment
   (#child-range! @textFragment 0 -1 true true)
