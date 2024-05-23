719f044ef912cc4aaae20b14975f0abe059acf92
Pokey Rule <755842+pokey@users.noreply.github.com>
2024-05-23 15:40:15 +0100

    cleanup

--- packages/cursorless-engine/src/languages/latex.ts
+++ packages/cursorless-engine/src/languages/latex.ts
@@ -5,11 +5,7 @@ import {
   TextEditor,
 } from "@cursorless/common";
 import type { SyntaxNode } from "web-tree-sitter";
-import {
-  NodeMatcherAlternative,
-  SelectionWithContext,
-  SelectionWithEditor,
-} from "../typings/Types";
+import { NodeMatcherAlternative, SelectionWithContext } from "../typings/Types";
 import { patternFinder } from "../util/nodeFinders";
 import {
   ancestorChainNodeMatcher,
@@ -155,15 +151,6 @@ function extractItemContent(
   };
 }
 
-const getStartTag = patternMatcher(`*.begin!`);
-const getEndTag = patternMatcher(`*.end!`);
-
-const getTags = (selection: SelectionWithEditor, node: SyntaxNode) => {
-  const startTag = getStartTag(selection, node);
-  const endTag = getEndTag(selection, node);
-  return startTag != null && endTag != null ? startTag.concat(endTag) : null;
-};
-
 const nodeMatchers: Partial<
   Record<SimpleScopeTypeType, NodeMatcherAlternative>
 > = {
@@ -190,10 +177,6 @@ const nodeMatchers: Partial<
   ),
 
   collectionItem: matcher(patternFinder("enum_item"), extractItemContent),
-
-  xmlBothTags: getTags,
-  xmlStartTag: getStartTag,
-  xmlEndTag: getEndTag,
 };
 
 export default createPatternMatchers(nodeMatchers);
