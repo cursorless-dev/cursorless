008baa11ebcf5a9c1a08c7da8a10c544e2e84800
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-06-14 16:49:56 +0200

    Ability to focus diff editor (#2388)
    Also took a crack at the problem of focusing the search editor
    Fixes #1722
    
    ## Checklist
    
    - [/] I have added
    [tests](https://www.cursorless.org/docs/contributing/test-case-recorder/)
    - [/] I have updated the
    [docs](https://github.com/cursorless-dev/cursorless/tree/main/docs) and
    [cheatsheet](https://github.com/cursorless-dev/cursorless/tree/main/cursorless-talon/src/cheatsheet)
    - [/] I have not broken the cheatsheet
    
    ---------
    
    Co-authored-by: Pokey Rule <755842+pokey@users.noreply.github.com>

--- packages/cursorless-engine/src/actions/SetSelection.ts
+++ packages/cursorless-engine/src/actions/SetSelection.ts
@@ -1,7 +1,6 @@
 import { Selection } from "@cursorless/common";
 import { ide } from "../singletons/ide.singleton";
 import { Target } from "../typings/target.types";
-import { setSelectionsAndFocusEditor } from "../util/setSelectionsAndFocusEditor";
 import { ensureSingleEditor } from "../util/targetUtils";
 import { SimpleAction, ActionReturnValue } from "./actions.types";
 
@@ -18,10 +17,9 @@ export class SetSelection implements SimpleAction {
     const editor = ensureSingleEditor(targets);
 
     const selections = targets.map(this.getSelection);
-    await setSelectionsAndFocusEditor(
-      ide().getEditableTextEditor(editor),
-      selections,
-    );
+    await ide()
+      .getEditableTextEditor(editor)
+      .setSelections(selections, { focusEditor: true });
 
     return {
       thatTargets: targets,
