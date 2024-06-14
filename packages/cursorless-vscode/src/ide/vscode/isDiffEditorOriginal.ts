f01e3f2b852d10bab15241f732e5da3ab56de0e1
Pokey Rule <755842+pokey@users.noreply.github.com>
2024-06-14 15:36:12 +0100

    Add doc string

--- packages/cursorless-vscode/src/ide/vscode/isDiffEditorOriginal.ts
+++ packages/cursorless-vscode/src/ide/vscode/isDiffEditorOriginal.ts
@@ -1,11 +1,15 @@
 import { TextEditor, window } from "vscode";
 
-
+/**
+ * @param editor The editor to check
+ * @returns `true` if the editor is the original (left-hand) side of a diff editor
+ */
 export function isDiffEditorOriginal(editor: TextEditor): boolean {
   if (editor.viewColumn != null) {
     return false;
   }
   const uri = editor.document.uri.toString();
-  return window.tabGroups.all.some((tabGroup) => tabGroup.tabs.find((tab: any) => tab.input.original?.toString() === uri)
+  return window.tabGroups.all.some((tabGroup) =>
+    tabGroup.tabs.find((tab: any) => tab.input.original?.toString() === uri),
   );
 }
