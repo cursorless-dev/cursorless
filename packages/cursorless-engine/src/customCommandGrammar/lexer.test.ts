5126170af7d5ea5f88fd1f9d4d880c20243f369a
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-06-19 02:55:21 +0200

    added test

--- packages/cursorless-engine/src/customCommandGrammar/lexer.test.ts
+++ packages/cursorless-engine/src/customCommandGrammar/lexer.test.ts
@@ -14,6 +14,15 @@ interface Fixture {
 
 const fixtures: Fixture[] = [
   {
+    input: "chuck",
+    expectedOutput: [
+      {
+        type: "simpleActionName",
+        value: "remove",
+      },
+    ],
+  },
+  {
     input: "funk",
     expectedOutput: [
       {
