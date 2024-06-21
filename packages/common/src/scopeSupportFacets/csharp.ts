4dfa628ee7402c25815a578fc0d08ee7eaf58f07
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-06-21 16:01:46 +0200

    Started migration

--- packages/common/src/scopeSupportFacets/csharp.ts
+++ packages/common/src/scopeSupportFacets/csharp.ts
@@ -17,4 +17,22 @@ export const csharpScopeSupport: LanguageScopeSupportFacetMap = {
   "class.iteration.block": supported,
   "className.iteration.document": supported,
   "className.iteration.block": supported,
+  switchStatementSubject: supported,
+  functionCall: supported,
+  "functionCall.constructor": supported,
+  functionCallee: supported,
+  "functionCallee.constructor": supported,
+
+  namedFunction: supported,
+  "namedFunction.constructor": supported,
+  "namedFunction.method": supported,
+
+  functionName: supported,
+  "functionName.constructor": supported,
+  "functionName.method": supported,
+  //   "name.function": supported,
+  //   "name.method": supported,
+  // "name.constructor": supported,
+
+  regularExpression: unsupported,
 };
