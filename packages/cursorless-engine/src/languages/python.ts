b688dd4e51706add41f4aa7414c290dd2cbb2e29
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-06-24 13:33:22 +0200

    Cleanup

--- packages/cursorless-engine/src/languages/python.ts
+++ packages/cursorless-engine/src/languages/python.ts
@@ -1,39 +1,13 @@
-import { Selection, SimpleScopeTypeType } from "@cursorless/common";
-import type { SyntaxNode } from "web-tree-sitter";
-import { NodeFinder, NodeMatcherAlternative } from "../typings/Types";
-import { argumentNodeFinder, patternFinder } from "../util/nodeFinders";
+import { SimpleScopeTypeType } from "@cursorless/common";
+import { NodeMatcherAlternative } from "../typings/Types";
+import { patternFinder } from "../util/nodeFinders";
 import {
   argumentMatcher,
   cascadingMatcher,
   createPatternMatchers,
   matcher,
 } from "../util/nodeMatchers";
-import {
-  argumentSelectionExtractor,
-  childRangeSelector,
-} from "../util/nodeSelectors";
-
-export const getTypeNode = (node: SyntaxNode) =>
-  node.children.find((child) => child.type === "type") ?? null;
-
-function itemNodeFinder(
-  parentType: string,
-  childType: string,
-  excludeFirstChild: boolean = false,
-): NodeFinder {
-  const finder = argumentNodeFinder(parentType);
-  return (node: SyntaxNode, selection?: Selection) => {
-    const childNode = finder(node, selection);
-    if (
-      childNode?.type === childType &&
-      (!excludeFirstChild ||
-        childNode.id !== childNode.parent?.firstNamedChild?.id)
-    ) {
-      return childNode;
-    }
-    return null;
-  };
-}
+import { childRangeSelector } from "../util/nodeSelectors";
 
 const nodeMatchers: Partial<
   Record<SimpleScopeTypeType, NodeMatcherAlternative>
