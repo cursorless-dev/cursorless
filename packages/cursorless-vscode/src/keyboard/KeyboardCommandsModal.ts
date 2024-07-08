4734033c672c9d30e965f921b3d2f113524fad42
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2024-07-08 09:10:17 -0400

    Keyboard cursor customization (#2443)
    Addresses https://github.com/cursorless-dev/cursorless/issues/2401, the
    ability to have custom cursor styling exclusively when in cursorless
    keyboard mode.
    
    Tried to follow the repo style as best as possible but please let me
    know if another way besides a record mapper is more idiomatic for TS.
    
    ## Checklist
    
    - [/] I have added
    [tests](https://www.cursorless.org/docs/contributing/test-case-recorder/)
    - [x] I have updated the
    [docs](https://github.com/cursorless-dev/cursorless/tree/main/docs) and
    [cheatsheet](https://github.com/cursorless-dev/cursorless/tree/main/cursorless-talon/src/cheatsheet)
    - _I could add docs in the visual a11y section but unclear if
    experimental keyboard support belongs there_
    - [x] I have not broken the cheatsheet
    
    ## Example 
    An image showing the cursor start out with a custom type, then change
    into `block` when keyboard mode is activated, thus adding more contrast.
    
    
    ![](https://github.com/cursorless-dev/cursorless/assets/70598503/b0dfd7bc-daa0-40bc-b052-20aed31c5d6e)
    
    ---------
    
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>
    Co-authored-by: Pokey Rule <755842+pokey@users.noreply.github.com>

--- packages/cursorless-vscode/src/keyboard/KeyboardCommandsModal.ts
+++ packages/cursorless-vscode/src/keyboard/KeyboardCommandsModal.ts
@@ -120,7 +120,7 @@ export default class KeyboardCommandsModal {
     this.inputDisposable = this.keyboardHandler.pushListener({
       handleInput: this.handleInput,
       displayOptions: {
-        cursorStyle: vscode.TextEditorCursorStyle.BlockOutline,
+        cursorStyle: this.keyboardConfig.getCursorStyle(),
         whenClauseContext: "cursorless.keyboard.modal.mode",
         statusBarText: "Listening...",
       },
