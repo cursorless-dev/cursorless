4299388e8317914319e4aa73aac6f017e65d89ec
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-07-11 17:44:43 +0200

    clean up

--- packages/cursorless-engine/src/core/HatTokenMapImpl.ts
+++ packages/cursorless-engine/src/core/HatTokenMapImpl.ts
@@ -155,24 +155,3 @@ export class HatTokenMapImpl implements HatTokenMap {
     this.prePhraseMapsSnapshotTimestamp = hrtime.bigint();
   }
 }
-
-export class DisabledHatTokenMap implements HatTokenMap {
-  async allocateHats() {
-    // Do nothing
-  }
-
-  async getReadableMap() {
-    return {
-      getEntries() {
-        return [];
-      },
-      getToken() {
-        throw new Error("Hat map is disabled");
-      },
-    };
-  }
-
-  dispose() {
-    // Do nothing
-  }
-}
