638ecdb3292dbbf0167afa87fd7b957842b730c7
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-07-14 12:37:27 +0200

    Update Pollyfill

--- packages/cursorless-talon-js/src/polyfill.ts
+++ packages/cursorless-talon-js/src/polyfill.ts
@@ -1,3 +1,16 @@
-(globalThis as any).process = {
+/* eslint-disable @typescript-eslint/naming-convention */
+const global = globalThis as any;
+
+global.process = {
   env: {},
 };
+
+class Collator {
+  constructor(_locales?: unknown, _options?: unknown) {}
+
+  compare(a: string, b: string) {
+    return a.localeCompare(b);
+  }
+}
+
+global.Intl = { Collator };
