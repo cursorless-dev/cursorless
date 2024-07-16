c5ff73202131c3db712b9a170f3d743231f33691
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-07-16 13:16:42 +0200

    Create register commands file

--- packages/cursorless-talon-js/src/extension.ts
+++ packages/cursorless-talon-js/src/extension.ts
@@ -1,70 +1,20 @@
 import "./polyfill";
 import type { RunMode } from "@cursorless/common";
-import {
-  createCursorlessEngine,
-  type CommandApi,
-} from "@cursorless/cursorless-engine";
-import { Context, actions } from "talon";
+import { createCursorlessEngine } from "@cursorless/cursorless-engine";
 import { TalonJsIDE } from "./ide/TalonJsIDE";
-
-const ctx = new Context();
-let commandApi: CommandApi | undefined;
-let ide: TalonJsIDE | undefined;
-
-ctx.matches = `
-not tag: user.cursorless
-`;
-
-ctx.action_class("user", {
-  private_cursorless_run_rpc_command_no_wait(
-    commandId: string,
-    command: unknown,
-  ): void {
-    void runCommand(commandId, command);
-  },
-
-  async private_cursorless_run_rpc_command_get(
-    commandId: string,
-    command: unknown,
-  ): Promise<unknown> {
-    return await runCommand(commandId, command);
-  },
-});
-
-async function runCommand(
-  commandId: string,
-  command: unknown,
-): Promise<unknown> {
-  try {
-    if (ide == null) {
-      throw Error("ide is not initialized.");
-    }
-    if (commandApi == null) {
-      throw Error("commandApi is not initialized.");
-    }
-    if (commandId !== "cursorless.command") {
-      throw Error(`Unknown command ID: ${commandId}`);
-    }
-
-    const editorState = actions.user.cursorless_js_get_editor_state();
-    ide.updateTextEditors(editorState);
-
-    return await commandApi.runCommandSafe(command);
-  } catch (error) {
-    print(error);
-    throw error;
-  }
-}
+import { registerCommands } from "./registerCommands";
 
 export async function activate(runMode: RunMode): Promise<void> {
   print(`activate talon.js @ ${runMode}`);
 
   try {
-    ide = new TalonJsIDE(runMode);
+    const ide = new TalonJsIDE(runMode);
 
     const engine = await createCursorlessEngine({ ide });
 
-    commandApi = engine.commandApi;
+    const commandApi = engine.commandApi;
+
+    registerCommands(ide, commandApi);
   } catch (error) {
     print(error);
   }
