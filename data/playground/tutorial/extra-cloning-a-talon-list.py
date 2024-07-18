6131fe96401e863efbb2bf941c25c29d0c0d9a3a
Pokey Rule <755842+pokey@users.noreply.github.com>
2024-07-18 11:36:17 +0100

    remove stuff

--- data/playground/tutorial/extra-cloning-a-talon-list.py
+++
@@ -1,9 +0,0 @@
-from talon import Context, Module
-
-mod = Module()
-ctx = Context()
-
-mod.list("cursorless_walkthrough_list", desc="My tutorial list")
-ctx.list["user.cursorless_walkthrough_list"] = {
-    "spoken form": "whatever",
-}
