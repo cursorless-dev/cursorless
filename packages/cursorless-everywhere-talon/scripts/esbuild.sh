d7dc2b7b3a69a7ff86c338cfbc42ee62b64e8d4f
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-07-18 03:35:21 +0200

    Another sed attempt

--- packages/cursorless-everywhere-talon/scripts/esbuild.sh
+++ packages/cursorless-everywhere-talon/scripts/esbuild.sh
@@ -19,4 +19,4 @@ fi
 
 # Talon javascript files needs to start with an import from Talon before any other code
 TALON_IMPORT='import { Context as DefaultContext } from "talon";'
-sed -i'' "1s/^/${TALON_IMPORT}\n\n/" out/talon.js
+sed -i'' -e "1s/^/${TALON_IMPORT}\n\n/" out/talon.js
