c1d52e603607672cdab6e385b59f45d96a1f96ad
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-07-21 15:51:32 +0200

    Remove collator polyfill

--- packages/cursorless-everywhere-talon/src/polyfill.ts
+++ packages/cursorless-everywhere-talon/src/polyfill.ts
@@ -4,17 +4,6 @@ global.process = {
   env: {},
 };
 
-class Collator {
-  constructor(_locales?: unknown, _options?: unknown) {}
-
-  compare(a: string, b: string) {
-    return a.localeCompare(b);
-  }
-}
-
-// eslint-disable-next-line @typescript-eslint/naming-convention
-global.Intl = { Collator };
-
 global.console = {
   log: print,
   error: print,
