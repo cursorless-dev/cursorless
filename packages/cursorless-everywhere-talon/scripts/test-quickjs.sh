c37e01fc106020aac2fe5527f34e0ec319ccdb0e
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-07-21 08:53:25 +0200

    ci tests

--- packages/cursorless-everywhere-talon/scripts/test-quickjs.sh
+++ packages/cursorless-everywhere-talon/scripts/test-quickjs.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [[ "$OSTYPE" != "linux-gnu"* ]]; then
+    echo "quickjs tests are only supported on Linux"
+    exit 0
+fi
+
 esbuild \
     --outfile=testOut/talon \
     --platform=neutral \
@@ -16,4 +21,9 @@ cp -r out/talon.js testOut/cursorless.mjs
 
 cd testOut
 
-qjs -I cursorless.mjs quickjsTest.js
+# curl -o qjs.zip https://bellard.org/quickjs/binary_releases/quickjs-win-x86_64-2024-01-13.zip
+curl -o qjs.zip https://bellard.org/quickjs/binary_releases/quickjs-linux-x86_64-2024-01-13.zip
+
+unzip qjs.zip
+
+./qjs -I cursorless.mjs quickjsTest.js
