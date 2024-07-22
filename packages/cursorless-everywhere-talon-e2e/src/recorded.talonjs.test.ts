1356d7c4d463662daf1acd586b9f77d2ba18c963
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-07-22 16:54:19 +0200

    filed er tests

--- packages/cursorless-everywhere-talon-e2e/src/recorded.talonjs.test.ts
+++ packages/cursorless-everywhere-talon-e2e/src/recorded.talonjs.test.ts
@@ -7,9 +7,14 @@ import {
 } from "@cursorless/common";
 import {
   activate,
+  type EditorState,
   type TalonJsIDE,
 } from "@cursorless/cursorless-everywhere-talon-core";
-import { getRecordedTestPaths, runRecordedTest } from "@cursorless/node-common";
+import {
+  getRecordedTestPaths,
+  loadFixture,
+  runRecordedTest,
+} from "@cursorless/node-common";
 import { constructTestHelpers } from "./constructTestHelpers";
 import { endToEndTestSetup } from "./endToEndTestSetUp";
 import talonMock from "./talonMock";
@@ -19,24 +24,47 @@ suite("TalonJS: Recorded test cases", async function () {
   const ideTestHelpers = ide.testHelpers!;
   const { getSpy } = endToEndTestSetup(this, ideTestHelpers);
 
-  getRecordedTestPaths().forEach(({ name, path }) =>
+  const tests = getRecordedTestPaths();
+
+  for (const { name, path } of tests) {
     test(
       name,
-      asyncSafety(
-        async () =>
-          await runRecordedTest({
-            path,
-            spyIde: getSpy(),
-            openNewTestEditor: (content, languageId) =>
-              openNewTestEditor(ideTestHelpers.talonJsIDE, content, languageId),
-            sleepWithBackoff,
-            testHelpers: constructTestHelpers(ideTestHelpers),
-            runCursorlessCommand,
-          }),
-      ),
-    ),
-  );
+      asyncSafety(async () => {
+        const shouldSkip = await shouldSkipTest(path);
+        if (shouldSkip) {
+          this.ctx.skip();
+        }
+
+        await runRecordedTest({
+          path,
+          spyIde: getSpy(),
+          openNewTestEditor: (content, languageId) =>
+            openNewTestEditor(ideTestHelpers.talonJsIDE, content, languageId),
+          sleepWithBackoff,
+          testHelpers: constructTestHelpers(ideTestHelpers),
+          runCursorlessCommand,
+        });
+      }),
+    );
+  }
 });
+
+async function shouldSkipTest(path: string): Promise<boolean> {
+  const fixture = await loadFixture(path);
+
+  if (
+    fixture.initialState.marks != null &&
+    Object.keys(fixture.initialState.marks).length > 0
+  ) {
+    return true;
+  }
+
+  if (fixture.languageId !== "plaintext") {
+    return true;
+  }
+
+  return false;
+}
 
 function sleepWithBackoff(_ms: number): Promise<void> {
   return Promise.resolve();
@@ -56,13 +84,19 @@ async function openNewTestEditor(
   content: string,
   languageId: string,
 ): Promise<TextEditor> {
-  ide.updateTextEditors({
+  const editorState: EditorState = {
     text: content,
     languageId,
     selections: [],
-  });
+  };
+  ide.updateTextEditors(editorState);
+  talonMock.getTestHelpers().setEditorState(editorState);
 
-  const editor = ide.activeTextEditor!;
+  const editor = ide.activeTextEditor;
+
+  if (editor == null) {
+    throw new Error("Could not open new editor.");
+  }
 
   // Override any user settings and make sure tests run with default tabs.
   editor.options = DEFAULT_TEXT_EDITOR_OPTIONS_FOR_TEST;
