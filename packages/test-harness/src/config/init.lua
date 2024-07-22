fea93b93262ba288cb80d9141ae6295cee97de00
Cedric Halbronn <cedric.halbronn@nccgroup.com>
2024-07-22 14:29:06 +0100

    docs: restore working CI + add tests architecture

--- packages/test-harness/src/config/init.lua
+++ packages/test-harness/src/config/init.lua
@@ -2,7 +2,7 @@ local temp_dir = os.getenv("TEMP_DIR")
 local repo_root = os.getenv("CURSORLESS_REPO_ROOT")
 
 vim.opt.runtimepath:append(temp_dir .. "/talon.nvim")
-vim.opt.runtimepath:append(repo_root .. "/cursorless.nvim")
+vim.opt.runtimepath:append(repo_root .. "/dist/cursorless.nvim")
 
 require("talon").setup()
 require("cursorless").setup()
