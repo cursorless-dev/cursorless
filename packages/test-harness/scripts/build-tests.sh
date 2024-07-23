aa59c7f5d9238bb810838588585ed712a358d140
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-07-23 12:33:27 +0200

    Restore build tests

--- packages/test-harness/scripts/build-tests.sh
+++ packages/test-harness/scripts/build-tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-TESTS="$(find .. -name '*.talonjs.test.ts')"
+TESTS="$(find .. -name '*.test.ts')"
 
 pnpm run build:base --outdir=dist --out-extension:.js=.cjs $TESTS
