923c4727c5141cd4ef55c8ef70682d5b96f528b5
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-07-23 21:14:44 +0200

    Update captures

--- cursorless-talon/src/modifiers/surrounding_pair.py
+++ cursorless-talon/src/modifiers/surrounding_pair.py
@@ -34,12 +34,16 @@ mod.list(
         "{user.cursorless_surrounding_pair_scope_type}"
     )
 )
-def cursorless_surrounding_pair_scope_type(m) -> str:
+def cursorless_surrounding_pair_scope_type(m) -> dict[str, str]:
     """Surrounding pair scope type"""
     try:
-        return m.cursorless_surrounding_pair_scope_type
+        delimiter = m.cursorless_surrounding_pair_scope_type
     except AttributeError:
-        return m.cursorless_selectable_paired_delimiter
+        delimiter = m.cursorless_selectable_paired_delimiter
+    return {
+        "type": "surroundingPair",
+        "delimiter": delimiter,
+    }
 
 
 @mod.capture(
@@ -48,12 +52,16 @@ def cursorless_surrounding_pair_scope_type(m) -> str:
         "{user.cursorless_surrounding_pair_scope_type_plural}"
     )
 )
-def cursorless_surrounding_pair_scope_type_plural(m) -> str:
+def cursorless_surrounding_pair_scope_type_plural(m) -> dict[str, str]:
     """Plural surrounding pair scope type"""
     try:
-        return m.cursorless_surrounding_pair_scope_type_plural
+        delimiter = m.cursorless_surrounding_pair_scope_type_plural
     except AttributeError:
-        return m.cursorless_selectable_paired_delimiter_plural
+        delimiter = m.cursorless_selectable_paired_delimiter_plural
+    return {
+        "type": "surroundingPair",
+        "delimiter": delimiter,
+    }
 
 
 @mod.capture(
@@ -61,39 +69,7 @@ def cursorless_surrounding_pair_scope_type_plural(m) -> str:
 )
 def cursorless_surrounding_pair(m) -> dict[str, Any]:
     """Expand to containing surrounding pair"""
-    try:
-        surrounding_pair_scope_type = m.cursorless_surrounding_pair_scope_type
-    except AttributeError:
-        surrounding_pair_scope_type = "any"
-
-    scope_type = {
-        "type": "surroundingPair",
-        "delimiter": surrounding_pair_scope_type,
-    }
-
-    with suppress(AttributeError):
-        scope_type["forceDirection"] = m.cursorless_delimiter_force_direction
-
-    return {
-        "type": "containingScope",
-        "scopeType": scope_type,
-    }
-
-
-@mod.capture(
-    rule="[{user.cursorless_delimiter_force_direction}] <user.cursorless_surrounding_pair_scope_type>"
-)
-def cursorless_surrounding_pair_plural(m) -> dict[str, Any]:
-    """Expand to containing surrounding pair"""
-    try:
-        surrounding_pair_scope_type = m.cursorless_surrounding_pair_scope_type
-    except AttributeError:
-        surrounding_pair_scope_type = "any"
-
-    scope_type = {
-        "type": "surroundingPair",
-        "delimiter": surrounding_pair_scope_type,
-    }
+    scope_type = m.cursorless_surrounding_pair_scope_type
 
     with suppress(AttributeError):
         scope_type["forceDirection"] = m.cursorless_delimiter_force_direction
