5de2b27b8c5f0057b4b2ab87ffc5336f1cda2e59
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-07-24 12:13:37 +0200

    Use xargs

--- packages/test-harness/scripts/build-tests.sh
+++ packages/test-harness/scripts/build-tests.sh
@@ -1,18 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Initialize an empty array to hold the test files
-TESTS=()
-
-# Read the output of the find command into the TESTS array
-while IFS= read -r file; do
-    TESTS+=("$file")
-done < <(find .. -name '*.test.ts')
-
-BATCH_SIZE=50
-SIZE=${#TESTS[@]}
-
-for ((i = 0; i < SIZE; i += BATCH_SIZE)); do
-    BATCH=${TESTS[@]:i:BATCH_SIZE}
-    pnpm run build:base --outdir=dist --out-extension:.js=.cjs $BATCH
-done
+find .. -name '*.test.ts' | xargs -n 50 pnpm run build:base --outdir=dist --out-extension:.js=.cjs
