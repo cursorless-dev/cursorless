3a23936dd76be37a6dc6a8b0acfcd2f9d40565ac
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-07-24 11:32:37 +0200

    Batch building/compiling tests

--- packages/test-harness/scripts/build-tests.sh
+++ packages/test-harness/scripts/build-tests.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-TESTS="$(find .. -name '*.test.ts')"
+mapfile -t TESTS < <(find .. -name '*.test.ts')
 
-pnpm run build:base --outdir=dist --out-extension:.js=.cjs $TESTS
+BATCH_SIZE=50
+SIZE=${#TESTS[@]}
+
+for ((i = 0; i < SIZE; i += BATCH_SIZE)); do
+    BATCH=${TESTS[@]:i:BATCH_SIZE}
+    pnpm run build:base --outdir=dist --out-extension:.js=.cjs $BATCH
+done
