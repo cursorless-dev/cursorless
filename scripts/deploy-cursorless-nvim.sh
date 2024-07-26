5dcbc41e94f29b0c02ae0b56a7a046dc4f736a6b
Pokey Rule <755842+pokey@users.noreply.github.com>
2024-07-26 12:19:40 +0100

    Improve deploy

--- scripts/deploy-cursorless-nvim.sh
+++ scripts/deploy-cursorless-nvim.sh
@@ -4,33 +4,34 @@ set -euo pipefail
 
 # Clone current cursorless.nvim main
 mkdir -p dist && cd dist
-git clone 'https://github.com/hands-free-vim/cursorless.nvim.git' cursorless.nvim-remote
+# git clone 'https://github.com/hands-free-vim/cursorless.nvim.git' cursorless.nvim-remote
 cd -
 
 out_dir=dist/cursorless.nvim-remote
 
 # Delete the old files
 cd "$out_dir"
-git rm -r README.md lua/ vim/ node/
+git rm -r '*'
 cd -
 
-#
-# Merge the build .js and the static files
-#
-
-# copy .lua, .vim dependencies, command-server and other static files
-cp -r cursorless.nvim/README.md "$out_dir/"
-cp -r cursorless.nvim/lua "$out_dir/"
-cp -r cursorless.nvim/vim "$out_dir/"
-cp -r cursorless.nvim/node "$out_dir/"
+# copy static files
+cp -r cursorless.nvim/* "$out_dir/"
 
 # copy the built .js file
 mkdir -p "$out_dir/node/cursorless-neovim/out"
 cp packages/cursorless-neovim/package.json "$out_dir/node/cursorless-neovim/"
 cp packages/cursorless-neovim/out/index.cjs "$out_dir/node/cursorless-neovim/out/"
 
+# Extract commit message and body
+commit_message="$(git log -1 --pretty=format:"%s" HEAD)"
+commit_body="$(git log -1 --pretty=format:"%b" HEAD)"
+author_name=$(git log -1 --pretty=format:"%an" HEAD)
+author_email=$(git log -1 --pretty=format:"%ae" HEAD)
+author_date=$(git log -1 --pretty=format:"%ad" --date=iso-strict HEAD)
+
 # Push to cursorless.nvim
 cd "$out_dir"
-git add *
-git commit -m "Deploy cursorless.nvim"
+git add .
+GIT_AUTHOR_NAME="$author_name" GIT_AUTHOR_EMAIL="$author_email" GIT_AUTHOR_DATE="$author_date" \
+  git commit -m "$commit_message" -m "$commit_body" || true
 git push
