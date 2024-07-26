58573a2613c1e430f81641cbe94d0374e708e81d
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-07-26 05:53:28 +0200

    clean up

--- packages/cursorless-everywhere-talon/src/types/Global.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace console {
-  function debug(...objs: any): void;
-  function log(...objs: any): void;
-  function warn(...objs: any): void;
-  function error(...objs: any): void;
-}
