f5aeea6ab66543ea0c46d8abde5dc19e3e3b9d15
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-07-28 18:15:33 +0200

    Migrate php type scope (#2491)
    ## Checklist
    
    - [x] I have added
    [tests](https://www.cursorless.org/docs/contributing/test-case-recorder/)
    - [/] I have updated the
    [docs](https://github.com/cursorless-dev/cursorless/tree/main/docs) and
    [cheatsheet](https://github.com/cursorless-dev/cursorless/tree/main/cursorless-talon/src/cheatsheet)
    - [/] I have not broken the cheatsheet

--- packages/common/src/scopeSupportFacets/php.ts
+++ packages/common/src/scopeSupportFacets/php.ts
@@ -10,5 +10,15 @@ export const phpScopeSupport: LanguageScopeSupportFacetMap = {
   "comment.line": supported,
   "comment.block": supported,
   "textFragment.string.singleLine": supported,
+
+  "type.argument.formal": supported,
+  "type.argument.formal.iteration": supported,
+  "type.cast": supported,
+  "type.field": supported,
+
+  "name.argument.formal": supported,
+  "name.argument.formal.iteration": supported,
+  "name.field": supported,
+
   disqualifyDelimiter: supported,
 };
--- packages/cursorless-engine/src/languages/php.ts
+++ packages/cursorless-engine/src/languages/php.ts
@@ -1,17 +1,11 @@
-import { Selection, TextEditor } from "@cursorless/common";
-import type { SyntaxNode } from "web-tree-sitter";
 import { SimpleScopeTypeType } from "@cursorless/common";
-import { NodeMatcherAlternative, SelectionWithContext } from "../typings/Types";
-import { patternFinder } from "../util/nodeFinders";
+import { NodeMatcherAlternative } from "../typings/Types";
 import {
   argumentMatcher,
-  cascadingMatcher,
   createPatternMatchers,
   leadingMatcher,
-  matcher,
   trailingMatcher,
 } from "../util/nodeMatchers";
-import { getNodeRange } from "../util/nodeSelectors";
 
 // Taken from https://www.php.net/manual/en/language.operators.assignment.php
 const assignmentOperators = [
@@ -34,40 +28,9 @@ const assignmentOperators = [
   "??=",
 ];
 
-/**
- * Given a node representing the text of a type cast, will return the
- * content range as the text inner type, and the outside range includes
- * the surrounding parentheses, so that "chuck type" deletes the parens
- * @param editor The editor containing the node
- * @param node The node to extract from; will be the content of the type cast without the surrounding parens
- * @returns The selection with context
- */
-function castTypeExtractor(
-  editor: TextEditor,
-  node: SyntaxNode,
-): SelectionWithContext {
-  const range = getNodeRange(node);
-  const contentRange = range;
-  const leftParenRange = getNodeRange(node.previousSibling!);
-  const rightParenRange = getNodeRange(node.nextSibling!.nextSibling!);
-  const removalRange = range.with(leftParenRange.start, rightParenRange.start);
-
-  return {
-    selection: new Selection(contentRange.start, contentRange.end),
-    context: {
-      removalRange,
-    },
-  };
-}
-
 const nodeMatchers: Partial<
   Record<SimpleScopeTypeType, NodeMatcherAlternative>
 > = {
-  type: cascadingMatcher(
-    trailingMatcher(["~cast_expression[type]"]),
-    matcher(patternFinder("cast_expression[type]"), castTypeExtractor),
-  ),
-
   value: leadingMatcher(
     [
       "array_element_initializer[1]",
