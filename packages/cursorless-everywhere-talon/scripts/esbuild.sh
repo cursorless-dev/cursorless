18f2915da7d77d3f3750910b27380c84706be6dc
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-07-28 16:56:08 +0200

    Added fix me

--- packages/cursorless-everywhere-talon/scripts/esbuild.sh
+++ packages/cursorless-everywhere-talon/scripts/esbuild.sh
@@ -12,6 +12,6 @@ esbuild \
   --external:talon \
   "$@"
 
-# Talon javascript files needs to start with an import from Talon before any other code
+# FIXME: Talon javascript files needs to start with an import from Talon before any other code
 TALON_IMPORT='import { Context as DefaultContext } from "talon";'
 sed -i'' -e "1s/^/${TALON_IMPORT}\n\n/" out/talon.js
