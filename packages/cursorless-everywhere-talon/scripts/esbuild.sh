42b0971b7aae820cf1194525b698de69febded9c
Pokey Rule <755842+pokey@users.noreply.github.com>
2024-07-28 15:04:00 +0100

    cleanup shell script

--- packages/cursorless-everywhere-talon/scripts/esbuild.sh
+++ packages/cursorless-everywhere-talon/scripts/esbuild.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 esbuild \
-    --outfile=out/talon.js \
-    --platform=neutral \
-    --format=esm \
-    --main-fields=main,module \
-    --conditions=cursorless:bundler \
-    --bundle \
-    --sourcemap \
-    --external:talon \
-    $@
-
-
-if [ $? -ne 0 ]; then
-    exit 1
-fi
-
+  --outfile=out/talon.js \
+  --platform=neutral \
+  --format=esm \
+  --main-fields=main,module \
+  --conditions=cursorless:bundler \
+  --bundle \
+  --sourcemap \
+  --external:talon \
+  "$@"
 
 # Talon javascript files needs to start with an import from Talon before any other code
 TALON_IMPORT='import { Context as DefaultContext } from "talon";'
