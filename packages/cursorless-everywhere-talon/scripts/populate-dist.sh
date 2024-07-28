3780b4aec81e7c7c4a0a005b91e488957a76d4ba
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-07-28 20:40:57 +0200

    Update script

--- packages/cursorless-everywhere-talon/scripts/populate-dist.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir dist
-
-cp -r out/. dist
-
-cp -r ../../cursorless-everywhere-talon/. dist
--- packages/test-harness/src/scripts/runTalonJsTests.ts
+++ packages/test-harness/src/scripts/runTalonJsTests.ts
@@ -1,6 +1,12 @@
 /**
- * Runs all Talon everywhere/JS tests in CI.
+ * Runs all Talon everywhere/JS tests.
  */
 import { TestType, runAllTests } from "../runAllTests";
 
-void runAllTests(TestType.talonJs);
+void (async () => {
+  try {
+    await runAllTests(TestType.talonJs);
+  } catch (_ex) {
+    process.exit(1);
+  }
+})();
