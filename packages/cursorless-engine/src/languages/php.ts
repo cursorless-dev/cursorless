22f47f59d90beebe9d135af0412dd25cd10d6f18
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-07-28 18:05:48 +0200

    remove unused

--- packages/cursorless-engine/src/languages/php.ts
+++ packages/cursorless-engine/src/languages/php.ts
@@ -1,13 +1,11 @@
-import { Selection, SimpleScopeTypeType, TextEditor } from "@cursorless/common";
-import type { SyntaxNode } from "web-tree-sitter";
-import { NodeMatcherAlternative, SelectionWithContext } from "../typings/Types";
+import { SimpleScopeTypeType } from "@cursorless/common";
+import { NodeMatcherAlternative } from "../typings/Types";
 import {
   argumentMatcher,
   createPatternMatchers,
   leadingMatcher,
   trailingMatcher,
 } from "../util/nodeMatchers";
-import { getNodeRange } from "../util/nodeSelectors";
 
 // Taken from https://www.php.net/manual/en/language.operators.assignment.php
 const assignmentOperators = [
@@ -29,32 +27,6 @@ const assignmentOperators = [
   ".=",
   "??=",
 ];
-
-/**
- * Given a node representing the text of a type cast, will return the
- * content range as the text inner type, and the outside range includes
- * the surrounding parentheses, so that "chuck type" deletes the parens
- * @param editor The editor containing the node
- * @param node The node to extract from; will be the content of the type cast without the surrounding parens
- * @returns The selection with context
- */
-function castTypeExtractor(
-  editor: TextEditor,
-  node: SyntaxNode,
-): SelectionWithContext {
-  const range = getNodeRange(node);
-  const contentRange = range;
-  const leftParenRange = getNodeRange(node.previousSibling!);
-  const rightParenRange = getNodeRange(node.nextSibling!.nextSibling!);
-  const removalRange = range.with(leftParenRange.start, rightParenRange.start);
-
-  return {
-    selection: new Selection(contentRange.start, contentRange.end),
-    context: {
-      removalRange,
-    },
-  };
-}
 
 const nodeMatchers: Partial<
   Record<SimpleScopeTypeType, NodeMatcherAlternative>
