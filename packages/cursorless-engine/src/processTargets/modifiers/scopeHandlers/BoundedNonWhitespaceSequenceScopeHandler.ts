cdadc46ccc72fa10b8d6843235aa808239d0faa3
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-07-29 01:50:15 +0200

    Stop using one of

--- packages/cursorless-engine/src/processTargets/modifiers/scopeHandlers/BoundedNonWhitespaceSequenceScopeHandler.ts
+++ packages/cursorless-engine/src/processTargets/modifiers/scopeHandlers/BoundedNonWhitespaceSequenceScopeHandler.ts
@@ -1,15 +1,25 @@
-import { Direction, Position, ScopeType, TextEditor } from "@cursorless/common";
+import {
+  Direction,
+  Position,
+  ScopeType,
+  TextEditor,
+  type Range,
+} from "@cursorless/common";
+import { TokenTarget } from "../../targets";
 import { BaseScopeHandler } from "./BaseScopeHandler";
+import { compareTargetScopes } from "./compareTargetScopes";
 import type { TargetScope } from "./scope.types";
-import type { ScopeIteratorRequirements } from "./scopeHandler.types";
+import type {
+  ScopeHandler,
+  ScopeIteratorRequirements,
+} from "./scopeHandler.types";
 import type { ScopeHandlerFactory } from "./ScopeHandlerFactory";
 
 export class BoundedNonWhitespaceSequenceScopeHandler extends BaseScopeHandler {
   public readonly scopeType = { type: "boundedNonWhitespaceSequence" } as const;
-  public readonly iterationScopeType: ScopeType = {
-    type: "line",
-  } as const;
   protected readonly isHierarchical = true;
+  private readonly nonWhitespaceSequenceScopeHandler: ScopeHandler;
+  private readonly surroundingPairScopeHandler: ScopeHandler;
 
   constructor(
     private scopeHandlerFactory: ScopeHandlerFactory,
@@ -17,6 +27,32 @@ export class BoundedNonWhitespaceSequenceScopeHandler extends BaseScopeHandler {
     private languageId: string,
   ) {
     super();
+
+    this.nonWhitespaceSequenceScopeHandler = this.scopeHandlerFactory.create(
+      { type: "nonWhitespaceSequence" },
+      this.languageId,
+    )!;
+
+    this.surroundingPairScopeHandler = this.scopeHandlerFactory.create(
+      {
+        type: "surroundingPair",
+        delimiter: "any",
+      },
+      this.languageId,
+    )!;
+  }
+
+  get iterationScopeType(): ScopeType {
+    return {
+      type: "oneOf",
+      scopeTypes: [
+        { type: "line" },
+        {
+          type: "surroundingPairInterior",
+          delimiter: "any",
+        },
+      ],
+    };
   }
 
   *generateScopeCandidates(
@@ -25,20 +61,66 @@ export class BoundedNonWhitespaceSequenceScopeHandler extends BaseScopeHandler {
     direction: Direction,
     hints: ScopeIteratorRequirements,
   ): Iterable<TargetScope> {
-    const scopeHandler = this.scopeHandlerFactory.create(
-      {
-        type: "oneOf",
-        scopeTypes: [
-          { type: "nonWhitespaceSequence" },
-          {
-            type: "surroundingPairInterior",
-            delimiter: "any",
-          },
-        ],
-      },
-      this.languageId,
-    )!;
+    const scopes = this.nonWhitespaceSequenceScopeHandler.generateScopes(
+      editor,
+      position,
+      direction,
+      hints,
+      //   {
+      //     ...hints,
+      //     allowAdjacentScopes: true,
+      //     containment: undefined,
+      //   },
+    );
 
-    yield* scopeHandler.generateScopes(editor, position, direction, hints);
+    for (const scope of scopes) {
+      const pairScopes = this.surroundingPairScopeHandler.generateScopes(
+        scope.editor,
+        // scope.domain.start,
+        // "forward",
+        position,
+        direction,
+        hints,
+        // {
+        // ...hints,
+
+        //   distalPosition: scope.domain.end,
+        // },
+      );
+
+      const allScopes = [scope];
+
+      for (const pairScope of pairScopes) {
+        const interiorRange = pairScope.getTargets(false)[0].getInterior()![0]
+          .contentRange;
+        const intersection = scope.domain.intersection(interiorRange);
+        if (intersection != null && !intersection.isEmpty) {
+          allScopes.push(createTargetScope(editor, intersection));
+        }
+      }
+
+      allScopes.sort((a, b) => compareTargetScopes(direction, position, a, b));
+
+      yield* allScopes;
+    }
   }
+}
+
+function createTargetScope(
+  editor: TextEditor,
+  contentRange: Range,
+): TargetScope {
+  return {
+    editor,
+    domain: contentRange,
+    getTargets(isReversed) {
+      return [
+        new TokenTarget({
+          editor,
+          isReversed,
+          contentRange,
+        }),
+      ];
+    },
+  };
 }
