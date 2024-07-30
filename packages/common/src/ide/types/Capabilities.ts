7086d56d62bfec2f1a71739eeffdb3295734dac8
Pokey Rule <755842+pokey@users.noreply.github.com>
2024-07-30 16:33:26 +0100

    docstring

--- packages/common/src/ide/types/Capabilities.ts
+++ packages/common/src/ide/types/Capabilities.ts
@@ -1,6 +1,14 @@
 import { CommandId } from "./CommandId";
 
 export interface Capabilities {
+  /**
+   * Capabilities of the commands that the IDE supports. Note that for many of
+   * these commands, if the IDE does not support them, Cursorless will have a
+   * fairly sophisticated fallback, so it may actually better to report
+   * `undefined`. This will vary per action, though. In the future We will
+   * improve our per-action types / docstrings to make this more clear; see
+   * #1233
+   */
   readonly commands: CommandCapabilityMap;
 }
 
