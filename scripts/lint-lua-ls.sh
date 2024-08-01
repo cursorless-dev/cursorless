6d6addc8bd3a9f2d7ec9002d0f781bf40bfe36b8
Aaron Adams <fidgetingbits@users.noreply.github.com>
2024-08-01 09:14:43 +0800

    feat: Add lua-language-server to CI (#2611)
    - Implement lua-language-server linter in pre-commit
    - Install via Nix due to lack of Ubuntu package
    - Apply patch for --check CLI bug using Nix overlay
    - Centralize linting config for VSCode, pre-commit, and CI
    
    Fixes #2576
    
    
    
    ## Checklist
    
    - [ ] I have added
    [tests](https://www.cursorless.org/docs/contributing/test-case-recorder/)
    - [ ] I have updated the
    [docs](https://github.com/cursorless-dev/cursorless/tree/main/docs) and
    [cheatsheet](https://github.com/cursorless-dev/cursorless/tree/main/cursorless-talon/src/cheatsheet)
    - [ ] I have not broken the cheatsheet
    
    ---------
    
    Co-authored-by: Pokey Rule <755842+pokey@users.noreply.github.com>

---
+++ scripts/lint-lua-ls.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# lua-language-server should be installed automatically by the flake.nix dev shell
+# or .github/workflows/pre-commit.yml
+if ! type lua-language-server &>/dev/null; then
+  echo "ERROR: lua-language-server is not installed. Please run 'nix develop' or install it manually."
+  exit 1
+fi
+
+if [ ! -e "${CURSORLESS_REPO_ROOT-nonexistent}" ]; then
+  CURSORLESS_REPO_ROOT=$(git rev-parse --show-toplevel)
+fi
+
+function check_file() {
+  local file="$1"
+  logpath="$(mktemp -d)"
+  rm -rf "$logpath/check.json"
+  result=$(lua-language-server --check "$file" \
+    --checklevel="Warning" \
+    --configpath="${CURSORLESS_REPO_ROOT}/.luarc.json" \
+    --logpath="$logpath")
+  if [[ ! "$result" == *"no problems found"* ]]; then
+    if [ -e "$logpath/check.json" ]; then
+      cat "$logpath/check.json"
+    else
+      echo "ERROR: lua-language-server failed to run."
+      echo "$result"
+    fi
+    return 1
+
+  fi
+  return 0
+}
+
+# lua-language-server doesn't support single file parsing, so check entire folder
+exit_code=0
+if ! check_file "${CURSORLESS_REPO_ROOT}"; then
+  exit_code=1
+fi
+
+exit $exit_code
