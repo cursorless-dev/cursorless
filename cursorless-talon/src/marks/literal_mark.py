07c5bc27215023cb85168a7660ed57ae94558753
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-08-02 09:04:47 +0200

    Added tags lists and settings

--- cursorless-talon/src/marks/literal_mark.py
+++ cursorless-talon/src/marks/literal_mark.py
@@ -1,20 +1,41 @@
-from talon import Module
+from talon import Context, Module
 
 from .mark_types import LiteralMark
 
 mod = Module()
 
+mod.list("private_cursorless_literal_mark", desc="Cursorless literal mark")
 
-# FIXME: Replace literal *heh* with a Talon list before we make this public
-@mod.capture(rule=("literal <user.text>"))
+# This is a private tag and should not be used by non Cursorless developers
+mod.tag(
+    "private_cursorless_literal_mark_no_prefix",
+    desc="Tag for enabling literal mark without prefix",
+)
+
+ctx_no_prefix = Context()
+ctx_no_prefix.matches = r"""
+tag: user.private_cursorless_literal_mark_no_prefix
+"""
+
+
+@mod.capture(rule="{user.private_cursorless_literal_mark} <user.text>")
 def cursorless_literal_mark(m) -> LiteralMark:
+    return construct_mark(m.text)
+
+
+@ctx_no_prefix.capture("user.cursorless_literal_mark", rule="<user.text>")
+def cursorless_literal_mark_no_prefix(m) -> LiteralMark:
+    return construct_mark(m.text)
+
+
+def construct_mark(text: str) -> LiteralMark:
     return {
         "type": "literal",
         "modifier": {
             "type": "preferredScope",
             "scopeType": {
                 "type": "customRegex",
-                "regex": construct_fuzzy_regex(m.text),
+                "regex": construct_fuzzy_regex(text),
                 "flags": "gui",
             },
         },
