5e1a70195fbf264cf4d8be20cc0fa9fcb32758e9
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-08-04 15:26:03 +0200

    Use collection item scope handler

--- packages/cursorless-engine/src/processTargets/ModifierStageFactoryImpl.ts
+++ packages/cursorless-engine/src/processTargets/ModifierStageFactoryImpl.ts
@@ -21,7 +21,6 @@ import {
   ExcludeInteriorStage,
   InteriorOnlyStage,
 } from "./modifiers/InteriorStage";
-import { ItemStage } from "./modifiers/ItemStage";
 import { LeadingStage, TrailingStage } from "./modifiers/LeadingTrailingStages";
 import { OrdinalScopeStage } from "./modifiers/OrdinalScopeStage";
 import { EndOfStage, StartOfStage } from "./modifiers/PositionStage";
@@ -124,8 +123,6 @@ export class ModifierStageFactoryImpl implements ModifierStageFactory {
     switch (modifier.scopeType.type) {
       case "notebookCell":
         return new NotebookCellStage(modifier);
-      case "collectionItem":
-        return new ItemStage(this.languageDefinitions, this, modifier);
       default:
         // Default to containing syntax scope using tree sitter
         return new LegacyContainingSyntaxScopeStage(
