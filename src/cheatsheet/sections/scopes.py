ccf5f08e6dcd0be9b631d376aee83dae7f6b9411
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-08-06 19:59:21 +0200

    Added cheatsheet description for short block (#2644)
    ## Checklist
    
    - [/] I have added
    [tests](https://www.cursorless.org/docs/contributing/test-case-recorder/)
    - [/] I have updated the
    [docs](https://github.com/cursorless-dev/cursorless/tree/main/docs) and
    [cheatsheet](https://github.com/cursorless-dev/cursorless/tree/main/cursorless-talon/src/cheatsheet)
    - [x] I have not broken the cheatsheet
    
    ---------
    
    Co-authored-by: Phil Cohen <phillip@phillip.io>

--- src/cheatsheet/sections/scopes.py
+++ src/cheatsheet/sections/scopes.py
@@ -9,7 +9,8 @@ def get_scopes():
             "scopeType",
             {
                 "argumentOrParameter": "Argument",
-                "boundedNonWhitespaceSequence": "Non whitespace sequence stopped by surrounding pair delimeters",
+                "boundedNonWhitespaceSequence": "Non-whitespace sequence bounded by surrounding pair delimeters",
+                "boundedParagraph": "Paragraph bounded by surrounding pair delimeters",
             },
         ),
         {
