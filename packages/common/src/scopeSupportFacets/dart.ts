e8c3a32783c63711b141233604f25ac30eb52fbc
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-10-26 15:59:38 -0500

    add: the start of dart lang support

---
+++ packages/common/src/scopeSupportFacets/dart.ts
@@ -0,0 +1,7 @@
+import type { LanguageScopeSupportFacetMap } from "./scopeSupportFacets.types";
+import { ScopeSupportFacetLevel } from "./scopeSupportFacets.types";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const { supported, unsupported, notApplicable } = ScopeSupportFacetLevel;
+
+export const dartScopeSupport: LanguageScopeSupportFacetMap = {};
