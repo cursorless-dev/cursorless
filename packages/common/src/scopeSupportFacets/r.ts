dfe9fcdf2b98e6135f7d64da2f62a11f3e82183c
bluedrink9 <bluedrink9@github.com>
2024-12-28 20:31:37 +1300

    Enable initial r support

---
+++ packages/common/src/scopeSupportFacets/r.ts
@@ -0,0 +1,8 @@
+import type { LanguageScopeSupportFacetMap } from "./scopeSupportFacets.types";
+import { ScopeSupportFacetLevel } from "./scopeSupportFacets.types";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const { supported, unsupported, notApplicable } = ScopeSupportFacetLevel;
+
+export const rScopeSupport: LanguageScopeSupportFacetMap = {
+};
