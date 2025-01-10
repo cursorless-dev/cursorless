42affedab7856f07b7f90460f51727526da3845f
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-01-10 01:42:03 +0100

    Increase performance for Tree sitter matches

--- packages/cursorless-engine/src/languages/TreeSitterQuery/TreeSitterQuery.ts
+++ packages/cursorless-engine/src/languages/TreeSitterQuery/TreeSitterQuery.ts
@@ -1,19 +1,25 @@
 import type { Position, TextDocument } from "@cursorless/common";
 import { showError, type TreeSitter } from "@cursorless/common";
-import { groupBy, uniq } from "lodash-es";
-import type { Point, Query } from "web-tree-sitter";
+import type Parser from "web-tree-sitter";
+import type { Query } from "web-tree-sitter";
 import { ide } from "../../singletons/ide.singleton";
 import { getNodeRange } from "../../util/nodeSelectors";
 import type {
+  ModifiableQueryCapture,
+  MutableQueryCapture,
   MutableQueryMatch,
-  QueryCapture,
   QueryMatch,
 } from "./QueryCapture";
 import { checkCaptureStartEnd } from "./checkCaptureStartEnd";
 import { isContainedInErrorNode } from "./isContainedInErrorNode";
+import { normalizeCaptureName } from "./normalizeCaptureName";
 import { parsePredicates } from "./parsePredicates";
+import { positionToPoint } from "./positionToPoint";
 import { predicateToString } from "./predicateToString";
-import { rewriteStartOfEndOf } from "./rewriteStartOfEndOf";
+import {
+  getStartOfEndOfRange,
+  rewriteStartOfEndOf,
+} from "./rewriteStartOfEndOf";
 import { treeSitterQueryCache } from "./treeSitterQueryCache";
 
 /**
@@ -67,6 +73,12 @@ export class TreeSitterQuery {
     return new TreeSitterQuery(treeSitter, query, predicates);
   }
 
+  hasCapture(name: string): boolean {
+    return this.query.captureNames.some(
+      (n) => normalizeCaptureName(n) === name,
+    );
+  }
+
   matches(
     document: TextDocument,
     start?: Position,
@@ -84,74 +96,111 @@ export class TreeSitterQuery {
     start?: Position,
     end?: Position,
   ): QueryMatch[] {
-    return this.query
-      .matches(this.treeSitter.getTree(document).rootNode, {
-        startPosition: start == null ? undefined : positionToPoint(start),
-        endPosition: end == null ? undefined : positionToPoint(end),
-      })
-      .map(
-        ({ pattern, captures }): MutableQueryMatch => ({
-          patternIdx: pattern,
-          captures: captures.map(({ name, node }) => ({
-            name,
-            node,
-            document,
-            range: getNodeRange(node),
-            insertionDelimiter: undefined,
-            allowMultiple: false,
-            hasError: () => isContainedInErrorNode(node),
-          })),
-        }),
-      )
-      .filter((match) =>
-        this.patternPredicates[match.patternIdx].every((predicate) =>
-          predicate(match),
-        ),
-      )
-      .map((match): QueryMatch => {
-        // Merge the ranges of all captures with the same name into a single
-        // range and return one capture with that name.  We consider captures
-        // with names `@foo`, `@foo.start`, and `@foo.end` to have the same
-        // name, for which we'd return a capture with name `foo`.
-        const captures: QueryCapture[] = Object.entries(
-          groupBy(match.captures, ({ name }) => normalizeCaptureName(name)),
-        ).map(([name, captures]) => {
-          captures = rewriteStartOfEndOf(captures);
-          const capturesAreValid = checkCaptureStartEnd(
-            captures,
-            ide().messages,
-          );
+    const results: QueryMatch[] = [];
+    const isTesting = ide().runMode === "test";
 
-          if (!capturesAreValid && ide().runMode === "test") {
-            throw new Error("Invalid captures");
-          }
+    const matches = this.query.matches(
+      this.treeSitter.getTree(document).rootNode,
+      {
+        startPosition: start != null ? positionToPoint(start) : undefined,
+        endPosition: end != null ? positionToPoint(end) : undefined,
+      },
+    );
 
-          return {
-            name,
-            range: captures
-              .map(({ range }) => range)
-              .reduce((accumulator, range) => range.union(accumulator)),
-            allowMultiple: captures.some((capture) => capture.allowMultiple),
-            insertionDelimiter: captures.find(
-              (capture) => capture.insertionDelimiter != null,
-            )?.insertionDelimiter,
-            hasError: () => captures.some((capture) => capture.hasError()),
-          };
-        });
+    for (const match of matches) {
+      const mutableMatch = createMutableQueryMatch(document, match);
 
-        return { ...match, captures };
+      if (this.runPredicates(mutableMatch)) {
+        results.push(createQueryMatch(mutableMatch, isTesting));
+      }
+    }
+
+    return results;
+  }
+
+  private runPredicates(match: MutableQueryMatch): boolean {
+    for (const predicate of this.patternPredicates[match.patternIdx]) {
+      if (!predicate(match)) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+function createMutableQueryMatch(
+  document: TextDocument,
+  match: Parser.QueryMatch,
+): MutableQueryMatch {
+  return {
+    patternIdx: match.pattern,
+    captures: match.captures.map(({ name, node }) => ({
+      name,
+      node,
+      document,
+      range: getNodeRange(node),
+      insertionDelimiter: undefined,
+      allowMultiple: false,
+      hasError: () => isContainedInErrorNode(node),
+    })),
+  };
+}
+
+function createQueryMatch(
+  match: MutableQueryMatch,
+  isTesting: boolean,
+): QueryMatch {
+  const result: ModifiableQueryCapture[] = [];
+  const resultMap = new Map<
+    string,
+    { acc: ModifiableQueryCapture; captures: MutableQueryCapture[] }
+  >();
+
+  // Merge the ranges of all captures with the same name into a single
+  // range and return one capture with that name.  We consider captures
+  // with names `@foo`, `@foo.start`, and `@foo.end` to have the same
+  // name, for which we'd return a capture with name `foo`.
+
+  for (const capture of match.captures) {
+    const name = normalizeCaptureName(capture.name);
+    const range = getStartOfEndOfRange(capture);
+    const existing = resultMap.get(name);
+
+    if (existing == null) {
+      const accumulator = {
+        name,
+        range,
+        allowMultiple: capture.allowMultiple,
+        insertionDelimiter: capture.insertionDelimiter,
+        hasError: () => capture.hasError(),
+      };
+      result.push(accumulator);
+      resultMap.set(name, {
+        acc: accumulator,
+        captures: [capture],
       });
+    } else {
+      existing.acc.range = existing.acc.range.union(range);
+      existing.acc.allowMultiple =
+        existing.acc.allowMultiple || capture.allowMultiple;
+      existing.acc.insertionDelimiter =
+        existing.acc.insertionDelimiter ?? capture.insertionDelimiter;
+      existing.acc.hasError = () => existing.captures.some((c) => c.hasError());
+      existing.captures.push(capture);
+    }
   }
 
-  get captureNames() {
-    return uniq(this.query.captureNames.map(normalizeCaptureName));
+  if (isTesting) {
+    for (const captureGroup of resultMap.values()) {
+      const capturesAreValid = checkCaptureStartEnd(
+        rewriteStartOfEndOf(captureGroup.captures),
+        ide().messages,
+      );
+      if (!capturesAreValid) {
+        throw new Error("Invalid captures");
+      }
+    }
   }
-}
 
-function normalizeCaptureName(name: string): string {
-  return name.replace(/(\.(start|end))?(\.(startOf|endOf))?$/, "");
-}
-
-function positionToPoint(start: Position): Point {
-  return { row: start.line, column: start.character };
+  return { captures: result };
 }
