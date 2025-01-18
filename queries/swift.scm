f2d8d64e7ba2b6881f9c69e20147682fde758bc5
atacan <atacandur@icloud.com>
2025-01-18 18:36:17 +0100

    swift.scm by adding class and protocol declarations.

--- queries/swift.scm
+++ queries/swift.scm
@@ -1,3 +1,12 @@
 (function_declaration
   name: (_) @functionName
 ) @namedFunction @functionName.domain
+
+(class_declaration
+  name: (_) @className
+) @class @className.domain
+
+[
+  (class_declaration)
+  (protocol_declaration)
+] @namedFunction.iteration @functionName.iteration
