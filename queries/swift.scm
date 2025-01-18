dbda618ccf02989a342af5dc8e7a36e5986948c6
atacan <atacandur@icloud.com>
2025-01-18 19:06:49 +0100

    Enhance swift.scm by adding support for import, property, typealias, array, dictionary, and regex declarations

--- queries/swift.scm
+++ queries/swift.scm
@@ -1,3 +1,9 @@
+[
+  (import_declaration)
+  (property_declaration)
+  (typealias_declaration)
+] @statement
+
 (function_declaration
   name: (_) @functionName
 ) @namedFunction @functionName.domain
@@ -10,3 +16,15 @@
   (class_declaration)
   (protocol_declaration)
 ] @namedFunction.iteration @functionName.iteration
+
+(array_literal) @list
+
+(dictionary_literal) @map
+
+(regex_literal) @regularExpression
+
+[
+  (class_declaration)
+  (protocol_declaration)
+  (function_declaration)
+] @statement.iteration
