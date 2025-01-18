231064a5b558a973f5121c1a47f6644128c590a7
atacan <atacandur@icloud.com>
2025-01-18 17:49:08 +0100

    start small

--- packages/common/src/scopeSupportFacets/swift.ts
+++ packages/common/src/scopeSupportFacets/swift.ts
@@ -4,50 +4,10 @@ import { ScopeSupportFacetLevel } from "./scopeSupportFacets.types";
 const { supported, unsupported, notApplicable } = ScopeSupportFacetLevel;
 
 export const swiftScopeSupport: LanguageScopeSupportFacetMap = {
-  // Collections
-  list: supported,
-  map: supported,
-
-  // Control flow
-  ifStatement: supported,
-  "condition.if": supported,
-  "condition.switchCase": supported,
-  "condition.switchCase.iteration": supported,
-
-  // Text fragments
-  "string.singleLine": supported,
-  "string.multiLine": supported,
-  "comment.line": supported,
-  "comment.block": supported,
-
-  // Statements and blocks
-  statement: supported,
-
-  // Classes and types
-  class: supported,
-  className: supported,
-  "type.class": supported,
-  "type.variable": supported,
-  "type.return": supported,
-  "type.field": supported,
-
-  // Functions
-  anonymousFunction: supported,
   namedFunction: supported,
-  functionCall: supported,
-  functionCallee: supported,
-
-  // Arguments and parameters
-  "argument.formal": supported,
-  "argument.actual": supported,
-
-  // Names and values
-  "name.function": supported,
-  "name.variable": supported,
-  "name.field": supported,
-  "value.variable": supported,
-  "value.field": supported,
-
-  // Swift-specific features
-  regularExpression: notApplicable, // Swift doesn't have regex literals
+  "namedFunction.method": supported,
+  "namedFunction.method.iteration.class": supported,
+  "namedFunction.constructor": supported,
+  "namedFunction.iteration": supported,
+  "namedFunction.iteration.document": supported,
 };
