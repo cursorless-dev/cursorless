bfabb549df1f328c06304a5aa372dd313c56d0ca
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-01-22 14:23:16 +0100

    Save files

--- packages/cursorless-vscode/src/migrateSnippets.ts
+++ packages/cursorless-vscode/src/migrateSnippets.ts
@@ -1,40 +1,104 @@
-import type { SnippetMap } from "@cursorless/common";
+import type {
+  SnippetMap,
+  SnippetVariable as SnippetVariableLegacy,
+} from "@cursorless/common";
 import * as fs from "node:fs/promises";
 import * as path from "node:path";
-import type { SnippetDocument } from "talon-snippets";
+import {
+  serializeSnippetFile,
+  type SnippetDocument,
+  type SnippetVariable,
+} from "talon-snippets";
 import type { VscodeSnippets } from "./VscodeSnippets";
 
-export async function migrateSnippets(snippets: VscodeSnippets, dir: string) {
-  console.log("migrateSnippets");
-  console.log(dir);
+export async function migrateSnippets(
+  snippets: VscodeSnippets,
+  targetDirectory: string,
+) {
   const userSnippetsDir = snippets.getUserDirectoryStrict();
   const files = await snippets.getSnippetPaths(userSnippetsDir);
 
   for (const file of files) {
-    await migrateFile(file);
+    await migrateFile(targetDirectory, file);
   }
 }
 
-async function migrateFile(filePath: string) {
+async function migrateFile(targetDirectory: string, filePath: string) {
   const fileName = path.basename(filePath, ".cursorless-snippets");
-  const snippetFile = await readFile(filePath);
+  const snippetFile = await readLegacyFile(filePath);
   const communitySnippetFile: SnippetDocument[] = [];
-
-  console.log(fileName);
-
-  console.log(snippetFile);
 
   for (const snippetName in snippetFile) {
     const snippet = snippetFile[snippetName];
-    console.log(snippetName);
-    console.log(snippet);
+
+    communitySnippetFile.push({
+      name: snippetName,
+      variables: parseVariables(snippet.variables),
+      insertionScopes: snippet.insertionScopeTypes,
+      // TODO: snippet.description
+    });
+
+    for (const def of snippet.definitions) {
+      communitySnippetFile.push({
+        body: def.body.map((line) => line.replaceAll("\t", "    ")),
+        languages: def.scope?.langIds,
+        variables: parseVariables(def.variables),
+        // TODO: def.scope?.scopeTypes
+        // TODO: def.scope?.excludeDescendantScopeTypes
+      });
+    }
+  }
+
+  let destinationPath = path.join(targetDirectory, `${fileName}.snippet`);
+  if (await fileExists(destinationPath)) {
+    destinationPath = path.join(
+      targetDirectory,
+      `${fileName}_CONFLICT.snippet`,
+    );
+  }
+  await writeCommunityFile(communitySnippetFile, destinationPath);
+}
+
+function parseVariables(
+  variables?: Record<string, SnippetVariableLegacy>,
+): SnippetVariable[] {
+  return Object.entries(variables ?? {}).map(
+    ([name, variable]): SnippetVariable => {
+      return {
+        name,
+        wrapperScope: variable.wrapperScopeType,
+        insertionFormatters: variable.formatter
+          ? [variable.formatter]
+          : undefined,
+        // TODO: variable.description
+      };
+    },
+  );
+}
+
+async function fileExists(filePath: string): Promise<boolean> {
+  try {
+    await fs.access(filePath);
+    return true;
+  } catch (_e) {
+    return false;
   }
 }
 
-async function readFile(filePath: string): Promise<SnippetMap> {
+async function readLegacyFile(filePath: string): Promise<SnippetMap> {
   const content = await fs.readFile(filePath, "utf8");
   if (content.length === 0) {
     return {};
   }
   return JSON.parse(content);
+}
+
+async function writeCommunityFile(
+  snippetFile: SnippetDocument[],
+  filePath: string,
+) {
+  const snippetText = serializeSnippetFile(snippetFile);
+  const file = await fs.open(filePath, "w");
+  await file.write(snippetText);
+  await file.close();
 }
