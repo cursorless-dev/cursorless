7646f6e103b03d4f436102698ce9003dd16c6b1f
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-01-23 18:14:56 +0100

    Use path return

--- cursorless-talon/src/actions/generate_snippet.py
+++ cursorless-talon/src/actions/generate_snippet.py
@@ -32,19 +32,19 @@ class UserActions:
             {
                 "name": "generateSnippet",
                 "target": target,
-                "dirPath": get_dir_path(),
+                "dirPath": str(get_dir_path()),
             }
         )
 
 
-def get_dir_path() -> str:
+def get_dir_path() -> Path:
     settings_dir = get_setting_dir()
     if settings_dir is not None:
         return settings_dir
     return get_community_snippets_dir()
 
 
-def get_setting_dir() -> str | None:
+def get_setting_dir() -> Path | None:
     try:
         setting_dir = settings.get("user.snippets_dir")
         if not setting_dir:
@@ -56,16 +56,16 @@ def get_setting_dir() -> str | None:
             user_dir = Path(actions.path.talon_user())
             dir = user_dir / dir
 
-        return str(dir.resolve())
+        return dir.resolve()
     except Exception:
         return None
 
 
-def get_community_snippets_dir() -> str:
+def get_community_snippets_dir() -> Path:
     files = glob.iglob(
         f"{actions.path.talon_user()}/**/snippets/snippets/*.snippet",
         recursive=True,
     )
     for file in files:
-        return str(Path(file).parent)
+        return Path(file).parent
     raise ValueError("Could not find community snippets directory")
