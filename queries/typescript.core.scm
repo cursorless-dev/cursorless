f4adfdbc217b9b59c0d01357b4fd5bd15eaee655
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-01-25 06:39:30 +0100

    Added support for function signatures in typescript

--- queries/typescript.core.scm
+++ queries/typescript.core.scm
@@ -4,6 +4,14 @@
 
 ;; import javascript.core.scm
 
+;;!! class Aaa { bbb(); }
+;;!              ^^^^^^
+(_
+  (method_signature) @statement.start
+  .
+  ";"? @statement.end
+)
+
 ;;!! function aaa(bbb = "ddd") {}
 ;;!               ^^^--------
 (required_parameter
@@ -41,6 +49,12 @@
 ;; Define these here because these node types don't exist in javascript.
 (_
   [
+    ;;!! function foo();
+    ;;!  ^^^^^^^^^^^^^^^
+    (function_signature
+      name: (_) @functionName @name
+    )
+
     ;;!! class Foo { foo() {} }
     ;;!              ^^^^^^^^
     ;;!! interface Foo { foo(): void; }
