b9c65c2f4a11a39e2d2647b47ffe137841a906a3
Phil Cohen <phillip@phillip.io>
2025-01-28 12:26:29 -0800

    Update packages/cursorless-engine/src/processTargets/modifiers/scopeHandlers/util/isHintsEveryScope.ts

--- packages/cursorless-engine/src/processTargets/modifiers/scopeHandlers/util/isHintsEveryScope.ts
+++ packages/cursorless-engine/src/processTargets/modifiers/scopeHandlers/util/isHintsEveryScope.ts
@@ -1,7 +1,7 @@
 import type { ScopeIteratorRequirements } from "../scopeHandler.types";
 
 /**
- * Returns true if the hints belong to the every scope modifier.
+ * Returns whether the hints belong to the every scope modifier.
  */
 export function isEveryScopeModifier(
   hints: ScopeIteratorRequirements,
