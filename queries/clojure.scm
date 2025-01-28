cursorless-dev/cursorless

commit d98bf092732b23d5a97717b9315033b4e32a1970

Collection item scope provider (#2683)
Fixes #1059

Related https://github.com/cursorless-dev/cursorless/issues/1052

## Checklist

- [x] I have added
[tests](https://www.cursorless.org/docs/contributing/test-case-recorder/)
- [/] I have updated the
[docs](https://github.com/cursorless-dev/cursorless/tree/main/docs) and
[cheatsheet](https://github.com/cursorless-dev/cursorless/tree/main/cursorless-talon/src/cheatsheet)
- [/] I have not broken the cheatsheet

---------

Co-authored-by: Phil Cohen <phillip@phillip.io>
Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

data/fixtures/recorded/languages/clojure/chuckItemZip.yml
@@ -29,9 +29,7 @@ finalState:
   documentContents: |-
     {
         :foo "bar",
-        ;; hello
-    ,
     }
   selections:
-    - anchor: {line: 4, character: 1}
-      active: {line: 4, character: 1}
+    - anchor: {line: 2, character: 1}
+      active: {line: 2, character: 1}

data/fixtures/recorded/languages/clojure/clearItemFine.yml
@@ -21,7 +21,7 @@ initialState:
       start: {line: 0, character: 2}
       end: {line: 0, character: 5}
 finalState:
-  documentContents: "{ :baz \"whatever\"}"
+  documentContents: "{}"
   selections:
     - anchor: {line: 0, character: 1}
       active: {line: 0, character: 1}

queries/clojure.scm
@@ -11,3 +11,33 @@
 (quoting_lit
   (list_lit)
 ) @list
+
+;;!! '(foo bar)
+;;!    ^^^ ^^^
+(list_lit
+  (_)? @_.leading.endOf
+  .
+  (_) @collectionItem.start
+  .
+  (_)? @_.trailing.startOf
+)
+
+(list_lit
+  open: "(" @collectionItem.iteration.start.startOf
+  close: ")" @collectionItem.iteration.end.endOf
+) @collectionItem.iteration.domain
+
+;;!! [foo bar]
+;;!   ^^^ ^^^
+(vec_lit
+  (_)? @_.leading.endOf
+  .
+  (_) @collectionItem.start
+  .
+  (_)? @_.trailing.startOf
+)
+
+(vec_lit
+  open: "[" @collectionItem.iteration.start.startOf
+  close: "]" @collectionItem.iteration.end.endOf
+) @collectionItem.iteration.domain

commit fede4a213f48bb9c0b2052288c194ceb574a37bf

Improve item collection definition for clojure (#2758)
Fix some of the clojure collection item regression from #2683

Related #2723


## Checklist

- [x] I have added
[tests](https://www.cursorless.org/docs/contributing/test-case-recorder/)
- [/] I have updated the
[docs](https://github.com/cursorless-dev/cursorless/tree/main/docs) and
[cheatsheet](https://github.com/cursorless-dev/cursorless/tree/main/cursorless-talon/src/cheatsheet)
- [/] I have not broken the cheatsheet

data/fixtures/recorded/languages/clojure/chuckItemZip.yml
@@ -29,7 +29,9 @@ finalState:
   documentContents: |-
     {
         :foo "bar",
+        ;; hello
+    ,
     }
   selections:
-    - anchor: {line: 2, character: 1}
-      active: {line: 2, character: 1}
+    - anchor: {line: 4, character: 1}
+      active: {line: 4, character: 1}

data/fixtures/recorded/languages/clojure/clearItemBat.yml
@@ -28,7 +28,9 @@ initialState:
 finalState:
   documentContents: |-
     {
-        ,
+        
+        ;; hello
+        "whatever",
     }
   selections:
     - anchor: {line: 1, character: 4}

data/fixtures/recorded/languages/clojure/clearItemFine.yml
@@ -21,7 +21,7 @@ initialState:
       start: {line: 0, character: 2}
       end: {line: 0, character: 5}
 finalState:
-  documentContents: "{}"
+  documentContents: "{ :baz \"whatever\"}"
   selections:
     - anchor: {line: 0, character: 1}
       active: {line: 0, character: 1}

queries/clojure.scm
@@ -8,6 +8,7 @@
 
 ;; A list is either a vector literal or a quoted list literal
 (vec_lit) @list
+
 (quoting_lit
   (list_lit)
 ) @list
@@ -17,14 +18,14 @@
 (list_lit
   (_)? @_.leading.endOf
   .
-  (_) @collectionItem.start
+  (_) @collectionItem
   .
   (_)? @_.trailing.startOf
 )
 
 (list_lit
-  open: "(" @collectionItem.iteration.start.startOf
-  close: ")" @collectionItem.iteration.end.endOf
+  open: "(" @collectionItem.iteration.start.endOf
+  close: ")" @collectionItem.iteration.end.startOf
 ) @collectionItem.iteration.domain
 
 ;;!! [foo bar]
@@ -32,12 +33,49 @@
 (vec_lit
   (_)? @_.leading.endOf
   .
-  (_) @collectionItem.start
+  (_) @collectionItem
   .
   (_)? @_.trailing.startOf
 )
 
 (vec_lit
-  open: "[" @collectionItem.iteration.start.startOf
-  close: "]" @collectionItem.iteration.end.endOf
+  open: "[" @collectionItem.iteration.start.endOf
+  close: "]" @collectionItem.iteration.end.startOf
+) @collectionItem.iteration.domain
+
+;; Keyword follow by a value
+(map_lit
+  (_)? @_.leading.endOf
+  .
+  (kwd_lit) @collectionItem.start
+  .
+  value: (_) @collectionItem.end
+  .
+  (_)? @_.trailing.startOf
+)
+
+;; Keyword followed by comment or closing brace
+(map_lit
+  (_)? @_.leading.endOf
+  .
+  (kwd_lit) @collectionItem.start
+  .
+  [
+    (comment) @_.trailing.startOf
+    "}"
+  ]
+)
+
+;; Non keyword value that is not preceded by a keyword. eg a string literal.
+(map_lit
+  _ @_dummy
+  .
+  value: (_) @collectionItem
+  (#not-type? @_dummy "kwd_lit")
+  (#not-type? @collectionItem "kwd_lit")
+)
+
+(map_lit
+  open: "{" @collectionItem.iteration.start.endOf
+  close: "}" @collectionItem.iteration.end.startOf
 ) @collectionItem.iteration.domain