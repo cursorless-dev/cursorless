62d49eb423a5676b29c9b9c99c0ab6b63bf920c1
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-01-28 21:10:35 +0100

    Make cheat sheet handle missing spoken forms (#2760)
    Today the cheat sheet generation crashes if the user has disabled
    certain spoken forms
    
    Fixes #2647
    
    ## Checklist
    
    - [/] I have added
    [tests](https://www.cursorless.org/docs/contributing/test-case-recorder/)
    - [/] I have updated the
    [docs](https://github.com/cursorless-dev/cursorless/tree/main/docs) and
    [cheatsheet](https://github.com/cursorless-dev/cursorless/tree/main/cursorless-talon/src/cheatsheet)
    - [/] I have not broken the cheatsheet

--- cursorless-talon/src/cheatsheet/sections/get_scope_visualizer.py
+++ cursorless-talon/src/cheatsheet/sections/get_scope_visualizer.py
@@ -1,29 +1,37 @@
-from ..get_list import get_list, get_raw_list, make_readable
+from ..get_list import ListItemDescriptor, get_list, get_raw_list, make_readable
 
 
-def get_scope_visualizer():
-    show_scope_visualizer = list(get_raw_list("show_scope_visualizer").keys())[0]
+def get_scope_visualizer() -> list[ListItemDescriptor]:
+    show_scope_visualizers = list(get_raw_list("show_scope_visualizer").keys())
+    show_scope_visualizer = (
+        show_scope_visualizers[0] if show_scope_visualizers else None
+    )
     visualization_types = get_raw_list("visualization_type")
 
-    return [
-        *get_list("hide_scope_visualizer", "command"),
-        {
-            "id": "show_scope_visualizer",
-            "type": "command",
-            "variations": [
-                {
-                    "spokenForm": f"{show_scope_visualizer} <scope>",
-                    "description": "Visualize <scope>",
-                },
-                *[
+    items = get_list("hide_scope_visualizer", "command")
+
+    if show_scope_visualizer:
+        items.append(
+            {
+                "id": "show_scope_visualizer",
+                "type": "command",
+                "variations": [
                     {
-                        "spokenForm": f"{show_scope_visualizer} <scope> {spoken_form}",
-                        "description": f"Visualize <scope> {make_readable(id).lower()} range",
-                    }
-                    for spoken_form, id in visualization_types.items()
+                        "spokenForm": f"{show_scope_visualizer} <scope>",
+                        "description": "Visualize <scope>",
+                    },
+                    *[
+                        {
+                            "spokenForm": f"{show_scope_visualizer} <scope> {spoken_form}",
+                            "description": f"Visualize <scope> {make_readable(id).lower()} range",
+                        }
+                        for spoken_form, id in visualization_types.items()
+                    ],
                 ],
-            ],
-        },
+            }
+        )
+
+    items.append(
         {
             "id": "show_scope_sidebar",
             "type": "command",
@@ -33,5 +41,7 @@ def get_scope_visualizer():
                     "description": "Show cursorless sidebar",
                 },
             ],
-        },
-    ]
+        }
+    )
+
+    return items
