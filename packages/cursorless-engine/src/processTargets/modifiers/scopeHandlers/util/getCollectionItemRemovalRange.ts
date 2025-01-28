e2723582c0ca0b8ef5b9699ccedaddbdc2fe09d7
Phil Cohen <phillip@phillip.io>
2025-01-28 12:36:08 -0800

    Update packages/cursorless-engine/src/processTargets/modifiers/scopeHandlers/util/getCollectionItemRemovalRange.ts

--- packages/cursorless-engine/src/processTargets/modifiers/scopeHandlers/util/getCollectionItemRemovalRange.ts
+++ packages/cursorless-engine/src/processTargets/modifiers/scopeHandlers/util/getCollectionItemRemovalRange.ts
@@ -4,9 +4,6 @@ import { getRangeLength } from "../../../../util/rangeUtils";
 
 /**
  * Picks which of the leading and trailing delimiter ranges to use for removal when both are present.
- *
- * If the leading one is longer/more specific, prefer to use that for removal; otherwise,
- * use undefined to force a fallback to the default behavior (often trailing)
  */
 export function getCollectionItemRemovalRange(
   isEveryScope: boolean,
