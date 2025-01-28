11b94f00a56e75b8d6818da02977434aa00eaec1
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-01-28 21:10:35 +0100

    Make cheat sheet handle missing spoken forms (#2760)
    Today the cheat sheet generation crashes if the user has disabled
    certain spoken forms
    
    Fixes #2647
    
    ## Checklist
    
    - [/] I have added
    [tests](https://www.cursorless.org/docs/contributing/test-case-recorder/)
    - [/] I have updated the
    [docs](https://github.com/cursorless-dev/cursorless/tree/main/docs) and
    [cheatsheet](https://github.com/cursorless-dev/cursorless/tree/main/cursorless-talon/src/cheatsheet)
    - [/] I have not broken the cheatsheet

--- src/cheatsheet/get_list.py
+++ src/cheatsheet/get_list.py
@@ -41,7 +41,7 @@ def get_raw_list(name: str) -> Mapping[str, str]:
     return typing.cast(dict[str, str], registry.lists[cursorless_list_name][0]).copy()
 
 
-def get_spoken_form_from_list(list_name: str, value: str) -> str:
+def get_spoken_form_from_list(list_name: str, value: str) -> str | None:
     """Get the spoken form of a value from a list.
 
     Args:
@@ -49,10 +49,15 @@ def get_spoken_form_from_list(list_name: str, value: str) -> str:
         value (str): The value to look up.
 
     Returns:
-        str: The spoken form of the value.
+        str: The spoken form of the value if found, otherwise None.
     """
     return next(
-        spoken_form for spoken_form, v in get_raw_list(list_name).items() if v == value
+        (
+            spoken_form
+            for spoken_form, v in get_raw_list(list_name).items()
+            if v == value
+        ),
+        None,
     )
 
 
