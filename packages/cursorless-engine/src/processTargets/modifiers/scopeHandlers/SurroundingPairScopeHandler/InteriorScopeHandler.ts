5c444eb895b4f9c173f042f796ac4faa2cf6a7a7
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-01-30 18:12:34 +0100

    revert comments

--- packages/cursorless-engine/src/processTargets/modifiers/scopeHandlers/SurroundingPairScopeHandler/InteriorScopeHandler.ts
+++ packages/cursorless-engine/src/processTargets/modifiers/scopeHandlers/SurroundingPairScopeHandler/InteriorScopeHandler.ts
@@ -79,12 +79,10 @@ export class InteriorScopeHandler extends BaseScopeHandler {
       this.languageId,
     );
 
-    /* 
-        If the scope type is explicit (ie, the user has specified a scope
-        type), then we want to prioritize language scopes. The user might
-        have said something like "inside element" and then we don't want to
-        yield the interior of the `<div>` pair first.
-    */
+    // If the scope type is explicit (ie, the user has specified a scope
+    // type), then we want to prioritize language scopes. The user might
+    // have said something like "inside element" and then we don't want to
+    // yield the interior of the `<div>` pair first.
     if (this.scopeType.explicitScopeType) {
       if (languageScopeHandler == null) {
         return pairScopeHandler;
@@ -114,12 +112,10 @@ export class InteriorScopeHandler extends BaseScopeHandler {
   }
 
   private shouldYield(targetDomain: Range, scope: TargetScope): boolean {
-    /* 
-        For an explicit scope type we only yield scopes that are contained within
-        the target domain. E.g the user said "inside token", then we don't want
-        to yield scopes that are larger than the token. The definition of an
-        interior is that it's inside the scope.
-    */
+    // For an explicit scope type we only yield scopes that are contained within
+    // the target domain. E.g the user said "inside token", then we don't want
+    // to yield scopes that are larger than the token. The definition of an
+    // interior is that it's inside the scope.
     return (
       !this.scopeType.explicitScopeType || targetDomain.contains(scope.domain)
     );
