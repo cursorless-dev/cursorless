e2dac23fc0a94a1e47b5f0abcf7585d381cad919
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-02-04 10:53:38 +0100

    Added padding

--- packages/common/src/scopeSupportFacets/scopeSupportFacetInfos.ts
+++ packages/common/src/scopeSupportFacets/scopeSupportFacetInfos.ts
@@ -749,6 +749,7 @@ export const scopeSupportFacetInfos: Record<
     description: "The body of an for/while loop",
     scopeType: { type: "interior" },
   },
+
   notebookCell: {
     description: "A cell in a notebook or a markdown code block",
     scopeType: "notebookCell",
