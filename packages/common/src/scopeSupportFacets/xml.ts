2c013e8690eaefdafd333a83826dadab77ba3960
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-02-12 18:05:40 +0100

    Update packages/common/src/scopeSupportFacets/xml.ts

--- packages/common/src/scopeSupportFacets/xml.ts
+++ packages/common/src/scopeSupportFacets/xml.ts
@@ -2,7 +2,7 @@ import type { LanguageScopeSupportFacetMap } from "./scopeSupportFacets.types";
 import { ScopeSupportFacetLevel } from "./scopeSupportFacets.types";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const { supported, notApplicable } = ScopeSupportFacetLevel;
+const { supported, unsupported, notApplicable } = ScopeSupportFacetLevel;
 
 export const xmlScopeSupport: LanguageScopeSupportFacetMap = {
   "comment.block": supported,
