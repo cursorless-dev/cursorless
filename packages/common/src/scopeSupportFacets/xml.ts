689146bc9c4bea2102dea27bc8da1396b23167a2
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-02-12 18:05:21 +0100

    Update packages/common/src/scopeSupportFacets/xml.ts

--- packages/common/src/scopeSupportFacets/xml.ts
+++ packages/common/src/scopeSupportFacets/xml.ts
@@ -1,6 +1,7 @@
 import type { LanguageScopeSupportFacetMap } from "./scopeSupportFacets.types";
 import { ScopeSupportFacetLevel } from "./scopeSupportFacets.types";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { supported, notApplicable } = ScopeSupportFacetLevel;
 
 export const xmlScopeSupport: LanguageScopeSupportFacetMap = {
