8e72138ef7b2ea6b7c250be1fcfcca7ff094d150
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-02-22 14:39:50 +0100

    Don't discard nodes with non adjacent error siblings

--- packages/cursorless-engine/src/languages/TreeSitterQuery/isContainedInErrorNode.ts
+++ packages/cursorless-engine/src/languages/TreeSitterQuery/isContainedInErrorNode.ts
@@ -6,10 +6,24 @@ import type { SyntaxNode } from "web-tree-sitter";
  * @returns True if the given node is contained in an error node
  */
 export function isContainedInErrorNode(node: SyntaxNode) {
-  let currentNode: SyntaxNode | null = node;
+  if (node.hasError) {
+    return true;
+  }
+
+  let currentNode: SyntaxNode | null = node.parent;
 
   while (currentNode != null) {
+    // Ancestral node has errors, but it was not siblings to the previous node
+    // that caused the problem. We don't want to discard a node when a sibling
+    // that isn't adjacent is erroring.
     if (currentNode.hasError) {
+      return false;
+    }
+    // A adjacent sibling node was causing the problem. ie we are right next to the error node.
+    if (
+      currentNode.previousSibling?.isError ||
+      currentNode.nextSibling?.isError
+    ) {
       return true;
     }
     currentNode = currentNode.parent;
