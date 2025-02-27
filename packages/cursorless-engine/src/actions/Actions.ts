af2cdaf3b04a75debf48afbe6ac53c652111564e
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-02-27 08:49:45 +0100

    Cleanup

--- packages/cursorless-engine/src/actions/Actions.ts
+++ packages/cursorless-engine/src/actions/Actions.ts
@@ -75,118 +75,112 @@ import { Decrement, Increment } from "./incrementDecrement";
  */
 export class Actions implements ActionRecord {
   constructor(
-    private treeSitter: TreeSitter,
-    private snippets: Snippets,
-    private rangeUpdater: RangeUpdater,
-    private modifierStageFactory: ModifierStageFactory,
+    treeSitter: TreeSitter,
+    snippets: Snippets,
+    rangeUpdater: RangeUpdater,
+    modifierStageFactory: ModifierStageFactory,
   ) {
-  this.addSelection = new AddSelection();
-  this.addSelectionBefore = new AddSelectionBefore();
-  this.addSelectionAfter = new AddSelectionAfter();
-  this.callAsFunction = new Call(this);
-  this.clearAndSetSelection = new Clear(this);
-  this.copyToClipboard = new CopyToClipboard(this, this.rangeUpdater);
-  this.cutToClipboard = new CutToClipboard(this);
-  this.decrement = new Decrement(this);
-  this.deselect = new Deselect();
-  this.editNew = new EditNew(this.rangeUpdater, this);
-  this.editNewLineAfter = new EditNewAfter(
-    this,
-    this.modifierStageFactory,
-  );
-  this.editNewLineBefore = new EditNewBefore(
-    this,
-    this.modifierStageFactory,
-  );
-  this.executeCommand = new ExecuteCommand(this.rangeUpdater);
-  this.extractVariable = new ExtractVariable(this.rangeUpdater);
-  this.findInDocument = new FindInDocument(this);
-  this.findInWorkspace = new FindInWorkspace(this);
-  this.flashTargets = new FlashTargets();
-  this.foldRegion = new Fold(this.rangeUpdater);
-  this.followLink = new FollowLink({ openAside: false });
-  this.followLinkAside = new FollowLink({ openAside: true });
-  this.generateSnippet = new GenerateSnippet(this.snippets);
-  this.getText = new GetText();
-  this.gitAccept = new GitAccept(this.rangeUpdater);
-  this.gitRevert = new GitRevert(this.rangeUpdater);
-  this.gitStage = new GitStage(this.rangeUpdater);
-  this.gitUnstage = new GitUnstage(this.rangeUpdater);
-  this.highlight = new Highlight();
-  this.increment = new Increment(this);
-  this.indentLine = new IndentLine(this.rangeUpdater);
-  this.insertCopyAfter = new InsertCopyAfter(
-    this.rangeUpdater,
-    this.modifierStageFactory,
-  );
-  this.insertCopyBefore = new InsertCopyBefore(
-    this.rangeUpdater,
-    this.modifierStageFactory,
-  );
-  this.insertEmptyLineAfter = new InsertEmptyLineAfter(
-    this.rangeUpdater,
-    this.modifierStageFactory,
-  );
-  this.insertEmptyLineBefore = new InsertEmptyLineBefore(
-    this.rangeUpdater,
-    this.modifierStageFactory,
-  );
-  this.insertEmptyLinesAround = new InsertEmptyLinesAround(
-    this.rangeUpdater,
-    this.modifierStageFactory,
-  );
-  this.insertSnippet = new InsertSnippet(
-    this.rangeUpdater,
-    this.snippets,
-    this,
-    this.modifierStageFactory,
-  );
-  this.joinLines = new JoinLines(this.rangeUpdater, this.modifierStageFactory);
-  this.breakLine = new BreakLine(this.rangeUpdater);
-  this.moveToTarget = new Move(this.rangeUpdater);
-  this.outdentLine = new OutdentLine(this.rangeUpdater);
-  this.pasteFromClipboard = new PasteFromClipboard(this.rangeUpdater, this);
-  this.randomizeTargets = new Random(this);
-  this.remove = new Remove(this.rangeUpdater);
-  this.rename = new Rename(this.rangeUpdater);
-  this.replace = new Replace(this.rangeUpdater);
-  this.replaceWithTarget = new Bring(this.rangeUpdater);
-  this.revealDefinition = new RevealDefinition(this.rangeUpdater);
-  this.revealTypeDefinition = new RevealTypeDefinition(this.rangeUpdater);
-  this.reverseTargets = new Reverse(this);
-  this.rewrapWithPairedDelimiter = new Rewrap(
-    this.rangeUpdater,
-    this.modifierStageFactory,
-  );
-  this.scrollToBottom = new ScrollToBottom();
-  this.scrollToCenter = new ScrollToCenter();
-  this.scrollToTop = new ScrollToTop();
-  this.setSelection = new SetSelection();
-  this.setSelectionAfter = new SetSelectionAfter();
-  this.setSelectionBefore = new SetSelectionBefore();
-  this.showDebugHover = new ShowDebugHover(this.rangeUpdater);
-  this.showHover = new ShowHover(this.rangeUpdater);
-  this.showQuickFix = new ShowQuickFix(this.rangeUpdater);
-  this.showReferences = new ShowReferences(this.rangeUpdater);
-  this.sortTargets = new Sort(this);
-  this.swapTargets = new Swap(this.rangeUpdater);
-  this.toggleLineBreakpoint = new ToggleBreakpoint(this.modifierStageFactory);
-  this.toggleLineComment = new ToggleLineComment(this.rangeUpdater);
-  this.unfoldRegion = new Unfold(this.rangeUpdater);
-  this.wrapWithPairedDelimiter = new Wrap(this.rangeUpdater);
-  this.wrapWithSnippet = new WrapWithSnippet(
-    this.rangeUpdater,
-    this.snippets,
-    this.modifierStageFactory,
-  );
+    this.addSelection = new AddSelection();
+    this.addSelectionBefore = new AddSelectionBefore();
+    this.addSelectionAfter = new AddSelectionAfter();
+    this.callAsFunction = new Call(this);
+    this.clearAndSetSelection = new Clear(this);
+    this.copyToClipboard = new CopyToClipboard(this, rangeUpdater);
+    this.cutToClipboard = new CutToClipboard(this);
+    this.decrement = new Decrement(this);
+    this.deselect = new Deselect();
+    this.editNew = new EditNew(rangeUpdater, this);
+    this.editNewLineAfter = new EditNewAfter(this, modifierStageFactory);
+    this.editNewLineBefore = new EditNewBefore(this, modifierStageFactory);
+    this.executeCommand = new ExecuteCommand(rangeUpdater);
+    this.extractVariable = new ExtractVariable(rangeUpdater);
+    this.findInDocument = new FindInDocument(this);
+    this.findInWorkspace = new FindInWorkspace(this);
+    this.flashTargets = new FlashTargets();
+    this.foldRegion = new Fold(rangeUpdater);
+    this.followLink = new FollowLink({ openAside: false });
+    this.followLinkAside = new FollowLink({ openAside: true });
+    this.generateSnippet = new GenerateSnippet(snippets);
+    this.getText = new GetText();
+    this.gitAccept = new GitAccept(rangeUpdater);
+    this.gitRevert = new GitRevert(rangeUpdater);
+    this.gitStage = new GitStage(rangeUpdater);
+    this.gitUnstage = new GitUnstage(rangeUpdater);
+    this.highlight = new Highlight();
+    this.increment = new Increment(this);
+    this.indentLine = new IndentLine(rangeUpdater);
+    this.insertCopyAfter = new InsertCopyAfter(
+      rangeUpdater,
+      modifierStageFactory,
+    );
+    this.insertCopyBefore = new InsertCopyBefore(
+      rangeUpdater,
+      modifierStageFactory,
+    );
+    this.insertEmptyLineAfter = new InsertEmptyLineAfter(
+      rangeUpdater,
+      modifierStageFactory,
+    );
+    this.insertEmptyLineBefore = new InsertEmptyLineBefore(
+      rangeUpdater,
+      modifierStageFactory,
+    );
+    this.insertEmptyLinesAround = new InsertEmptyLinesAround(
+      rangeUpdater,
+      modifierStageFactory,
+    );
+    this.insertSnippet = new InsertSnippet(
+      rangeUpdater,
+      snippets,
+      this,
+      modifierStageFactory,
+    );
+    this.joinLines = new JoinLines(rangeUpdater, modifierStageFactory);
+    this.breakLine = new BreakLine(rangeUpdater);
+    this.moveToTarget = new Move(rangeUpdater);
+    this.outdentLine = new OutdentLine(rangeUpdater);
+    this.pasteFromClipboard = new PasteFromClipboard(rangeUpdater, this);
+    this.randomizeTargets = new Random(this);
+    this.remove = new Remove(rangeUpdater);
+    this.rename = new Rename(rangeUpdater);
+    this.replace = new Replace(rangeUpdater);
+    this.replaceWithTarget = new Bring(rangeUpdater);
+    this.revealDefinition = new RevealDefinition(rangeUpdater);
+    this.revealTypeDefinition = new RevealTypeDefinition(rangeUpdater);
+    this.reverseTargets = new Reverse(this);
+    this.rewrapWithPairedDelimiter = new Rewrap(
+      rangeUpdater,
+      modifierStageFactory,
+    );
+    this.scrollToBottom = new ScrollToBottom();
+    this.scrollToCenter = new ScrollToCenter();
+    this.scrollToTop = new ScrollToTop();
+    this.setSelection = new SetSelection();
+    this.setSelectionAfter = new SetSelectionAfter();
+    this.setSelectionBefore = new SetSelectionBefore();
+    this.showDebugHover = new ShowDebugHover(rangeUpdater);
+    this.showHover = new ShowHover(rangeUpdater);
+    this.showQuickFix = new ShowQuickFix(rangeUpdater);
+    this.showReferences = new ShowReferences(rangeUpdater);
+    this.sortTargets = new Sort(this);
+    this.swapTargets = new Swap(rangeUpdater);
+    this.toggleLineBreakpoint = new ToggleBreakpoint(modifierStageFactory);
+    this.toggleLineComment = new ToggleLineComment(rangeUpdater);
+    this.unfoldRegion = new Unfold(rangeUpdater);
+    this.wrapWithPairedDelimiter = new Wrap(rangeUpdater);
+    this.wrapWithSnippet = new WrapWithSnippet(
+      rangeUpdater,
+      snippets,
+      modifierStageFactory,
+    );
 
-  this["experimental.setInstanceReference"] = new SetSpecialTarget(
-    "instanceReference",
-  );
+    this["experimental.setInstanceReference"] = new SetSpecialTarget(
+      "instanceReference",
+    );
 
-  this["private.showParseTree"] = new ShowParseTree(this.treeSitter);
-  this["private.getTargets"] = new GetTargets();
-  this["private.setKeyboardTarget"] = new SetSpecialTarget("keyboard");
+    this["private.showParseTree"] = new ShowParseTree(treeSitter);
+    this["private.getTargets"] = new GetTargets();
+    this["private.setKeyboardTarget"] = new SetSpecialTarget("keyboard");
   }
 
   addSelection;
