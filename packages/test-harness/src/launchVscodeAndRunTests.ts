d708b074ec11a296a42e628dca6c3cd99a25dca9
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-03-22 01:31:33 +0100

    thack

--- packages/test-harness/src/launchVscodeAndRunTests.ts
+++ packages/test-harness/src/launchVscodeAndRunTests.ts
@@ -88,6 +88,7 @@ export async function launchVscodeAndRunTests(extensionTestsPath: string) {
   } catch (err) {
     console.error("Test run threw exception:");
     console.error(err);
+    console.error((err as Error).stack);
     process.exit(1);
   }
 }
