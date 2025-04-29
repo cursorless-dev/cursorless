9bf2243ad74eca27c79c67f456deeb7722d36e73
Trillium Smith <spiteless@gmail.com>
2025-04-28 20:12:49 -0700

    refactor: Restrucutre html generation method in loadTestCaseFixture

--- packages/test-case-component/src/loadTestCaseFixture.ts
+++ packages/test-case-component/src/loadTestCaseFixture.ts
@@ -13,79 +13,20 @@ type StepType = { stepName: "initialState" | "middleState" | "finalState" }
 export type DataFixture = TestCaseFixture & StepType
 
 export async function loadTestCaseFixture(data: loadFixtureProps) {
+  const { before, during, after } = await generateHtml(data)
+
+  const { command, filename, languageId: language } = data
+
+  const returnObj = {
+    before, during, after, command, filename, language
+  }
   try {
-    const before = await getBefore({
-      stateName: "initialState",
-      state: data.initialState,
-      languageId: data.languageId,
-    });
 
-    const during = await getDuring(data);
+    return returnObj
 
-    const after = await getAfter({
-      stateName: "finalState",
-      state: data.finalState,
-      languageId: data.languageId,
-    });
-
-    return {
-      language: data.languageId,
-      command: data.command.spokenForm,
-      during,
-      before,
-      after,
-      filename: data.filename,
-    };
   } catch (e) {
-    console.log("error", e);
-    console.log(JSON.stringify(data, null, 2));
+    console.error("error", e);
     throw e;
   }
 }
 
-type Foo = TestCaseSnapshot & TargetPlainObject;
-
-async function getAfter({
-  stateName,
-  state,
-  languageId,
-}: {
-  stateName: string;
-  state: Foo;
-  languageId: BundledLanguage;
-}) {
-  if (!state) {
-    throw new Error("finalState is undefined");
-  }
-  return await safeGenerateHtml({ stateName, state, languageId });
-}
-
-type DataFixture = Partial<TestCaseFixture & TargetPlainObject>
-
-async function getDuring(data: DataFixture) {
-  const { command, ide } = data
-  const stateName = "middleState"
-  const state = data.initialState
-  const languageId = data.languageId as BundledLanguage
-  const genObj: TestCaseSnapshot & { stateName: string } = { stateName, state, languageId, raw: data }
-  if (command) {
-    genObj.command = command
-  }
-  if (ide) {
-    genObj.ide = ide
-  }
-
-  return await generateHtml(genObj);
-}
-
-async function getBefore({
-  stateName,
-  state,
-  languageId,
-}: {
-  stateName: string;
-  state: TestCaseSnapshot;
-  languageId: BundledLanguage;
-}) {
-  return await safeGenerateHtml({ stateName, state, languageId });
-}
