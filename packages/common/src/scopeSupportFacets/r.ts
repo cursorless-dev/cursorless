2642bf6fb4d021a3c47a954c24f4f89fa275a736
bluedrink9 <bluedrink9@users.noreply.github.com>
2025-05-16 08:49:05 +1200

    add initial bunch of r scopes

--- packages/common/src/scopeSupportFacets/r.ts
+++ packages/common/src/scopeSupportFacets/r.ts
@@ -6,6 +6,184 @@ const { supported, unsupported, notApplicable } = ScopeSupportFacetLevel;
 
 export const rScopeSupport: LanguageScopeSupportFacetMap = {
   "comment.line": supported,
-  ifStatement: supported,
+  "ifStatement": supported,
   "argument.actual": supported,
+  "argumentList.formal": supported,
+  "condition.if": supported,
+  "condition.while": unsupported,
+
+  "namedFunction": supported,
+  "anonymousFunction": supported,
+  "functionName": supported,
+  "functionCall": supported,
+  "functionCallee": supported,
+
+  "name.assignment": supported,
+  "name.assignment.pattern": notApplicable,
+  "name.variable": supported,
+  "value.assignment": supported,
+  "value.variable": supported,
+  "value.variable.pattern": notApplicable,
+
+  // "interior.function": unsupported,
+  // "interior.lambda": unsupported,
+  // "interior.if": unsupported,
+  // "interior.try": unsupported,
+
+  command: notApplicable,
+  element: notApplicable,
+  startTag: notApplicable,
+  endTag: notApplicable,
+  tags: notApplicable,
+  attribute: unsupported,
+  environment: unsupported,
+  // Not applicable for base language but might be useful for markdown or quarto
+  section: notApplicable,
+  "section.iteration.document": notApplicable,
+  "section.iteration.parent": notApplicable,
+  notebookCell: notApplicable,
+
+  map: unsupported,
+  regularExpression: unsupported,
+  switchStatementSubject: unsupported,
+  fieldAccess: unsupported,
+  "string.singleLine": unsupported,
+  "string.multiLine": unsupported,
+
+  // "statement.class"
+  // "statement.iteration.document"
+  // "statement.iteration.block"
+  class: unsupported,
+  // "class.iteration.document"
+  // "class.iteration.block"
+  className: unsupported,
+  // "className.iteration.document"
+  // "className.iteration.block"
+  // "namedFunction.method"
+  // "namedFunction.method.iteration.class"
+  // "namedFunction.constructor"
+  // "namedFunction.iteration.block"
+  // "namedFunction.iteration.document"
+  // "functionName.method"
+  // "functionName.method.iteration.class"
+  // "functionName.constructor"
+  // "functionName.iteration.block"
+  // "functionName.iteration.document"
+  // "functionCall.constructor"
+  // "functionCallee.constructor"
+  // "argument.actual"
+  // "argument.actual.iteration"
+  // "argument.actual.method"
+  // "argument.actual.method.iteration"
+  // "argument.actual.constructor"
+  // "argument.actual.constructor.iteration"
+  // "argument.formal"
+  // "argument.formal.iteration"
+  // "argument.formal.method"
+  // "argument.formal.method.iteration"
+  // "argument.formal.constructor"
+  // "argument.formal.constructor.iteration"
+  // "argument.formal.lambda"
+  // "argument.formal.lambda.iteration"
+  // "argumentList.formal.method"
+  // "argumentList.formal.constructor"
+  // "argumentList.formal.lambda"
+  // "textFragment.comment.line"
+  // "textFragment.comment.block"
+  // "textFragment.string.singleLine"
+  // "textFragment.string.multiLine"
+  // "textFragment.element"
+  // disqualifyDelimiter
+  // pairDelimiter
+  "branch.if": unsupported,
+  // "branch.loop"
+  // "branch.if.iteration"
+  // "branch.try"
+  // "branch.try.iteration"
+  // "branch.switchCase"
+  // "branch.switchCase.iteration"
+  // "branch.ternary"
+  // "collectionItem.unenclosed"
+  // "collectionItem.unenclosed.iteration"
+  // "condition.if"
+  // "condition.while"
+  // "condition.doWhile"
+  // "condition.for"
+  // "condition.ternary"
+  // "condition.switchCase"
+  // "condition.switchCase.iteration"
+  // "name.assignment"
+  // "name.assignment.pattern"
+  // "name.variable"
+  // "name.variable.pattern"
+  // "name.foreach"
+  // "name.function"
+  // "name.method"
+  // "name.constructor"
+  // "name.class"
+  // "name.field"
+  // "name.resource"
+  // "name.resource.iteration"
+  // "name.argument.actual"
+  // "name.argument.actual.iteration"
+  // "name.argument.formal"
+  // "name.argument.formal.iteration"
+  // "name.argument.formal.method"
+  // "name.argument.formal.method.iteration"
+  // "name.argument.formal.constructor"
+  // "name.argument.formal.constructor.iteration"
+  // "name.iteration.block"
+  // "name.iteration.document"
+  // "key.attribute"
+  // "key.mapPair"
+  // "key.mapPair.iteration"
+  // "value.mapPair"
+  // "value.mapPair.iteration"
+  // "value.foreach"
+  // "value.attribute"
+  "value.return": unsupported,
+  // "value.return.lambda"
+  // "value.field"
+  // "value.yield"
+  // "value.resource"
+  // "value.resource.iteration"
+  // "value.argument.formal"
+  // "value.argument.formal.iteration"
+  // "value.argument.actual"
+  // "value.argument.actual.iteration"
+  // "value.argument.formal.method"
+  // "value.argument.formal.method.iteration"
+  // "value.argument.formal.constructor"
+  // "value.argument.formal.constructor.iteration"
+  // "value.typeAlias": notApplicable,
+  "type.variable": notApplicable,
+  "type.argument.formal": notApplicable,
+  "type.argument.formal.iteration": notApplicable,
+  "type.argument.formal.method": notApplicable,
+  "type.argument.formal.method.iteration": notApplicable,
+  "type.argument.formal.constructor": notApplicable,
+  "type.argument.formal.constructor.iteration": notApplicable,
+  "type.return": notApplicable,
+  "type.field": notApplicable,
+  "type.field.iteration": notApplicable,
+  "type.foreach": notApplicable,
+  "type.interface": notApplicable,
+  "type.enum": notApplicable,
+  "type.class": notApplicable,
+  "type.alias": notApplicable,
+  "type.cast": notApplicable,
+  "type.typeArgument": notApplicable,
+  "type.typeArgument.iteration": notApplicable,
+  "type.resource": notApplicable,
+  "type.resource.iteration": notApplicable,
+  "interior.element": notApplicable,
+  "interior.command": notApplicable,
+  "interior.cell": notApplicable,
+  "interior.class": unsupported,
+  // "interior.switchCase"
+  // "interior.ternary"
+  // "interior.loop"
+  // "interior.resource"
+
+  "name.foreach": unsupported,
 };
--- queries/r.scm
+++ queries/r.scm
@@ -6,21 +6,132 @@
   (if_statement)
   (repeat_statement)
   (function_definition)
+  (while_statement)
+  (function_definition)
 ] @statement
 
+;;!! # hello
+;;!  ^^^^^^^
 (comment) @comment
-(if_statement) @ifStatement
-(argument) @argumentOrParameter
-(call) @functionCall
-(identifier) @identifier
 
-;; ;; https://github.com/r-lib/tree-sitter-r/blob/main/queries/highlights.scm
+;;!! if (x > 0) { }
+;;!  ^^^^^^^^^^^^^^
+(if_statement) @ifStatement
+
+;; ;;!! if (TRUE) { print("hello") } else { print("world") }
+;; ;;!      ^^^^   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+;; (if_statement
+;;   "if" @branch.start
+;;   (_) @condition
+;;   consequence: (braced_expression) @branch.end
+;;   alternative: (braced_expression)? @branch.end
+;; ) @branch.iteration
+
+;;!! foo(x)
+;;!      ^
+(argument) @argumentOrParameter
+;;!! foo(x)
+;;!  ^^^^^^
+(call) @functionCall
+
+;;!! function(x){ }
+;;!  ^^^^^^^^^^^^^^
+(function_definition) @anonymousFunction
+
+;;!! function("bar")
+;;!           ^^^^^
+(
+  (parameters
+    (_)? @_.leading.endOf
+    .
+    (_) @argumentOrParameter
+    .
+    (_)? @_.trailing.startOf
+  ) @_dummy
+  (#single-or-multi-line-delimiter! @argumentOrParameter @_dummy ", " ",\n")
+)
+(_
+  parameters: (_
+    "(" @argumentList.start.endOf @argumentOrParameter.iteration.start.endOf
+    ")" @argumentList.end.startOf @argumentOrParameter.iteration.end.startOf
+  ) @_dummy
+  (#empty-single-multi-delimiter! @argumentList.start.endOf @_dummy "" ", " ",\n")
+) @argumentList.domain @argumentOrParameter.iteration.domain
+
+(parameters
+  "(" @argumentOrParameter.iteration.start.endOf
+  ")" @argumentOrParameter.iteration.end.startOf
+) @argumentOrParameter.iteration.domain
+
+;;!! abc <- function(x){ }
+;;!  ^^^^^^^^^^^^^^^^^^^^^
+(binary_operator
+  ;;!! abc <- function(x){ }
+  ;;!  ^^^
+  lhs: (identifier) @functionName
+  rhs: (function_definition
+    name: "function"
+    parameters: (parameters)
+    body: (braced_expression) @interior
+  )
+) @namedFunction
+
+;;!! foo()
+;;!  ^^^^^
+;;!  -----
+(call) @functionCall
+
+;;!! foo()
+;;!  ^^^
+;;!  -----
+(call
+  (identifier) @functionCallee
+)
+
+;; Technically lists and arrays are just calls to the function `list` or `c`
+;; TODO: should this include the function name/brackets?
+;;!! list(1, 2, 3)
+;;!  ^^^^^^^^^^^^^
+(call
+  function: (identifier) @functionName
+  (#match? @functionName "^(c|list)$")
+) @list
+
+(binary_operator
+  ;;!! hello <- "world"
+  ;;!  ^^^^^
+  ;;!  -----
+  lhs: (identifier) @name
+  operator: "<-"
+  ;;!! hello <- "world"
+  ;;!           ^^^^^^^
+  ;;!  -----
+  rhs: (_) @value
+)
+(binary_operator
+  ;;!! hello <- "world"
+  ;;!  ^^^^^
+  ;;!  -----
+  lhs: (identifier) @name
+  operator: "="
+  ;;!! hello <- "world"
+  ;;!           ^^^^^^^
+  ;;!  -----
+  rhs: (_) @value
+)
+
+;;!! foo(hello)
+;;!      ^^^^^
+;;!  -----
+;; (identifier) @value
+
+;; ;; from https://github.com/r-lib/tree-sitter-r/blob/main/queries/highlights.scm
 ;; ;; Plus magrittr operators
-;; [ "?" ":=" "=" "<-" "<<-" "->" "->>"
+;; operator: [ "?" ":=" "=" "<-" "<<-" "->" "->>"
 ;;   "~" "|>" "||" "|" "&&" "&"
 ;;   "<" "<=" ">" ">=" "==" "!="
 ;;   "+" "-" "*" "/" "::" ":::"
 ;;   "**" "^" "$" "@" ":" "%in%"
 ;;   "%>%" "%<>%" "%T>%" "%$%"
 ;;   "special"
-;; ] @operator
+;; ] @disqualifyDelimiter
