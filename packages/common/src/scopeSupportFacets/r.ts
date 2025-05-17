84600fff7f40bb44a71916114d83e8acc6fe4922
bluedrink9 <bluedrink9@users.noreply.github.com>
2025-05-17 15:18:08 +1200

    (r) clean up facet list

--- packages/common/src/scopeSupportFacets/r.ts
+++ packages/common/src/scopeSupportFacets/r.ts
@@ -5,185 +5,92 @@ import { ScopeSupportFacetLevel } from "./scopeSupportFacets.types";
 const { supported, unsupported, notApplicable } = ScopeSupportFacetLevel;
 
 export const rScopeSupport: LanguageScopeSupportFacetMap = {
-  "comment.line": supported,
-  "ifStatement": supported,
+  "anonymousFunction": supported,
   "argument.actual": supported,
   "argumentList.formal": supported,
+  "comment.line": supported,
   "condition.if": supported,
-  "condition.while": unsupported,
-
-  "namedFunction": supported,
-  "anonymousFunction": supported,
-  "functionName": supported,
   "functionCall": supported,
   "functionCallee": supported,
-
+  "functionName": supported,
+  "ifStatement": supported,
   "name.assignment": supported,
-  "name.assignment.pattern": notApplicable,
   "name.variable": supported,
+  "namedFunction": supported,
   "value.assignment": supported,
   "value.variable": supported,
-  "value.variable.pattern": notApplicable,
 
-  // "interior.function": unsupported,
-  // "interior.lambda": unsupported,
-  // "interior.if": unsupported,
-  // "interior.try": unsupported,
-
-  command: notApplicable,
-  element: notApplicable,
-  startTag: notApplicable,
-  endTag: notApplicable,
-  tags: notApplicable,
+  "branch.if": unsupported,
+  "branch.if.iteration": unsupported,
+  "branch.loop": unsupported,
+  "branch.switchCase": unsupported,
+  "branch.switchCase.iteration": unsupported,
+  "branch.ternary": unsupported,
+  "branch.try": unsupported,
+  "branch.try.iteration": unsupported,
+  "condition.for": unsupported,
+  "condition.ternary": unsupported,
+  "condition.while": unsupported,
+  "interior.class": unsupported,
+  "interior.function": unsupported,
+  "interior.if": unsupported,
+  "interior.lambda": unsupported,
+  "interior.try": unsupported,
+  "key.attribute": unsupported,
+  "name.class": unsupported,
+  "name.field": unsupported,
+  "name.foreach": unsupported,
+  "name.function": unsupported,
+  "name.method": unsupported,
+  "statement.class": unsupported,
+  "string.multiLine": unsupported,
+  "string.singleLine": unsupported,
+  "value.attribute": unsupported,
+  "value.foreach": unsupported,
+  "value.return": unsupported,
   attribute: unsupported,
+  class: unsupported,
+  className: unsupported,
   environment: unsupported,
-  // Not applicable for base language but might be useful for markdown or quarto
-  section: notApplicable,
-  "section.iteration.document": notApplicable,
-  "section.iteration.parent": notApplicable,
-  notebookCell: notApplicable,
-
+  fieldAccess: unsupported,
   map: unsupported,
   regularExpression: unsupported,
   switchStatementSubject: unsupported,
-  fieldAccess: unsupported,
-  "string.singleLine": unsupported,
-  "string.multiLine": unsupported,
 
-  // "statement.class"
-  // "statement.iteration.document"
-  // "statement.iteration.block"
-  class: unsupported,
-  // "class.iteration.document"
-  // "class.iteration.block"
-  className: unsupported,
-  // "className.iteration.document"
-  // "className.iteration.block"
-  // "namedFunction.method"
-  // "namedFunction.method.iteration.class"
-  // "namedFunction.constructor"
-  // "namedFunction.iteration.block"
-  // "namedFunction.iteration.document"
-  // "functionName.method"
-  // "functionName.method.iteration.class"
-  // "functionName.constructor"
-  // "functionName.iteration.block"
-  // "functionName.iteration.document"
-  // "functionCall.constructor"
-  // "functionCallee.constructor"
-  // "argument.actual"
-  // "argument.actual.iteration"
-  // "argument.actual.method"
-  // "argument.actual.method.iteration"
-  // "argument.actual.constructor"
-  // "argument.actual.constructor.iteration"
-  // "argument.formal"
-  // "argument.formal.iteration"
-  // "argument.formal.method"
-  // "argument.formal.method.iteration"
-  // "argument.formal.constructor"
-  // "argument.formal.constructor.iteration"
-  // "argument.formal.lambda"
-  // "argument.formal.lambda.iteration"
-  // "argumentList.formal.method"
-  // "argumentList.formal.constructor"
-  // "argumentList.formal.lambda"
-  // "textFragment.comment.line"
-  // "textFragment.comment.block"
-  // "textFragment.string.singleLine"
-  // "textFragment.string.multiLine"
-  // "textFragment.element"
-  // disqualifyDelimiter
-  // pairDelimiter
-  "branch.if": unsupported,
-  // "branch.loop"
-  // "branch.if.iteration"
-  // "branch.try"
-  // "branch.try.iteration"
-  // "branch.switchCase"
-  // "branch.switchCase.iteration"
-  // "branch.ternary"
-  // "collectionItem.unenclosed"
-  // "collectionItem.unenclosed.iteration"
-  // "condition.if"
-  // "condition.while"
-  // "condition.doWhile"
-  // "condition.for"
-  // "condition.ternary"
-  // "condition.switchCase"
-  // "condition.switchCase.iteration"
-  // "name.assignment"
-  // "name.assignment.pattern"
-  // "name.variable"
-  // "name.variable.pattern"
-  // "name.foreach"
-  // "name.function"
-  // "name.method"
-  // "name.constructor"
-  // "name.class"
-  // "name.field"
-  // "name.resource"
-  // "name.resource.iteration"
-  // "name.argument.actual"
-  // "name.argument.actual.iteration"
-  // "name.argument.formal"
-  // "name.argument.formal.iteration"
-  // "name.argument.formal.method"
-  // "name.argument.formal.method.iteration"
-  // "name.argument.formal.constructor"
-  // "name.argument.formal.constructor.iteration"
-  // "name.iteration.block"
-  // "name.iteration.document"
-  // "key.attribute"
-  // "key.mapPair"
-  // "key.mapPair.iteration"
-  // "value.mapPair"
-  // "value.mapPair.iteration"
-  // "value.foreach"
-  // "value.attribute"
-  "value.return": unsupported,
-  // "value.return.lambda"
-  // "value.field"
-  // "value.yield"
-  // "value.resource"
-  // "value.resource.iteration"
-  // "value.argument.formal"
-  // "value.argument.formal.iteration"
-  // "value.argument.actual"
-  // "value.argument.actual.iteration"
-  // "value.argument.formal.method"
-  // "value.argument.formal.method.iteration"
-  // "value.argument.formal.constructor"
-  // "value.argument.formal.constructor.iteration"
-  // "value.typeAlias": notApplicable,
-  "type.variable": notApplicable,
+  "interior.cell": notApplicable,
+  "interior.command": notApplicable,
+  "interior.element": notApplicable,
+  "name.assignment.pattern": notApplicable,
+  // Not applicable for base language but might be useful for markdown or quarto
+  "section.iteration.document": notApplicable,
+  "section.iteration.parent": notApplicable,
+  "type.alias": notApplicable,
   "type.argument.formal": notApplicable,
+  "type.argument.formal.constructor": notApplicable,
+  "type.argument.formal.constructor.iteration": notApplicable,
   "type.argument.formal.iteration": notApplicable,
   "type.argument.formal.method": notApplicable,
   "type.argument.formal.method.iteration": notApplicable,
-  "type.argument.formal.constructor": notApplicable,
-  "type.argument.formal.constructor.iteration": notApplicable,
-  "type.return": notApplicable,
+  "type.cast": notApplicable,
+  "type.class": notApplicable,
+  "type.enum": notApplicable,
   "type.field": notApplicable,
   "type.field.iteration": notApplicable,
   "type.foreach": notApplicable,
   "type.interface": notApplicable,
-  "type.enum": notApplicable,
-  "type.class": notApplicable,
-  "type.alias": notApplicable,
-  "type.cast": notApplicable,
-  "type.typeArgument": notApplicable,
-  "type.typeArgument.iteration": notApplicable,
   "type.resource": notApplicable,
   "type.resource.iteration": notApplicable,
-  "interior.element": notApplicable,
-  "interior.command": notApplicable,
-  "interior.cell": notApplicable,
-  "interior.class": unsupported,
-  // "interior.switchCase"
-  // "interior.ternary"
-  // "interior.loop"
-  // "interior.resource"
-
-  "name.foreach": unsupported,
+  "type.return": notApplicable,
+  "type.typeArgument": notApplicable,
+  "type.typeArgument.iteration": notApplicable,
+  "type.variable": notApplicable,
+  "value.typeAlias": notApplicable,
+  "value.variable.pattern": notApplicable,
+  command: notApplicable,
+  element: notApplicable,
+  endTag: notApplicable,
+  notebookCell: notApplicable,
+  section: notApplicable,
+  startTag: notApplicable,
+  tags: notApplicable,
 };
