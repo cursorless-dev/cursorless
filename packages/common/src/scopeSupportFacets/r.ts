6f7c455a43f608727326b24246d8e80784215737
Silico_Biomancer <BlueDrink9@users.noreply.github.com>
2025-05-24 09:38:53 +1200

    Update packages/common/src/scopeSupportFacets/r.ts
    Co-authored-by: Andreas Arvidsson <andreas.arvidsson87@gmail.com>

--- packages/common/src/scopeSupportFacets/r.ts
+++ packages/common/src/scopeSupportFacets/r.ts
@@ -1,7 +1,6 @@
 import type { LanguageScopeSupportFacetMap } from "./scopeSupportFacets.types";
 import { ScopeSupportFacetLevel } from "./scopeSupportFacets.types";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { supported, unsupported, notApplicable } = ScopeSupportFacetLevel;
 
 export const rScopeSupport: LanguageScopeSupportFacetMap = {
