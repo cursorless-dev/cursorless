2244577c44ee7ea38ebe575f38311b9f3b821404
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-06-02 18:01:29 +0200

    Migrate argument

--- queries/latex.scm
+++ queries/latex.scm
@@ -56,8 +56,8 @@
 (
   (_
     text: (_
-      (_) @name
-    ) @name.removal
+      (_) @name @argumentOrParameter
+    ) @_.removal
   ) @name.domain
   (#type?
     @name.domain
@@ -75,17 +75,17 @@
 ;; !        ^^^^^
 (begin
   name: (_
-    text: (_) @name
-  )
-) @_.domain
+    text: (_) @name @argumentOrParameter
+  ) @_.removal
+) @name.domain
 
 ;;!! \end{quote}
 ;; !      ^^^^^
 (end
   name: (_
-    text: (_) @name
-  )
-) @_.domain
+    text: (_) @name @argumentOrParameter
+  ) @_.removal
+) @name.domain
 
 [
   (displayed_equation)
@@ -123,21 +123,80 @@
   (tikz_library_import)
   (begin)
   (end)
-] @functionCall
+] @functionCall @argumentOrParameter.iteration
 
 (
   (_
-    command: _ @functionCall.start
+    command: _ @functionCall.start @argumentOrParameter.iteration.start
     .
-    (_)? @functionCall.end
+    (_)? @functionCall.end @argumentOrParameter.iteration.end
   ) @_dummy
   (#type?
     @_dummy
+    subparagraph
     paragraph
     subsubsection
     subsection
     section
     chapter
     part
+  )
+)
+
+(
+  (_
+    [
+      (curly_group)
+      (curly_group_text)
+      (curly_group_text_list)
+      (curly_group_path)
+      (curly_group_path_list)
+      (curly_group_command_name)
+      (curly_group_key_value)
+      (curly_group_glob_pattern)
+      (curly_group_impl)
+      (brack_group)
+      (brack_group_text)
+      (brack_group_argc)
+      (brack_group_key_value)
+    ] @argumentOrParameter @_.removal
+  ) @_dummy
+  (#character-range! @argumentOrParameter 1 -1)
+  (#type?
+    @_dummy
+    displayed_equation
+    generic_command
+    inline_formula
+    math_set
+    block_comment
+    package_include
+    class_include
+    latex_include
+    biblatex_include
+    bibtex_include
+    graphics_include
+    svg_include
+    inkscape_include
+    verbatim_include
+    import_include
+    caption
+    citation
+    label_definition
+    label_reference
+    label_reference_range
+    label_number
+    new_command_definition
+    old_command_definition
+    let_command_definition
+    environment_definition
+    glossary_entry_definition
+    glossary_entry_reference
+    acronym_definition
+    acronym_reference
+    theorem_definition
+    color_definition
+    color_set_definition
+    color_reference
+    tikz_library_import
   )
 )
