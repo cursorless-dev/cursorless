eecda33f26dc81d25424e96e6b523d630ab67db2
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-06-21 12:27:37 +0200

    Update vscode capabilities

--- packages/cursorless-vscode/src/ide/vscode/VscodeCapabilities.ts
+++ packages/cursorless-vscode/src/ide/vscode/VscodeCapabilities.ts
@@ -1,8 +1,16 @@
 import type { Capabilities, CommandCapabilityMap } from "@cursorless/common";
+import { nodeGetRunMode } from "@cursorless/node-common";
+import * as semver from "semver";
+import * as vscode from "vscode";
+
+// FIXME: In newer versions of vscode the `editor.action.clipboardCopyAction`
+// command appears to be disabled / sandboxed in test mode.
+const supportsCopy =
+  nodeGetRunMode() !== "test" || semver.lt(vscode.version, "1.98.0");
 
 const COMMAND_CAPABILITIES: CommandCapabilityMap = {
+  clipboardCopy: supportsCopy ? { acceptsLocation: false } : undefined,
   clipboardPaste: true,
-  clipboardCopy: { acceptsLocation: false },
   toggleLineComment: { acceptsLocation: false },
   indentLine: { acceptsLocation: false },
   outdentLine: { acceptsLocation: false },
