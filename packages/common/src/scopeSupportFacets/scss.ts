30f90e09de7ef3111d19e4c666fecae3b132cce6
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-06-28 11:25:52 +0200

    Added missing scopes to SCSS

--- packages/common/src/scopeSupportFacets/scss.ts
+++ packages/common/src/scopeSupportFacets/scss.ts
@@ -9,8 +9,6 @@ export const scssScopeSupport: LanguageScopeSupportFacetMap = {
 
   "comment.line": supported,
 
-  "condition.if": supported,
-
   namedFunction: supported,
   "namedFunction.iteration.block": supported,
   "namedFunction.iteration.document": supported,
@@ -27,15 +25,15 @@ export const scssScopeSupport: LanguageScopeSupportFacetMap = {
   "value.argument.formal.iteration": supported,
   "value.return": supported,
 
-  ifStatement: supported,
-
   "textFragment.comment.line": supported,
+
+  ifStatement: supported,
+  "branch.if": supported,
+  "branch.if.iteration": supported,
+  "interior.if": supported,
+  "condition.if": supported,
 
   // Unsupported
 
-  "branch.if": unsupported,
-  "branch.if.iteration": unsupported,
-
   "interior.function": unsupported,
-  "interior.if": unsupported,
 };
