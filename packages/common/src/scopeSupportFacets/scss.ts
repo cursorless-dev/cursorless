18b1752a44fa406173783d5c849c5e11dab2c8da
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-06-28 11:34:50 +0200

    Clean up

--- packages/common/src/scopeSupportFacets/scss.ts
+++ packages/common/src/scopeSupportFacets/scss.ts
@@ -2,7 +2,7 @@ import { cssScopeSupport } from "./css";
 import type { LanguageScopeSupportFacetMap } from "./scopeSupportFacets.types";
 import { ScopeSupportFacetLevel } from "./scopeSupportFacets.types";
 
-const { supported, unsupported } = ScopeSupportFacetLevel;
+const { supported } = ScopeSupportFacetLevel;
 
 export const scssScopeSupport: LanguageScopeSupportFacetMap = {
   ...cssScopeSupport,
