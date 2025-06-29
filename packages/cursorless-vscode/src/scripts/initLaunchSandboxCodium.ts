9edd9ad27b4c107b03d043d4df8bea3de4662560
Cocona <pexium128@gmail.com>
2025-06-28 20:57:11 -0400

    hacky workaround to OpenVSX Marketplace stuff

--- packages/cursorless-vscode/src/scripts/initLaunchSandboxCodium.ts
+++ packages/cursorless-vscode/src/scripts/initLaunchSandboxCodium.ts
@@ -3,17 +3,27 @@
  */
 import { extensionDependencies } from "@cursorless/common";
 import * as cp from "child_process";
+import * as path from "node:path"
 
 const extraExtensions = ["pokey.command-server"];
 
 async function main() {
   try {
+    
     const args = [
       "--profile=cursorlessDevelopment",
       ...[...extensionDependencies, ...extraExtensions].flatMap(
         (dependency) => ["--install-extension", dependency],
       ),
     ];
+
+    // remove attempt to get vscode-tree-sitter-query from OpenVSX marketplace
+    var index: number = args.findIndex((value) => value.includes("vscode-tree-sitter-query"))
+    args.splice(index, 1)
+
+    // add vscode-tree-sitter-query via .vsix file
+    const location : string = path.join("..", "..", "data", "vsix", "jrieken.vscode-tree-sitter-query-0.0.6.vsix")
+    args.push("--profile=cursorlessDevelopment --install-extension ".concat(location))
 
     if (process.argv.includes("--force")) {
       args.push("--force");
