74c13ab26e6a809ed322f9f8eadc2b008d7c54c6
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-07-01 21:18:00 +0200

    scm scss

--- queries/scss.scm
+++ queries/scss.scm
@@ -97,8 +97,8 @@
     (_) @argumentOrParameter
     .
     (_)? @_.trailing.startOf
-  )
-  (#insertion-delimiter! @argumentOrParameter ", ")
+  ) @_dummy
+  (#single-or-multi-line-delimiter! @argumentOrParameter @_dummy ", " ",\n")
 )
 
 (_
@@ -111,7 +111,7 @@
 (parameters
   "(" @name.iteration.start.endOf @value.iteration.start.endOf
   ")" @name.iteration.end.startOf @value.iteration.end.startOf
-) @name.iteration.domain @value.iteration.domain
+)
 
 ;;!! foo($foo: 123)
 ;;!      ^^^^  ^^^
