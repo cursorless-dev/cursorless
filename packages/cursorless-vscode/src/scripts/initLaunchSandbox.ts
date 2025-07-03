c7b00ddb3a8adae45bd8f0ca1896c4fd42667b01
rinOfTheStars <pexium128@gmail.com>
2025-07-03 00:38:20 -0400

    Setup script consolidation, update CONTRIBUTING.mdx
    - Consolidate initLaunchSandbox.ts and initLaunchSandboxCodium.ts into a single script which determines which editor-cli tool pair is installed automatically.
    - Remove pointers to now-removed initLaunchSandboxCodium.ts in package.json and packages/cursorless-vscode/package.json
    - Remove auto-installation of missing extension from VSCodium setup from .vsix file, add documentation on how to install it manually instead to CONTRIBUTING.mdx
    - Remove data/vsix dir and all files within due to above
    - Update CONTRIBUTING.mdx to note that running pnpm compile is not strictly required to contribute to Cursorless
    - Update CONTRIBUTING.mdx to remove reference to the specific version settings profiles were added, and instead just tell contributors to raise an issue if they're a problem for some reason
    - Fix odd line break at the top of CONTRIBUTING.mdx

--- packages/cursorless-vscode/src/scripts/initLaunchSandbox.ts
+++ packages/cursorless-vscode/src/scripts/initLaunchSandbox.ts
@@ -7,8 +7,47 @@ import { extensionDependencies } from "@cursorless/common";
 import * as cp from "child_process";
 
 const extraExtensions = ["pokey.command-server"];
+const vsCodeToolName = "code";
+const vsCodiumToolName = "codium";
 
 async function main() {
+
+  // CLI tool name check
+
+  var cliToolName: string = vsCodeToolName;
+  var foundTool: boolean = false;
+  
+  console.log("Checking avalible VSC cli tool...");
+
+  while (!foundTool) {
+    try {
+      const checkToolSubprocess = cp.spawn(cliToolName, ["-v"], {
+      stdio: "inherit",
+      shell: true,
+    });
+
+    await new Promise<void>((resolve, reject) => {
+      checkToolSubprocess.on("error", reject);
+      checkToolSubprocess.on("exit", (code) => {
+        if (code === 0) {
+          foundTool = true;
+          resolve();
+        } else {
+          reject(new Error(`Process returned code ${code}`));
+        }
+      });
+    });
+
+    } catch(error) {
+      if (cliToolName === vsCodeToolName) {
+        cliToolName = vsCodiumToolName;
+      } else if (cliToolName === vsCodiumToolName) {
+        console.error("No VSC command line interface found!");
+        process.exit(2);
+      }
+    }
+  }
+  console.log("Found '%s' cli tool!", vsCodeToolName)
   try {
     const args = [
       "--profile=cursorlessDevelopment",
@@ -21,8 +60,16 @@ async function main() {
       args.push("--force");
     }
 
+    // Do not attempt to install jrieken:vscode-tree-sitter-query if editor is VSCodium
+    if (cliToolName === vsCodiumToolName) {
+      const index: number = args.findIndex((value) => value.includes("vscode-tree-sitter-query"))
+      args.splice(index, 1)
+      console.log("Not installing jrieken:vscode-tree-sitter-query as it is not on the OpenVSX Marketplace.")
+      console.log("You should install this extension manually. Check the Cursorless contributor documentation for more info.")
+    }
+
     // Install extension dependencies
-    const subprocess = cp.spawn("code", args, {
+    const subprocess = cp.spawn(cliToolName, args, {
       stdio: "inherit",
       shell: true,
     });
