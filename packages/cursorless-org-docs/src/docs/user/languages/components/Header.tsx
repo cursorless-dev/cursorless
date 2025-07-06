bc1a8b13b5ff3a96b690e7bbb09697058e9173d5
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-07-06 12:30:13 +0200

    Clean up

--- packages/cursorless-org-docs/src/docs/user/languages/components/Header.tsx
+++ packages/cursorless-org-docs/src/docs/user/languages/components/Header.tsx
@@ -1,19 +1,14 @@
 import React from "react";
 
 export function H2({ children }: { children: string }) {
-  return <Header level={2}>{children}</Header>;
+  return renderHeader(2, children);
 }
 
 export function H3({ children }: { children: string }) {
-  return <Header level={3}>{children}</Header>;
+  return renderHeader(3, children);
 }
 
-interface HeaderProps {
-  level: number;
-  children: string;
-}
-
-function Header({ level, children }: HeaderProps) {
+function renderHeader(level: number, children: string): React.JSX.Element {
   const Tag = `h${level}` as keyof React.JSX.IntrinsicElements;
   const href = children.toLowerCase().replaceAll(" ", "-");
   return (
