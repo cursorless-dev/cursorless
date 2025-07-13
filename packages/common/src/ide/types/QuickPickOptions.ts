9b245a5715b36fae55e6ff1318dbf3fb12835e86
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-07-14 01:37:39 +0200

    Default to document language id for scope recorder

--- packages/common/src/ide/types/QuickPickOptions.ts
+++ packages/common/src/ide/types/QuickPickOptions.ts
@@ -2,19 +2,7 @@ export interface UnknownValuesOptions {
   allowed: true;
 
   /**
-   * An optional template to use when displaying the new option to the user. The
-   * template must include `{}` as a substring, which will be replaced with the
-   * user's input when displaying an option to add the unknown item.
-   *
-   * For example:
-   *
-   * "Add new value '{}' →"
-   *
-   * In this case, when the user types `foo`, the following option will be
-   * displayed in the quickpick that will allow the user to select the unknown
-   * value `foo`:
-   *
-   * "Add new value 'foo' →"
+
    */
   newValueTemplate?: string;
 }
@@ -26,8 +14,27 @@ export interface QuickPickOptions {
   title?: string;
 
   /**
+   * An optional string that represents the default value to be selected in the quick pick.
+   */
+  defaultValue?: string;
+
+  /**
    * Indicates whether the quick pick should allow unknown values, and if so,
    * how to handle them.
+   *
+   * If provided as a string: An optional template to use when displaying the new
+   * option to the user. The template must include `{}` as a substring, which will
+   * be replaced with the user's input when displaying an option to add the unknown item.
+   *
+   * For example:
+   *
+   * "Add new value '{}' →"
+   *
+   * In this case, when the user types `foo`, the following option will be
+   * displayed in the quickpick that will allow the user to select the unknown
+   * value `foo`:
+   *
+   * "Add new value 'foo' →"
    */
-  unknownValues?: UnknownValuesOptions;
+  unknownValues?: boolean | string;
 }
