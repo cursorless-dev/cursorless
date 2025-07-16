2390ec4cbd8727cadb19a683461ccbde7efe5fe7
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-07-16 14:29:44 +0200

    r

--- queries/r.scm
+++ queries/r.scm
@@ -134,15 +134,16 @@
 ;; Function calls
 ;;!! foo()
 ;;!  ^^^^^
-;;!  -----
-(call) @functionCall @argumentOrParameter.iteration.domain
-
-;;!! foo()
 ;;!  ^^^
 ;;!  -----
-(call
-  (identifier) @functionCallee
-) @_.domain
+(
+  (call
+    (identifier) @functionCallee.end
+  ) @functionCall @functionCallee.start.startOf @functionCallee.domain
+  (#call-chain! @functionCall ".")
+  (#call-chain! @functionCallee.start.startOf ".")
+  (#call-chain! @functionCallee.domain ".")
+)
 
 ;; Technically lists and arrays are just calls to the function `list` or `c`
 ;;!! list(1, 2, 3)
