29a62e43542d93683ea0cb3610d8d84c9c4f6a65
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-07-21 11:06:45 +0200

    typo

--- packages/cursorless-engine/src/languages/TreeSitterQuery/setRange.ts
+++ packages/cursorless-engine/src/languages/TreeSitterQuery/setRange.ts
@@ -3,6 +3,6 @@ import type { MutableQueryCapture } from "./QueryCapture";
 
 export function setRange(capture: MutableQueryCapture, range: Range) {
   capture.range = range;
-  // Clear the node since the range has changed ander ranged no longer matches a specific node
+  // Clear the node since the range has changed and the range no longer matches a specific node
   capture.node = undefined;
 }
