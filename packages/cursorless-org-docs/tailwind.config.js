2acc5a9414c2b6fcf8b0503e6a34468aaf089544
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-07-26 10:05:20 +0200

    Restore tailwind config files

---
+++ packages/cursorless-org-docs/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+
+export const content = ["./src/**/*.{js,ts,jsx,tsx}"];
+
+export const corePlugins = {
+  preflight: false,
+};
+
+export const plugins = [];
