659337d1ad3fdd98991db616b425c36b11c069d6
rinOfTheStars <pexium128@gmail.com>
2025-09-06 21:07:36 -0400

    gdscript doesnt have explicit interfaces, so this is an easy one

--- packages/common/src/scopeSupportFacets/gdscript.ts
+++ packages/common/src/scopeSupportFacets/gdscript.ts
@@ -5,4 +5,16 @@ const { supported, unsupported, notApplicable } = ScopeSupportFacetLevel;
 
 export const gdscriptScopeSupport: LanguageScopeSupportFacetMap = {
 
+    // not applicable
+    "statement.interface": notApplicable,
+    "statement.field.interface": notApplicable,
+    "statement.iteration.interface": notApplicable,
+    "name.interface": notApplicable,
+    "name.field.interface": notApplicable,
+    "name.iteration.interface": notApplicable,
+    "type.field.interface": notApplicable,
+    "type.interface": notApplicable,
+    "type.iteration.interface": notApplicable,
+    "interior.interface": notApplicable,
+
 }
