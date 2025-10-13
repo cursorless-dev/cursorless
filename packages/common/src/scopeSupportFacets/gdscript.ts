73b29a059ffbed09d6d1e6e71e63bf21004e22b6
rinOfTheStars <pexium128@gmail.com>
2025-10-13 13:59:20 -0400

    run prettier and fix comment formatting as suggested

--- packages/common/src/scopeSupportFacets/gdscript.ts
+++ packages/common/src/scopeSupportFacets/gdscript.ts
@@ -1,19 +1,19 @@
 import type { LanguageScopeSupportFacetMap } from "./scopeSupportFacets.types";
 import { ScopeSupportFacetLevel } from "./scopeSupportFacets.types";
 
-//eslint-disable-next-line
+// eslint-disable-next-line
 const { supported, unsupported, notApplicable } = ScopeSupportFacetLevel;
 
 export const gdscriptScopeSupport: LanguageScopeSupportFacetMap = {
-    // not applicable
-    "statement.interface": notApplicable,
-    "statement.field.interface": notApplicable,
-    "statement.iteration.interface": notApplicable,
-    "name.interface": notApplicable,
-    "name.field.interface": notApplicable,
-    "name.iteration.interface": notApplicable,
-    "type.field.interface": notApplicable,
-    "type.interface": notApplicable,
-    "type.iteration.interface": notApplicable,
-    "interior.interface": notApplicable,
+  // not applicable
+  "statement.interface": notApplicable,
+  "statement.field.interface": notApplicable,
+  "statement.iteration.interface": notApplicable,
+  "name.interface": notApplicable,
+  "name.field.interface": notApplicable,
+  "name.iteration.interface": notApplicable,
+  "type.field.interface": notApplicable,
+  "type.interface": notApplicable,
+  "type.iteration.interface": notApplicable,
+  "interior.interface": notApplicable,
 };
