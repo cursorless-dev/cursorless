7b3369bcff4a8b7ac0c055ce579566b406e9089e
bluedrink9 <bluedrink9@users.noreply.github.com>
2025-11-29 18:27:15 +1300

    build: Update nix flake
    Brings the node js version up to date with the required one

--- flake.nix
+++ flake.nix
@@ -22,47 +22,9 @@
             pkgs = import nixpkgs {
               inherit system;
               overlays = [
-                # Updated neovim-node-client is pending merge:
-                # https://github.com/NixOS/nixpkgs/pull/317333
                 (final: prev: {
-                  nodePackages = prev.nodePackages // {
-                    neovim = prev.buildNpmPackage rec {
-                      pname = "neovim-node-client";
-                      version = "5.1.1-dev.0";
-                      src = prev.fetchFromGitHub {
-                        owner = "neovim";
-                        repo = "node-client";
-                        rev = "d99ececf115ddc8ade98467417c1bf0120b676b5";
-                        hash = "sha256-eiKyhJNz7kH2iX55lkn7NZYTj6yaSZLMZxqiqPxDIPs=";
-                      };
-                      npmDeps = prev.fetchNpmDeps {
-                        inherit src;
-                        hash = "sha256-UoMq+7evskxtZygycxLBgeUtwrET8jYKeZwMiXdBMAw=";
-                      };
-                      postInstall = ''
-                        mkdir -p $out/bin
-                        ln -s $out/lib/node_modules/neovim/node_modules/.bin/neovim-node-host $out/bin
-                      '';
-                    };
-                  };
                   neovim = prev.neovim.override { withNodeJs = true; };
-                  # There is a recent bug that prevents cli --check invocation:
-                  # See #2613
-                  lua-language-server = prev.lua-language-server.overrideAttrs {
-                    postPatch =
-                      let
-                        patch = prev.fetchurl {
-                          url = "https://github.com/LuaLS/lua-language-server/pull/2775.patch";
-                          sha256 = "sha256-5hjuNzBHLp9kiD6O8jTL5YlvaqR8IuJPHchIZE2/p/Q=";
-                        };
-                      in
-                      ''
-                        patch -p1 < ${patch}
-                      ''
-                      + prev.lua-language-server.postPatch;
-                  };
                 })
-
               ];
             };
           }
@@ -72,12 +34,6 @@
       ] (nixpkgs.lib.importTOML ./pyproject.toml).tool.ruff.target-version;
     in
     {
-      packages = forEachSupportedSystem (
-        { pkgs }:
-        {
-          lua-language-server = pkgs.lua-language-server;
-        }
-      );
       devShells = forEachSupportedSystem (
         { pkgs }:
         {
@@ -90,14 +46,8 @@
               [
                 pkgs.corepack
                 pkgs.vsce
-                pkgs.nodejs
+                pkgs.nodejs_20
 
-                # https://github.com/NixOS/nixpkgs/pull/251418
-                (pkgs.pre-commit.overrideAttrs (previousAttrs: {
-                  makeWrapperArgs = ''
-                    --set PYTHONPATH $PYTHONPATH
-                  '';
-                }))
                 python
                 pkgs.lua-language-server # language server used by pre-commit hooks
 
