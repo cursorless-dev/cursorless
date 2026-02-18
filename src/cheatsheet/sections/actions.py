e539f5bf4f902087a6ca4c7fd0b0a9c70fa72183
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2026-02-18 08:29:33 +0100

    Update cheatsheet   (#3180)
    * Updated snippet actions
    * Added snippet to legend
    * Updated defaults.json

--- src/cheatsheet/sections/actions.py
+++ src/cheatsheet/sections/actions.py
@@ -9,7 +9,7 @@ def get_actions() -> list[ListItemDescriptor]:
     for name in ACTION_LIST_NAMES:
         all_actions.update(get_raw_list(name))
 
-    multiple_target_action_names = [
+    complex_action_names = [
         "replaceWithTarget",
         "moveToTarget",
         "swapTargets",
@@ -18,16 +18,17 @@ def get_actions() -> list[ListItemDescriptor]:
         "wrapWithPairedDelimiter",
         "rewrap",
         "pasteFromClipboard",
+        "insertSnippet",
     ]
     simple_actions = {
         f"{key} <target>": value
         for key, value in all_actions.items()
-        if value not in multiple_target_action_names
+        if value not in complex_action_names
     }
     complex_actions = {
         value: key
         for key, value in all_actions.items()
-        if value in multiple_target_action_names
+        if value in complex_action_names
     }
 
     swap_connectives = list(get_raw_list("swap_connective").keys())
@@ -39,10 +40,11 @@ def get_actions() -> list[ListItemDescriptor]:
         {
             "editNewLineAfter": "Edit new line/scope after",
             "editNewLineBefore": "Edit new line/scope before",
+            "experimental.setInstanceReference": "Set instance reference",
         },
     )
 
-    fixtures: dict[str, list[tuple[Callable, str]]] = {
+    complex_action_defs: dict[str, list[tuple[Callable, str]]] = {
         "replaceWithTarget": [
             (
                 lambda value: f"{value} <target> <destination>",
@@ -89,7 +91,11 @@ def get_actions() -> list[ListItemDescriptor]:
             (
                 lambda value: f"<pair> {value} <target>",
                 "Wrap <target> with <pair>",
-            )
+            ),
+            (
+                lambda value: f"<snippet> {value} <target>",
+                "Wrap <target> with <snippet>",
+            ),
         ],
         "rewrap": [
             (
@@ -97,10 +103,16 @@ def get_actions() -> list[ListItemDescriptor]:
                 "Rewrap <target> with <pair>",
             )
         ],
+        "insertSnippet": [
+            (
+                lambda value: f"{value} <snippet> <destination>",
+                "Insert snippet at <destination>",
+            )
+        ],
     }
 
     if swap_connective:
-        fixtures["swapTargets"] = [
+        complex_action_defs["swapTargets"] = [
             (
                 lambda value: f"{value} <target 1> {swap_connective} <target 2>",
                 "Swap <target 1> with <target 2>",
@@ -111,7 +123,8 @@ def get_actions() -> list[ListItemDescriptor]:
             ),
         ]
 
-    for action_id, variations in fixtures.items():
+    for action_id, variations in complex_action_defs.items():
+        # This happens if the user has disabled the spoken form for a complex action
         if action_id not in complex_actions:
             continue
         action = complex_actions[action_id]
@@ -128,5 +141,4 @@ def get_actions() -> list[ListItemDescriptor]:
                 ],
             }
         )
-
     return items
