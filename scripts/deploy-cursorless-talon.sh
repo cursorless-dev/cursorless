55608639150e769f7ed9cc71e440f656b3ff3c52
Pokey Rule <755842+pokey@users.noreply.github.com>
2026-03-08 04:46:56 +0000

    Add shell check pre commit hook (#2574)
    ## Checklist
    
    - [ ] I have added
    [tests](https://www.cursorless.org/docs/contributing/test-case-recorder/)
    - [ ] I have updated the
    [docs](https://github.com/cursorless-dev/cursorless/tree/main/docs) and
    [cheatsheet](https://github.com/cursorless-dev/cursorless/tree/main/cursorless-talon/src/cheatsheet)
    - [ ] I have not broken the cheatsheet
    
    ---------
    
    Co-authored-by: Phil Cohen <phillip@phillip.io>
    Co-authored-by: Andreas Arvidsson <andreas.arvidsson87@gmail.com>

--- scripts/deploy-cursorless-talon.sh
+++ scripts/deploy-cursorless-talon.sh
@@ -8,20 +8,20 @@ set -euo pipefail
 git switch -c cursorless-talon-staging origin/cursorless-talon-staging
 
 # Exit if there were no changes to the cursorless-talon directory
-[[ "$(git rev-parse $GITHUB_SHA:cursorless-talon)" == "$(git rev-parse cursorless-talon-staging^{tree})" ]] && exit 0
+[[ "$(git rev-parse "${GITHUB_SHA}:cursorless-talon")" == "$(git rev-parse 'cursorless-talon-staging^{tree}')" ]] && exit 0
 
 # Fetch current cursorless-talon main
 git remote add cursorless-talon 'https://github.com/cursorless-dev/cursorless-talon.git'
 git fetch cursorless-talon
 
 # Sanity check that cursorless-talon/main is identical to cursorless-talon-staging
-[[ "$(git rev-parse cursorless-talon/main^{tree})" == "$(git rev-parse cursorless-talon-staging^{tree})" ]]
+[[ "$(git rev-parse 'cursorless-talon/main^{tree}')" == "$(git rev-parse 'cursorless-talon-staging^{tree}')" ]]
 
 # Tag previous version of staging branch
 git tag cursorless-talon-staging-previous cursorless-talon-staging
 
 # Update the staging branch
-git switch -c github-sha $GITHUB_SHA
+git switch -c github-sha "$GITHUB_SHA"
 git subtree split --prefix=cursorless-talon --branch=cursorless-talon-staging
 
 # Sanity check that the previous staging commit is an ancestor of the new one
@@ -32,11 +32,11 @@ git switch -c cursorless-talon-main cursorless-talon/main
 git cherry-pick cursorless-talon-staging-previous..cursorless-talon-staging
 
 # Sanity check that cursorless-talon-main is identical to cursorless-talon-staging
-[[ "$(git rev-parse cursorless-talon-main^{tree})" == "$(git rev-parse cursorless-talon-staging^{tree})" ]]
+[[ "$(git rev-parse 'cursorless-talon-main^{tree}')" == "$(git rev-parse 'cursorless-talon-staging^{tree}')" ]]
 
 # Sanity check that cursorless-talon-main is identical to cursorless-talon
 # subdirectory of cursorless-vscode
-[[ "$(git rev-parse cursorless-talon-main^{tree})" == "$(git rev-parse $GITHUB_SHA:cursorless-talon)" ]]
+[[ "$(git rev-parse 'cursorless-talon-main^{tree}')" == "$(git rev-parse "${GITHUB_SHA}:cursorless-talon")" ]]
 
 # Push to cursorless-talon
 git push cursorless-talon cursorless-talon-main:main
