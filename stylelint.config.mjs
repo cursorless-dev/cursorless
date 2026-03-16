16228b7f5a853917db790bc8606dd08b335bdef6
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2026-03-16 02:00:22 +0100

    Added stylelint (#3225)

---
+++ stylelint.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: ["stylelint-config-standard", "stylelint-config-standard-scss"],
+  rules: {
+    "at-rule-empty-line-before": null,
+  },
+};
