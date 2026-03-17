18739e81b95545ab0984d695c664a77d3e512a12
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2026-03-17 18:54:49 +0100

    Restored check for window

--- packages/cheatsheet/src/lib/utils/useIsHighlighted.ts
+++ packages/cheatsheet/src/lib/utils/useIsHighlighted.ts
@@ -1,11 +1,17 @@
 import { useCallback, useEffect, useState } from "preact/hooks";
 
+// Check if window is defined (so if in the browser or in node.js).
+const isBrowser = typeof window !== "undefined";
+
 /**
  * Returns `true` if the URL hash is equal to the given `id`
  * @param id The id to match the hash against
  * @returns Boolean indicating whether the hash matches the given id
  */
 export default function useIsHighlighted(id: string): boolean {
+  if (!isBrowser) {
+    return false;
+  }
   const hash = useHash();
   return hash === `#${id}`;
 }
