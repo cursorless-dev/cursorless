8002e76191af7c8e7b7c429ae8acbdc2cec8ec76
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2026-03-17 18:48:49 +0100

    Remove skipLibCheck from tsconfig files

--- packages/cheatsheet/src/lib/utils/useIsHighlighted.ts
+++ packages/cheatsheet/src/lib/utils/useIsHighlighted.ts
@@ -1,15 +1,29 @@
-import { useHash } from "react-use";
-
-// Check if window is defined (so if in the browser or in node.js).
-const isBrowser = typeof window !== "undefined";
+import { useCallback, useEffect, useState } from "preact/hooks";
 
 /**
  * Returns `true` if the URL hash is equal to the given `id`
  * @param id The id to match the hash against
  * @returns Boolean indicating whether the hash matches the given id
  */
-export default function useIsHighlighted(id: string) {
-  const [hash, _] = isBrowser ? useHash() : ["", null];
-
-  return hash.length > 1 && hash.substring(1) === id;
+export default function useIsHighlighted(id: string): boolean {
+  const hash = useHash();
+  return hash === `#${id}`;
 }
+
+const useHash = () => {
+  const [hash, setHash] = useState(() => window.location.hash);
+
+  const onHashChange = useCallback(() => {
+    setHash(window.location.hash);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("hashchange", onHashChange);
+
+    return () => {
+      window.removeEventListener("hashchange", onHashChange);
+    };
+  }, []);
+
+  return hash;
+};
