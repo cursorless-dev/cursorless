7a641ca38e924982670259ab02f903c01c7aded6
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2026-05-22 17:20:32 +0200

    Fixed bug where snippet tests failed due to left over snippet holes from previous tests.

--- packages/lib-vscode-common/src/testUtil/closeUiElements.ts
+++ packages/lib-vscode-common/src/testUtil/closeUiElements.ts
@@ -1,6 +1,8 @@
 import { commands } from "vscode";
 
 export async function closeUiElements() {
+  // If we're in a snippet, leave it. This is needed or some snippet tests will fail.
+  await commands.executeCommand("leaveSnippet");
   // Many times running these tests opens the sidebar, which slows performance. Close it.
   await commands.executeCommand("workbench.action.closeSidebar");
   // Close the find widget as well, since it can also be open and cause performance issues.
