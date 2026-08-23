259b3df59acad0bbd02a838eacffbc7eac3fd357
rinOfTheStars <rin@artificesoft.com>
2026-08-22 20:34:34 -0400

    run precommit

--- resources/queries/swift.scm
+++ resources/queries/swift.scm
@@ -4,71 +4,71 @@
 (comment) @comment @textFragment
 ;; Protocol decl.
 (protocol_declaration
-    name: (_) @name
-    body: (protocol_body
-        "{" @interior.start.endOf
-        "}" @interior.end.startOf
-        .
-    )
+  name: (_) @name
+  body: (protocol_body
+    "{" @interior.start.endOf
+    "}" @interior.end.startOf
+    .
+  )
 ) @statement
 
 ;;if statement -- todo seperate main if branch from else and else if branches
 (
-    (if_statement) @ifStatement @statement @branch.domain
-    (#not-parent-type? @ifStatement if_statement)
+  (if_statement) @ifStatement @statement @branch.domain
+  (#not-parent-type? @ifStatement if_statement)
 )
 
 (
-    (if_statement
-        "if" @branch.start.startOf @branch.removal.start
-        condition: (_) @condition
-        (statements) @interior.domain
-        .
-        "}" @branch.end.endOf @branch.removal.end
-        (else)? @branch.removal.end.startOf 
-    ) @condition.domain
+  (if_statement
+    "if" @branch.start.startOf @branch.removal.start
+    condition: (_) @condition
+    (statements) @interior.domain
+    .
+    "}" @branch.end.endOf @branch.removal.end
+    (else)? @branch.removal.end.startOf
+  ) @condition.domain
 )
 
 (
-    (else) @branch.start @condition.domain.start
-    (if_statement
-        condition: (_) @condition
-        (statements) @interior.domain
-        .
-        "}" @branch.end.endOf @condition.domain.end
-    )
+  (else) @branch.start @condition.domain.start
+  (if_statement
+    condition: (_) @condition
+    (statements) @interior.domain
+    .
+    "}" @branch.end.endOf @condition.domain.end
+  )
 )
 
 ;; generic property delc. -- todo a lot on this ngl
 (property_declaration
-    name: (_) @name
-    ;;(type_annotation:
-    ;;    ":" 
-    ;;)
+  name: (_) @name
+  ;;(type_annotation:
+  ;;    ":"
+  ;;)
 ) @statement
 
 ;; Generic interior
 (_
-    "{" @interior.start.endOf
-    "}" @interior.end.startOf
+  "{" @interior.start.endOf
+  "}" @interior.end.startOf
 )
 
 ;; Non-enum class (struct/class) decl.
 (class_declaration
-    name: (_) @name
-    body: (class_body
-        "{" @interior.start.endOf
-        "}" @interior.end.startOf
-        .
-    )
+  name: (_) @name
+  body: (class_body
+    "{" @interior.start.endOf
+    "}" @interior.end.startOf
+    .
+  )
 ) @statement @class
 
 ;; Enum "class: decl.
 (class_declaration
-    name: (_) @name
-    body: (enum_class_body
-        "{" @interior.start.endOf
-        "}" @interior.end.startOf
-        .
-    )
+  name: (_) @name
+  body: (enum_class_body
+    "{" @interior.start.endOf
+    "}" @interior.end.startOf
+    .
+  )
 ) @statement
