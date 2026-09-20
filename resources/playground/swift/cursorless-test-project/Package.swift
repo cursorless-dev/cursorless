dff09a26edd63ffc742d4af224ac8aca48fb4fe4
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2026-09-20 09:03:12 +0200

    Update

--- resources/playground/swift/cursorless-test-project/Package.swift
+++
@@ -1,8 +0,0 @@
-// swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
-import PackageDescription
-
-let package = Package(
-    name: "cursorless-test-project"
-)
